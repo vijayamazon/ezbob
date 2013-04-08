a2145287c6b1a38c5e3b262327605beae29e0ef9
Gorbach Alexey <gorbach.alexey@gmail.com>
2013-04-08 10:20:38 +0200

    stored procedures and new tables were added to initial setup

---
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/MonitoredSites.sql
@@ -0,0 +1,15 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MonitoredSites]') AND type in (N'U'))
+DROP TABLE [dbo].[MonitoredSites]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MonitoredSites](
+	[Site] [varchar](300) NOT NULL,
+ CONSTRAINT [PK_MonitoredSites] PRIMARY KEY CLUSTERED 
+(
+	[Site] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ DataBaseScripts/MsSql/Scripts/07_PROCEDURES/UpdateExperianFirstIdToHandle.sql
@@ -0,0 +1,14 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateExperianFirstIdToHandle]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateExperianFirstIdToHandle]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE UpdateExperianFirstIdToHandle
+	@FirstIdToHandle VARCHAR(100)
+AS
+BEGIN
+	UPDATE ExperianDataAgentConfigs SET CfgValue = @FirstIdToHandle WHERE CfgKey = 'FirstIdToHandle'
+END
+GO
