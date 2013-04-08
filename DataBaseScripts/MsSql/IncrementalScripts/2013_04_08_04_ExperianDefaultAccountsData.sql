3245cde23662346436e6c5ce056a0fcf5512ee99
Gorbach Alexey <gorbach.alexey@gmail.com>
2013-04-08 10:06:43 +0200

    experian tables were added

---
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_04_08_04_ExperianDefaultAccountsData.sql
@@ -0,0 +1,27 @@
+IF OBJECT_ID ('dbo.ExperianDefaultAccountsData') IS NOT NULL
+BEGIN
+	PRINT 'ExperianDefaultAccountsData exists'
+	DROP TABLE ExperianDefaultAccountsData
+END
+ELSE
+BEGIN
+CREATE TABLE dbo.ExperianDefaultAccountsData
+(
+	Id INT IDENTITY, -- Internal Default Account Id
+	ServiceLogId BIGINT NOT NULL, 
+	CustomerId INT NOT NULL, 
+	AccountType VARCHAR(3),
+	DefMonth DATETIME, 
+	Balance INT, 
+	CurrentDefBalance INT,
+	CONSTRAINT FK_ExperianDefaultAccountsData_ServiceLogId FOREIGN KEY (ServiceLogId) REFERENCES dbo.MP_ServiceLog (Id),
+	CONSTRAINT FK_ExperianDefaultAccountsData_CustomerId FOREIGN KEY (CustomerId) REFERENCES dbo.Customer (Id),
+	CONSTRAINT FK_ExperianDefaultAccountsData_ExperianAccountType FOREIGN KEY (AccountType) REFERENCES dbo.ExperianAccountTypes (Id)
+)
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
---
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_04_08_05_MonitoredSites.sql
@@ -0,0 +1,22 @@
+IF OBJECT_ID ('dbo.MonitoredSites') IS NOT NULL
+BEGIN
+	PRINT 'MonitoredSites exists'
+	DROP TABLE MonitoredSites
+END
+ELSE
+BEGIN
+	CREATE TABLE dbo.MonitoredSites
+	(
+		Site VARCHAR(300) NOT NULL,
+		CONSTRAINT PK_MonitoredSites PRIMARY KEY (Site)
+	)
+
+	INSERT INTO MonitoredSites VALUES ('http://www.ezbob.com')
+	--INSERT INTO MonitoredSites VALUES ('http://App.ezbob.com')
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
