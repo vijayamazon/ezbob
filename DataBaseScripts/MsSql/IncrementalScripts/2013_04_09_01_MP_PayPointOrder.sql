7dd4938330b20c1548db9d915e42ddb545483837
Yuly Saar <yulys@ezbob.com>
2013-04-09 13:51:52 +0300

    Fixed sqls

--- DataBaseScripts/MsSql/IncrementalScripts/2013_04_09_01_MP_PayPointOrder.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_04_09_01_MP_PayPointOrder.sql
@@ -3,8 +3,7 @@ BEGIN
 	PRINT 'MP_PayPointOrder exists'	
 	DROP TABLE MP_PayPointOrder
 END
-ELSE
-BEGIN
+
 CREATE TABLE dbo.MP_PayPointOrder
 	(
 	  Id                                         INT IDENTITY NOT NULL
@@ -15,7 +14,6 @@ CREATE TABLE dbo.MP_PayPointOrder
 	, CONSTRAINT FK_MP_PayPointOrder_MP_CustomerMarketPlace FOREIGN KEY (CustomerMarketPlaceId) REFERENCES dbo.MP_CustomerMarketPlace (Id)
 	)
 GO
-END
 
 CREATE INDEX IX_MP_EkmOrderCustomerMarketPlaceId
 	ON dbo.MP_PayPointOrder (CustomerMarketPlaceId)
