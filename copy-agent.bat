79f1dc4d3917f5c5164ea5d2f08c169607a46061
Gorbach Alexey <gorbach.alexey@gmail.com>
2013-04-09 12:32:01 +0200

    a copy agent sample script

---
+++ copy-agent.bat
@@ -0,0 +1,5 @@
+robocopy App\service\agent\ c:\temp\service\agent\ /e
+robocopy Integration\NodeEzBobLib\bin\Debug\ c:\temp\service\agent\ /e
+robocopy Integration\CustomSchedulers\bin\Debug\ c:\temp\service\agent\ /e
+robocopy Integration\EzBob.Signals\bin\Debug\ c:\temp\service\agent\ /e
+robocopy Integration\EKM\bin\Debug\ c:\temp\service\agent\ /e
