95bde61ab20605ba814d13a16d043ae64e055877
Yuly Saar <yulys@ezbob.com>
2013-04-11 15:24:47 +0300

    Added copying of connectors to script

--- copy-agent.bat
+++ copy-agent.bat
@@ -4,5 +4,7 @@ robocopy Integration\NodeEzBobLib\bin\Debug\ c:\ezbobSrv\ /e
 robocopy Integration\CustomSchedulers\bin\Debug\ c:\ezbobSrv\ /e
 robocopy Integration\EzBob.Signals\bin\Debug\ c:\ezbobSrv\ /e
 robocopy Integration\EKM\bin\Debug\ c:\ezbobSrv\ /e
+robocopy Integration\Volusion\bin\Debug\ c:\ezbobSrv\ /e
+robocopy Integration\PayPoint\bin\Debug\ c:\ezbobSrv\ /e
 copy Lib\Scorto.Core\DBSQLServer.dll c:\ezbobSrv\ 
 net start ScortoService
