3988bbf4b266b88956431e97bb90db3ec45c20c7
stas_dulman <stasd@ezbob.com>
2013-04-11 17:44:03 +0300

    refactoring ekmMarketPlacesController

--- App/PluginWeb/EzBob.Web/Areas/Customer/Controllers/VolusionMarketPlacesController.cs
+++ App/PluginWeb/EzBob.Web/Areas/Customer/Controllers/VolusionMarketPlacesController.cs
@@ -14,113 +14,127 @@ using log4net;
 using EzBob.CommonLib.Security;
 using EzBob.Web.ApplicationCreator;
 
-namespace EzBob.Web.Areas.Customer.Controllers {
-	public class VolusionAccountModel {
-		public int id { get; set; }
-		public string login { get; set; }
-		public string password { get; set; }
-		public string displayName { get { return login; } }
+namespace EzBob.Web.Areas.Customer.Controllers
+{
+    public class VolusionAccountModel
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string displayName { get { return login; } }
 
-		public static VolusionAccountModel ToModel(MP_CustomerMarketPlace account) {
-			return new VolusionAccountModel {
-				id = account.Id,
-				login = account.DisplayName,
-				password = Encryptor.Decrypt(account.SecurityData)
-			};
-		} // constructor
-	} // class VolusionAccountModel
+        public static VolusionAccountModel ToModel(MP_CustomerMarketPlace account)
+        {
+            return new VolusionAccountModel
+            {
+                id = account.Id,
+                login = account.DisplayName,
+                password = Encryptor.Decrypt(account.SecurityData)
+            };
+        } // constructor
+    } // class VolusionAccountModel
 
-	public class VolusionMarketPlacesController : Controller {
-		private static readonly ILog _log = LogManager.GetLogger(typeof(VolusionMarketPlacesController));
-		private readonly IEzbobWorkplaceContext _context;
-		private readonly ICustomerRepository _customers;
-		private readonly IRepository<MP_MarketplaceType> _mpTypes;
-		private readonly IRepository<MP_CustomerMarketPlace> _marketplaces;
-		private EZBob.DatabaseLib.Model.Database.Customer _customer;
-		private readonly IMPUniqChecker _mpChecker;
-		private readonly IAppCreator _appCreator;
-		private readonly VolusionConnector _validator = new VolusionConnector();
+    public class VolusionMarketPlacesController : Controller
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(VolusionMarketPlacesController));
+        private readonly IEzbobWorkplaceContext _context;
+        private readonly ICustomerRepository _customers;
+        private readonly IRepository<MP_MarketplaceType> _mpTypes;
+        private readonly IRepository<MP_CustomerMarketPlace> _marketplaces;
+        private EZBob.DatabaseLib.Model.Database.Customer _customer;
+        private readonly IMPUniqChecker _mpChecker;
+        private readonly IAppCreator _appCreator;
+        private readonly VolusionConnector _validator = new VolusionConnector();
 
-		public VolusionMarketPlacesController(
-			IEzbobWorkplaceContext context,
-			ICustomerRepository customers,
-			IRepository<MP_MarketplaceType> mpTypes,
-			IRepository<MP_CustomerMarketPlace> marketplaces,
-			IMPUniqChecker mpChecker,
-			IAppCreator appCreator
-		) {
-			_context = context;
-			_customers = customers;
-			_mpTypes = mpTypes;
-			_marketplaces = marketplaces;
-			_customer = context.Customer;
-			_mpChecker = mpChecker;
-			_appCreator = appCreator;
-		} // constructor
+        public VolusionMarketPlacesController(
+            IEzbobWorkplaceContext context,
+            ICustomerRepository customers,
+            IRepository<MP_MarketplaceType> mpTypes,
+            IRepository<MP_CustomerMarketPlace> marketplaces,
+            IMPUniqChecker mpChecker,
+            IAppCreator appCreator
+        )
+        {
+            _context = context;
+            _customers = customers;
+            _mpTypes = mpTypes;
+            _marketplaces = marketplaces;
+            _customer = context.Customer;
+            _mpChecker = mpChecker;
+            _appCreator = appCreator;
+        } // constructor
 
-		[Transactional]
-		public JsonNetResult Accounts() {
-			var oVsi = new VolusionServiceInfo();
+        [Transactional]
+        public JsonNetResult Accounts()
+        {
+            var oVsi = new VolusionServiceInfo();
 
-			List<VolusionAccountModel> volusions = _customer
-				.CustomerMarketPlaces
-				.Where(mp => mp.Marketplace.InternalId == oVsi.InternalId)
-				.Select(VolusionAccountModel.ToModel)
-				.ToList();
+            List<VolusionAccountModel> volusions = _customer
+                .CustomerMarketPlaces
+                .Where(mp => mp.Marketplace.InternalId == oVsi.InternalId)
+                .Select(VolusionAccountModel.ToModel)
+                .ToList();
 
-			return this.JsonNet(volusions);
-		} // Accounts
+            return this.JsonNet(volusions);
+        } // Accounts
 
-		[Transactional]
-		[Ajax]
-		[HttpPost]
-		public JsonNetResult Accounts(VolusionAccountModel model) {
-			string errorMsg;
+        [Transactional]
+        [Ajax]
+        [HttpPost]
+        public JsonNetResult Accounts(VolusionAccountModel model)
+        {
+            string errorMsg;
 
-			if (!_validator.Validate(model.login, model.password, out errorMsg)) {
-				var errorObject = new { error = errorMsg };
-				return this.JsonNet(errorObject);
-			} // if
+            if (!_validator.Validate(model.login, model.password, out errorMsg))
+            {
+                var errorObject = new { error = errorMsg };
+                return this.JsonNet(errorObject);
+            } // if
 
-			try {
-				var customer = _context.Customer;
-				var username = model.login;
-				var volusion = new VolusionDatabaseMarketPlace();
+            try
+            {
+                var customer = _context.Customer;
+                var username = model.login;
+                var volusion = new VolusionDatabaseMarketPlace();
 
-				_mpChecker.Check(volusion.InternalId, customer, username);
+                _mpChecker.Check(volusion.InternalId, customer, username);
 
-				var oVsi = new VolusionServiceInfo();
+                var oVsi = new VolusionServiceInfo();
 
-				int marketPlaceId = _mpTypes
-					.GetAll()
-					.First(a => a.InternalId == oVsi.InternalId)
-					.Id;
+                int marketPlaceId = _mpTypes
+                    .GetAll()
+                    .First(a => a.InternalId == oVsi.InternalId)
+                    .Id;
 
-				var mp = new MP_CustomerMarketPlace {
-					Marketplace = _mpTypes.Get(marketPlaceId),
-					DisplayName = model.displayName,
-					SecurityData = Encryptor.EncryptBytes(model.password),
-					Customer = _customer,
-					Created = DateTime.UtcNow,
-					UpdatingStart = DateTime.UtcNow,
-					Updated = DateTime.UtcNow,
-					UpdatingEnd = DateTime.UtcNow
-				};
+                var mp = new MP_CustomerMarketPlace
+                {
+                    Marketplace = _mpTypes.Get(marketPlaceId),
+                    DisplayName = model.displayName,
+                    SecurityData = Encryptor.EncryptBytes(model.password),
+                    Customer = _customer,
+                    Created = DateTime.UtcNow,
+                    UpdatingStart = DateTime.UtcNow,
+                    Updated = DateTime.UtcNow,
+                    UpdatingEnd = DateTime.UtcNow
+                };
 
-				_customer.CustomerMarketPlaces.Add(mp);
-				_appCreator.EbayAdded(customer, mp.Id);
-				return this.JsonNet(EKMAccountModel.ToModel(mp));
-			}
-			catch (MarketPlaceAddedByThisCustomerException e) {
-				return this.JsonNet(new { error = DbStrings.StoreAddedByYou });
-			}
-			catch (MarketPlaceIsAlreadyAddedException e) {
-				return this.JsonNet(new { error = DbStrings.StoreAlreadyExistsInDb });
-			}
-			catch (Exception e) {
-				_log.Error(e);
-				return this.JsonNet(new { error = e.Message });
-			} // try
-		} // Accounts
-	} // class VolusionMarketPlacesController
+                _customer.CustomerMarketPlaces.Add(mp);
+                _appCreator.EbayAdded(customer, mp.Id);
+                return this.JsonNet(EkmAccountModel.ToModel(mp));
+            }
+            catch (MarketPlaceAddedByThisCustomerException e)
+            {
+                return this.JsonNet(new { error = DbStrings.StoreAddedByYou });
+            }
+            catch (MarketPlaceIsAlreadyAddedException e)
+            {
+                return this.JsonNet(new { error = DbStrings.StoreAlreadyExistsInDb });
+            }
+            catch (Exception e)
+            {
+                _log.Error(e);
+                return this.JsonNet(new { error = e.Message });
+            } // try
+        } // Accounts
+    } // class VolusionMarketPlacesController
 } // namespace
