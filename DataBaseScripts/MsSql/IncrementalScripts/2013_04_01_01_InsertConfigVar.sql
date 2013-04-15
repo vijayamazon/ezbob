786c8c7ded6f6b28fc7285c0e134ef1c03055162
Arnautov <v.arnautov@scorto.com.ua>
2013-04-15 16:48:59 +0300

    * existance check

--- DataBaseScripts/MsSql/IncrementalScripts/2013_04_01_01_InsertConfigVar.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_04_01_01_InsertConfigVar.sql
@@ -1,2 +1,4 @@
+delete from ConfigurationVariables where name = 'EnableAutomaticRejection'
+GO
 INSERT INTO ConfigurationVariables (Name, [Value], [Description])
 VALUES ('EnableAutomaticRejection', '1', 'if "1" system will reject customers automatically without any Underwriter actions')
