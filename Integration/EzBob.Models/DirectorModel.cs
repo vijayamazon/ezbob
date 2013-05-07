095556e94ddd02471bb1897adc6acfd202b99dbc
Alex Boursouk <alexbo@ezbob.com>
2013-05-07 19:12:50 +0300

    EZ 231 https://ezbobtech.atlassian.net/browse/EZ-231

--- Integration/EzBob.Models/DirectorModel.cs
+++ Integration/EzBob.Models/DirectorModel.cs
@@ -4,55 +4,53 @@ using ApplicationMng.Model;
 using EZBob.DatabaseLib.Model.Database;
 using EzBob.Web.Code;
 using Iesi.Collections.Generic;
-using System.Linq; 
+using System.Linq;
+using log4net;
 
-namespace EzBob.Web.Areas.Customer.Models
-{
-    public class DirectorModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Middle { get; set; }
-        public string Surname { get; set; }
+namespace EzBob.Web.Areas.Customer.Models {
+	public class DirectorModel {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(DirectorModel));
 
-        public string DateOfBirth { get; set; }
-        public CustomerAddress[] DirectorAddress { get; set; }
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Middle { get; set; }
+		public string Surname { get; set; }
+		public char Gender { get; set; }
 
-        public Director FromModel()
-        {
-            Director director = null;
+		public string DateOfBirth { get; set; }
+		public CustomerAddress[] DirectorAddress { get; set; }
 
-            try{
-                director = new Director
-                       {
-                           Id = Id,
-                           Name = Name.Trim(),
-                           Surname = Surname.Trim(),
-                           Middle = Middle.Trim(),
-                           DateOfBirth = DateOfBirth.IndexOf("-", StringComparison.Ordinal) == -1 ? DateTime.ParseExact(DateOfBirth, "d/M/yyyy", CultureInfo.InvariantCulture) : (DateTime?) null,
-                           DirectorAddress = DirectorAddress == null ? null : new HashedSet<CustomerAddress>(DirectorAddress)
-                       };
-            }
-            catch(Exception)
-            {
-            }
+		public Director FromModel() {
+			Director director = null;
 
-            return director;
+			try {
+				return director = new Director {
+					Id = Id,
+					Name = Name.Trim(),
+					Surname = Surname.Trim(),
+					Middle = (Middle ?? "").Trim(),
+					DateOfBirth = DateOfBirth.IndexOf("-", StringComparison.Ordinal) == -1 ? DateTime.ParseExact(DateOfBirth, "d/M/yyyy", CultureInfo.InvariantCulture) : (DateTime?)null,
+					DirectorAddress = DirectorAddress == null ? null : new HashedSet<CustomerAddress>(DirectorAddress),
+					Gender = (Gender)Enum.Parse(typeof(EZBob.DatabaseLib.Model.Database.Gender), Gender.ToString())
+				};
+			}
+			catch (Exception e) {
+				_log.Error("Failed to convert DirectorModel to Director", e);
+			}
 
-        }
+			return director;
+		} // FromModel
 
-        public static DirectorModel FromDirector(Director director)
-        {
-         
-                return new DirectorModel
-                           {
-                               Id = director.Id,
-                               Name = director.Name,
-                               Middle = director.Middle,
-                               Surname = director.Surname,
-                               DateOfBirth = FormattingUtils.FormatDateToString(director.DateOfBirth, "-/-/-"),
-                               DirectorAddress = director.DirectorAddress.ToArray()
-                           };
-        }
-    }
-}
+		public static DirectorModel FromDirector(Director director) {
+			return new DirectorModel {
+				Id = director.Id,
+				Name = director.Name,
+				Middle = director.Middle,
+				Surname = director.Surname,
+				DateOfBirth = FormattingUtils.FormatDateToString(director.DateOfBirth, "-/-/-"),
+				DirectorAddress = director.DirectorAddress.ToArray(),
+				Gender = director.Gender.ToString()[0]
+			};
+		} // FromDirector
+	} // class DirectorModel
+} // namespace
