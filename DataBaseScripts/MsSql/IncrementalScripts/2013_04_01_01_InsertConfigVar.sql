d56d9e2aba5440f9e2d8776aa1f4241255aae473
Arnautov <v.arnautov@scorto.com.ua>
2013-05-07 16:26:19 +0300

    *  ConfigurationVariables.EnableAutomaticRejection default value is changed to False

--- DataBaseScripts/MsSql/IncrementalScripts/2013_04_01_01_InsertConfigVar.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_04_01_01_InsertConfigVar.sql
@@ -1,4 +1,4 @@
 delete from ConfigurationVariables where name = 'EnableAutomaticRejection'
 GO
 INSERT INTO ConfigurationVariables (Name, [Value], [Description])
-VALUES ('EnableAutomaticRejection', '1', 'if "1" system will reject customers automatically without any Underwriter actions')
+VALUES ('EnableAutomaticRejection', '0', 'if "1" system will reject customers automatically without any Underwriter actions')
