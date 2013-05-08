cf2452b33245610822937b9af7b8a27a22f5502c
Shubin Igor <shubin.igor90@gmail.com>
2013-05-08 13:54:29 +0300

    fix for paypoint template

--- App/PluginWeb/EzBob.Web/Controllers/PaypointTemplateController.cs
+++ App/PluginWeb/EzBob.Web/Controllers/PaypointTemplateController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
-using Nustache.Core;
 
 namespace EzBob.Web.Controllers
 {
