613922bf7593eb68cb1233cee7a480fb0e00695f
Alex Boursouk <alexbo@ezbob.com>
2013-05-19 11:21:01 +0300

    Incremental update fix: take 2

--- DataBaseScripts/MsSql/IncrementalScripts/2013_05_13_01_IsLoanTypeSelectionAllowed.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_05_13_01_IsLoanTypeSelectionAllowed.sql
@@ -1,13 +1,21 @@
 DECLARE @TableName sysname
 DECLARE @DefaultConstraint sysname
 DECLARE @DropStmt NVARCHAR(500)
+DECLARE @ColName sysname
 
 SET @TableName = 'CashRequests'
+SET @ColName = 'IsLoanTypeSelectionAllowed'
 
-IF EXISTS (SELECT * FROM syscolumns WHERE name = 'IsLoanTypeSelectionAllowed' AND id = OBJECT_ID(@TableName))
+IF EXISTS (SELECT * FROM syscolumns WHERE name = @ColName AND id = OBJECT_ID(@TableName))
 BEGIN
-	SELECT @DefaultConstraint = NAME FROM sys.default_constraints WHERE parent_object_id = object_ID(@TableName) AND NAME = 'DF_CashRequests_IsLoanType'
-		
+	SELECT @DefaultConstraint = dc.name
+	FROM sys.default_constraints dc
+	INNER JOIN syscolumns c
+		ON dc.parent_column_id = c.colorder
+		AND dc.parent_object_id = c.id
+		AND c.name = @ColName
+		AND c.id = OBJECT_ID(@TableName)
+
 	SET @DropStmt = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT ' + @DefaultConstraint
 
 	EXEC(@DropStmt)
@@ -22,9 +30,16 @@ END
 
 SET @TableName = 'Customer'
 
-IF EXISTS (SELECT * FROM syscolumns WHERE name = 'IsLoanTypeSelectionAllowed' AND id = OBJECT_ID(@TableName))
+IF EXISTS (SELECT * FROM syscolumns WHERE name = @ColName AND id = OBJECT_ID(@TableName))
 BEGIN
-	SELECT @DefaultConstraint = NAME FROM sys.default_constraints WHERE parent_object_id = object_ID(@TableName) AND NAME = 'DF_Customer_IsLoanType'
+	SELECT @DefaultConstraint = dc.name
+	FROM sys.default_constraints dc
+	INNER JOIN syscolumns c
+		ON dc.parent_column_id = c.colorder
+		AND dc.parent_object_id = c.id
+		AND c.name = @ColName
+		AND c.id = OBJECT_ID(@TableName)
+
 	SET @DropStmt = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT ' + @DefaultConstraint
 
 	EXEC(@DropStmt)
