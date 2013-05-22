e490923fed2743384887837ef807e602e7883c45
stas_dulman <stasd@ezbob.com>
2013-05-22 14:28:51 +0300

    fixing incremental issue on production

--- DataBaseScripts/MsSql/IncrementalScripts/2013_05_09_01_PacnetAgentConfigsTable.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_05_09_01_PacnetAgentConfigsTable.sql
@@ -4,26 +4,20 @@ BEGIN
 END
 ELSE
 BEGIN
-CREATE TABLE dbo.PacnetAgentConfigs
-(
-	CfgKey VARCHAR(100) NOT NULL,
-	CfgValue VARCHAR(100),
-	CONSTRAINT PK_PacnetAgentConfigs PRIMARY KEY (CfgKey)
-)
+	CREATE TABLE dbo.PacnetAgentConfigs
+	(
+		CfgKey VARCHAR(100) NOT NULL,
+		CfgValue VARCHAR(100),
+		CONSTRAINT PK_PacnetAgentConfigs PRIMARY KEY (CfgKey)
+	)
+
+	INSERT INTO PacnetAgentConfigs VALUES ('MailboxReconnectionIntervalSeconds', '10')
+	INSERT INTO PacnetAgentConfigs VALUES ('LoginAddress', 'pacnetreport@ezbob.com')
+	INSERT INTO PacnetAgentConfigs VALUES ('LoginPassword', 'ezbob2012$')
+	INSERT INTO PacnetAgentConfigs VALUES ('Port', '993')
+	INSERT INTO PacnetAgentConfigs VALUES ('Server', 'imap.gmail.com')
+	INSERT INTO PacnetAgentConfigs VALUES ('MailBeeLicenseKey', 'MN700-E9212517212D217021D6F428312E-52A1')
 END
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-INSERT INTO PacnetAgentConfigs VALUES ('MailboxReconnectionIntervalSeconds', '10')
-INSERT INTO PacnetAgentConfigs VALUES ('LoginAddress', 'pacnetreport@ezbob.com')
-INSERT INTO PacnetAgentConfigs VALUES ('LoginPassword', 'ezbob2012$')
-INSERT INTO PacnetAgentConfigs VALUES ('Port', '993')
-INSERT INTO PacnetAgentConfigs VALUES ('Server', 'imap.gmail.com')
-INSERT INTO PacnetAgentConfigs VALUES ('MailBeeLicenseKey', 'MN700-E9212517212D217021D6F428312E-52A1')
 
 GO
 
