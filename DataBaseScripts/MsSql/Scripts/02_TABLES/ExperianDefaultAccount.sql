5ec31861a2b8111aa8f8badc0f1010133e8b7175
stas_dulman <stasd@ezbob.com>
2013-05-28 12:00:11 +0300

    updates from download script

---
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/ExperianDefaultAccount.sql
@@ -0,0 +1,20 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ExperianDefaultAccount]') AND type in (N'U'))
+DROP TABLE [dbo].[ExperianDefaultAccount]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ExperianDefaultAccount](
+	[Id] [int] NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[DateAdded] [datetime] NOT NULL,
+	[AccountType] [nvarchar](100) NULL,
+	[Date] [datetime] NULL,
+	[DelinquencyType] [nvarchar](100) NULL,
+ CONSTRAINT [PK_ExperianDefaultAccounts] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
--- DataBaseScripts/MsSql/Scripts/02_TABLES/YodleeBanks.sql
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/YodleeBanks.sql
@@ -9,9 +9,15 @@ CREATE TABLE [dbo].[YodleeBanks](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](300) NULL,
 	[ContentServiceId] [int] NOT NULL,
+	[ParentBank] [nvarchar](100) NOT NULL,
+	[Active] [bit] NOT NULL,
  CONSTRAINT [PK_YodleeBanks] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[YodleeBanks] ADD  DEFAULT ('abc') FOR [ParentBank]
+GO
+ALTER TABLE [dbo].[YodleeBanks] ADD  DEFAULT ((1)) FOR [Active]
 GO
