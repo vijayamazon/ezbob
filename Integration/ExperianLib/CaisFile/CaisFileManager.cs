b2eb591834553c82b92c8dbc2d526d9fb62c4169
Gorbach Alexey <gorbach.alexey@gmail.com>
2013-05-28 08:02:25 +0300

    cais file manager class added

---
+++ Integration/ExperianLib/CaisFile/CaisFileManager.cs
@@ -0,0 +1,28 @@
+﻿namespace ExperianLib.CaisFile
+{
+    public class CaisFileManager
+    {
+        private static BusinessCaisFileData _business;
+        private static CaisFileData _consumer;
+
+        public static BusinessCaisFileData GetBusinessCaisFileData()
+        {
+            return _business ?? (_business = new BusinessCaisFileData());
+        }
+
+        public static void RemoveBusinessCaisFileData()
+        {
+            _business = null;
+        }
+
+        public static CaisFileData GetCaisFileData()
+        {
+            return _consumer ?? (_consumer = new CaisFileData());
+        }
+
+        public static void RemoveCaisFileData()
+        {
+            _consumer = null;
+        }
+    }
+}
