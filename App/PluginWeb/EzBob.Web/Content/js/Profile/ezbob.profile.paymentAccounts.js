673a85549c14cc3ad6211e3d716e94ec2e827294
Prokofiev Yuriy <yprokofev@scorto.com.ua>
2013-05-29 17:21:10 +0300

    EZ-463, duplicated PayPal card has been removed from "Payment Accounts".

--- App/PluginWeb/EzBob.Web/Content/js/Profile/ezbob.profile.paymentAccounts.js
+++ App/PluginWeb/EzBob.Web/Content/js/Profile/ezbob.profile.paymentAccounts.js
@@ -4,7 +4,7 @@ EzBob.Profile = EzBob.Profile || {};
 EzBob.PaymentAccountModel = Backbone.Model.extend({
     defaults: {
         displayName: null,
-        type: "PayPal"
+        type: "Bank"
     }
 });
 
@@ -15,35 +15,10 @@ EzBob.PaymentAccounts = Backbone.Collection.extend({
 EzBob.Profile.PaymentAccountsView = Backbone.View.extend({
     initialize: function () {
         this.template = _.template($('#payment-accounts-template').html());
-
-        this.model.on('change:paypalAccounts', this.render, this);
     },
     render: function () {
-        var accounts = new EzBob.PaymentAccounts(this.model.get('paypalAccounts'));
-        if (this.model.get('BankAccountNumber')) {
-            accounts.add({ displayName: this.model.get('BankAccountNumber'), type: "Bank" });
-        }
+        var accounts = new EzBob.PaymentAccounts({ displayName: this.model.get('BankAccountNumber'), type: "Bank" });
         this.$el.html(this.template({ accounts: accounts.toJSON() }));
         return this;
-    },
-    events: {
-        'click .add-paypal': 'addPayPal'
-    },
-    addPayPal: function () {
-        //this.$el.empty();
-        if (!EzBob.Config.PayPalEnabled) {
-            EzBob.ShowMessage("The PayPal service is temporary offline. Contact us for support.");
-            return false;
-        }
-
-        var payPalInfoView = new EzBob.PayPalInfoView({ el: this.$el });
-        payPalInfoView.on('ready', this.paypalAdded, this);
-        payPalInfoView.on('back', this.render, this);
-        payPalInfoView.render();
-        return false;
-    },
-    paypalAdded: function () {
-        this.model.fetch();
-        this.render();
     }
 });
