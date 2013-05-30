446789e901cf74cdb377c519d602afadbf2c5924
Prokofiev Yuriy <yprokofev@scorto.com.ua>
2013-05-30 11:43:47 +0300

    ez-463, updated.

--- App/PluginWeb/EzBob.Web/Content/js/Profile/ezbob.profile.paymentAccounts.js
+++ App/PluginWeb/EzBob.Web/Content/js/Profile/ezbob.profile.paymentAccounts.js
@@ -1,12 +1,7 @@
 ﻿var EzBob = EzBob || {};
 EzBob.Profile = EzBob.Profile || {};
 
-EzBob.PaymentAccountModel = Backbone.Model.extend({
-    defaults: {
-        displayName: null,
-        type: "Bank"
-    }
-});
+EzBob.PaymentAccountModel = Backbone.Model.extend();
 
 EzBob.PaymentAccounts = Backbone.Collection.extend({
     model: EzBob.PaymentAccountModel
@@ -15,9 +10,14 @@ EzBob.PaymentAccounts = Backbone.Collection.extend({
 EzBob.Profile.PaymentAccountsView = Backbone.View.extend({
     initialize: function () {
         this.template = _.template($('#payment-accounts-template').html());
+
+        this.model.on('change:paypalAccounts', this.render, this);
     },
     render: function () {
-        var accounts = new EzBob.PaymentAccounts({ displayName: this.model.get('BankAccountNumber'), type: "Bank" });
+        var accounts = new EzBob.PaymentAccounts();
+        if (this.model.get('BankAccountNumber')) {
+            accounts.add({ displayName: this.model.get('BankAccountNumber'), type: "Bank" });
+        }
         this.$el.html(this.template({ accounts: accounts.toJSON() }));
         return this;
     }
