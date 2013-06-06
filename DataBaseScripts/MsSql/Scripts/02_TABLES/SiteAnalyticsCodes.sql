3dfa5b9e89cb9e8aba1f048c05a74789e15e53fb
stas_dulman <stasd@ezbob.com>
2013-06-06 14:10:59 +0300

    updating scripts after download incremental update

---
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/SiteAnalyticsCodes.sql
@@ -0,0 +1,17 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SiteAnalyticsCodes]') AND type in (N'U'))
+DROP TABLE [dbo].[SiteAnalyticsCodes]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SiteAnalyticsCodes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](300) NOT NULL,
+	[Description] [nvarchar](300) NULL,
+ CONSTRAINT [PK_SiteAnalyticsCodes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ DataBaseScripts/MsSql/Scripts/10_CONSTRAINTS/FK_CashRequests_DiscountPlan.sql
@@ -0,0 +1,8 @@
+﻿IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_CashRequests_DiscountPlan]') AND parent_object_id = OBJECT_ID(N'[dbo].[CashRequests]'))
+ALTER TABLE [dbo].[CashRequests] DROP CONSTRAINT [FK_CashRequests_DiscountPlan]
+GO
+ALTER TABLE [dbo].[CashRequests]  WITH CHECK ADD  CONSTRAINT [FK_CashRequests_DiscountPlan] FOREIGN KEY([DiscountPlanId])
+REFERENCES [dbo].[DiscountPlan] ([Id])
+GO
+ALTER TABLE [dbo].[CashRequests] CHECK CONSTRAINT [FK_CashRequests_DiscountPlan]
+GO
