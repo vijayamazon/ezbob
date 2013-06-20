4c1e4f75a62a1bbe4c3ad499e795f40d78fe5b6a
Alex Boursouk <alexbo@ezbob.com>
2013-06-20 20:21:23 +0300

    Update to Ezbob.Database library.

---
+++ Tools/EzbobUtils/DbConnection/DbException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ezbob.Database {
+	#region class DbException
+
+	public class DbException : Exception {
+		#region public
+
+		#region constructor
+
+		public DbException(string sMsg) : base(sMsg) {} // constructor
+
+		public DbException(string sMsg, Exception oInner) : base(sMsg, oInner) {} // constructor
+
+		#endregion constructor
+
+		#endregion public
+	} // class DbException
+
+	#endregion class DbException
+} // namespace Ezbob.Database
