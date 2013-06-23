ce1a23ed5d44762ea9fc70edcef33189b0c59fb4
Yuly Saar <yulys@ezbob.com>
2013-06-23 09:47:12 +0300

    Fix incremental - creation of IX_LoanSchedule_LoanId

--- DataBaseScripts/MsSql/IncrementalScripts/2013_06_05_01_Index.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_06_05_01_Index.sql
@@ -1,9 +1,9 @@
-IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[LoanSchedule]') AND name = N'IX_LoanSchedule_LoanId')
-RETURN
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[LoanSchedule]') AND name = N'IX_LoanSchedule_LoanId')
+BEGIN
+	CREATE NONCLUSTERED INDEX [IX_LoanSchedule_LoanId]
+	ON [dbo].[LoanSchedule] ([Status])
+	INCLUDE ([Date],[LoanId])
+END
 GO
 
 
-CREATE NONCLUSTERED INDEX [IX_LoanSchedule_LoanId]
-ON [dbo].[LoanSchedule] ([Status])
-INCLUDE ([Date],[LoanId])
-GO
