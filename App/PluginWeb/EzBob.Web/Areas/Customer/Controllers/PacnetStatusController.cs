16b4d99e6b444ed9f8edd5ab837f5246a8c96873
Yuly Saar <yulys@ezbob.com>
2013-06-24 13:04:27 +0300

    Fixed EZ-627: Removed card details from the loan summary

--- App/PluginWeb/EzBob.Web/Areas/Customer/Controllers/PacnetStatusController.cs
+++ App/PluginWeb/EzBob.Web/Areas/Customer/Controllers/PacnetStatusController.cs
@@ -1,13 +1,13 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using EzBob.Web.Areas.Customer.Models;
-using EzBob.Web.Code;
-using EzBob.Web.Infrastructure;
-using Scorto.Web;
-
-namespace EzBob.Web.Areas.Customer.Controllers
+﻿namespace EzBob.Web.Areas.Customer.Controllers
 {
-    public class    PacnetStatusController : Controller
+	using System.Linq;
+	using System.Web.Mvc;
+	using Models;
+	using Code;
+	using Infrastructure;
+	using Scorto.Web;
+
+    public class PacnetStatusController : Controller
     {
         private readonly IEzbobWorkplaceContext _context;
 
@@ -19,25 +19,18 @@ namespace EzBob.Web.Areas.Customer.Controllers
         [Transactional]
         public ActionResult Index()
         {
-            var amount = (decimal)TempData["amount"];
-            var bankNumber = (string)TempData["bankNumber"];
-            var card_no = TempData["card_no"];
+			var customer = _context.Customer;
+			var loan = customer.Loans.Last();
+			ViewData["LoanId"] = loan.Id;
+			ViewData["Amount"] = FormattingUtils.FormatPounds(_context.Customer.Loans.Last().LoanAmount);
+			ViewData["bankNumber"] = customer.BankAccount.AccountNumber;
+			ViewData["schedule"] = loan.Schedule.ToModel();
+			ViewData["name"] = customer.PersonalInfo.FirstName + " " + customer.PersonalInfo.Surname;
+			ViewData["email"] = customer.Name;
+			ViewData["loanNo"] = loan.RefNumber;
+			ViewData["SetupFee"] = loan.SetupFee > 0 ? FormattingUtils.FormatPounds(loan.SetupFee) : "";
+			ViewData["Total"] = FormattingUtils.FormatPounds(loan.Balance + loan.SetupFee);
 
-            var customer = _context.Customer;
-            var loan = customer.Loans.Last();
-
-            ViewData["Amount"] = FormattingUtils.FormatPounds(amount);
-            ViewData["LoanId"] = loan.Id;
-
-            ViewData["bankNumber"] = bankNumber;
-            ViewData["card_no"] = card_no;
-            ViewData["schedule"] = loan.Schedule.ToModel();
-            ViewData["name"] = customer.PersonalInfo.FirstName + " " + customer.PersonalInfo.Surname;
-            ViewData["email"] = customer.Name;
-            
-            ViewData["loanNo"] = loan.RefNumber;
-            ViewData["SetupFee"] = loan.SetupFee > 0 ? FormattingUtils.FormatPounds(loan.SetupFee) : "";
-            ViewData["Total"] = FormattingUtils.FormatPounds(loan.Balance + loan.SetupFee);
             return View("Index" );
         }
     }
