e69f29ca0612c583364e65a6866683819df177e1
stas_dulman <stasd@ezbob.com>
2013-06-24 23:34:37 +0300

    fixing the SP

--- DataBaseScripts/MsSql/IncrementalScripts/2013_06_24_02_GetLastStepCustomersSP.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_06_24_02_GetLastStepCustomersSP.sql
@@ -10,13 +10,13 @@ CREATE PROCEDURE GetLastStepCustomers
 	@DateEnd      DATETIME
 AS
 BEGIN
-	SELECT DISTINCT c.Name AS eMail, c.FirstName AS FirstName, c.Surname AS SurName, ManagerApprovedSum AS MaxApproved 
+	SELECT DISTINCT c.Name AS eMail, c.FirstName AS FirstName, c.Surname AS SurName, cr.ManagerApprovedSum AS MaxApproved 
 	FROM CashRequests cr, Customer c  
 	WHERE cr.UnderwriterDecision = 'Approved'
 	AND cr.IdCustomer = c.Id 
 	AND UnderwriterDecisionDate BETWEEN @DateStart AND @DateEnd
 	AND cr.Id IN (SELECT cr.Id FROM CashRequests cr EXCEPT SELECT l.RequestCashId FROM Loan l)
-	AND ManagerApprovedSum IS NOT NULL 
+	AND cr.ManagerApprovedSum IS NOT NULL 
 	AND c.IsTest = 0
 END
 GO
