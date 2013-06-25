3dd442329c9032fc3e622e652fc5e39b99568d6f
Shubin Igor <shubin.igor90@gmail.com>
2013-06-25 15:09:09 +0300

    fix directors validation

--- App/PluginWeb/EzBob.Web/Content/js/Wizard/yourInfo/ezbob.yourInfo.directors.js
+++ App/PluginWeb/EzBob.Web/Content/js/Wizard/yourInfo/ezbob.yourInfo.directors.js
@@ -1,115 +1,115 @@
 ﻿var EzBob = EzBob || {};
 
 EzBob.DirectorMainView = Backbone.View.extend({
-	initialize: function (options) {
-		this.preffix = options.name || 'directors';
-		this.template = _.template($('#director-edit-template').html());
-		this.directorTemplate = _.template($('#oneDirector').html());
-		this.model = new EzBob.DirectorsModels();
-		this.name = 'DirectorAddress';
-	},
-	events: {
-		"click #addDirector": "addDirector",
-		"click .removeDirector": "removeDirector",
+    initialize: function(options) {
+        this.preffix = options.name || 'directors';
+        this.template = _.template($('#director-edit-template').html());
+        this.directorTemplate = _.template($('#oneDirector').html());
+        this.model = new EzBob.DirectorsModels();
+        this.name = 'DirectorAddress';
+    },
+    events: {
+        "click #addDirector": "addDirector",
+        "click .removeDirector": "removeDirector",
 
-		"change .director_name_part": function (jqEvent) { this.directorNamePartChanged(jqEvent.target); },
-		"keyup  .director_name_part": function (jqEvent) { this.directorNamePartChanged(jqEvent.target); },
-		"cut    .director_name_part": function (jqEvent) { this.directorNamePartChanged(jqEvent.target); },
-		"paste  .director_name_part": function (jqEvent) { this.directorNamePartChanged(jqEvent.target); },
+        "change .director_name_part": function(jqEvent) { this.directorNamePartChanged(jqEvent.target); },
+        "keyup  .director_name_part": function(jqEvent) { this.directorNamePartChanged(jqEvent.target); },
+        "cut    .director_name_part": function(jqEvent) { this.directorNamePartChanged(jqEvent.target); },
+        "paste  .director_name_part": function(jqEvent) { this.directorNamePartChanged(jqEvent.target); },
 
-		"change .director_gender": function (jqEvent) { this.directorGenderChanged(jqEvent.target); },
-		"keyup  .director_gender": function (jqEvent) { this.directorGenderChanged(jqEvent.target); },
-		"cut    .director_gender": function (jqEvent) { this.directorGenderChanged(jqEvent.target); },
-		"paste  .director_gender": function (jqEvent) { this.directorGenderChanged(jqEvent.target); },
+        "change .director_gender": function(jqEvent) { this.directorGenderChanged(jqEvent.target); },
+        "keyup  .director_gender": function(jqEvent) { this.directorGenderChanged(jqEvent.target); },
+        "cut    .director_gender": function(jqEvent) { this.directorGenderChanged(jqEvent.target); },
+        "paste  .director_gender": function(jqEvent) { this.directorGenderChanged(jqEvent.target); },
 
-		"change .director_date": function (jqEvent) { this.directorDateChanged(jqEvent.target); },
-		"keyup  .director_date": function (jqEvent) { this.directorDateChanged(jqEvent.target); },
-		"cut    .director_date": function (jqEvent) { this.directorDateChanged(jqEvent.target); },
-		"paste  .director_date": function (jqEvent) { this.directorDateChanged(jqEvent.target); }
-	},
-	directorNamePartChanged: function (oTarget) {
-		oTarget = $(oTarget);
-		var oIcon = oTarget.closest('div').find('img.field_status').first();
+        "change .director_date": function(jqEvent) { this.directorDateChanged(jqEvent.target); },
+        "keyup  .director_date": function(jqEvent) { this.directorDateChanged(jqEvent.target); },
+        "cut    .director_date": function(jqEvent) { this.directorDateChanged(jqEvent.target); },
+        "paste  .director_date": function(jqEvent) { this.directorDateChanged(jqEvent.target); }
+    },
+    directorNamePartChanged: function(oTarget) {
+        oTarget = $(oTarget);
+        var oIcon = oTarget.closest('div').find('img.field_status').first();
 
-		if (oTarget.val() == '') {
-			oIcon.field_status('clear', 'immediately');
-			return;
-		} // if
+        if (oTarget.val() == '') {
+            oIcon.field_status('clear', 'immediately');
+            return;
+        } // if
 
-		var sStatusName = '';
+        var sStatusName = '';
 
-		if (oIcon.hasClass('required'))
-			sStatusName = oTarget.val() ? 'ok' : 'fail';
-		else
-			sStatusName = oTarget.val() ? 'ok' : 'empty';
+        if (oIcon.hasClass('required'))
+            sStatusName = oTarget.val() ? 'ok' : 'fail';
+        else
+            sStatusName = oTarget.val() ? 'ok' : 'empty';
 
-		oIcon.field_status('set', sStatusName);
-	}, // directorNamePartChanged
-	directorGenderChanged: function (oTarget) {
-		oTarget = $(oTarget);
-		var oParent = oTarget.closest('div');
+        oIcon.field_status('set', sStatusName);
+    }, // directorNamePartChanged
+    directorGenderChanged: function(oTarget) {
+        oTarget = $(oTarget);
+        var oParent = oTarget.closest('div');
 
-		var nCheckedCount = 0;
+        var nCheckedCount = 0;
 
-		oParent.find('.director_gender').each(function (idx, oChk) {
-			if ($(oChk).attr('checked')) {
-				nCheckedCount++;
-				return false;
-			} // if
+        oParent.find('.director_gender').each(function(idx, oChk) {
+            if ($(oChk).attr('checked')) {
+                nCheckedCount++;
+                return false;
+            } // if
 
-			return true;
-		});
+            return true;
+        });
 
-		if (0 == nCheckedCount)
-			oParent.find('img.field_status').field_status('clear', 'right now');
-		else
-			oParent.find('img.field_status').field_status('set', 'ok');
-	}, // directorGenderChanged
-	directorDateChanged: function(oTarget) {
-		var oDiv = $(oTarget).closest('div');
+        if (0 == nCheckedCount)
+            oParent.find('img.field_status').field_status('clear', 'right now');
+        else
+            oParent.find('img.field_status').field_status('set', 'ok');
+    }, // directorGenderChanged
+    directorDateChanged: function(oTarget) {
+        var oDiv = $(oTarget).closest('div');
 
-		var nGoodValueCount = 0;
+        var nGoodValueCount = 0;
 
-		oDiv.find('.director_date').each(function() {
-			var o = $(this);
+        oDiv.find('.director_date').each(function() {
+            var o = $(this);
 
-			var sEmptyValue = o.attr('empty_value');
-			var sValue = o.val();
+            var sEmptyValue = o.attr('empty_value');
+            var sValue = o.val();
 
-			var bEmpty = !sValue || (sEmptyValue && (sValue == sEmptyValue));
+            var bEmpty = !sValue || (sEmptyValue && (sValue == sEmptyValue));
 
-			if (!bEmpty)
-				nGoodValueCount++;
-		}); // each
+            if (!bEmpty)
+                nGoodValueCount++;
+        }); // each
 
-		var sStatusName = '';
+        var sStatusName = '';
 
-		if (nGoodValueCount == 3)
-			sStatusName = 'ok';
-		else if (nGoodValueCount > 0)
-			sStatusName = 'fail';
+        if (nGoodValueCount == 3)
+            sStatusName = 'ok';
+        else if (nGoodValueCount > 0)
+            sStatusName = 'fail';
 
-		if ('' == sStatusName)
-			oDiv.find('img.field_status').field_status('clear', 'immediately');
-		else
-			oDiv.find('img.field_status').field_status('set', sStatusName);
-	}, // directorDateChanged
-	
-	render: function () {
-		this.$el.html(this.template());
-		this.directorArea = this.$el.find('.directorArea');
-		return this;
-	},
-	renderDirector: function () {
-		var that = this;
+        if ('' == sStatusName)
+            oDiv.find('img.field_status').field_status('clear', 'immediately');
+        else
+            oDiv.find('img.field_status').field_status('set', sStatusName);
+    }, // directorDateChanged
 
-		this.directorArea.html(this.directorTemplate({ preffix: this.preffix, directors: this.model.toJSON() }));
-		this.directorArea.find('.ezDateTime').splittedDateTime();
+    render: function() {
+        this.$el.html(this.template());
+        this.directorArea = this.$el.find('.directorArea');
+        return this;
+    },
+    renderDirector: function() {
+        var that = this;
 
-		var oFieldStatusIcons = this.$el.find('IMG.field_status');
-		oFieldStatusIcons.filter('.required').field_status({ required: true });
-		oFieldStatusIcons.not('.required').field_status({ required: false });
-        /*
+        this.directorArea.html(this.directorTemplate({ preffix: this.preffix, directors: this.model.toJSON() }));
+        this.directorArea.find('.ezDateTime').splittedDateTime();
+
+        var oFieldStatusIcons = this.$el.find('IMG.field_status');
+        oFieldStatusIcons.filter('.required').field_status({ required: true });
+        oFieldStatusIcons.not('.required').field_status({ required: false });
+        
 		$.each(this.model.models, function (i, val) {
 			var addressElem = that.preffix + 'Address' + i,
 				name = that.preffix + "[" + i + "]." + that.name,
@@ -119,14 +119,14 @@ EzBob.DirectorMainView = Backbone.View.extend({
 			addressView.render().$el.appendTo(that.directorArea.find("#" + addressElem));
 			SetDefaultDate(dateOfBirthValName, val.get("DateOfBirth"));
 		});
-        */
-		
+        
 
-		this.$el.find('.director_name_part').each(function () { that.directorNamePartChanged(this); });
-		this.$el.find('.director_gender').first().each(function () { that.directorGenderChanged(this); });
-	    
-		this.$el.attardi_labels('toggle_all');
-		/*this.$el.find('.director_date').first().each(function () { that.directorDateChanged(this); });*/
+
+        this.$el.find('.director_name_part').each(function() { that.directorNamePartChanged(this); });
+        this.$el.find('.director_gender').first().each(function() { that.directorGenderChanged(this); });
+
+        this.$el.attardi_labels('toggle_all');
+        /*this.$el.find('.director_date').first().each(function () { that.directorDateChanged(this); });*/
         /*
 		this.$el.find('.director_date, img.field_status').each(function() {
 			var oDateUIComponent = $(this);
@@ -143,48 +143,48 @@ EzBob.DirectorMainView = Backbone.View.extend({
 
 			oDateUIComponent.attr('id', sID);
 		});*/
-	},
-	addDirector: function () {
-		this.model.add(new EzBob.DirectorsModel({ Address: new EzBob.AddressModels() }));
-		this.updateModel();
-		this.renderDirector();
+    },
+    addDirector: function() {
+        this.model.add(new EzBob.DirectorsModel({ Address: new EzBob.AddressModels() }));
+        this.updateModel();
+        this.renderDirector();
 
-		return false;
-	},
-	removeDirector: function (el) {
-		this.updateModel();
-		var num = $(el.currentTarget).attr('number') - 1;
-		this.model.remove(this.model.at(num));
-		this.renderDirector();
-		return false;
-	},
-	updateModel: function () {
-		var model = SerializeArrayToEasyObject(this.directorArea.parents('form').serializeArray()),
-			that = this;
-		$.each(this.model.models, function (i, oneModel) {
-			$.each(oneModel.toJSON(), function (key) {
-				$.each(model, function (k, formModelValue) {
-					var valueName = that.preffix + "[" + i + "]." + key;
-					if (k == valueName) {
-						oneModel.set(key, formModelValue);
-					}
-				});
-			});
-		});
-	}
+        return false;
+    },
+    removeDirector: function(el) {
+        this.updateModel();
+        var num = $(el.currentTarget).attr('number') - 1;
+        this.model.remove(this.model.at(num));
+        this.renderDirector();
+        return false;
+    },
+    updateModel: function() {
+        var model = SerializeArrayToEasyObject(this.directorArea.parents('form').serializeArray()),
+            that = this;
+        $.each(this.model.models, function(i, oneModel) {
+            $.each(oneModel.toJSON(), function(key) {
+                $.each(model, function(k, formModelValue) {
+                    var valueName = that.preffix + "[" + i + "]." + key;
+                    if (k == valueName) {
+                        oneModel.set(key, formModelValue);
+                    }
+                });
+            });
+        });
+    }
 });
 
 EzBob.DirectorsModel = Backbone.Model.extend({
-	defaults: {
-		Name: "",
-		Middle: "",
-		Surname: "",
-		Gender: "",
-		DateOfBirth: "",
-		Address: new EzBob.AddressModels()
-	}
+    defaults: {
+        Name: "",
+        Middle: "",
+        Surname: "",
+        Gender: "",
+        DateOfBirth: "",
+        Address: new EzBob.AddressModels()
+    }
 });
 
 EzBob.DirectorsModels = Backbone.Collection.extend({
-	model: EzBob.DirectorModel
+    model: EzBob.DirectorModel
 });
