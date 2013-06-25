e74f74fa65dff0a66c86f6f55874e6fbd557369e
Yuly Saar <yulys@ezbob.com>
2013-06-25 11:14:26 +0300

    Fixed yodlee's oauth signature generation. Implemented retries for items that are not yet refreshed.

--- Integration/YodleeLib/YodleeMain.cs
+++ Integration/YodleeLib/YodleeMain.cs
@@ -1,6 +1,7 @@
 ﻿namespace YodleeLib
 {
 	using System;
+	using System.Threading;
 	using StructureMap;
 	using config;
 	using log4net;
@@ -13,6 +14,7 @@
 		private readonly ServerVersionManagementService serverVersionManagementService = new ServerVersionManagementService();
 		private static IYodleeMarketPlaceConfig config;
 		private bool isLoggedIn;
+		private int numOfRetriesForGetItemSummary;
 		
 		public YodleeMain()
 		{
@@ -53,45 +55,58 @@
 
 		public string GetFinalUrl(int csId, string callback, string username, string password)
 		{
-			string url = config.AddAccountURL;
-			const string sParams = "?access_type=oauthdeeplink&displayMode=desktop&_csid=";
-			var oAuthBase = new OAuthBase();
-			string timestamp = oAuthBase.GenerateTimeStamp();
-			string nonce = oAuthBase.GenerateNonce();
-			string applicationKey = config.ApplicationKey;
-			string applicationToken = config.ApplicationToken;
-
-			if (!isLoggedIn)
+			try
 			{
-				LoginUser(username, password);
+				string url = config.AddAccountURL;
+				const string sParams = "?access_type=oauthdeeplink&displayMode=desktop&_csid=";
+				var oAuthBase = new OAuthBase();
+				if (!isLoggedIn)
+				{
+					LoginUser(username, password);
+				}
+				var lu = new LoginUser();
+				OAuthAccessToken token = lu.getAccessTokens(UserContext);
+
+				string signature = null;
+				string normalizedUrl = null;
+				string normalizedRequestParameters = null;
+				while (string.IsNullOrEmpty(signature) || signature.Contains("+"))
+				{
+					string timestamp = oAuthBase.GenerateTimeStamp();
+					string nonce = oAuthBase.GenerateNonce();
+					string applicationKey = config.ApplicationKey;
+					string applicationToken = config.ApplicationToken;
+
+					string signatureBase = oAuthBase.GenerateSignatureBase(
+						new Uri(string.Format("{0}{1}{2}", url, sParams, csId)),
+						applicationKey,
+						token.token,
+						token.tokenSecret,
+						"GET",
+						timestamp,
+						nonce,
+						OAuthBase.HMACSHA1SignatureType,
+						callback,
+						out normalizedUrl,
+						out normalizedRequestParameters
+						);
+
+					signature = oAuthBase.GenerateSignature(signatureBase, applicationToken, token.tokenSecret);
+				}
+
+				string finalUrl = string.Format("{0}?{1}&oauth_signature={2}",
+					normalizedUrl,
+					normalizedRequestParameters,
+					signature
+				);
+
+				return finalUrl;
 			}
-			var lu = new LoginUser();
-			OAuthAccessToken token = lu.getAccessTokens(UserContext);
-
-			string normalizedUrl;
-			string normalizedRequestParameters;
-			string signatureBase = oAuthBase.GenerateSignatureBase(
-				new Uri(string.Format("{0}{1}{2}", url, sParams, csId)),
-				applicationKey,
-				token.token,
-				token.tokenSecret,
-				"GET",
-				timestamp,
-				nonce,
-				OAuthBase.HMACSHA1SignatureType,
-				callback,
-				out normalizedUrl,
-				out normalizedRequestParameters
-			);
-
-			string signature = oAuthBase.GenerateSignature(signatureBase, applicationToken, token.tokenSecret);
-			string finalUrl = string.Format("{0}?{1}&oauth_signature={2}",
-				normalizedUrl,
-				normalizedRequestParameters,
-				signature
-			);
-
-			return finalUrl;
+			catch (Exception e)
+			{
+				Log.ErrorFormat("Exception while generating yodlee redirection url:{0}", e);
+				throw;
+			}
 		}
 
 		public long GetItemId(string username, string password, out string displayname, out long csId)
@@ -99,32 +114,12 @@
 			displayname = string.Empty;
 			csId = -1;
 			LoginUser(username, password);
-			var di = new DisplayItemInfo();
 
-			object[] oa;
-			try
-			{
-				oa = di.displayItemSummariesWithoutItemData(UserContext);
-			}
-			catch (Exception e)
-			{
-				Log.ErrorFormat("Failure getting yodlee item. Error:{0}", e);
-				return -1;
-			}
+			numOfRetriesForGetItemSummary = 5;
+			ItemSummary itemSummary = GetItemSummary(username); 
 
-			if (oa == null || oa.Length == 0)
+			if (itemSummary == null)
 			{
-				// No items were found for the user.
-				Log.InfoFormat("No items were found for the user '{0}'", username);
-				return -1;
-			}
-
-			var itemSummary = (ItemSummary)oa[oa.Length - 1];
-
-			if (itemSummary.refreshInfo.statusCode != 0)
-			{
-				Log.WarnFormat("Item status code is not '0' but '{0}' for user {1}", itemSummary.refreshInfo.statusCode, username);
-				//RemoveItem(itemSummary.itemId); // TODO: we should delete for credentials associated failures but not all of them
 				return -1;
 			}
 
@@ -134,6 +129,45 @@
 			Log.InfoFormat("Received yodlee item id: '{0}'", itemSummary.itemId);
 
 			return itemSummary.itemId;
+		}
+
+		private ItemSummary GetItemSummary(string username)
+		{
+			var di = new DisplayItemInfo();
+			object[] oa;
+			try
+			{
+				oa = di.displayItemSummariesWithoutItemData(UserContext);
+			}
+			catch (Exception e)
+			{
+				Log.ErrorFormat("Failure getting yodlee item. Error:{0}", e);
+				return null;
+			}
+
+			if (oa == null || oa.Length == 0)
+			{
+				// No items were found for the user.
+				Log.InfoFormat("No items were found for the user '{0}'", username);
+				return null;
+			}
+
+			var itemSummary = (ItemSummary)oa[oa.Length - 1];
+
+			if (itemSummary.refreshInfo.statusCode != 0)
+			{
+				Log.WarnFormat("Item status code is not '0' but '{0}' for user {1}", itemSummary.refreshInfo.statusCode, username);
+				numOfRetriesForGetItemSummary--;
+				if (numOfRetriesForGetItemSummary > 0)
+				{
+					Log.InfoFormat("Will retry {0} more time\\s", numOfRetriesForGetItemSummary);
+					Thread.Sleep(5000);
+					return GetItemSummary(username);
+				}
+				return null;
+			}
+
+			return itemSummary;
 		}
 
 		private void RemoveItem(long itemId)
