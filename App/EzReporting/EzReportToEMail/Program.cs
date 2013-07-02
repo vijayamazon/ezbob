b8a8d21f1a5e00f49a6196290d0c36601bd49c95
Alex Boursouk <alexbo@ezbob.com>
2013-07-02 12:40:42 +0300

    Reports to email app bug fix.

--- App/EzReporting/EzReportToEMail/Program.cs
+++ App/EzReporting/EzReportToEMail/Program.cs
@@ -9,13 +9,13 @@ namespace EzReportToEMail {
 			var log = new LegacyLog();
 			var env = new Ezbob.Context.Environment(log);
 
-			DateTime dNow = DateTime.Now;
+			DateTime dNow = DateTime.Today;
 
 			if ((args.Length > 1) && (args[0] == "--date"))
 				DateTime.TryParseExact(args[1], "yyyy-MM-dd", new CultureInfo("en-GB"), DateTimeStyles.None, out dNow);
 
 			log.Info("Current environment is {0}", env.Context);
-			log.Info("Running with current date {0}", dNow.ToString("yyyy-MM-dd"));
+			log.Info("Running with current date {0}", dNow.ToString("MMMM d yyyy H:mm:ss"));
 
 			var reportsHandler = new EmailReportHandler(new SqlConnection(log), log);
 			reportsHandler.ExecuteReportHandler(dNow);
