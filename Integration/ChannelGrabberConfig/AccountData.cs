23442c8b9c1885dbc06a7b6f8ca6fb195f3f277a
Prokofiev Yuriy <yprokofev@scorto.com.ua>
2013-07-09 15:36:41 +0300

    EZ-696, change error text.

--- Integration/ChannelGrabberConfig/AccountData.cs
+++ Integration/ChannelGrabberConfig/AccountData.cs
@@ -118,7 +118,7 @@ namespace Integration.ChannelGrabberConfig {
 				throw new ConfigException("Validation is not complete.");
 
 			if (!XmlUtil.IsEqual(doc, "validity", "true"))
-				throw new ConfigException("Cannot validate: invalid credentials.");
+				throw new ConfigException("Cannot validate: invalid credentials or not authorized.");
 		} // Validate
 
 		#endregion method Validate
