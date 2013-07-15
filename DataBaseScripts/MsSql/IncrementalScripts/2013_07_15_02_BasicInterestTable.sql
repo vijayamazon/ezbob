9fd23baa46741e20ba3473dde35880a615445643
Arnautov <v.arnautov@scorto.com.ua>
2013-07-15 14:59:01 +0300

    + new table with basic interest rates

---
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_07_15_02_BasicInterestTable.sql
@@ -0,0 +1,33 @@
+CREATE TABLE dbo.BasicInterestRate
+	(
+	FromScore int NOT NULL,
+	ToScore int NOT NULL,
+	LoanIntrestBase float(53) NOT NULL
+	)  ON [PRIMARY]
+GO
+
+
+IF NOT EXISTS (SELECT * FROM BasicInterestRate WHERE LoanIntrestBase = '0.07')
+	INSERT INTO BasicInterestRate (FromScore, ToScore, LoanIntrestBase)
+		VALUES ('0', '649', '0.07')
+GO
+
+IF NOT EXISTS (SELECT * FROM BasicInterestRate WHERE LoanIntrestBase = '0.06')
+	INSERT INTO BasicInterestRate (FromScore, ToScore, LoanIntrestBase)
+		VALUES ('650', '849', '0.06')
+GO
+
+IF NOT EXISTS (SELECT * FROM BasicInterestRate WHERE LoanIntrestBase = '0.05')
+	INSERT INTO BasicInterestRate (FromScore, ToScore, LoanIntrestBase)
+		VALUES ('850', '999', '0.05')
+GO
+
+IF NOT EXISTS (SELECT * FROM BasicInterestRate WHERE LoanIntrestBase = '0.04')
+	INSERT INTO BasicInterestRate (FromScore, ToScore, LoanIntrestBase)
+		VALUES ('1000', '1099', '0.04')
+GO
+
+IF NOT EXISTS (SELECT * FROM BasicInterestRate WHERE LoanIntrestBase = '0.03')
+	INSERT INTO BasicInterestRate (FromScore, ToScore, LoanIntrestBase)
+		VALUES ('1100', '9999', '0.03')
+GO
