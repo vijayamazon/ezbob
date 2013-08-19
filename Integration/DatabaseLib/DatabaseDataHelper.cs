79baa71703ca3a3e17585cd3d10fdaf0833e84bb
Yuly Saar <yulys@ezbob.com>
2013-08-19 12:18:44 +0300

    EZ-864: MP error status is changed in every recheck

--- Integration/DatabaseLib/DatabaseDataHelper.cs
+++ Integration/DatabaseLib/DatabaseDataHelper.cs
@@ -325,6 +325,11 @@ namespace EZBob.DatabaseLib
 			{
 				updatingHistoryRecord.Error = exception.Message;
 				statusText = "With error!";
+				customerMarketPlace.UpdateError = exception.Message;
+			}
+			else
+			{
+				customerMarketPlace.UpdateError = null;
 			}
 
 			WriteToLog(string.Format("End update data for umi: id: {0}, name: {1}. {2}", customerMarketPlace.Id, customerMarketPlace.DisplayName, statusText));
