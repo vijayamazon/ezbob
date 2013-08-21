51e8ffa9ff3bdd6314ab8414dd04b4e75dd4ec53
Alexey Gorbach <gorbach.alexey@gmail.com>
2013-08-21 19:26:40 +0300

    unit test for ErrorRetryingInfo deserialization added

---
+++ App/PluginWeb/EzBob.Tests/Serialization/DeserializeFixture.cs
@@ -0,0 +1,18 @@
+﻿using EzBob.CommonLib;
+using NUnit.Framework;
+
+namespace EzBob.Tests.Serialization
+{
+    [TestFixture]
+    [Ignore]
+    public class DeserializeFixture
+    {
+        [Test]
+        public void can_deserialize_error_retrying_info()
+        {
+            var xml = "<ErrorRetryingInfo UseLastTimeOut=\"false\" MinorTimeoutInSeconds=\"60\" EnableRetrying=\"true\"><IterationSettings Index=\"1\" CountRequestsExpectError=\"10\" TimeOutAfterRetryingExpiredInMinutes=\"30\" /><IterationSettings Index=\"2\" CountRequestsExpectError=\"5\" TimeOutAfterRetryingExpiredInMinutes=\"0\" /></ErrorRetryingInfo>";
+            var data = SerializeDataHelper.DeserializeTypeFromString<ErrorRetryingInfo>(xml);
+            Assert.That(data.Info, Is.Not.Null);
+        }
+    }
+}
