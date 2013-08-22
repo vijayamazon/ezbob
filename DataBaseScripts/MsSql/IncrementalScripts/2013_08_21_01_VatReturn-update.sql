b3682e4050b5234d4d78aae93269c4d70e326d0b
Alex Boursouk <alexbo@ezbob.com>
2013-08-22 17:17:06 +0300

    Bug fix in _MarketplaceValuesTemplate.cshtml

--- DataBaseScripts/MsSql/IncrementalScripts/2013_08_21_01_VatReturn-update.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_08_21_01_VatReturn-update.sql
@@ -1,5 +1,7 @@
 IF OBJECT_ID('MP_VatReturnRecords') IS NOT NULL
 BEGIN
+	ALTER TABLE MP_VatReturnRecords DROP CONSTRAINT DF_VatReturnRecord_RegNo
+
 	ALTER TABLE MP_VatReturnRecords DROP COLUMN RegistrationNo
 
 	ALTER TABLE MP_VatReturnRecords ADD RegistrationNo BIGINT NOT NULL CONSTRAINT DF_VatReturnRecord_RegNo DEFAULT (0)
