aaa9abaee8f47d2df21fb39bdfb2f64254835f6a
Arnautov <v.arnautov@scorto.com.ua>
2013-08-23 18:06:14 +0300

    EZ-571 implementation finished

--- DataBaseScripts/MsSql/IncrementalScripts/2013_08_23_01_LoanSheduleCustomDate.sql
+++ DataBaseScripts/MsSql/IncrementalScripts/2013_08_23_01_LoanSheduleCustomDate.sql
@@ -1,3 +1,3 @@
-ALTER TABLE dbo.LoanSchedule ADD
-	CustomInstallmentDate date NULL
+IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE Name = N'CustomInstallmentDate' and Object_ID = Object_ID(N'LoanSchedule'))
+ALTER TABLE dbo.LoanSchedule ADD CustomInstallmentDate date NULL
 GO
