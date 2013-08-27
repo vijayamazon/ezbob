3915d72ca0349b6e232374d6abc15be57845f6ba
stas_dulman <stasd@ezbob.com>
2013-08-27 15:48:28 +0300

    ez-890 Call Management report

--- DataBaseScripts/MsSql/Scripts/07_PROCEDURES/RptCallManagement.sql
+++ DataBaseScripts/MsSql/Scripts/07_PROCEDURES/RptCallManagement.sql
@@ -36,12 +36,13 @@ WHERE c.Id NOT IN
 	 OR Name LIKE '%liatvanir%'
 	 OR Name LIKE '%q@q%'
 	 OR Name LIKE '%1@1%'
-	 OR C.IsTest=1)
+	 OR C.IsTest=1
+	 )
 
------------ GET Loan Data---------
+----------- GET Loan Count Data---------
 IF OBJECT_ID('tempdb..#temp2') IS NOT NULL DROP TABLE #temp2
 
-SELECT c.Id cId, l.Status LoanStatus, count(l.Id) AmountOfLoans, ISNULL(sum(l.Balance), 0) OutstandingBalance
+SELECT c.Id cId, count(l.Id) AmountOfLoans, ISNULL(sum(l.Balance), 0) OutstandingBalance
 INTO #temp2
 FROM Customer c
 LEFT JOIN Loan l ON c.Id = l.CustomerId
@@ -52,21 +53,58 @@ WHERE c.Id NOT IN
 					OR Name LIKE '%liatvanir%'
 					OR Name LIKE '%q@q%'
 					OR Name LIKE '%1@1%'
-					OR C.IsTest=1)
-GROUP BY c.Id, l.Status
-HAVING count(l.Id) > 0
+					OR C.IsTest=1
+					)
+GROUP BY c.Id
 
+----------- GET Loan Status---------
+IF OBJECT_ID('tempdb..#temp3') IS NOT NULL DROP TABLE #temp3
+SELECT x.cId, 
+COALESCE(
+  CASE WHEN x.StatusId='0' THEN 'Late' ELSE NULL END, 
+  CASE WHEN x.StatusId='1' THEN 'Live' ELSE NULL END,
+  CASE WHEN x.StatusId='2' THEN 'PaidOff' ELSE NULL END,
+  CASE WHEN x.StatusId='3' THEN '-' ELSE NULL END)
+  AS LoanStatus
+ INTO #temp3
+ FROM 
+ (
+SELECT t.cId, min(t.Status) StatusId FROM 
+(
+SELECT c.Id cId, 
+  COALESCE(
+  CASE WHEN l.Status='Late' THEN '0' ELSE NULL END, 
+  CASE WHEN l.Status='Live' THEN '1' ELSE NULL END,
+  CASE WHEN l.Status='PaidOff' THEN '2' ELSE NULL END,
+  CASE WHEN l.Status IS NULL THEN '3' ELSE NULL END)
+  AS Status
+FROM Customer c
+LEFT JOIN Loan l ON c.Id = l.CustomerId
+WHERE c.Id NOT IN 
+				  ( SELECT C.Id
+					FROM Customer C 
+					WHERE Name LIKE '%ezbob%'
+					OR Name LIKE '%liatvanir%'
+					OR Name LIKE '%q@q%'
+					OR Name LIKE '%1@1%'
+				  	OR C.IsTest=1
+				  )
+) t
+GROUP BY t.cId
+) x
 
 --------- FINAL TABLE MERGE ----------
 
 SELECT T1.eMail, T1.name, T1.Status, T1.OpenOffer, T2.AmountOfLoans, T2.OutstandingBalance,
-T2.LoanStatus, T1.CRMStatus, T1.CRMAction, T1.CRMComment
-FROM #temp2 T2  
-LEFT JOIN #temp1 T1 ON T1.cId = T2.cId
+T3.LoanStatus, T1.CRMStatus, T1.CRMAction, T1.CRMComment
+FROM #temp1 T1  
+LEFT JOIN #temp2 T2 ON T1.cId = T2.cId
+LEFT JOIN #temp3 T3 ON T1.cId = T3.cId
 
 
 DROP TABLE #temp1
 DROP TABLE #temp2
+DROP TABLE #temp3
 
 END
 GO
