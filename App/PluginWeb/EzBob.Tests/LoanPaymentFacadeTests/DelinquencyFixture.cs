e02bf082dd52a22c1c62aed995ebf9506116c4f9
Alexey Gorbach <gorbach.alexey@gmail.com>
2013-08-28 12:35:32 +0300

    DelinquencyFixture added

---
+++ App/PluginWeb/EzBob.Tests/LoanPaymentFacadeTests/DelinquencyFixture.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace EzBob.Tests.LoanPaymentFacadeTests
+{
+    public class DelinquencyFixture : LoanPaymentsTestBase
+    {
+        [Test]
+        [Ignore]
+        public void delinquency_for_one_missed_installment()
+        {
+            CreateLoan(Parse("2012-01-01 12:00:00.000"), 1000);
+            MakePayment(389, Parse("2012-02-15 12:00:00.000"));
+
+            Assert.That(_loan.MaxDelinquencyDays, Is.EqualTo(15));
+        }
+    }
+}
