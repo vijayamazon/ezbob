3a9a8f75e2cff5e3be24dfb2b35da33da5bc2a0e
stas_dulman <stasd@ezbob.com>
2013-09-02 17:11:33 +0300

    Download script

---
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/MP_RtiTaxMonthRecords.sql
@@ -0,0 +1,18 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MP_RtiTaxMonthRecords]') AND type in (N'U'))
+DROP TABLE [dbo].[MP_RtiTaxMonthRecords]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MP_RtiTaxMonthRecords](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerMarketPlaceId] [int] NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[CustomerMarketPlaceUpdatingHistoryRecordId] [int] NOT NULL,
+ CONSTRAINT [PK_RtiTaxMonthRecords] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ DataBaseScripts/MsSql/Scripts/10_CONSTRAINTS/FK_RtiTaxMonthRecord_MP.sql
@@ -0,0 +1,8 @@
+﻿IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_RtiTaxMonthRecord_MP]') AND parent_object_id = OBJECT_ID(N'[dbo].[MP_RtiTaxMonthRecords]'))
+ALTER TABLE [dbo].[MP_RtiTaxMonthRecords] DROP CONSTRAINT [FK_RtiTaxMonthRecord_MP]
+GO
+ALTER TABLE [dbo].[MP_RtiTaxMonthRecords]  WITH CHECK ADD  CONSTRAINT [FK_RtiTaxMonthRecord_MP] FOREIGN KEY([CustomerMarketPlaceId])
+REFERENCES [dbo].[MP_CustomerMarketPlace] ([Id])
+GO
+ALTER TABLE [dbo].[MP_RtiTaxMonthRecords] CHECK CONSTRAINT [FK_RtiTaxMonthRecord_MP]
+GO
