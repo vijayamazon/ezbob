d47e082e3f4c21a658ef4d94a11cf3f3b0fce61a
stas_dulman <stasd@ezbob.com>
2013-09-02 19:23:05 +0300

    Download script try 2

---
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/MP_RtiTaxMonthEntries.sql
@@ -0,0 +1,21 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MP_RtiTaxMonthEntries]') AND type in (N'U'))
+DROP TABLE [dbo].[MP_RtiTaxMonthEntries]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MP_RtiTaxMonthEntries](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RecordId] [int] NOT NULL,
+	[DateStart] [datetime] NOT NULL,
+	[DateEnd] [datetime] NOT NULL,
+	[AmountPaid] [decimal](18, 2) NOT NULL,
+	[AmountDue] [decimal](18, 2) NOT NULL,
+	[CurrencyCode] [nvarchar](3) NOT NULL,
+ CONSTRAINT [PK_RtiTaxMonthEntries] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
