0f03518da138617c631c58b84008e7678b89c592
stas_dulman <stasd@ezbob.com>
2013-09-04 09:02:01 +0300

    EZ-899 improved finished loans report - renamed to Repaid Loans, fixed number of loans, removed date filtering, added customers with 1/2 installments left, added last underwriter decision column, ordered by last finished loan and by status (finished/1 installment/2 installments)

--- DataBaseScripts/MsSql/Scripts/07_PROCEDURES/RptFinishedLoans.sql
+++ DataBaseScripts/MsSql/Scripts/07_PROCEDURES/RptFinishedLoans.sql
@@ -14,7 +14,7 @@ BEGIN
 SET @DateEnd = CONVERT(DATE, @DateEnd)
 SET @DateStart = CONVERT(DATE, @DateStart)
 
-IF datediff(day, @DateStart, @DateEnd) = 1 SET @DateStart = dateadd(week, -1,@DateEnd)
+--IF datediff(day, @DateStart, @DateEnd) = 1 SET @DateStart = dateadd(week, -1,@DateEnd)
 
 ---------------------------CRM Notes------------------------------ 
  IF OBJECT_ID('tempdb..#CRMNotes') IS NOT NULL DROP TABLE #CRMNotes
@@ -50,7 +50,7 @@ INTO
   
 --------------------------Finished Loans-----------------------------  
 IF OBJECT_ID('tempdb..#FinishedLoans') IS NOT NULL DROP TABLE #FinishedLoans
-SELECT c.Id cId, count(l.Id) NumberOfLoans, l.DateClosed DateClosed
+SELECT c.Id cId, count(l.Id) NumberOfLoans, max(l.DateClosed) DateClosed, 'Fully Repaid' AS LoanStatus
 INTO #FinishedLoans
 FROM Customer c
 INNER JOIN Loan l
@@ -63,7 +63,36 @@ AND c.Id NOT IN
   ON cc.Id = ll.CustomerId 
   WHERE ll.DateClosed IS NULL 
   GROUP BY cc.Id)
-GROUP BY c.Id, l.DateClosed
+GROUP BY c.Id
+
+----------------------Almost Finished Loans--------------------------
+IF OBJECT_ID('tempdb..#CountStillToPay') IS NOT NULL DROP TABLE #CountStillToPay
+SELECT LoanId, Count  
+INTO #CountStillToPay
+FROM (
+SELECT LoanId, count(Status) Count  FROM LoanSchedule
+WHERE Status = 'StillToPay'
+GROUP BY LoanId ) x
+WHERE x.Count <=2
+
+IF OBJECT_ID('tempdb..#AlmostFinishedLoans') IS NOT NULL DROP TABLE #AlmostFinishedLoans
+SELECT c.Id cId, count(l.Id) NumberOfLoans, NULL DateClosed, (convert(NVARCHAR,max(p.Count)) + ' Installments Left') AS LoanStatus
+INTO #AlmostFinishedLoans
+FROM Customer c
+INNER JOIN Loan l ON c.Id = l.CustomerId
+JOIN #CountStillToPay p ON p.LoanId = l.Id
+GROUP BY c.Id
+
+
+IF OBJECT_ID('tempdb..#Merged') IS NOT NULL DROP TABLE #Merged
+SELECT x.cId cId, x.NumberOfLoans NumberOfLoans, x.DateClosed DateClosed, x.LoanStatus LoanStatus
+INTO #Merged
+FROM
+(SELECT * FROM #FinishedLoans
+UNION 
+SELECT * FROM #AlmostFinishedLoans) x
+
+
 
 --------------------------MaxApprovedSum-----------------------------  
 IF OBJECT_ID('tempdb..#MaxApproved') IS NOT NULL DROP TABLE #MaxApproved
@@ -79,10 +108,11 @@ IF OBJECT_ID('tempdb..#MaxApproved') IS NOT NULL DROP TABLE #MaxApproved
   CR.CRMStatus CRMStatus,
   CR.CRMAction CRMAction,
   CR.CRMComment CRMComment,
-  max(L.DateClosed) DateClosed
+  L.DateClosed DateClosed,
+  L.LoanStatus LoanStatus
   INTO #MaxApproved
  FROM
-  #FinishedLoans L 
+  #Merged L 
   LEFT JOIN Customer C ON C.Id = L.cId
   INNER JOIN CashRequests O
    ON C.Id = O.IdCustomer
@@ -99,15 +129,35 @@ IF OBJECT_ID('tempdb..#MaxApproved') IS NOT NULL DROP TABLE #MaxApproved
     OR Name LIKE '%q@q%'
     OR Name LIKE '%1@1%'
     OR C.IsTest=1)
- AND C.CollectionStatus = 0 -- Not Enabled Good (Disabled/Legal/Fraud/FraudSuspect/Risky/Bad)
- GROUP BY C.Id, C.Name, C.FullName,L.NumberOfLoans,C.DaytimePhone, C.MobilePhone, CR.CRMStatus, CR.CRMAction, CR.CRMComment
+ AND C.CollectionStatus = 0 -- Is Enabled Good (Disabled/Legal/Fraud/FraudSuspect/Risky/Bad)
+ GROUP BY C.Id, C.Name, C.FullName,L.NumberOfLoans,C.DaytimePhone, C.MobilePhone, CR.CRMStatus, CR.CRMAction, CR.CRMComment,L.DateClosed, L.LoanStatus
+-------------------------Underwriter Descision-----------------
+IF OBJECT_ID('tempdb..#Decision') IS NOT NULL DROP TABLE #Decision;
+
+WITH e AS
+(
+     SELECT UnderwriterDecision, UnderwriterDecisionDate, IdCustomer cId ,
+         ROW_NUMBER() OVER
+         (
+             PARTITION BY IdCustomer
+             ORDER BY UnderwriterDecisionDate DESC
+         ) AS Recency
+     FROM CashRequests
+     WHERE UnderwriterDecisionDate IS NOT NULL
+)
+
+SELECT *
+INTO #Decision
+FROM e
+WHERE Recency = 1
 
 --------------------------Final Merge-------------------------- 
- SELECT Id, Email, Name, MAX(NumberOfLoans) NumberOfLoans, MaxApprovedSum, DayPhone, MobilePhone, CRMStatus, CRMAction, CRMComment
- FROM #MaxApproved
- WHERE DateClosed >= @DateStart AND DateClosed <= @DateEnd
- GROUP BY Id, Email, Name, NumberOfLoans, MaxApprovedSum, DayPhone, MobilePhone, CRMStatus, CRMAction, CRMComment, DateClosed 
- ORDER BY DateClosed DESC 
+ SELECT m.Id Id, m.Email Email, m.Name Name, m.NumberOfLoans NumberOfLoans,m.LoanStatus, m.MaxApprovedSum, m.DayPhone, m.MobilePhone, m.CRMStatus, m.CRMAction, m.CRMComment, d.UnderwriterDecision
+ FROM #MaxApproved m
+ LEFT JOIN #Decision d ON m.Id = d.cId
+ --WHERE DateClosed >= @DateStart AND DateClosed <= @DateEnd
+ --GROUP BY m.Id, m.Email, m.Name, m.NumberOfLoans, m.MaxApprovedSum, m.DayPhone, m.MobilePhone, m.CRMStatus, m.CRMAction, m.CRMComment, m.DateClosed, m.LoanStatus, d.UnderwriterDecision
+ ORDER BY m.DateClosed DESC, m.LoanStatus
  
  
  ----------------Drop Temp Tables------------------------------
@@ -115,5 +165,9 @@ IF OBJECT_ID('tempdb..#MaxApproved') IS NOT NULL DROP TABLE #MaxApproved
  DROP TABLE #CRMFinal
  DROP TABLE #FinishedLoans
  DROP TABLE #MaxApproved
+ DROP TABLE #AlmostFinishedLoans
+ DROP TABLE #CountStillToPay
+ DROP TABLE #Merged
+ 
 END
 GO
