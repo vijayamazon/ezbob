a0c6e01c17f7c38353721a0d65a11ce48fd04602
Igor Shubin <ishubin@scorto.com.ua>
2013-09-05 10:30:31 +0300

    run download script

--- DataBaseScripts/MsSql/Scripts/02_TABLES/MP_YodleeTransactionCategories.sql
+++ DataBaseScripts/MsSql/Scripts/02_TABLES/MP_YodleeTransactionCategories.sql
@@ -6,7 +6,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[MP_YodleeTransactionCategories](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[CategoryId] [nvarchar](300) NOT NULL,
 	[Name] [nvarchar](300) NOT NULL,
 	[Type] [nvarchar](300) NOT NULL,
@@ -15,9 +14,4 @@ CREATE TABLE [dbo].[MP_YodleeTransactionCategories](
 	[CategoryId] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-CREATE NONCLUSTERED INDEX [IX_MP_YodleeTransactionCategory] ON [dbo].[MP_YodleeTransactionCategories] 
-(
-	[CategoryId] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
