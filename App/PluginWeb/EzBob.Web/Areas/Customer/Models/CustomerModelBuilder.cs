f6ffa9b04f153c4ea248629a3e042dd566555b41
Yuly Saar <yulys@ezbob.com>
2013-09-18 00:50:38 +0200

    Fixed typo

--- App/PluginWeb/EzBob.Web/Areas/Customer/Models/CustomerModelBuilder.cs
+++ App/PluginWeb/EzBob.Web/Areas/Customer/Models/CustomerModelBuilder.cs
@@ -12,16 +12,16 @@ using PaymentServices.Calculators;
 
 namespace EzBob.Web.Areas.Customer.Models
 {
-    public class CustomerModelBuilder
+	public class CustomerModelBuilder
     {
         private readonly ISecurityQuestionRepository _questions;
         private readonly ICustomerRepository _customerRepository;
         private readonly IUsersRepository _users;
-        private readonly LoanPaymentFacade _facade;
-        private readonly PaymentRolloverRepository _paymentRolloverRepository;
+		private readonly LoanPaymentFacade _facade;
+		private readonly PaymentRolloverRepository _paymentRolloverRepository;
         private ChangeLoanDetailsModelBuilder _changeLoanDetailsModelBuilder;
 
-        public CustomerModelBuilder(ISecurityQuestionRepository questions, ICustomerRepository customerRepository, IUsersRepository users, PaymentRolloverRepository paymentRolloverRepository)
+		public CustomerModelBuilder(ISecurityQuestionRepository questions, ICustomerRepository customerRepository, IUsersRepository users, PaymentRolloverRepository paymentRolloverRepository)
         {
             _questions = questions;
             _customerRepository = customerRepository;
@@ -33,63 +33,63 @@ namespace EzBob.Web.Areas.Customer.Models
 
         public CustomerModel BuildWizardModel(EZBob.DatabaseLib.Model.Database.Customer cus)
         {
-            var customerdModel = new CustomerModel() {loggedIn = cus != null, bankAccountAdded = false};
+            var customerModel = new CustomerModel() {loggedIn = cus != null, bankAccountAdded = false};
 
-            if (!customerdModel.loggedIn) return customerdModel;
+            if (!customerModel.loggedIn) return customerModel;
 
             var customer = _customerRepository.GetAndInitialize(cus.Id);
             var user = _users.Get(cus.Id);
 
-            if (customer == null) return customerdModel;
+            if (customer == null) return customerModel;
 
-            customerdModel.Id = customer.Id;
-            customerdModel.userName = user.Name;
-            customerdModel.Email = customer.Name;
-            customerdModel.EmailState = customer.EmailState.ToString();
+            customerModel.Id = customer.Id;
+            customerModel.userName = user.Name;
+            customerModel.Email = customer.Name;
+            customerModel.EmailState = customer.EmailState.ToString();
 
-            customerdModel.CustomerPersonalInfo = customer.PersonalInfo;
-            customerdModel.BusinessTypeReduced = customerdModel.CustomerPersonalInfo == null ?
+            customerModel.CustomerPersonalInfo = customer.PersonalInfo;
+            customerModel.BusinessTypeReduced = customerModel.CustomerPersonalInfo == null ?
                 TypeOfBusinessReduced.Personal.ToString() : customer.PersonalInfo.TypeOfBusiness.Reduce().ToString();
 
-            customerdModel.bankAccountAdded = customer.HasBankAccount;
+            customerModel.bankAccountAdded = customer.HasBankAccount;
             if (customer.HasBankAccount)
             {
-                customerdModel.BankAccountNumber = customer.BankAccount.AccountNumber;
-                customerdModel.SortCode = customer.BankAccount.SortCode;
+                customerModel.BankAccountNumber = customer.BankAccount.AccountNumber;
+                customerModel.SortCode = customer.BankAccount.SortCode;
             }
 
-            customerdModel.ebayMarketPlaces = customer.GetEbayMarketPlaces();
-			customerdModel.amazonMarketPlaces = customer.GetAmazonMarketPlaces();
-			customerdModel.ekmShops = customer.GetEkmShops();
-			customerdModel.freeAgentAccounts = customer.GetFreeAgentAccounts();
-			customerdModel.sageAccounts = customer.GetSageAccounts();
-			customerdModel.payPointAccounts = customer.GetPayPointAccounts();
-			customerdModel.yodleeAccounts = customer.GetYodleeAccounts();
-			customerdModel.cgShops = customer.GetChannelGrabberShops();
-            customerdModel.paypalAccounts = customer.GetPayPalAccountsSimple();
+            customerModel.ebayMarketPlaces = customer.GetEbayMarketPlaces();
+			customerModel.amazonMarketPlaces = customer.GetAmazonMarketPlaces();
+			customerModel.ekmShops = customer.GetEkmShops();
+			customerModel.freeAgentAccounts = customer.GetFreeAgentAccounts();
+			customerModel.sageAccounts = customer.GetSageAccounts();
+			customerModel.payPointAccounts = customer.GetPayPointAccounts();
+			customerModel.yodleeAccounts = customer.GetYodleeAccounts();
+			customerModel.cgShops = customer.GetChannelGrabberShops();
+            customerModel.paypalAccounts = customer.GetPayPalAccountsSimple();
 
-			customerdModel.LastApprovedLoanTypeID = customer.LastCashRequest != null ? customer.LastCashRequest.LoanType.Id : 0;
-			customerdModel.LastApprovedRepaymentPeriod = customer.LastCashRequest != null ? customer.LastCashRequest.RepaymentPeriod : 0;
+			customerModel.LastApprovedLoanTypeID = customer.LastCashRequest != null ? customer.LastCashRequest.LoanType.Id : 0;
+			customerModel.LastApprovedRepaymentPeriod = customer.LastCashRequest != null ? customer.LastCashRequest.RepaymentPeriod : 0;
 
-            customerdModel.Medal = customer.Medal.HasValue ? customer.Medal.ToString() : "";
+            customerModel.Medal = customer.Medal.HasValue ? customer.Medal.ToString() : "";
 
-            customerdModel.CreditSum = customer.CreditSum;
-	        customerdModel.IsLoanTypeSelectionAllowed = customer.IsLoanTypeSelectionAllowed;
-            customerdModel.CreditResult = customer.CreditResult.ToString();
-            customerdModel.OfferStart = customer.OfferStart;
-            customerdModel.OfferValidUntil = customer.OfferValidUntil;
-            customerdModel.Status = customer.Status.ToString();
+            customerModel.CreditSum = customer.CreditSum;
+	        customerModel.IsLoanTypeSelectionAllowed = customer.IsLoanTypeSelectionAllowed;
+			customerModel.CreditResult = customer.CreditResult.ToString();
+            customerModel.OfferStart = customer.OfferStart;
+            customerModel.OfferValidUntil = customer.OfferValidUntil;
+            customerModel.Status = customer.Status.ToString();
 
-            customerdModel.Loans = customer.Loans
+            customerModel.Loans = customer.Loans
                                                 .OrderBy(l => l.Status)
                                                 .ThenByDescending(l => l.Date)
                                                 .Select(l => LoanModel.FromLoan(l, new LoanRepaymentScheduleCalculator(l, null), new LoanRepaymentScheduleCalculator(l, DateTime.UtcNow)))
                                                 .ToList();
 
-            customerdModel.TotalBalance = customerdModel.Loans.Sum(l => l.Balance);
-            customerdModel.PrincipalBalance = customer.ActiveLoans.Sum(l => l.LoanAmount);
-            customerdModel.TotalEarlyPayment = customerdModel.Loans.Sum(l => l.TotalEarlyPayment);
-            customerdModel.TotalLatePayment = customerdModel.Loans.Where(l => l.Status == LoanStatus.Late.ToString()).Sum(l => l.Late);
+            customerModel.TotalBalance = customerModel.Loans.Sum(l => l.Balance);
+            customerModel.PrincipalBalance = customer.ActiveLoans.Sum(l => l.LoanAmount);
+            customerModel.TotalEarlyPayment = customerModel.Loans.Sum(l => l.TotalEarlyPayment);
+            customerModel.TotalLatePayment = customerModel.Loans.Where(l => l.Status == LoanStatus.Late.ToString()).Sum(l => l.Late);
 
             var nextPayment = (
                                 from loan in customer.ActiveLoans
@@ -100,11 +100,11 @@ namespace EzBob.Web.Areas.Customer.Models
                                 select repayment).FirstOrDefault();
             if (nextPayment != null)
             {
-                customerdModel.NextPayment = nextPayment.AmountDue;
-                customerdModel.NextPaymentDate = nextPayment.Date;
+                customerModel.NextPayment = nextPayment.AmountDue;
+                customerModel.NextPaymentDate = nextPayment.Date;
             }
 
-            customerdModel.TotalPayEarlySavings = _facade.CalculateSavings(customer, DateTime.UtcNow);
+            customerModel.TotalPayEarlySavings = _facade.CalculateSavings(customer, DateTime.UtcNow);
 
             var account = new AccountSettingsModel();
 
@@ -116,7 +116,7 @@ namespace EzBob.Web.Areas.Customer.Models
                 Answer = user.SecurityAnswer
             };
 
-            customerdModel.AccountSettings = account;
+            customerModel.AccountSettings = account;
 
             var payments = from loan in customer.Loans
                            from tran in loan.Transactions
@@ -127,30 +127,30 @@ namespace EzBob.Web.Areas.Customer.Models
 
             if(lastPayment != null)
             {
-                customerdModel.LastPaymentTotal = lastPayment.Amount;
-                customerdModel.LastPaymentPrincipal = lastPayment.Principal;
-                customerdModel.LastPaymentInterest = lastPayment.Interest;
-                customerdModel.LastPaymentFees = lastPayment.Fees;
+                customerModel.LastPaymentTotal = lastPayment.Amount;
+                customerModel.LastPaymentPrincipal = lastPayment.Principal;
+                customerModel.LastPaymentInterest = lastPayment.Interest;
+                customerModel.LastPaymentFees = lastPayment.Fees;
             }
 
-            customerdModel.GreetingMailSentDate = customer.GreetingMailSentDate;
+            customerModel.GreetingMailSentDate = customer.GreetingMailSentDate;
 
-            customerdModel.LimitedInfo = LimitedInfoMap.FromLimitedInfo(customer.LimitedInfo);
+            customerModel.LimitedInfo = LimitedInfoMap.FromLimitedInfo(customer.LimitedInfo);
 
-            customerdModel.NonLimitedInfo = NonLimitedInfoMap.FromLimitedInfo(customer.NonLimitedInfo);
+            customerModel.NonLimitedInfo = NonLimitedInfoMap.FromLimitedInfo(customer.NonLimitedInfo);
 
             if (customer.AddressInfo != null)
             {
-                customerdModel.PersonalAddress = customer.AddressInfo.PersonalAddress.ToArray();
-                customerdModel.LimitedAddress = customer.AddressInfo.LimitedCompanyAddress.ToArray();
-                customerdModel.NonLimitedAddress = customer.AddressInfo.NonLimitedCompanyAddress.ToArray();
+                customerModel.PersonalAddress = customer.AddressInfo.PersonalAddress.ToArray();
+                customerModel.LimitedAddress = customer.AddressInfo.LimitedCompanyAddress.ToArray();
+                customerModel.NonLimitedAddress = customer.AddressInfo.NonLimitedCompanyAddress.ToArray();
             }               
 
-            customerdModel.ApplyCount = customer.ApplyCount;
-            customerdModel.CreditCardNo = customer.CreditCardNo;
-            customerdModel.PayPointCards = FillPayPointCards(customer);
+            customerModel.ApplyCount = customer.ApplyCount;
+            customerModel.CreditCardNo = customer.CreditCardNo;
+            customerModel.PayPointCards = FillPayPointCards(customer);
 
-            customerdModel.ActiveRollovers = _paymentRolloverRepository
+            customerModel.ActiveRollovers = _paymentRolloverRepository
                 .GetRolloversForCustomer(customer.Id)
                 .Where(x => x.Status == RolloverStatus.New)
                 .Select(x => new RolloverModel
@@ -171,17 +171,17 @@ namespace EzBob.Web.Areas.Customer.Models
                         });
 
 
-			customerdModel.IsDisabled = !customer.CollectionStatus.CurrentStatus.IsEnabled;
-			customerdModel.CustomerStatusName = customer.CollectionStatus.CurrentStatus.Name;
-            customerdModel.HasRollovers = customerdModel.ActiveRollovers.Any();
+			customerModel.IsDisabled = !customer.CollectionStatus.CurrentStatus.IsEnabled;
+			customerModel.CustomerStatusName = customer.CollectionStatus.CurrentStatus.Name;
+            customerModel.HasRollovers = customerModel.ActiveRollovers.Any();
 
             var cr = cus.LastCashRequest;
-            customerdModel.IsLoanDetailsFixed = !_changeLoanDetailsModelBuilder.IsAmountChangingAllowed(cr);
+            customerModel.IsLoanDetailsFixed = !_changeLoanDetailsModelBuilder.IsAmountChangingAllowed(cr);
 
-	        customerdModel.LoyaltyPoints = customer.LoyaltyPoints();
-	        customerdModel.IsOffline = customer.IsOffline;
+	        customerModel.LoyaltyPoints = customer.LoyaltyPoints();
+	        customerModel.IsOffline = customer.IsOffline;
 
-            return customerdModel;
+            return customerModel;
         }
 
         private decimal GetRolloverPayValue(Loan loan)
