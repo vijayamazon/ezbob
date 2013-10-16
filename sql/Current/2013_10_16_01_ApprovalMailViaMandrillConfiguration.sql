e476ee4ef87c63c382eb55d2c9e25df40d1e57d1
Alex Boursouk <alexbo@ezbob.com>
2013-10-16 16:41:49 +0300

    Incremental update.

--- sql/Current/2013_10_16_01_ApprovalMailViaMandrillConfiguration.sql
+++ sql/Current/2013_10_16_01_ApprovalMailViaMandrillConfiguration.sql
@@ -1,11 +1,11 @@
 DECLARE @TemplateId INT
+
 IF EXISTS (SELECT 1 FROM MandrillTemplate WHERE NAME = 'Mandrill - Approval (1st time)')
 BEGIN
 	SELECT @TemplateId = Id FROM MandrillTemplate WHERE NAME = 'Mandrill - Approval (1st time)'
 END
-ELSE
-BEGIN
-	INSERT INTO MandrillTemplate VALUES ('Mandrill - Approval (1st time)')
+ELSE BEGIN
+	INSERT INTO MandrillTemplate(Name) VALUES ('Mandrill - Approval (1st time)')
 	SELECT @TemplateId = Id FROM MandrillTemplate WHERE NAME = 'Mandrill - Approval (1st time)'
 END
 
@@ -13,18 +13,18 @@ UPDATE MailTemplateRelation SET MandrillTemplateId = @TemplateId WHERE InternalT
 GO
 
 DECLARE @TemplateId INT
+
 IF EXISTS (SELECT 1 FROM MandrillTemplate WHERE NAME = 'Mandrill - Approval (not 1st time)')
 BEGIN
 	SELECT @TemplateId = Id FROM MandrillTemplate WHERE NAME = 'Mandrill - Approval (not 1st time)'
 END
-ELSE
-BEGIN
-	INSERT INTO MandrillTemplate VALUES ('Mandrill - Approval (not 1st time)')
+ELSE BEGIN
+	INSERT INTO MandrillTemplate(Name) VALUES ('Mandrill - Approval (not 1st time)')
 	SELECT @TemplateId = Id FROM MandrillTemplate WHERE NAME = 'Mandrill - Approval (not 1st time)'
 END
 
 IF NOT EXISTS (SELECT 1 FROM MailTemplateRelation WHERE InternalTemplateName = 'Congratulations you are qualified - not first.docx')
 BEGIN
-	INSERT INTO MailTemplateRelation VALUES ('Congratulations you are qualified - not first.docx', @TemplateId)
+	INSERT INTO MailTemplateRelation(InternalTemplateName, MandrillTemplateId) VALUES ('Congratulations you are qualified - not first.docx', @TemplateId)
 END
 GO
