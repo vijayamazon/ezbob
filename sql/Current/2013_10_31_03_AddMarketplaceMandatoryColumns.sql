e26af8414bf45b10afe1ebb4265a7ddfb1679aec
Yuly Saar <yulys@ezbob.com>
2013-11-03 09:30:45 +0200

    Moved DB scripts to Nov 13 folder

---
+++ sql/Current/2013_10_31_03_AddMarketplaceMandatoryColumns.sql
@@ -0,0 +1,18 @@
+﻿IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE Name = N'MandatoryOnline' and Object_ID = Object_ID(N'MP_MarketplaceType'))
+BEGIN 
+	ALTER TABLE MP_MarketplaceType ADD MandatoryOnline BIT
+END 
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE Name = N'MandatoryOffline' and Object_ID = Object_ID(N'MP_MarketplaceType'))
+BEGIN 
+	ALTER TABLE MP_MarketplaceType ADD MandatoryOffline BIT
+END 
+GO
+
+IF EXISTS (SELECT 1 FROM MP_MarketplaceType WHERE MandatoryOnline IS NULL)
+BEGIN
+	UPDATE MP_MarketplaceType SET MandatoryOnline = 0, MandatoryOffline = 0
+	UPDATE MP_MarketplaceType SET MandatoryOffline = 1 WHERE Name = 'HMRC'
+END
+GO
