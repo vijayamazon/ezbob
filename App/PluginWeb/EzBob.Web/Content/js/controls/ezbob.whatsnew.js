7323cf3640af080c895d50a545b2d66214bc4d7a
stas_dulman <stasd@ezbob.com>
2013-11-19 16:16:11 +0200

    EZ-1249 whats new buttons z-index

--- App/PluginWeb/EzBob.Web/Content/js/controls/ezbob.whatsnew.js
+++ App/PluginWeb/EzBob.Web/Content/js/controls/ezbob.whatsnew.js
@@ -1,6 +1,7 @@
-﻿///<reference path="~/Content/js/lib/backbone.js" />
-///<reference path="~/Content/js/lib/underscore.js" />
-///<reference path="~/Content/js/App/ezbob.app.js" />
+﻿/// <reference path="~/Content/js/lib/backbone.js" />
+/// <reference path="~/Content/js/lib/underscore.js" />
+/// <reference path="~/Content/js/App/ezbob.app.js" />
+/// <reference path="~/Content/js/lib/alertify.js" />
 
 var EzBob = EzBob || {};
 
@@ -19,7 +20,6 @@ EzBob.WhatsNewView = Backbone.View.extend({
                 if (result.noWhatsNew || !result.success) {
                     that.clearAll();
                 } else {
-                    console.log(result);
                     that.whatsNewId = result.whatsNewId;
                     that.showWhatsNew(result.whatsNew);
                 }
@@ -32,8 +32,6 @@ EzBob.WhatsNewView = Backbone.View.extend({
         this.$el.prepend(whatsNewDiv);
         whatsNewDiv.alert();
         this.$el.notification();
-        var buttonsDiv = $('<div class="whats-new-buttons"><input type="button" value="Got it" class="btn blue" id="btnGotIt" /><input type="button" value="Show later" class="btn blue" id="btnShowLater" /></div>');
-        this.$el.append(buttonsDiv);
         this.$el.show();
     },
     render: function () {
