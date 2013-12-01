3a99fccf90b65c0b7fe8c70a7d154e531fc51247
Yuly Saar <yulys@ezbob.com>
2013-12-01 13:19:59 +0200

    First version for EzbobService

---
+++ Backend/EzbobService/EzbobService.svc.cs
@@ -0,0 +1,36 @@
+﻿namespace EzbobService
+{
+	using log4net;
+	using Strategies;
+
+	public class EzbobService : IEzbobService
+	{
+		private readonly Strategies strategies = new Strategies();
+		private static readonly ILog log = LogManager.GetLogger(typeof(EzbobService));
+
+		public void Greeting(string customerEmail, string confirmEmailAddress, int custumerId)
+		{
+			strategies.Greeting(customerEmail, confirmEmailAddress, custumerId);
+		}
+
+		/* Examples
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+		 */
+	}
+}
---
+++ Backend/EzbobService/IEzbobService.cs
@@ -0,0 +1,43 @@
+﻿namespace EzbobService
+{
+	using System.Runtime.Serialization;
+	using System.ServiceModel;
+	
+	[ServiceContract]
+	public interface IEzbobService
+	{
+		[OperationContract]
+		void Greeting(string customerEmail, string confirmEmailAddress, int custumerId);
+
+		/* Examples
+		[OperationContract]
+		string GetData(int value);
+
+		[OperationContract]
+		CompositeType GetDataUsingDataContract(CompositeType composite);
+		*/
+	}
+
+
+	/* Example of custom return type
+	[DataContract]
+	public class CompositeType
+	{
+		bool boolValue = true;
+		string stringValue = "Hello ";
+
+		[DataMember]
+		public bool BoolValue
+		{
+			get { return boolValue; }
+			set { boolValue = value; }
+		}
+
+		[DataMember]
+		public string StringValue
+		{
+			get { return stringValue; }
+			set { stringValue = value; }
+		}
+	}*/
+}
