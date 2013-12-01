8bdf3febe56473cf0b7e4a8cd0b40a916a08bd31
stas_dulman <stasd@ezbob.com>
2013-12-01 10:27:22 +0200

    small fixes, UW login gui

--- App/PluginWeb/EzBob.Web/Controllers/PostcodeController.cs
+++ App/PluginWeb/EzBob.Web/Controllers/PostcodeController.cs
@@ -1,26 +1,22 @@
 ﻿using System.Web.Mvc;
 using EzBob.Web.Code.PostCode;
 using EzBob.Web.Infrastructure;
-using NHibernate;
 using Scorto.Web;
-using log4net;
 
 namespace EzBob.Web.Controllers
 {
+	[Authorize]
     public class PostcodeController : Controller
     {
-        
         private readonly IEzbobWorkplaceContext _context;
 
-        private static readonly ILog _log = LogManager.GetLogger(typeof(PostcodeController));
         private readonly IPostCodeFacade _facade;
-        public PostcodeController( ISession session, IEzbobWorkplaceContext context, IPostCodeFacade facade)
+        public PostcodeController(IEzbobWorkplaceContext context, IPostCodeFacade facade)
         {
             _context = context;
             _facade = facade;
         }
 
-        [Authorize]
         [OutputCache(VaryByParam = "postCode", Duration = 3600 * 24 * 7)]
         [Transactional]
         public JsonNetResult GetAddressFromPostCode(string postCode)
@@ -28,7 +24,6 @@ namespace EzBob.Web.Controllers
             return this.JsonNet(_facade.GetAddressFromPostCode(_context.Customer, postCode));
         }
 
-        [Authorize]
         [OutputCache(VaryByParam = "id", Duration = 3600 * 24 * 7)]
         [Transactional]
         public JsonNetResult GetFullAddressFromPostCode(string id)
