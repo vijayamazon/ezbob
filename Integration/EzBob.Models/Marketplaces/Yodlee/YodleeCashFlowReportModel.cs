cd679fce8ab5594289bcd98102ccfc5e5a315026
stas_dulman <stasd@ezbob.com>
2013-12-11 09:17:47 +0200

    EZ-1311 yodlee to hmrc report fixes

--- Integration/EzBob.Models/Marketplaces/Yodlee/YodleeCashFlowReportModel.cs
+++ Integration/EzBob.Models/Marketplaces/Yodlee/YodleeCashFlowReportModel.cs
@@ -174,7 +174,7 @@
 
 			yodlee.BankStatementDataModel = new BankStatementDataModel();
 			DateTime from = new DateTime(yodlee.MinDateDict.First().Key / 100, yodlee.MinDateDict.First().Key % 100, yodlee.MinDateDict.Last().Value);
-			DateTime to = new DateTime(yodlee.MaxDateDict.First().Key / 100, yodlee.MaxDateDict.First().Key % 100, yodlee.MaxDateDict.Last().Value);
+			DateTime to = new DateTime(yodlee.MaxDateDict.Last().Key / 100, yodlee.MaxDateDict.Last().Key % 100, yodlee.MaxDateDict.Last().Value);
 
 			yodlee.BankStatementDataModel.Period = string.Format("{0} - {1}", from.ToString("MMM yy", CultureInfo.InvariantCulture), to.ToString("MMM yy", CultureInfo.InvariantCulture));
 			yodlee.BankStatementDataModel.Revenues = yodlee.YodleeCashFlowReportModelDict.ContainsKey("3Revenues Online") ? yodlee.YodleeCashFlowReportModelDict["3Revenues Online"][TotalColumn] : 0;
@@ -184,8 +184,8 @@
 			yodlee.BankStatementDataModel.Tax = yodlee.YodleeCashFlowReportModelDict.ContainsKey("7Salaries and Tax Taxes") ? yodlee.YodleeCashFlowReportModelDict["7Salaries and Tax Taxes"][TotalColumn] : 0;
 			yodlee.BankStatementDataModel.ActualLoansRepayment = yodlee.YodleeCashFlowReportModelDict.ContainsKey("7Loan Repayments ") ? yodlee.YodleeCashFlowReportModelDict["7Loan Repayments "][TotalColumn] : 0;
 			yodlee.BankStatementDataModel.TotalValueAdded = yodlee.BankStatementDataModel.Revenues - yodlee.BankStatementDataModel.Opex;
-			yodlee.BankStatementDataModel.PercentOfRevenues = yodlee.BankStatementDataModel.Revenues / yodlee.BankStatementDataModel.TotalValueAdded;
-			yodlee.BankStatementDataModel.Ebida = yodlee.BankStatementDataModel.Salaries + yodlee.BankStatementDataModel.Tax;
+			yodlee.BankStatementDataModel.PercentOfRevenues = yodlee.BankStatementDataModel.TotalValueAdded / yodlee.BankStatementDataModel.Revenues;
+			yodlee.BankStatementDataModel.Ebida = yodlee.BankStatementDataModel.TotalValueAdded - (yodlee.BankStatementDataModel.Salaries + yodlee.BankStatementDataModel.Tax);
 			yodlee.BankStatementDataModel.FreeCashFlow = yodlee.BankStatementDataModel.Ebida - yodlee.BankStatementDataModel.ActualLoansRepayment;
 
 			
