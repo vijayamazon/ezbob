1ec388d0a79c037ba9c5e4f277ce672c49f991df
Yuly Saar <yulys@ezbob.com>
2013-12-15 12:17:30 +0200

    EZ-1351 + EZ-1352

---
+++ Backend/Strategies/FirstOfMonthStatusNotifier.cs
@@ -0,0 +1,15 @@
+﻿namespace EzBob.Backend.Strategies
+{
+	using Models;
+	using log4net;
+
+	public class FirstOfMonthStatusNotifier
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(FirstOfMonthStatusNotifier));
+
+		public void Execute()
+		{
+			new FirstOfMonthStatusStrategyHelper().SendFirstOfMonthStatusMail();
+		}
+	}
+}
---
+++ Backend/Strategies/LateBy14Days.cs
@@ -0,0 +1,47 @@
+﻿namespace EzBob.Backend.Strategies
+{
+	using System.Collections.Generic;
+	using System.Data;
+	using System.Globalization;
+	using DbConnection;
+	using log4net;
+
+	public class LateBy14Days
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(LateBy14Days));
+		private readonly StrategiesMailer mailer = new StrategiesMailer();
+
+		public void Execute()
+		{
+			DataTable dt = DbConnection.ExecuteSpReader("GetLateBy14DaysAndUpdate");
+			foreach (DataRow row in dt.Rows)
+			{
+				int loanId = int.Parse(row["LoanId"].ToString());
+				bool is14DaysLate = bool.Parse(row["Is14DaysLate"].ToString());
+				string signDate = row["SignDate"].ToString();
+				decimal loanAmount = decimal.Parse(row["LoanAmount"].ToString());
+				decimal principal = decimal.Parse(row["Principal"].ToString());
+				decimal interest = decimal.Parse(row["Interest"].ToString());
+				decimal fees = decimal.Parse(row["Fees"].ToString());
+				decimal total = decimal.Parse(row["Total"].ToString());
+				string mail = row["Email"].ToString();
+
+				if (is14DaysLate)
+				{
+					var variables = new Dictionary<string, string>
+						{
+							{"SignDate", signDate},
+							{"LoanAmount", loanAmount.ToString(CultureInfo.InvariantCulture)},
+							{"Principal", principal.ToString(CultureInfo.InvariantCulture)},
+							{"Interest", interest.ToString(CultureInfo.InvariantCulture)},
+							{"Fees", fees.ToString(CultureInfo.InvariantCulture)},
+							{"Total", total.ToString(CultureInfo.InvariantCulture)}
+						};
+					mailer.SendToCustomerAndEzbob(variables, mail, "Mandrill - 14 days notification email", "Please contact us immediately in order to make full payment on all outstanding debts");
+
+					DbConnection.ExecuteSpNonQuery("SetLateBy14Days", DbConnection.CreateParam("LoanId", loanId));
+				}
+			}
+		}
+	}
+}
