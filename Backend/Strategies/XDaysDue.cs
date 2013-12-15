0f70d693ec08220c77d592375dc073bc23c70dca
Yuly Saar <yulys@ezbob.com>
2013-12-15 11:48:38 +0200

    EZ-1350: 2\5 days due notifications

---
+++ Backend/Strategies/XDaysDue.cs
@@ -0,0 +1,73 @@
+﻿namespace EzBob.Backend.Strategies
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Data;
+	using System.Globalization;
+	using PaymentServices.PacNet;
+	using Web.Code;
+	using log4net;
+	using DbConnection;
+
+	public class XDaysDue
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(XDaysDue));
+		private readonly StrategiesMailer mailer = new StrategiesMailer();
+
+		public void Execute()
+		{
+			DataTable dt = DbConnection.ExecuteSpReader("GetCustomersFiveDaysDue");
+			foreach (DataRow row in dt.Rows)
+			{
+				int loanScheduleId = int.Parse(row["id"].ToString());
+				decimal amountDue = decimal.Parse(row["AmountDue"].ToString());
+				string firstName = row["FirstName"].ToString();
+				string mail = row["Email"].ToString();
+				DateTime sceduledDate = DateTime.Parse(row["SceduledDate"].ToString());
+				string creditCard = row["CreditCardNo"].ToString();
+
+				string subject = string.Format("Dear {0}, your ezbob monthly automatic loan re-payment is due in 5 days", firstName);
+
+				var variables = new Dictionary<string, string>
+					{
+						{"FirstName", firstName},
+						{"AmountDueScalar", amountDue.ToString(CultureInfo.InvariantCulture)},
+						{"Date", FormattingUtils.FormatDateToString(sceduledDate)},
+						{"DebitCard", creditCard}
+					};
+				mailer.SendToCustomerAndEzbob(variables, mail, "Mandrill - 5 days notice", subject);
+
+				DbConnection.ExecuteSpNonQuery("UpdateFiveDaysDueMailSent",
+				                               DbConnection.CreateParam("Id", loanScheduleId),
+											   DbConnection.CreateParam("UpdateFiveDaysDueMailSent", true));
+
+			}
+
+			dt = DbConnection.ExecuteSpReader("GetCustomersTwoDaysDue");
+			foreach (DataRow row in dt.Rows)
+			{
+				int loanScheduleId = int.Parse(row["id"].ToString());
+				decimal amountDue = decimal.Parse(row["AmountDue"].ToString());
+				string firstName = row["FirstName"].ToString();
+				string mail = row["Email"].ToString();
+				DateTime sceduledDate = DateTime.Parse(row["SceduledDate"].ToString());
+				string creditCard = row["CreditCardNo"].ToString();
+
+				string subject = string.Format("Dear {0}, your ezbob monthly automatic loan re-payment is due in 48 hours", firstName);
+
+				var variables = new Dictionary<string, string>
+					{
+						{"FirstName", firstName},
+						{"AmountDueScalar", amountDue.ToString(CultureInfo.InvariantCulture)},
+						{"Date", FormattingUtils.FormatDateToString(sceduledDate)},
+						{"DebitCard", creditCard}
+					};
+				mailer.SendToCustomerAndEzbob(variables, mail, "Mandrill - 2 days notice", subject);
+
+				DbConnection.ExecuteSpNonQuery("UpdateTwoDaysDueMailSent",
+											   DbConnection.CreateParam("Id", loanScheduleId),
+											   DbConnection.CreateParam("UpdateTwoDaysDueMailSent", true));
+			}
+		}
+	}
+}
---
+++ sql/Current/2013_12_15_02_GetCustomersTwoDaysDue.sql
@@ -0,0 +1,42 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.GetCustomersTwoDaysDue') AND type in (N'P', N'PC'))
+DROP PROCEDURE dbo.GetCustomersTwoDaysDue
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE dbo.GetCustomersTwoDaysDue 
+AS
+BEGIN
+	SELECT 
+		ls.id, 
+		ls.AmountDue, 
+		c.FirstName, 
+		c.Name AS Email, 
+		convert(date, ls.Date) AS SceduledDate, 
+		c.CreditCardNo
+	FROM 
+		Customer c 
+		JOIN Loan l ON 
+			l.CustomerId = c.Id 
+		JOIN CustomerStatuses cs ON 
+			cs.Id = c.CollectionStatus AND 
+			cs.IsEnabled = 1 
+		JOIN LoanSchedule ls ON 
+			ls.LoanId = l.Id AND 
+			ls.Status = 'StillToPay' AND 
+			ls.AmountDue > 0 AND 
+			ls.Date  >= GETUTCDATE() AND 
+			convert(date, ls.Date)  <= DateAdd(dd,2 ,GETUTCDATE()) AND 
+			(
+				ls.TwoDaysDueMailSent IS NULL OR 
+				ls.TwoDaysDueMailSent = 0 OR 
+				ls.TwoDaysDueMailSent = 2
+			)
+		LEFT JOIN LoanOptions lo ON 
+			lo.LoanId = l.Id
+	WHERE 
+		lo.AutoPayment IS NULL OR 
+		lo.AutoPayment = 1
+END
+GO
