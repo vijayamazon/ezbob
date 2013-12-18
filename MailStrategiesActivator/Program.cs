c6269e64c4b28bfff1df95514f5068bb5cc5bbec
Yuly Saar <yulys@ezbob.com>
2013-12-18 12:55:45 +0200

    EZ-1373: mail strategy activator - not working

---
+++ MailStrategiesActivator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MailStrategiesActivator
+{
+	using EzBob.Backend.Strategies.MailStrategies;
+
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			string strategyName = "Greeting"; // extract from args
+
+			switch (strategyName)
+			{
+				case "Greeting":
+					int customerId = int.Parse("123"); // args1
+					string confirmEmailAddress = "args2";
+					var g = new Greeting(customerId, confirmEmailAddress);
+					g.Execute();
+					break;
+				default:
+					Console.WriteLine(string.Format("Unsupported strategy name:{0}", strategyName));
+					break;
+			}
+		}
+	}
+}
