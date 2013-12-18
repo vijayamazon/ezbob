80cb9cf63117f8567ef3317c7f571a26084cebd8
Yuly Saar <yulys@ezbob.com>
2013-12-18 12:12:44 +0200

    Fixed misplaced sql

--- sql/Current/2013_12_18_01_AddCapitalExpenditure.sql
+++ sql/Current/2013_12_18_01_AddCapitalExpenditure.sql
@@ -1,18 +1,5 @@
-IF EXISTS (SELECT * FROM sys.check_constraints WHERE name = 'CHK_WSS_StepName')
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE Name = N'CapitalExpenditure' and Object_ID = Object_ID(N'Customer'))    
 BEGIN
-	ALTER TABLE WizardStepSequence DROP CONSTRAINT CHK_WSS_StepName
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM WizardStepTypes WHERE WizardStepTypeName = 'companydetails')
-BEGIN
-	INSERT INTO WizardStepTypes (WizardStepTypeID, TheLastOne, WizardStepTypeName, WizardStepTypeDescription) VALUES
-		(6, 0, 'companydetails', 'Company details (type, name, address, etc)')
-
-	INSERT INTO WizardStepSequence (ID, StepName, OnlineProgressBarPct, OfflineProgressBarPct, WizardStepType) VALUES
-		(3, 'companydetails', 90, 50, 6)
-END
-GO
-
-UPDATE WizardStepTypes SET WizardStepTypeDescription = 'Personal details (name, address, phone number)' WHERE WizardStepTypeName = 'details'
+	ALTER TABLE Customer ADD CapitalExpenditure DECIMAL(18) DEFAULT(0)
+END 
 GO
