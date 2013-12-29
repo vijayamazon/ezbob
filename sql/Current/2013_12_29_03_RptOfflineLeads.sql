a45a10d3c99c5c6cefb2920ad3c32a44c4f8e138
Alex Boursouk <alexbo@ezbob.com>
2013-12-29 15:17:09 +0200

    EZ-1411 Sort 'Offline leads' report by wizard step

---
+++ sql/Current/2013_12_29_03_RptOfflineLeads.sql
@@ -0,0 +1,34 @@
+IF OBJECT_ID('RptOfflineLeads') IS NULL
+BEGIN
+	EXECUTE('CREATE PROCEDURE RptOfflineLeads AS SELECT 1')
+END
+GO
+
+ALTER PROCEDURE RptOfflineLeads
+@DateStart DATETIME,
+@DateEnd DATETIME
+AS
+SELECT
+	c.Id,
+	c.GreetingMailSentDate,
+	c.CreditResult,
+	c.Name,
+	c.Fullname,
+	w.WizardStepTypeDescription,
+	c.DaytimePhone,
+	c.MobilePhone,
+	c.OverallTurnOver
+FROM
+	Customer c
+	INNER JOIN WizardStepTypes w ON c.WizardStep = w.WizardStepTypeID
+WHERE
+	@DateStart <= c.GreetingMailSentDate AND c.GreetingMailSentDate < @DateEnd
+	AND
+	c.IsTest = 0
+	AND
+	c.IsOffline = 1
+ORDER BY
+	w.TheLastOne DESC,
+	c.GreetingMailSentDate,
+	c.Fullname
+GO
