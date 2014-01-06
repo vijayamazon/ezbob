bb6e47b024eafd3357d2bec93ce5a00b75a6649b
stas_dulman <stasd@ezbob.com>
2014-01-06 15:05:52 +0000

    UwGridRegistered fixed.

---
+++ sql/Current/2014_01_06_01_UwGridRegistered.sql
@@ -0,0 +1,43 @@
+IF OBJECT_ID('UwGridRegistered') IS NULL
+	EXECUTE('CREATE PROCEDURE UwGridRegistered AS SELECT 1')
+GO
+
+ALTER PROCEDURE UwGridRegistered
+@WithTest BIT,
+@WithAll BIT
+AS
+SELECT
+	c.Id AS CustomerID,
+	c.Name AS Email,
+	(CASE w.TheLastOne WHEN 1 THEN 'registered' ELSE 'credit calculation' END) AS UserStatus,
+	c.GreetingMailSentDate AS RegDate,
+	ISNULL(t.Name, '') AS MpTypeName,
+	(CASE
+		WHEN m.UpdatingStart IS NULL THEN 'Never started'
+		WHEN m.UpdateError IS NOT NULL AND LTRIM(RTRIM(m.UpdateError)) != '' THEN 'Error'
+		WHEN m.UpdatingStart IS NOT NULL AND m.UpdatingEnd IS NULL THEN 'In progress'
+		ELSE 'Done'
+	END) AS MpStatus,	
+	w.WizardStepTypeDescription AS WizardStep,
+	(CASE c.IsOffline WHEN 1 THEN 'Offline' WHEN 0 THEN 'Online' ELSE '' END) AS SegmentType,
+	(CASE WHEN c.CreditResult = 'Late' OR c.IsWasLate = 1 THEN 'iswaslate' ELSE '' END) AS IsWasLate
+FROM
+	Customer c
+	INNER JOIN WizardStepTypes w ON c.WizardStep = w.WizardStepTypeID
+	LEFT JOIN MP_CustomerMarketPlace m ON c.Id = m.CustomerId
+	LEFT JOIN MP_MarketplaceType t ON m.MarketPlaceId = t.Id
+WHERE
+	(
+		@WithTest = 1 OR (@WithTest != 1 AND c.IsTest = 0)
+	)
+	AND
+	c.CreditResult IS NULL
+	AND
+	(
+		@WithAll = 1 OR (@WithAll != 1 AND c.GreetingMailSentDate >= DATEADD(day, -7, GETDATE()))
+	)
+ORDER BY
+	c.Id DESC,
+	t.Id
+GO
+
