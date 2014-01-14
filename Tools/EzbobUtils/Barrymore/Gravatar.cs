7952049207a6e539557a80bbbeb1c6f6b139c990
Alex Boursouk <alexbo@ezbob.com>
2014-01-14 19:38:09 +0200

    UW: use gravatar icon for underwriter in top left corner

---
+++ Tools/EzbobUtils/Barrymore/Gravatar.cs
@@ -0,0 +1,35 @@
+﻿namespace Ezbob.Utils {
+	using System;
+	using System.Text;
+	using System.Web;
+
+	#region class Gravatar
+
+	public static class Gravatar {
+		#region method Url
+
+		public static string Url(string sEmail, string sDefaultImgUrl = null, int nImgSize = 0) {
+			var os = new StringBuilder();
+
+			os.AppendFormat("https://secure.gravatar.com/avatar/{0}", MiscUtils.MD5(sEmail.Trim().ToLower()).ToLower());
+
+			string sSeparator = "?";
+
+			if (!string.IsNullOrWhiteSpace(sDefaultImgUrl)) {
+				os.AppendFormat("{0}d={1}", sSeparator, HttpUtility.UrlEncode(sDefaultImgUrl));
+				sSeparator = "&";
+			} // if
+
+			if (nImgSize > 0) {
+				os.AppendFormat("{0}s={1}", sSeparator, nImgSize);
+				sSeparator = "&";
+			} // if
+
+			return os.ToString();
+		} // Url
+
+		#endregion method Url
+	} // class Gravatar
+
+	#endregion class Gravatar
+} // namespace Ezbob.Utils
---
+++ Tools/EzbobUtils/Barrymore/MiscUtils.cs
@@ -0,0 +1,29 @@
+﻿namespace Ezbob.Utils {
+	using System.Text;
+
+	#region class Utils
+
+	public static class MiscUtils {
+		#region method MD5
+
+		public static string MD5(string input) {
+			// step 1, calculate MD5 hash from input
+			var md5 = System.Security.Cryptography.MD5.Create();
+
+			byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+			byte[] hash = md5.ComputeHash(inputBytes);
+
+			// step 2, convert byte array to hex string
+			var sb = new StringBuilder();
+
+			for (int i = 0; i < hash.Length; i++)
+				sb.Append(hash[i].ToString("X2"));
+
+			return sb.ToString();
+		} // MD5
+
+		#endregion method MD5
+	} // class MiscUtils
+
+	#endregion class MiscUtils
+} // namespace Ezbob.Utils
