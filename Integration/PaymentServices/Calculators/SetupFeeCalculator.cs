881a3411adffafd4b1da5f76c3c191ed76ee72e3
stas_dulman <stasd@ezbob.com>
2014-02-02 10:36:50 +0200

    fixing default setup fee if manual fee is null or 0

--- Integration/PaymentServices/Calculators/SetupFeeCalculator.cs
+++ Integration/PaymentServices/Calculators/SetupFeeCalculator.cs
@@ -9,7 +9,7 @@
 	{
 		private readonly int _setupFeeFixed;
 		private readonly decimal _setupFeePercent;
-	    private readonly bool _useMax;
+		private readonly bool _useMax;
 
 		private readonly bool _setupFee;
 		private readonly bool _brokerFee;
@@ -30,28 +30,35 @@
 
 		public decimal Calculate(decimal amount)
 		{
+			//use manual fee
 			if (_setupFee || _brokerFee)
 			{
-				if (_manualAmount.HasValue || _manualPercent.HasValue)
+				if ((_manualAmount.HasValue && _manualAmount.Value > 0) || (_manualPercent.HasValue && _manualPercent.Value > 0))
 				{
-					return Math.Max(Math.Floor(amount*(_manualPercent.HasValue ? _manualPercent.Value : 0M)),
-					                _manualAmount.HasValue ? _manualAmount.Value : 0);
+					return Math.Max(Math.Floor(amount * (_manualPercent.HasValue ? _manualPercent.Value : 0M)),
+									_manualAmount.HasValue ? _manualAmount.Value : 0);
 				}
 			}
 
+			//use broker fee
 			if (_brokerFee)
 			{
 				return CalculateBroker(amount);
 			}
+
+
+			//use default fee
 			if (_setupFee)
 			{
 				if (_useMax)
 				{
-					return Math.Max(Math.Floor(amount*_setupFeePercent*0.01m), _setupFeeFixed);
+					return Math.Max(Math.Floor(amount * _setupFeePercent * 0.01m), _setupFeeFixed);
 				}
 
-				return Math.Min(Math.Floor(amount*_setupFeePercent*0.01m), _setupFeeFixed);
+				return Math.Min(Math.Floor(amount * _setupFeePercent * 0.01m), _setupFeeFixed);
 			}
+
+			//don't use fee
 			return 0M;
 		}
 
@@ -61,6 +68,6 @@
 			return brokerFeeRepository.GetFee((int)amount);
 		}
 
-		
+
 	}
 }
