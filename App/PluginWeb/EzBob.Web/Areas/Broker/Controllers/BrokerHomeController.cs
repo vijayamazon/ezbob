57beca5888c60ebc39d0dfa575458e5e8f7be37c
Yuly Saar <yulys@ezbob.com>
2014-02-17 11:06:46 +0200

    Restored commented code

--- App/PluginWeb/EzBob.Web/Areas/Broker/Controllers/BrokerHomeController.cs
+++ App/PluginWeb/EzBob.Web/Areas/Broker/Controllers/BrokerHomeController.cs
@@ -205,8 +205,7 @@
 
 			try
 			{
-				oRecords = null;
-				//oRecords = m_oAppCreator.BrokerLoadCustomerList(sContactEmail);
+				oRecords = m_oAppCreator.BrokerLoadCustomerList(sContactEmail);
 			}
 			catch (Exception e) {
 				oLog.Debug("Failed to load customers request for contact email {0}", sContactEmail);
