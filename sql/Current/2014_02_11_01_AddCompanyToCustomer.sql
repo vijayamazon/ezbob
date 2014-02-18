1b1cb6ac24a1e7e014144328d9e504cb20319355
Yuly Saar <yulys@ezbob.com>
2014-02-18 11:15:32 +0200

    will be done manually after the release

--- sql/Current/2014_02_11_01_AddCompanyToCustomer.sql
+++ sql/Current/2014_02_11_01_AddCompanyToCustomer.sql
@@ -23,8 +23,9 @@ GO
 	
 IF EXISTS (SELECT 1 FROM sys.columns WHERE Name = N'CustomerId' and Object_ID = Object_ID(N'Company'))
 BEGIN 
-	ALTER TABLE Company DROP FK_Company_Customer
-	ALTER TABLE Company DROP COLUMN CustomerId
+	-- Column should be dropped
+	--ALTER TABLE Company DROP FK_Company_Customer
+	--ALTER TABLE Company DROP COLUMN CustomerId
 END 
 GO
 
