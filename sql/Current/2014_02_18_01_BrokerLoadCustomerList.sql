a3460c26556f06dc5c3856296189217eea64a142
Alex Boursouk <alexbo@ezbob.com>
2014-02-18 13:34:37 +0200

    EZ-1239, EZ-1240 broker module

---
+++ sql/Current/2014_02_18_01_BrokerLoadCustomerList.sql
@@ -0,0 +1,35 @@
+IF OBJECT_ID('BrokerLoadCustomerList') IS NULL
+	EXECUTE('CREATE PROCEDURE BrokerLoadCustomerList AS SELECT 1')
+GO
+
+ALTER PROCEDURE BrokerLoadCustomerList
+@ContactEmail NVARCHAR(255)
+AS
+BEGIN
+	DECLARE @BrokerID INT
+
+	SELECT @BrokerID = BrokerID FROM Broker WHERE ContactEmail = @ContactEmail
+
+	SELECT
+		c.Id AS CustomerID,
+		c.FirstName AS FirstName,
+		c.Surname AS LastName,
+		c.Name AS Email,
+		w.WizardStepTypeDescription AS WizardStep,
+		ISNULL(c.CreditResult, 'In process') AS Status,
+		c.ApplyForLoan AS ApplyDate,
+		ISNULL(t.Name, '') AS MpTypeName,
+		ISNULL(l.LoanAmount, 0) AS LoanAmount,
+		l.Date AS LoanDate
+	FROM
+		Customer c
+		INNER JOIN WizardStepTypes w ON c.WizardStep = w.WizardStepTypeID
+		LEFT JOIN MP_CustomerMarketPlace m ON c.Id = m.CustomerId
+		LEFT JOIN MP_MarketplaceType t ON m.MarketPlaceId = t.Id
+		LEFT JOIN Loan l ON l.CustomerId = c.Id AND l.Position = 0
+	WHERE
+		c.BrokerID = @BrokerID
+	ORDER BY
+		c.Id
+END
+GO
