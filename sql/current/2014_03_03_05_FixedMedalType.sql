402b53b7267eb659ff7432a6bc13293b074624ec
Yuly Saar <yulys@ezbob.com>
2014-03-03 14:46:06 +0200

    EZ-1813: Medal appears properly in UW's medal calculator tab

---
+++ sql/current/2014_03_03_05_FixedMedalType.sql
@@ -0,0 +1,5 @@
+UPDATE CustomerScoringResult SET Medal = 'Silver' WHERE Medal = '6'
+UPDATE CustomerScoringResult SET Medal = 'Gold' WHERE Medal = '8'
+UPDATE CustomerScoringResult SET Medal = 'Platinum' WHERE Medal = '10'
+UPDATE CustomerScoringResult SET Medal = 'Diamond' WHERE Medal = '12'
+GO
