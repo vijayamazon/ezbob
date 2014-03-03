eb1860f0cb4f5163f217e12be16e7bd304cda09c
stas_dulman <stasd@ezbob.com>
2014-03-03 18:01:50 +0200

    fix OfferPercentRanges in medal calculation constants

--- Backend/Strategies/ScoreCalculation/Constants.cs
+++ Backend/Strategies/ScoreCalculation/Constants.cs
@@ -173,11 +173,11 @@ namespace EzBob.Backend.Strategies.ScoreCalculation
 		#region Offer Percent Range
 
 		public static readonly List<RangeOfferPercent> OfferPercentRanges = new List<RangeOfferPercent> {
-			new RangeOfferPercent {MinValue = null,MaxValue = 649, OfferPercent = 0.07M },
-			new RangeOfferPercent {MinValue = 650,MaxValue = 849, OfferPercent = 0.06M },
-			new RangeOfferPercent {MinValue = 850,MaxValue = 999, OfferPercent = 0.05M },
-			new RangeOfferPercent {MinValue = 1000,MaxValue = 1099, OfferPercent = 0.04M },
-			new RangeOfferPercent {MinValue = 1100,MaxValue = null, OfferPercent = 0.03M },
+			new RangeOfferPercent {MinValue = null,MaxValue = 649, OfferPercent = 0.06M },
+			new RangeOfferPercent {MinValue = 650,MaxValue = 849, OfferPercent = 0.05M },
+			new RangeOfferPercent {MinValue = 850,MaxValue = 999, OfferPercent = 0.04M },
+			new RangeOfferPercent {MinValue = 1000,MaxValue = 1099, OfferPercent = 0.03M },
+			new RangeOfferPercent {MinValue = 1100,MaxValue = null, OfferPercent = 0.02M },
 		};
 
 		#endregion Offer Percent Range
