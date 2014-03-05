1248d11db1d921069d6d7ab3667f05f1227c4c35
Yuly Saar <yulys@ezbob.com>
2014-03-05 15:02:23 +0200

    Removed old pay pal objects

---
+++ sql/Current/2014_03_05_02_RemoveUnusedPayPalObjects.sql
@@ -0,0 +1,39 @@
+IF OBJECT_ID (N'dbo.GetExpensesPayPalTransactionsByPayerAndRange') IS NOT NULL
+	DROP FUNCTION dbo.GetExpensesPayPalTransactionsByPayerAndRange
+GO
+IF OBJECT_ID (N'dbo.GetExpensesPayPalTransactionsByRange') IS NOT NULL
+	DROP FUNCTION dbo.GetExpensesPayPalTransactionsByRange
+GO
+IF OBJECT_ID (N'dbo.GetIncomePayPalTransactionsByPayer') IS NOT NULL
+	DROP FUNCTION dbo.GetIncomePayPalTransactionsByPayer
+GO
+IF OBJECT_ID (N'dbo.GetIncomePayPalTransactionsByPayerAndRange') IS NOT NULL
+	DROP FUNCTION dbo.GetIncomePayPalTransactionsByPayerAndRange
+GO
+IF OBJECT_ID (N'dbo.GetIncomePayPalTransactionsByRange') IS NOT NULL
+	DROP FUNCTION dbo.GetIncomePayPalTransactionsByRange
+GO
+IF OBJECT_ID (N'dbo.GetTotalExpensesPayPalTransactions') IS NOT NULL
+	DROP FUNCTION dbo.GetTotalExpensesPayPalTransactions
+GO
+IF OBJECT_ID (N'dbo.GetTotalIncomePayPalTransactions') IS NOT NULL
+	DROP FUNCTION dbo.GetTotalIncomePayPalTransactions
+GO
+IF OBJECT_ID (N'dbo.GetTotalTransactionsPayPalTransactions') IS NOT NULL
+	DROP FUNCTION dbo.GetTotalTransactionsPayPalTransactions
+GO
+IF OBJECT_ID (N'dbo.GetTransactionsCountPayPalTransactionsByRange') IS NOT NULL
+	DROP FUNCTION dbo.GetTransactionsCountPayPalTransactionsByRange
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MP_GetPayPalActivity]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[MP_GetPayPalActivity]
+GO
+IF OBJECT_ID (N'dbo.GetBiggestExpensesPayPalTransactions') IS NOT NULL
+	DROP FUNCTION dbo.GetBiggestExpensesPayPalTransactions
+GO
+IF OBJECT_ID (N'dbo.GetBiggestIncomePayPalTransactions') IS NOT NULL
+	DROP FUNCTION dbo.GetBiggestIncomePayPalTransactions
+GO
+IF OBJECT_ID (N'dbo.GetExpensesPayPalTransactionsByPayer') IS NOT NULL
+	DROP FUNCTION dbo.GetExpensesPayPalTransactionsByPayer
+GO
