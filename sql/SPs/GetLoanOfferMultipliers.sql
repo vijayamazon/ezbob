e07af13afb444cfef4b88b3d31f07e4bc4876083
Yuly Saar <yulys@ezbob.com>
2014-03-12 21:56:56 +0200

    EZ-1643: Start of loan offer multiplier

---
+++ sql/SPs/GetLoanOfferMultipliers.sql
@@ -0,0 +1,19 @@
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetLoanOfferMultipliers]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetLoanOfferMultipliers]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[GetLoanOfferMultipliers]
+AS
+BEGIN
+	SELECT 
+		Id,
+		StartScore,
+		EndScore,
+		Multiplier 
+	FROM 
+		LoanOfferMultiplier
+END
+GO
