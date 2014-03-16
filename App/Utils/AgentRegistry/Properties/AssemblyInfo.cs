a05b12e03fd5b10f06328bd6684929d0e2c5da91
stas_dulman <stasd@ezbob.com>
2014-03-16 11:22:15 +0200

    EZ-1368 removing redundant AgentRegistry and StrategySchedules projects from solution

--- App/Utils/AgentRegistry/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("AgentRegistry")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("b257a6e7-12b8-4e66-afe6-c9339f50c74c")]
--- Lib/Publisher/Plug-ins/StrategySchedules/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("StrategySchedules")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("e5a997e9-87cb-4755-9dfa-26eae1511dbe")]
