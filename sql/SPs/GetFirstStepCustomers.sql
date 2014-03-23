971c1c98235a320e01cb569342162914cc534bb1
Alex Boursouk <alexbo@ezbob.com>
2014-03-23 12:15:19 +0200

    EZ-1879 Broker emails

--- sql/SPs/GetFirstStepCustomers.sql
+++ sql/SPs/GetFirstStepCustomers.sql
@@ -1,118 +1,84 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFirstStepCustomers]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[GetFirstStepCustomers]
+IF OBJECT_ID('GetFirstStepCustomers') IS NULL
+	EXECUTE('CREATE PROCEDURE GetFirstStepCustomers AS SELECT 1')
 GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[GetFirstStepCustomers] 
-	(@DateStart    DATETIME,
-	@DateEnd      DATETIME)
+
+ALTER PROCEDURE GetFirstStepCustomers
+@DateStart DATETIME,
+@DateEnd DATETIME,
+@IncludeTest BIT = 0
 AS
 BEGIN
-	if OBJECT_ID('tempdb..#Shops') is not NULL
-BEGIN
-                DROP TABLE #Shops
-END
- 
-if OBJECT_ID('tempdb..#MaxOffer') is not NULL
-BEGIN
-                DROP TABLE #MaxOffer
-END
- 
-if OBJECT_ID('tempdb..#SumOfLoans') is not NULL
-BEGIN
-                DROP TABLE #SumOfLoans
-END
- 
-if OBJECT_ID('tempdb..#tmp1') is not NULL
-BEGIN
-                DROP TABLE #tmp1
-END
- 
-if OBJECT_ID('tempdb..#AnualTurnover') is not NULL
-BEGIN
-                DROP TABLE #AnualTurnover
-END
+	SET NOCOUNT ON;
+	
+	--------------------------------------------------------------------------
+	--
+	-- Drop temp tables (just in case).
+	--
+	--------------------------------------------------------------------------
+	
+	IF OBJECT_ID('tempdb..#Shops') IS NOT NULL
+		DROP TABLE #Shops
 
----- # OF SHOPS PER CUSTOMER ----
- 
-SELECT CustomerId,count(MarketPlaceId) AS Shops
-INTO #Shops
-FROM MP_CustomerMarketPlace
-GROUP BY CustomerId
- 
----- MAX OFFER THAT WAS OFFERED TO CUSTOMER ----
- 
-SELECT IdCustomer AS CustomerId,max(ManagerApprovedSum) MaxApproved
-INTO #MaxOffer
-FROM CashRequests
-GROUP BY IdCustomer
-               
----- SUM OF LOANS THAT CUST GOT -----
- 
-SELECT CustomerId,sum(LoanAmount) SumOfLoans
-INTO #SumOfLoans
-FROM Loan
-GROUP BY CustomerId
- 
------ CALC FOR ANUAL TURNOVER -----
-SELECT IdCustomer AS CustomerId,max(CreationDate)AS CreationDate
-INTO #tmp1
-FROM CashRequests
-GROUP BY IdCustomer
-ORDER BY 1
- 
-SELECT A.CustomerId,A.creationdate,R.MedalType,R.AnualTurnover,R.ExpirianRating
-INTO #AnualTurnover
-FROM #tmp1 A
-JOIN CashRequests R ON R.IdCustomer = A.CustomerId
-WHERE R.CreationDate = A.CreationDate
- 
---------------------------------------------------------------------------
+	--------------------------------------------------------------------------
+	--
+	-- # OF SHOPS PER CUSTOMER
+	--
+	--------------------------------------------------------------------------
 
-SELECT x.eMail
-FROM
-(
- 
-SELECT
-                   Customer.Id,
-                   Customer.Name AS eMail,
-                   Customer.GreetingMailSentDate AS DateRegister,
-                   Customer.FirstName,
-                   Customer.Surname SurName,
-                   Customer.DaytimePhone,
-                   Customer.MobilePhone,
-                   #Shops.Shops,
-                   #MaxOffer.MaxApproved,
-                   #SumOfLoans.SumOfLoans,
-                   T.AnualTurnover,
-                   T.ExpirianRating,
-                   T.MedalType,
-                   CASE
-                   WHEN T.MedalType='Silver' THEN ROUND(T.AnualTurnover*0.06,0)
-                   WHEN T.MedalType='Gold' THEN ROUND(T.AnualTurnover*0.08,0)
-                   WHEN T.MedalType='Platinum' THEN ROUND(T.AnualTurnover*0.10,0)
-                   WHEN T.MedalType='Diamond' THEN ROUND(T.AnualTurnover*0.12,0)
-                   END PhoneOffer
-FROM   Customer
- 
-LEFT JOIN #Shops ON #Shops.CustomerId = Customer.Id
-LEFT JOIN #MaxOffer ON #MaxOffer.CustomerId = Customer.Id
-LEFT JOIN #SumOfLoans ON #SumOfLoans.CustomerId = Customer.Id
-LEFT JOIN #AnualTurnover T ON T.CustomerId = Customer.Id
-WHERE
-                                Customer.IsTest = 0
-                                AND Name NOT like '%ezbob%'
-                                AND Name NOT LIKE '%liatvanir%'
-                                AND Customer.GreetingMailSentDate >= @DateStart AND Customer.GreetingMailSentDate < @DateEnd
-                               
- 
-GROUP BY Customer.Id,Customer.Name,Customer.FirstName,Customer.Surname,Customer.DaytimePhone,Customer.MobilePhone,
-                                Customer.GreetingMailSentDate,#Shops.Shops,#MaxOffer.MaxApproved,#SumOfLoans.SumOfLoans,T.ExpirianRating,
-                                T.AnualTurnover,T.MedalType
- ) x
- WHERE
- x.FirstName IS NULL AND x.SurName IS NULL AND x.Shops IS NULL
+	SELECT
+		CustomerId,
+		COUNT(MarketPlaceId) AS Shops
+	INTO
+		#Shops
+	FROM
+		MP_CustomerMarketPlace
+	GROUP BY
+		CustomerId
+
+	--------------------------------------------------------------------------
+	--
+	-- Main request
+	--
+	--------------------------------------------------------------------------
+
+	SELECT
+		Customer.Name AS eMail
+	FROM
+		Customer
+		LEFT JOIN #Shops ON #Shops.CustomerId = Customer.Id
+	WHERE
+		(
+			@IncludeTest = 1
+			OR (
+				Customer.IsTest = 0
+				AND
+				Customer.Name NOT like '%ezbob%'
+			)
+		)
+		AND
+		Customer.Name NOT LIKE '%liatvanir%'
+		AND
+		@DateStart <= Customer.GreetingMailSentDate AND Customer.GreetingMailSentDate < @DateEnd
+		AND
+		Customer.FirstName IS NULL
+		AND
+		Customer.SurName IS NULL
+		AND
+		#Shops.Shops IS NULL
+
+	--------------------------------------------------------------------------
+	--
+	-- Drop temp tables.
+	--
+	--------------------------------------------------------------------------
+
+	IF OBJECT_ID('tempdb..#Shops') IS NOT NULL
+		DROP TABLE #Shops
+
+	--------------------------------------------------------------------------
+	--
+	-- The End.
+	--
+	--------------------------------------------------------------------------
 END
 GO
