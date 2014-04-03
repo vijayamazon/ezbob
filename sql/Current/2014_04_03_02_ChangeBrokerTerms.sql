fdfbea676d4e9548998cdbdc4846cd3a5650f71a
Yuly Saar <yulys@ezbob.com>
2014-04-03 15:02:19 +0300

    Broker terms: Open links in new tabs + fixed weird characters

---
+++ sql/Current/2014_04_03_02_ChangeBrokerTerms.sql
@@ -0,0 +1,232 @@
+IF OBJECT_ID('TR_BrokerTerms') IS NOT NULL
+	DROP TRIGGER TR_BrokerTerms
+GO
+
+UPDATE BrokerTerms
+SET BrokerTerms = '<h2 align=center style="text-align:center;"><strong>Broker and
+Intermediary Terms and Conditions</strong></h2>
+
+<p>The following terms and conditions govern Your
+relationship with Orange Money Ltd. (trading as EZBob) and participation in Our
+broker and intermediary program ("Program"). Our privacy policy, found at: <a
+href="http://www.ezbob.com/privacy-and-cookies" target="_blank">http://www.ezbob.com/privacy-and-cookies</a>,
+is incorporated in these terms and conditions and made a part hereof. These
+terms and conditions are an important legal document and by accepting they will
+be legally binding upon You. Please read them carefully and print out a copy
+for Your records before You agree to them.</p>
+
+<p>References in these
+terms and conditions to &quot;We&quot;, &quot;Us&quot; and &quot;Our&quot;,
+means, Orange Money Ltd. References to &quot;You&quot; and &quot;Your&quot;
+means the person identified in the registration process <u>and</u> any person
+who accesses Your Account (defined below) whether as an individual or on behalf
+of such entity.</p>
+
+<p>Orange Money Ltd. is authorized
+and regulated by the Financial Control Authority. Our Interim Permissions Reference
+Number&nbsp;is 647816.</p>
+
+<p>By registering and participating in the
+Program You will be able to earn a commissions or fees by introducing Us to
+potential borrowers and/or assist Your clients in applying for loans using Our
+proprietary on-line lending platform.</p>
+
+<p><u>Registration.</u></p>
+
+<p>You warrant to Us that all information
+provided to Us in the course of Your registration is true and accurate in all
+respects. You will update Us if any of the information You provide to Us
+changes.</p>
+
+<p>On registering with Us, You must provide a
+username and email address and create a password which will allow You to access
+Your account (&quot;Account&quot;). Your username and password are personal to Your Account
+and are not transferable.Your username and password allow You to access Your
+Account and are the methods used by Us to identify You and so You must keep
+them secure at all times. You are responsible for all information and activity
+on Your Account by anyone using Your username and password. You must provide Us
+with immediate notice of any breach of security, loss, theft or unauthorised
+use of a username or password.</p>
+
+<p>If We suspect that the person logged into
+Your Account is not You or We suspect illegal or fraudulent activity or
+unauthorised use, We may cancel or suspend Your Account without advance notice.</p>
+
+<p>As part of the
+registration process You authorize Us to obtain and review Your credit file and
+other reports and statements from credit reference agencies (<i>e.g.</i>
+CallCredit, Experian, Equifax, and fraud prevention agencies. </p>
+
+<p>&nbsp;</p>
+
+<p style="margin-bottom:6.0pt;">You shall
+at all times be an independent contractor, and not a co-venturer, agent,
+employee, or representative of Us. It will be Your sole obligation to report as
+self-employment income all compensation received from Us, and You will hold the
+Company harmless to the extent of any obligations imposed on Us by law to pay
+any withholding taxes, social insurance payments, unemployment or disability
+insurance or similar items in connection with any payments made by Us to You
+hereunder.</p>
+
+<p style="margin-bottom:6.0pt;">Your
+access and use of Our website in connection with Your participation in the
+Program and otherwise is governed by the terms and conditions found at: <a
+href="http://www.ezbob.com/terms-and-conditions" target="_blank">http://www.ezbob.com/terms-and-conditions</a>.</p>
+
+<p><u>Loan Applications</u>.</p>
+
+<p>You warrant that You shall disclose in full
+to each Applicant the amount of any commission, fee or other payment You may be
+entitled to received from Us in connection with said Applicant''s application
+for and/or acceptance of a loan from Us.</p>
+
+<p>If You refer a potential borrower
+(&quot;Applicant&quot;) to Us or assist or make a loan application on behalf of an Applicant,
+You warrant that You are duly authorised to act on such Applicant. You
+expressly acknowledge that if the Applicant is an &quot;individual&quot;, as such person
+is defined in the Consumer Credit Act 1974 (including a natural person, a sole
+trader or a partnership of two or three persons, not all of whom are bodies
+corporate) You must have a valid credit brokerage or credit intermediary
+license (as applicable) under the Consumer Credit Act 1974, and upon Our
+request provide Us with the details of such license.</p>
+
+<p><strong>Without derogating from
+the generality of the foregoing, You warrant that if You assist or complete a loan
+application on behalf of an Applicant, You have authority from said Applicant
+to perform all the acts that You will be performing on such Applicant''s behalf,
+including without limitation:</strong></p>
+
+<p>(a) authority to provide all the details of
+the Applicant as required by the loan application process and to agree for such
+details to be used in accordance with these terms and conditions and Our
+privacy policy (found at <a href="http://www.ezbob.com/privacy-and-cookies" target="_blank">http://www.ezbob.com/privacy-and-cookies</a>);</p>
+
+<p>(b) authority to set up a loan account for
+the Applicant on Our website (<a href="http://www.ezbob.com/" target="_blank">www.ezbob.com</a>);and</p>
+
+<p>(c) authority to allow Us to obtain, access
+and review the Applicant''s credit file and other reports and statements from
+credit reference agencies (<i>e.g.</i> CallCredit, Experian, Equifax, and fraud
+prevention agencies);</p>
+
+<p>In making a loan application on behalf of an
+Applicant, You warrant that You are not aware of any circumstances that would
+mean that the borrower will be unable to repay the loan. You warrant and
+represent that You have disclosed to Us any circumstances that You are aware of
+which could or might result in a material adverse change in the Applicant''s
+financial condition, business or assets or ability to repay a loan to Us.</p>
+
+<p>Before You make a loan application on behalf
+of a Applicant, You must apply full customer due diligence measures to the Applicant
+in accordance with the Money Laundering Regulations 2007 and any other
+applicable laws, regulations, codes of practice and guidance relating to money
+laundering, including without limitation guidance of the Joint Money Laundering
+Steering Group. You must make available to Us, on request, copies of all data,
+documents and other information used for such verification and retain the
+customer due diligence records for a minimum period of 5 years from the date on
+which We rely on the records.</p>
+
+<p>You acknowledge and warrant that You shall
+advise each Applicant that We use Our own internal guidelines and policies when
+assessing loan applications and have complete discretion as to whether We
+offer the Applicant a loan and on what terms (if any).</p>
+
+<p><strong>Before You provide
+information about the borrower to Us, You must tell the borrower how their
+information will be used and provide them with a copy of the relevant section
+of Our privacy policy (</strong><a
+href="http://www.ezbob.com/privacy-and-cookies" target="_blank">http://www.ezbob.com/privacy-and-cookies</a>).</p>
+
+<p>You acknowledge and agree that You are not an
+agent of Orange Money Ltd. and will not hold yourself out to be Our agent. You
+acknowledge that You may not make any representations or give any warranty on
+behalf of Us nor have the authority to enter into any agreement on behalf of Us
+or otherwise bind Us.</p>
+
+<p>You warrant that, in respect of all of Your
+dealings with Us, You are operating and will operate in full compliance with the
+Consumer Credit Act 1974, and all related legislation and hold all required
+licenses and registrations applicable to Your activities hereunder</p>
+
+<p><strong>You agree to indemnify,
+and keep indemnified, Us and Our affiliates, officers, shareholders, directors
+and employees from and against all costs, expenses, damages, loss, liabilities,
+demands, claims, actions or proceedings which We may suffer or incur arising
+out of Your breach of the obligations out of the warranties and obligations
+contained in these terms and conditions.</strong></p>
+
+<p><u>Commission</u></p>
+
+<p>If an Applicant which You have first
+introduced to Us using Your Account is offered and accepts one or more loans
+from Us We will pay You commissions in the amounts and under the other terms as
+set forth in the commission scheme (&quot;Commission Scheme&quot;) which will be provided
+to You separately upon establishment of Your Account.</p>
+
+<p style="margin-bottom:6.0pt;">All
+payments to You hereunder (if any) will be deemed to be inclusive of VAT, if
+applicable. You will be responsible for the payments of all taxes and other levies
+of any kind which result from this Agreement. The Company shall deduct from any
+commission payable all amounts required by law.</p>
+
+<p style="margin-bottom:6.0pt;">The
+payment of the commission, if any, shall be the entire compensation to which You
+are entitled from Us, and is inclusive of all costs and expenses that You may
+incur in connection with Your participation in the Program.</p>
+
+<p><u>Termination</u></p>
+
+<p style="margin-bottom:6.0pt;">Either You
+or We may terminate Your participation in the Program at any time, with or
+without cause, upon written advice to that effect to the other. Commissions
+which you have earned prior to the date of termination will be paid in
+accordance with the Commission Scheme.</p>
+
+<p><u>Miscellaneous</u></p>
+
+<p>No failure or delay in exercising any right,
+power or remedy by Us shall constitute a waiver by Us of, or impair or preclude
+any further exercise of, that or any right, power or remedy arising under these
+terms and conditions or otherwise.</p>
+
+<p>These terms and conditions (including the
+privacy policy and website terms referenced herein) set out the entire
+agreement between You and Us with respect to Your participation in the Program
+and supersede any and all representations, communications and prior agreements
+(written or oral) made by You or Us.</p>
+
+<p>These terms and conditions are personal to You
+and You may not assign or transfer them to any other person without Our prior written
+permission.</p>
+
+<p>No term of these terms and conditions is
+enforceable pursuant to the Contracts (Rights of Third Parties) Act 1999 by any
+person who is not a party to it.</p>
+
+<p>We reserve the right to change these
+terms and conditions from time to time, and the amended terms will be posted on
+Our website. Any revised terms shall take effect as at the date when the
+change is made and posted.</p>
+
+<p>Should You have any questions about these
+terms and conditions, or wish to contact Us for any reason whatsoever, please
+contact Us via Your Account.</p>
+
+<p>These terms and conditions are governed by
+English law. In the event of any matter or dispute arising out of or in
+connection with these terms and conditions, You and We shall submit to the
+non-exclusive jurisdiction of the English courts.</p>
+
+<p>These terms and conditions were last updated
+on April 5, 2014.</p>'
+WHERE BrokerTermsID = 1
+GO
+
+CREATE TRIGGER TR_BrokerTerms ON BrokerTerms
+INSTEAD OF UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	RAISERROR('Updating table BrokerTerms is disabled.', 11, 1)
+END
+GO
