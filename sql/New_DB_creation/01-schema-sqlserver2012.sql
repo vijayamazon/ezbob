f29ed66e604a662ae1aabd2a233cea6cc7c236d2
Yuly Saar <yulys@ezbob.com>
2014-04-16 13:17:18 +0300

    Removed unused SP

--- sql/New_DB_creation/01-schema-sqlserver2012.sql
+++ sql/New_DB_creation/01-schema-sqlserver2012.sql
@@ -7628,31 +7628,6 @@ BEGIN
 END
 
 GO
-/****** Object:  StoredProcedure [dbo].[pPacnetBalance]    Script Date: 04-Nov-13 5:03:46 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[pPacnetBalance]
-AS
-BEGIN
-	DECLARE @reservedAmount decimal(18, 4);
-	DECLARE @pacNet decimal(18, 4);
-	DECLARE @lastUpdate datetime;
-	DECLARE @loans decimal(18,4);
-
-
-	select @reservedAmount = sum(c.CreditSum)  from Customer c where c.CreditSum > 0 and c.CreditResult = 'Approved' 
-	and c.Status = 'Approved' and c.ValidFor >= GETUTCDATE() and c.ApplyForLoan <= GETUTCDATE()
-
-	select top(1) @pacNet = pb.Amount, @lastUpdate = pb.Date from PacNetBalance pb order by pb.Date desc
-
-	select @loans = sum(l.LoanAmount - l.SetupFee) from Loan l where l.Date > @lastUpdate
-
-	select ISNULL(@pacNet, 0) as PacnetBalance, @reservedAmount as ReservedAmount, @lastUpdate as Date, @loans as Loans, (ISNULL(@pacNet, 0) - @loans) as Adjusted
-END
-
-GO
 /****** Object:  StoredProcedure [dbo].[ResetAllSignals]    Script Date: 04-Nov-13 5:03:46 PM ******/
 SET ANSI_NULLS ON
 GO
