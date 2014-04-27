48caca636aa4a1c5688d85f56e2e12fa220596de
stas_dulman <stasd@ezbob.com>
2014-04-27 13:28:36 +0300

    EZ-1688 traffic report sp fix

--- sql/SPs/RptTraficReport_Customers.sql
+++ sql/SPs/RptTraficReport_Customers.sql
@@ -1,19 +1,20 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RptTraficReport_Customers]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[RptTraficReport_Customers]
+IF OBJECT_ID('RptTraficReport_Customers') IS NULL
+	EXECUTE('CREATE PROCEDURE RptTraficReport_Customers AS SELECT 1')
 GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[RptTraficReport_Customers] 
-	(@DateStart DATE, @DateEnd DATE)
+ALTER PROCEDURE [dbo].[RptTraficReport_Customers] 
+ (@DateStart DATE, @DateEnd DATE)
 AS
 BEGIN
 
-SELECT count(c.Id) Customers, count(l.Id) NumOfLoans, sum(l.LoanAmount) LoanAmount, c.ReferenceSource, c.GoogleCookie 
+SELECT count(DISTINCT c.Id) Customers, count(l.Id) NumOfLoans, sum(l.LoanAmount) LoanAmount, c.ReferenceSource, c.GoogleCookie 
 FROM Customer c LEFT JOIN Loan l ON l.CustomerId=c.Id 
-WHERE c.GreetingMailSentDate>=@DateStart AND c.GreetingMailSentDate<@DateEnd 
+WHERE c.GreetingMailSentDate>=@DateStart AND c.GreetingMailSentDate<@DateEnd AND c.IsTest=0
 GROUP BY c.ReferenceSource, c.GoogleCookie
 
 END
+GO
