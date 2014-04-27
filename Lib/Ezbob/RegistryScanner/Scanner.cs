71cae14a8ec63505fdaa3fc0ef7d73bf48fe0f95
Yuly Saar <yulys@ezbob.com>
2014-04-27 14:36:53 +0300

    Removed dead code

--- Lib/Ezbob/RegistryScanner/Scanner.cs
+++ Lib/Ezbob/RegistryScanner/Scanner.cs
@@ -1,23 +1,10 @@
-﻿namespace Scorto.RegistryScanner {
-	public interface IRequiresConfigurationOnStartup {
-		void Configure();
-	} // interface
-} // namespace
-
-namespace Ezbob.RegistryScanner {
+﻿namespace Ezbob.RegistryScanner {
 	using System.Reflection;
-
-	using Scorto.RegistryScanner;
-
 	using StructureMap;
 	using StructureMap.Graph;
 
-	using log4net;
-
 	public class Scanner {
 		public static void Register() {
-			ILog oLog = LogManager.GetLogger(typeof (Scanner));
-
 			ObjectFactory.Initialize((IInitializationExpression oInitExp) => oInitExp.Scan((IAssemblyScanner oScanner) => { 
 				oScanner.AssembliesFromApplicationBaseDirectory((Assembly oAssembly) => {
 					if (oAssembly.FullName.EndsWith("Tests"))
@@ -33,21 +20,7 @@ namespace Ezbob.RegistryScanner {
 				oScanner.Exclude((System.Type oType) => oType.Name.StartsWith("Fake") || oType.Name.EndsWith("Fake"));
 
 				oScanner.LookForRegistries();
-
-				oScanner.AddAllTypesOf(typeof(IRequiresConfigurationOnStartup));
 			}));
-
-			oLog.Debug("Ezbob.RegistryScanner.Scanner.Register - configure list - begin");
-
-			foreach (IRequiresConfigurationOnStartup current in ObjectFactory.GetAllInstances<IRequiresConfigurationOnStartup>()) {
-				// This loop should never be entered but just in case...
-				// It should be debug, but Error was chosen in order to receive an email.
-				oLog.ErrorFormat("Ezbob.RegistryScanner.Scanner.Register - configure {0}.", current.GetType());
-
-				current.Configure();
-			} // for each
-
-			oLog.Debug("Ezbob.RegistryScanner.Scanner.Register - configure list - end");
 		} // Register
 
 		private static readonly string[] ms_aryTypes = new [] {
