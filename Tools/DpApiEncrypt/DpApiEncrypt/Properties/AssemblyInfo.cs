f3d7066186abf302348762465878792190635ef0
Yuly Saar <yulys@ezbob.com>
2014-04-27 09:16:43 +0300

    Removed dpapiencrypt

--- Tools/DpApiEncrypt/DpApiEncrypt/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("DpApiEncrypt")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("392af202-7c57-497a-a4b6-13650420fa1a")]
