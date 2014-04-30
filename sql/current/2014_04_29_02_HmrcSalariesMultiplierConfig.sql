9c17e6c251fb11015e919d092ca32d8a7d20efc8
Alex Boursouk <alexbo@ezbob.com>
2014-04-30 19:37:40 +0300

    Query type fixes.

--- sql/current/2014_04_29_02_HmrcSalariesMultiplierConfig.sql
+++ sql/current/2014_04_29_02_HmrcSalariesMultiplierConfig.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS (SELECT * FROM ConfigurationVariables WHERE Name='GoogleTagManagementProd')
+IF NOT EXISTS (SELECT * FROM ConfigurationVariables WHERE Name='HmrcSalariesMultiplier')
 BEGIN
-	INSERT INTO ConfigurationVariables (Name, Value, Description) VALUES (''HmrcSalariesMultiplier'', 0, 'Hmrc salaries multiplier for last 4 period column calculation')
+	INSERT INTO ConfigurationVariables (Name, Value, Description) VALUES ('HmrcSalariesMultiplier', 0, 'Hmrc salaries multiplier for last 4 period column calculation')
 END
