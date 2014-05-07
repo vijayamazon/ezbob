79e95df081cb783dea804b9827459a5de0d1453e
Yuly Saar <yulys@ezbob.com>
2014-05-07 12:33:02 +0300

    EZ-2102: New binding method

--- App/PluginWeb/EzBob.Web/Content/js/Underwriter/profile/Calculator/ezbob.underwriter.pricingModelCalculations.js
+++ App/PluginWeb/EzBob.Web/Content/js/Underwriter/profile/Calculator/ezbob.underwriter.pricingModelCalculations.js
@@ -5,29 +5,78 @@ EzBob.Underwriter.PricingModelCalculationsModel = Backbone.Model.extend({
     url: window.gRootPath + "Underwriter/PricingModelCalculations/Index/"
 });
 
-EzBob.Underwriter.PricingModelCalculationsView = Backbone.View.extend({
+EzBob.Underwriter.PricingModelCalculationsView = Backbone.Marionette.ItemView.extend({
+    template: "#pricing-model-calculation-template",
+    
     initialize: function () {
-        this.template = _.template($('#pricing-model-calculation-template').html());
+        this.modelBinder = new Backbone.ModelBinder();
         this.model.on('change reset fetch sync', this.render, this);
         this.inputsExpanded = false;
         this.outputsExpanded = false;
     },
+    
+    bindings: {
+        LoanAmount: {
+            selector: "#loanAmount",
+            converter: EzBob.BindingConverters.moneyFormat
+        },
+        InitiationFee: {
+            selector: "#initiationFee",
+            converter: EzBob.BindingConverters.moneyFormat
+        },
+        Cogs: {
+            selector: "#cogs",
+            converter: EzBob.BindingConverters.moneyFormat
+        },
+        OpexAndCapex: {
+            selector: "#opexAndCapex",
+            converter: EzBob.BindingConverters.moneyFormat
+        },
+        ProfitBeforeTax: {
+            selector: "#profitBeforeTax",
+            converter: EzBob.BindingConverters.moneyFormat
+        },
+
+        InterestRate: {
+            selector: "#interestRate",
+            converter: EzBob.BindingConverters.percentsFormat
+        },
+        DefaultRate: {
+            selector: "#defaultRate",
+            converter: EzBob.BindingConverters.percentsFormat
+        },
+        TenureAsPercentOfLoanTerm: {
+            selector: "#tenureAsPercentOfLoanTerm",
+            converter: EzBob.BindingConverters.percentsFormat
+        },
+        CollectionRate: {
+            selector: "#collectionRate",
+            converter: EzBob.BindingConverters.percentsFormat
+        },
+        DebtPercentOfCapital: {
+            selector: "#debtPercentOfCapital",
+            converter: EzBob.BindingConverters.percentsFormat
+        },
+        CostOfDebt: {
+            selector: "#costOfDebt",
+            converter: EzBob.BindingConverters.percentsFormat
+        },
+
+        LoanTerm: {
+            selector: "#loanTerm",
+            converter: EzBob.BindingConverters.autonumericFormat
+        },
+        InterestOnlyPeriod: {
+            selector: "#interestOnlyPeriod",
+            converter: EzBob.BindingConverters.autonumericFormat
+        },
+        TenureAsMonthsOfLoanTerm: {
+            selector: "#tenureAsMonthsOfLoanTerm",
+            converter: EzBob.BindingConverters.autonumericFormat
+        }
+    },
 
     events: {
-        'focusout #loanAmount': 'loanAmountChanged',
-        'focusout #loanTerm': 'loanTermChanged',
-        'focusout #interestRate': 'interestRateChanged',
-        'focusout #defaultRate': 'defaultRateChanged',
-        'focusout #tenureAsPercentOfLoanTerm': 'tenureAsPercentOfLoanTermChanged',
-        'focusout #collectionRate': 'collectionRateChanged',
-        'focusout #debtPercentOfCapital': 'debtPercentOfCapitalChanged',
-        'focusout #costOfDebt': 'costOfDebtChanged',
-        'focusout #initiationFee': 'initiationFeeChanged',
-        'focusout #cogs': 'cogsChanged',
-        'focusout #opexAndCapex': 'opexAndCapexChanged',
-        'focusout #profitBeforeTax': 'profitBeforeTaxChanged',
-        'focusout #interestOnlyPeriod': 'interestOnlyPeriodChanged',
-        'focusout #tenureAsMonthsOfLoanTerm': 'tenureAsMonthsOfLoanTermChanged',
         'click #pricingModelResetButton': 'resetClicked',
         'click #pricingModelCalculateButton': 'calculateClicked',
         'click #expandCollapseInputsButton': 'expandCollapseInputsClicked',
@@ -63,8 +112,7 @@ EzBob.Underwriter.PricingModelCalculationsView = Backbone.View.extend({
     },
 
     resetClicked: function () {
-        this.model.fetch();
-        this.render();
+        //this.model.fetch(); // TODO: make it work
     },
 
     calculateClicked: function () {
@@ -86,89 +134,17 @@ EzBob.Underwriter.PricingModelCalculationsView = Backbone.View.extend({
             that.model.set('TotalCost', res.TotalCost);
             that.model.set('ProfitBeforeTaxOutput', res.ProfitBeforeTaxOutput);
             that.model.set('Balance', res.Balance);
-            that.render();
             UnBlockUi();
         });
     },
 
-    interestOnlyPeriodChanged: function () {
-        this.model.set('InterestOnlyPeriod', this.$el.find('#interestOnlyPeriod').autoNumericGet());
+    serializeData: function() {
+        var data = this.model.toJSON();
+        return { model: data };
     },
 
-    tenureAsMonthsOfLoanTermChanged: function () {
-        this.model.set('TenureAsMonthsOfLoanTerm', this.$el.find('#tenureAsMonthsOfLoanTerm').autoNumericGet());
-    },
-
-    costOfDebtChanged: function () {
-        this.model.set('CostOfDebt', this.$el.find('#costOfDebt').autoNumericGet());
-    },
-
-    initiationFeeChanged: function () {
-        this.model.set('InitiationFee', this.$el.find('#initiationFee').autoNumericGet());
-    },
-
-    cogsChanged: function () {
-        this.model.set('Cogs', this.$el.find('#cogs').autoNumericGet());
-    },
-
-    opexAndCapexChanged: function () {
-        this.model.set('OpexAndCapex', this.$el.find('#opexAndCapex').autoNumericGet());
-    },
-
-    profitBeforeTaxChanged: function () {
-        this.model.set('ProfitBeforeTax', this.$el.find('#profitBeforeTax').autoNumericGet());
-    },
-
-    defaultRateChanged: function () {
-        this.model.set('DefaultRate', this.$el.find('#defaultRate').autoNumericGet());
-    },
-
-    tenureAsPercentOfLoanTermChanged: function () {
-        this.model.set('TenureAsPercentOfLoanTerm', this.$el.find('#tenureAsPercentOfLoanTerm').autoNumericGet());
-    },
-
-    collectionRateChanged: function () {
-        // TODO: figure percents issue
-        var value = this.$el.find('#collectionRate').val();
-        this.model.set('CollectionRate', this.$el.find('#collectionRate').autoNumericGet());
-        this.$el.find('#collectionRate').val(value);
-    },
-
-    debtPercentOfCapitalChanged: function () {
-        this.model.set('DebtPercentOfCapital', this.$el.find('#debtPercentOfCapital').autoNumericGet());
-    },
-
-    loanAmountChanged: function () {
-        this.model.set('LoanAmount', this.$el.find('#loanAmount').autoNumericGet());
-    },
-    
-    loanTermChanged: function () {
-        this.model.set('LoanTerm', this.$el.find('#loanTerm').autoNumericGet());
-    },
-    
-    interestRateChanged: function () {
-        this.model.set('InterestRate', this.$el.find('#interestRate').autoNumericGet());
-    },
-
-    render: function () {
-        this.$el.html(this.template({ model: this.model.toJSON() }));
-        
-        this.$el.find('#interestRate').percentFormat();
-        this.$el.find('#defaultRate').percentFormat();
-        this.$el.find('#tenureAsPercentOfLoanTerm').percentFormat();
-        this.$el.find('#collectionRate').percentFormat();
-        this.$el.find('#debtPercentOfCapital').percentFormat();
-        this.$el.find('#costOfDebt').percentFormat();
-
-        this.$el.find('#loanAmount').moneyFormat();
-        this.$el.find('#initiationFee').moneyFormat();
-        this.$el.find('#cogs').moneyFormat();
-        this.$el.find('#opexAndCapex').moneyFormat();
-        this.$el.find('#profitBeforeTax').moneyFormat();
-
-        this.$el.find('#loanTerm').numericOnly(2);
-        this.$el.find('#interestOnlyPeriod').numericOnly(2);
-        this.$el.find('#tenureAsMonthsOfLoanTerm').numericOnly(2);
+    onRender: function () {
+        this.modelBinder.bind(this.model, this.el, this.bindings);
         return this;
     }
 });
