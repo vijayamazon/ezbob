c78eec5276932122bae1a7db6b73e47c10bd8a5c
stas_dulman <stasd@ezbob.com>
2014-05-11 12:45:19 +0300

    EZ-1940 uw bugs update fix

--- App/PluginWeb/EzBob.Web/Areas/Underwriter/Controllers/BugsController.cs
+++ App/PluginWeb/EzBob.Web/Areas/Underwriter/Controllers/BugsController.cs
@@ -1,161 +1,98 @@
-﻿using System;
-using System.Web.Mvc;
-using ApplicationMng.Repository;
-using EZBob.DatabaseLib.Model.Database;
-using EZBob.DatabaseLib.Model.Database.Repository;
-using EzBob.Web.Infrastructure;
-using System.Linq;
-
-namespace EzBob.Web.Areas.Underwriter.Controllers
+﻿namespace EzBob.Web.Areas.Underwriter.Controllers
 {
+	using System;
+	using System.Web.Mvc;
+	using EZBob.DatabaseLib.Model.Database;
+	using EZBob.DatabaseLib.Model.Database.Repository;
+	using Infrastructure;
+	using System.Linq;
 	using EZBob.DatabaseLib.Model.Database.UserManagement;
 	using Infrastructure.Attributes;
+	using Models;
 
 	public class BugsController : Controller
-    {
-        private readonly IBugRepository _bugs;
-        private readonly IEzbobWorkplaceContext _context;
-        private readonly ICustomerRepository _customers;
-        private readonly IUsersRepository _users;
+	{
+		private readonly IBugRepository _bugs;
+		private readonly IEzbobWorkplaceContext _context;
+		private readonly ICustomerRepository _customers;
+		private readonly IUsersRepository _users;
 
-        public BugsController(IBugRepository bugs, IEzbobWorkplaceContext context, ICustomerRepository customers, IUsersRepository users)
-        {
-            _bugs = bugs;
-            _context = context;
-            _customers = customers;
-            _users = users;
-        }
+		public BugsController(IBugRepository bugs,
+			IEzbobWorkplaceContext context,
+			ICustomerRepository customers, 
+			IUsersRepository users)
+		{
+			_bugs = bugs;
+			_context = context;
+			_customers = customers;
+			_users = users;
+		}
 
-        [Ajax]
-        [HttpPost]
-        [Transactional]
-        public JsonResult CreateBug(BugModel bug)
-        {
-            var b = bug.FromModel(_customers, _users);
-            b.UnderwriterOpened = _context.User;
-            _bugs.Save(b);
-            return Json(new {});
-        }
-        
-        [Ajax]
-        [Transactional]
-        public JsonResult UpdateBug(BugModel bug)
-        {
-            var b = bug.FromModel(_customers, _users);
-            b.UnderwriterOpened = _context.User;
-            _bugs.Update(b);
-            return Json(new { }, JsonRequestBehavior.AllowGet);
-        }
+		[Ajax]
+		[HttpPost]
+		[Transactional]
+		public JsonResult CreateBug(BugModel bug)
+		{
+			var b = bug.FromModel(_customers, _users);
+			b.UnderwriterOpened = _context.User;
+			_bugs.Save(b);
+			return Json(new { });
+		}
 
-        [Ajax]
-        [HttpGet]
-        [Transactional]
-        public JsonResult TryGet(int customerid, string bugtype, int? mp, int? director)
-        {
-            var bug = _bugs.Search(customerid, bugtype, mp, director);
-            return Json(BugModel.ToModel(bug), JsonRequestBehavior.AllowGet);
-        }
+		[Ajax]
+		[Transactional]
+		public JsonResult UpdateBug(BugModel bugModel)
+		{
+			var bug = _bugs.Get(bugModel.Id);
 
-        [Ajax]
-        [HttpPost]
-        [Transactional]
-        public JsonResult Close(BugModel bugm)
-        {
-            var bug = _bugs.Get(bugm.Id);
-            bug.State = BugState.Closed;
-            bug.DateClosed = DateTime.UtcNow;
-            bug.UnderwriterClosed = _context.User;
-            bug.TextClosed = bugm.TextClosed;
-            return Json(BugModel.ToModel(bug));
-        }
+			bug.UnderwriterOpened = _context.User;
+			bug.TextOpened = bugModel.TextOpened;
+			_bugs.Update(bug);
+			return Json(new { }, JsonRequestBehavior.AllowGet);
+		}
 
-        [Ajax]
-        [HttpPost]
-        [Transactional]
-        public JsonResult Reopen(BugModel bugModel)
-        {
-            var bug = _bugs.Get(bugModel.Id);
-            bug.State = BugState.Reopened;
-            bug.DateOpened = DateTime.UtcNow;
-            bug.UnderwriterOpened = _context.User;
-            return Json(BugModel.ToModel(bug));
-        }
+		[Ajax]
+		[HttpGet]
+		[Transactional]
+		public JsonResult TryGet(int customerid, string bugtype, int? mp, int? director)
+		{
+			var bug = _bugs.Search(customerid, bugtype, mp, director);
+			return Json(BugModel.ToModel(bug), JsonRequestBehavior.AllowGet);
+		}
 
-        [Ajax]
-        [HttpGet]
-        public JsonResult GetAllForCustomer(int customerId)
-        {
-            var bugs = _bugs.GetAll()
-                .Where(x => x.Customer.Id == customerId)
-                .Select(x=> BugModel.ToModel(x));
-            return Json(bugs, JsonRequestBehavior.AllowGet);
-        }
-    }
+		[Ajax]
+		[HttpPost]
+		[Transactional]
+		public JsonResult Close(BugModel bugm)
+		{
+			var bug = _bugs.Get(bugm.Id);
+			bug.State = BugState.Closed;
+			bug.DateClosed = DateTime.UtcNow;
+			bug.UnderwriterClosed = _context.User;
+			bug.TextClosed = bugm.TextClosed;
+			return Json(BugModel.ToModel(bug));
+		}
 
-    public class BugModel
-    {
-        public int Id { get; set; }
-        public int? CustomerId { get; set; }
-        public string Type { get; set; }
-        public int? MarketPlaceId { get; set; }
-        public int? DirectorId { get; set; }
-        public DateTime DateOpened { get; set; }
-        public DateTime? DateClosed { get; set; }
-        public string TextOpened { get; set; }
-        public string TextClosed { get; set; }
-        public string UnderwriterOpenedName { get; set; }
-        public string UnderwriterClosedName { get; set; }
-        public int UnderwriterOpenedId { get; set; }
-        public int? UnderwriterClosedId { get; set; }
-        public string State { get; set; }
+		[Ajax]
+		[HttpPost]
+		[Transactional]
+		public JsonResult Reopen(BugModel bugModel)
+		{
+			var bug = _bugs.Get(bugModel.Id);
+			bug.State = BugState.Reopened;
+			bug.DateOpened = DateTime.UtcNow;
+			bug.UnderwriterOpened = _context.User;
+			return Json(BugModel.ToModel(bug));
+		}
 
-        public Bug FromModel(ICustomerRepository customers, IUsersRepository users)
-        {
-            return new Bug()
-                       {
-                           Id = Id,
-                           Customer = CustomerId == null ? null : customers.Load(CustomerId.Value),
-                           DateOpened = DateOpened,
-                           DateClosed = DateClosed,
-                           MarketPlaceId = MarketPlaceId,
-                           CreditBureauDirectorId = DirectorId,
-                           TextOpened = TextOpened,
-                           TextClosed = TextClosed,
-                           Type = Type,
-                           UnderwriterClosed = UnderwriterClosedId == null ? null : users.Load(UnderwriterClosedId.Value),
-                           UnderwriterOpened = users.Load(UnderwriterOpenedId)
-                       };
-        }
-
-        public static BugModel ToModel(Bug bug)
-        {
-            if (bug == null) return null;
-            var bugModel = new BugModel()
-                               {
-                                   Id = bug.Id, 
-                                   CustomerId = bug.Customer.Id, 
-                                   DateOpened = bug.DateOpened, 
-                                   DateClosed = bug.DateClosed, 
-                                   MarketPlaceId = bug.MarketPlaceId, 
-                                   TextOpened = bug.TextOpened, 
-                                   TextClosed = bug.TextClosed, 
-                                   Type = bug.Type,
-                                   State = bug.State.ToString(),
-                                   DirectorId = bug.CreditBureauDirectorId
-                               };
-
-            if (bug.UnderwriterOpened != null)
-            {
-                bugModel.UnderwriterOpenedId = bug.UnderwriterOpened.Id;
-                bugModel.UnderwriterOpenedName = bug.UnderwriterOpened.FullName;
-            }
-            if (bug.UnderwriterClosed != null)
-            {
-                bugModel.UnderwriterClosedId = bug.UnderwriterClosed.Id;
-                bugModel.UnderwriterClosedName = bug.UnderwriterClosed.FullName;
-            }
-
-            return bugModel;
-        }
-    }
+		[Ajax]
+		[HttpGet]
+		public JsonResult GetAllForCustomer(int customerId)
+		{
+			var bugs = _bugs.GetAll()
+				.Where(x => x.Customer.Id == customerId)
+				.Select(x => BugModel.ToModel(x));
+			return Json(bugs, JsonRequestBehavior.AllowGet);
+		}
+	}
 }
---
+++ App/PluginWeb/EzBob.Web/Areas/Underwriter/Models/BugModel.cs
@@ -0,0 +1,74 @@
+﻿namespace EzBob.Web.Areas.Underwriter.Models
+{
+	using EZBob.DatabaseLib.Model.Database;
+	using EZBob.DatabaseLib.Model.Database.Repository;
+	using EZBob.DatabaseLib.Model.Database.UserManagement;
+	using System;
+
+	public class BugModel
+	{
+		public int Id { get; set; }
+		public int? CustomerId { get; set; }
+		public string Type { get; set; }
+		public int? MarketPlaceId { get; set; }
+		public int? DirectorId { get; set; }
+		public DateTime DateOpened { get; set; }
+		public DateTime? DateClosed { get; set; }
+		public string TextOpened { get; set; }
+		public string TextClosed { get; set; }
+		public string UnderwriterOpenedName { get; set; }
+		public string UnderwriterClosedName { get; set; }
+		public int UnderwriterOpenedId { get; set; }
+		public int? UnderwriterClosedId { get; set; }
+		public string State { get; set; }
+
+		public Bug FromModel(ICustomerRepository customers, IUsersRepository users)
+		{
+			return new Bug
+			{
+				Id = Id,
+				Customer = CustomerId == null ? null : customers.Load(CustomerId.Value),
+				DateOpened = DateOpened,
+				DateClosed = DateClosed,
+				MarketPlaceId = MarketPlaceId,
+				CreditBureauDirectorId = DirectorId,
+				TextOpened = TextOpened,
+				TextClosed = TextClosed,
+				Type = Type,
+				UnderwriterClosed = UnderwriterClosedId == null ? null : users.Load(UnderwriterClosedId.Value),
+				UnderwriterOpened = users.Load(UnderwriterOpenedId)
+			};
+		}
+
+		public static BugModel ToModel(Bug bug)
+		{
+			if (bug == null) return null;
+			var bugModel = new BugModel
+			{
+				Id = bug.Id,
+				CustomerId = bug.Customer.Id,
+				DateOpened = bug.DateOpened,
+				DateClosed = bug.DateClosed,
+				MarketPlaceId = bug.MarketPlaceId,
+				TextOpened = bug.TextOpened,
+				TextClosed = bug.TextClosed,
+				Type = bug.Type,
+				State = bug.State.ToString(),
+				DirectorId = bug.CreditBureauDirectorId
+			};
+
+			if (bug.UnderwriterOpened != null)
+			{
+				bugModel.UnderwriterOpenedId = bug.UnderwriterOpened.Id;
+				bugModel.UnderwriterOpenedName = bug.UnderwriterOpened.FullName;
+			}
+			if (bug.UnderwriterClosed != null)
+			{
+				bugModel.UnderwriterClosedId = bug.UnderwriterClosed.Id;
+				bugModel.UnderwriterClosedName = bug.UnderwriterClosed.FullName;
+			}
+
+			return bugModel;
+		}
+	}
+}
