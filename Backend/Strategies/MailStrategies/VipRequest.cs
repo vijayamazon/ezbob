2fc8ea4041bb4e329e2c4c21dbb143c543e21db5
stas_dulman <stasd@ezbob.com>
2014-05-11 09:19:58 +0300

    EZ-2068 vip mail fix if no customer id

--- Backend/Strategies/MailStrategies/VipRequest.cs
+++ Backend/Strategies/MailStrategies/VipRequest.cs
@@ -1,17 +1,20 @@
-﻿namespace EzBob.Backend.Strategies.MailStrategies {
+﻿namespace EzBob.Backend.Strategies.MailStrategies
+{
 	using API;
 	using Ezbob.Logger;
 	using Ezbob.Database;
 
-	public class VipRequest : AStrategy {
+	public class VipRequest : AStrategy
+	{
 		private readonly int _customerId;
 		private readonly string _fullName;
 		private readonly string _email;
 		private readonly string _phone;
 		private MailMetaData _mailMetaData;
 		private readonly StrategiesMailer m_oMailer;
-		
-		public VipRequest(int customerId, string fullName, string email, string phone, AConnection oDb, ASafeLog oLog) : base(oDb, oLog)
+
+		public VipRequest(int customerId, string fullName, string email, string phone, AConnection oDb, ASafeLog oLog)
+			: base(oDb, oLog)
 		{
 			_customerId = customerId;
 			_fullName = fullName;
@@ -21,7 +24,7 @@
 		}
 
 		public override string Name { get { return "VipRequest"; } }
-		
+
 		public override void Execute()
 		{
 			SetTemplateAndVariables();
@@ -30,12 +33,20 @@
 
 		#region method SetTemplateAndVariables
 
-		protected void SetTemplateAndVariables() {
+		protected void SetTemplateAndVariables()
+		{
 			_mailMetaData = new MailMetaData("VipRequest");
-			
+
 			if (_customerId > 0)
 			{
-				_mailMetaData.Add("CustomerId", string.Format("<a alt='Open this customer in underwriter.' href='https://{1}/UnderWriter/Customers?customerid={0}' title='Open this customer in underwriter.' style='font-weight:bold;color:black;background-color:#bfffcf!important' target='_blank'>{0}</a>", _customerId, ConfigManager.CurrentValues.Instance.UnderwriterSite.Value));
+				_mailMetaData.Add("CustomerId",
+								  string.Format(
+									  "<a alt='Open this customer in underwriter.' href='https://{1}/UnderWriter/Customers?customerid={0}' title='Open this customer in underwriter.' style='font-weight:bold;color:black;background-color:#bfffcf!important' target='_blank'>{0}</a>",
+									  _customerId, ConfigManager.CurrentValues.Instance.UnderwriterSite.Value));
+			}
+			else
+			{
+				_mailMetaData.Add("CustomerId", string.Empty);
 			}
 
 			_mailMetaData.Add("FullName", _fullName);
