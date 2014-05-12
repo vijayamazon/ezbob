0b17294701d44a7ff644b8db075228402bd45c35
Yuly Saar <yulys@ezbob.com>
2014-05-12 09:38:17 +0300

    EZ-2151: More outputs calculation

--- Backend/Strategies/PricingModel/PricingModelCalculate.cs
+++ Backend/Strategies/PricingModel/PricingModelCalculate.cs
@@ -1,8 +1,12 @@
 ﻿namespace EzBob.Backend.Strategies.PricingModel
 {
+	using System;
 	using System.Data;
+	using EZBob.DatabaseLib.Model.Database.Loans;
+	using EZBob.DatabaseLib.Model.Loans;
 	using Ezbob.Database;
 	using Ezbob.Logger;
+	using PaymentServices.Calculators;
 
 	public class PricingModelCalculate : AStrategy
 	{
@@ -19,30 +23,49 @@
 
 		private readonly int customerId;
 		public PricingModelModel Model { get; private set; }
-		
+
+		private Loan CreateLoan()
+		{
+			var sfc = new SetupFeeCalculator(false, false, 0, 0);
+			var setupFee = sfc.Calculate(Model.LoanAmount);
+
+			var calculator = new LoanScheduleCalculator { Interest = Model.MonthlyInterestRate, Term = Model.LoanTerm };
+			LoanType lt = new StandardLoanType();
+			var loan = new Loan
+			{
+				LoanAmount = Model.LoanAmount,
+				Date = DateTime.UtcNow,
+				LoanType = lt,
+				CashRequest = null,
+				SetupFee = setupFee,
+				LoanLegalId = 1
+			};
+			calculator.Calculate(Model.LoanAmount, loan, loan.Date, Model.InterestOnlyPeriod);
+			return loan;
+		}
 		public override void Execute()
 		{
 			Model.FeesRevenue = Model.SetupFeePounds;
 
-
-			// These values should be calculated accoring to loan calculation variables
-			Model.InterestRevenue = 0; // Sum the middle column in the approve loan window
-			// Get Loan calculation variables... follow loan approval process to get APR and outstandingprincipal
-			Model.CostOfDebtOutput = 0;
-			for (int i = 0; i < Model.LoanTerm; i++)
-			{
-				//Model.CostOfDebtOutput +=
-			}
-			Model.Apr = 1.23m;
-
-
-			Model.Revenue = Model.FeesRevenue + Model.InterestRevenue;
-
-
-			Model.MonthlyInterestRate = 0.34m;
+			Model.MonthlyInterestRate = 0.34m; // TODO: implement calc
 			Model.SetupFeeForEuLoan = 0.03m;
 
+			Loan loan = CreateLoan();
+			var calc = new LoanRepaymentScheduleCalculator(loan, loan.Date);
+			calc.GetState();
+			var aprCalc = new APRCalculator();
+			Model.Apr = (decimal)(loan.LoanAmount == 0 ? 0 : aprCalc.Calculate(loan.LoanAmount, loan.Schedule, loan.SetupFee, loan.Date));
 
+			Model.CostOfDebtOutput = 0;
+			Model.InterestRevenue = 0;
+			foreach (LoanScheduleItem scheuldeItem in loan.Schedule)
+			{
+				Model.InterestRevenue += scheuldeItem.Interest;
+				Model.CostOfDebtOutput += scheuldeItem.AmountDue * Model.DebtPercentOfCapital * Model.CostOfDebt / 12;
+			}
+			Model.InterestRevenue *= 1 - Model.DefaultRate;
+
+			Model.Revenue = Model.FeesRevenue + Model.InterestRevenue;
 
 			int consumerScore = DB.ExecuteScalar<int>(
 				"GetExperianScore",
@@ -59,10 +82,6 @@
 			Model.NetLossFromDefaults = (1 - Model.CollectionRate) * Model.LoanAmount * Model.DefaultRate;
 			Model.ProfitMarkupOutput = Model.ProfitMarkup * Model.Revenue;
 			Model.AnnualizedInterestRate = Model.TenureMonths != 0 ? (Model.MonthlyInterestRate * 12) + (Model.SetupFeePercents * 12 / Model.TenureMonths) : 0;
-
-
-			
-
 
 			Model.TotalCost = Model.CostOfDebtOutput + Model.Cogs + Model.OpexAndCapex + Model.NetLossFromDefaults;
 		}
