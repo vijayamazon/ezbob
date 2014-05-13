334eb41a60d9cac60c81ef6429c3966bc667fe6a
Yuly Saar <yulys@ezbob.com>
2014-05-13 17:57:06 +0300

    EZ-2151: Handling missing company score

--- Backend/Strategies/PricingModel/GetPricingModelDefaultRate.cs
+++ Backend/Strategies/PricingModel/GetPricingModelDefaultRate.cs
@@ -32,11 +32,18 @@
 				new QueryParameter("CustomerId", customerId)
 			);
 
-			int companyScore = DB.ExecuteScalar<int>(
+			int companyScore = 0;
+			DataTable dt = DB.ExecuteReader(
 				"GetCompanyScore",
 				CommandSpecies.StoredProcedure,
 				new QueryParameter("CustomerId", customerId)
 			);
+
+			if (dt.Rows.Count == 1)
+			{
+				var sr = new SafeReader(dt.Rows[0]);
+				companyScore = sr["Score"];
+			}
 
 			decimal companyValue = GetDefaultRateCompany(companyScore);
 			decimal customerValue = GetDefaultRateCustomer(consumerScore);
