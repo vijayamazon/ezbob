ca5297d612dc93964e6ed00c572f8812ca0cd397
stas_dulman <stasd@ezbob.com>
2014-05-18 11:01:57 +0300

    disable underwriter on prod web machine

--- App/PluginWeb/EzBob.Web/Areas/Underwriter/UnderwriterAreaRegistration.cs
+++ App/PluginWeb/EzBob.Web/Areas/Underwriter/UnderwriterAreaRegistration.cs
@@ -1,29 +1,26 @@
-﻿using System.Web.Mvc;
-using EzBob.Web.Infrastructure;
-using StructureMap;
-
-namespace EzBob.Web.Areas.Underwriter
+﻿namespace EzBob.Web.Areas.Underwriter
 {
-	using ConfigManager;
+	using System.Web.Mvc;
+	using System.Configuration;
 
 	public class UnderwriterAreaRegistration : AreaRegistration
-    {
-        public override string AreaName
-        {
-            get
-            {
-                return "Underwriter";
-            }
-        }
+	{
+		public override string AreaName
+		{
+			get
+			{
+				return "Underwriter";
+			}
+		}
 
-        public override void RegisterArea(AreaRegistrationContext context)
-        {
-            if (!CurrentValues.Instance.ManagementPartEnabled) return;
-            context.MapRoute(
-                "Underwriter_default",
-                "Underwriter/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+		public override void RegisterArea(AreaRegistrationContext context)
+		{
+			if (!bool.Parse(ConfigurationManager.AppSettings["UnderwriterEnabled"])) return;
+			context.MapRoute(
+				"Underwriter_default",
+				"Underwriter/{controller}/{action}/{id}",
+				new { action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
 }
