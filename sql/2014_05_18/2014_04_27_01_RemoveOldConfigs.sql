dd40624a8e8c9d41370157d7673010a370abda98
Yuly Saar <yulys@ezbob.com>
2014-05-18 09:00:00 +0300

    Removed sql code of transition time configs

--- sql/2014_05_18/2014_04_27_01_RemoveOldConfigs.sql
+++
@@ -1,5 +0,0 @@
-IF EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name = 'UseNewCaisStrategies')
-BEGIN
-	DELETE FROM ConfigurationVariables WHERE Name IN ('UseNewCaisStrategies','UseNewFraudCheckerStrategy','UseNewMailStrategies','UseNewMainStrategy','UseNewUpdateCustomerMpsStrategy','UseNewUpdateMpStrategy')
-END
-GO
