a1bfa75c8f3fa911091715046e59f64dd3167d0e
Yuly Saar <yulys@ezbob.com>
2014-05-18 14:17:44 +0300

    EZ-2161: Removed some SPs and DB objects

--- sql/Current/2014_05_18_05_RemoveStuff.sql
+++ sql/Current/2014_05_18_05_RemoveStuff.sql
@@ -25,3 +25,36 @@ GO
 IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[StrategyEngine_ExecStatePush]') AND TYPE IN (N'P', N'PC'))
 DROP PROCEDURE [dbo].[StrategyEngine_ExecStatePush]
 GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Application_Lock]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[Application_Lock]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Application_UnLock]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[Application_UnLock]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[App_History_LockEvent]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[App_History_LockEvent]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[App_History_Report]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[App_History_Report]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[App_History_Insert]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[App_History_Insert]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[App_History_ExitEvent]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[App_History_ExitEvent]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[App_History_ComingEvent]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[App_History_ComingEvent]
+GO
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[App_History_UnlockEvent]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[App_History_UnlockEvent]
+GO
+
