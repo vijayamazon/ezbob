0008152970e4425de46c0bf7f68bd29d22e089e9
Yuly Saar <yulys@ezbob.com>
2014-05-18 17:30:21 +0300

    EZ-2161: Removed some SPs and DB objects

---
+++ sql/Current/2014_05_18_10_RemoveStuff.sql
@@ -0,0 +1,95 @@
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BehavioralReport_GetList]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[BehavioralReport_GetList]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BehavioralReport_GetListAll]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[BehavioralReport_GetListAll]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BehavioralReport_GetPath]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[BehavioralReport_GetPath]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BehavioralReport_Insert]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[BehavioralReport_Insert]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BehavioralReport_MarkAsRead]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[BehavioralReport_MarkAsRead]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_AssignProduct]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_AssignProduct]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_CoreGetParams]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_CoreGetParams]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_DeleteParam]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_DeleteParam]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_DeleteProduct]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_DeleteProduct]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_GetLinkedStr]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_GetLinkedStr]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_GetParams]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_GetParams]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[creditproduct_getproductbyname]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[creditproduct_getproductbyname]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_GetProductList]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_GetProductList]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_IsParamExists]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_IsParamExists]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_UpdateParams]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_UpdateParams]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CreditProduct_UpdateProduct]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[CreditProduct_UpdateProduct]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetCreditProductParams]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GetCreditProductParams]
+GO
+
+
+
+
+
+
+
+
+
+
+
+IF OBJECT_ID('Creditproduct_Strategyrel') IS NOT NULL
+BEGIN
+	DROP TABLE Creditproduct_Strategyrel
+END
+GO
+IF OBJECT_ID('CreditProduct_Sign') IS NOT NULL
+BEGIN
+	DROP TABLE CreditProduct_Sign
+END
+GO
+IF OBJECT_ID('CreditProduct_Products') IS NOT NULL
+BEGIN
+	DROP TABLE CreditProduct_Products
+END
+GO
+IF OBJECT_ID('CreditProduct_Params') IS NOT NULL
+BEGIN
+	DROP TABLE CreditProduct_Params
+END
+GO
+IF OBJECT_ID('BehavioralReports') IS NOT NULL
+BEGIN
+	DROP TABLE BehavioralReports
+END
+GO
+IF OBJECT_ID('AppAdditionalData') IS NOT NULL
+BEGIN
+	DROP TABLE AppAdditionalData
+END
+GO
+
+
+
