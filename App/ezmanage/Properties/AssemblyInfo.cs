98895e5a368ab9cb11a6ffa648ea9985d9f577db
Yuly Saar <yulys@ezbob.com>
2014-05-18 08:46:17 +0300

    EZ-2106: Retired ezmanage

--- App/ezmanage/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("ezmanage")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("982be75f-6f0c-4502-a159-d82106b9bc38")]
