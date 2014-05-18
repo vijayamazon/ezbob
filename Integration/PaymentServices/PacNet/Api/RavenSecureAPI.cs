09916bda8123e514fa6702858efbb92b5c431530
stas_dulman <stasd@ezbob.com>
2014-05-18 14:46:38 +0100

    pacnet fix

--- Integration/PaymentServices/PacNet/Api/RavenSecureAPI.cs
+++ Integration/PaymentServices/PacNet/Api/RavenSecureAPI.cs
@@ -4,77 +4,79 @@
 	using Support;
 	using ConfigManager;
 
-    /**
-     * <p>An API to Raven functionality that is primarily driven through the
-     * use of key-value parameter pairs, but whose values cannot be set directly,
-     * only interrogated, with some relatively static parameters able to be read
-     * from the application's default settings.</p>
-     *
-     * @author warren
-     */
-    public abstract class RavenSecureAPI : KeyValueConfigurableReadableAPI
-    {
-        /** supplies current timestamps */
-        protected static TimestampProvider TimestampProvider = new TimestampProvider();
+	/**
+	 * <p>An API to Raven functionality that is primarily driven through the
+	 * use of key-value parameter pairs, but whose values cannot be set directly,
+	 * only interrogated, with some relatively static parameters able to be read
+	 * from the application's default settings.</p>
+	 *
+	 * @author warren
+	 */
+	public abstract class RavenSecureAPI : KeyValueConfigurableReadableAPI
+	{
+		/** supplies current timestamps */
+		protected static TimestampProvider TimestampProvider = new TimestampProvider();
 
-        /** supplies HMAC signatures to authenticate the receiver to Raven */
-        protected SignatureProvider signatureProvider;
+		/** supplies HMAC signatures to authenticate the receiver to Raven */
+		protected SignatureProvider signatureProvider;
 
-        /** the RAPI version being targetted */
-        protected String rapiVersion;
-        /** the base Raven server URL */
-        protected String site;
-        /* the client's username */
-        protected String userName;
-        /** the client's private secret */
-        protected String secret;
-        protected String prefix;
+		/** the RAPI version being targetted */
+		protected String rapiVersion;
+		/** the base Raven server URL */
+		protected String site;
+		/* the client's username */
+		protected String userName;
+		/** the client's private secret */
+		protected String secret;
+		protected String prefix;
 
-        /**
-         * <p>Constructs an initialized instance of the receiver.</p>
-         *
-         * @throws RavenConfigurationException if the application's default settings do not
-         * exist or cannot be found or if any mandatory parameters are missing
-         */
-        public RavenSecureAPI()
+		/**
+		 * <p>Constructs an initialized instance of the receiver.</p>
+		 *
+		 * @throws RavenConfigurationException if the application's default settings do not
+		 * exist or cannot be found or if any mandatory parameters are missing
+		 */
+		public RavenSecureAPI()
 		{
-			rapiVersion = CurrentValues.Instance.PacnetRAVEN_RAPIVERSION;
-	        site = CurrentValues.Instance.PacnetRAVEN_GATEWAY;
-			userName = CurrentValues.Instance.PacnetRAVEN_USERNAME;
-			secret = CurrentValues.Instance.PacnetRAVEN_SECRET;
-			prefix = CurrentValues.Instance.PacnetRAVEN_PREFIX;
 
-            try
-            {
-                signatureProvider = new SignatureProvider(this.secret);
-            }
-            catch (Exception e)
-            {
-                throw new RavenConfigurationException("Problem with secret or algorithm.", e);
-            }
-        }
+			this.rapiVersion = ConfigureParamAsValue("RAPIVersion", "RAVEN_RAPIVERSION", "2", true, CurrentValues.Instance.PacnetRAVEN_RAPIVERSION);
+			this.site = ConfigureParam("RAVEN_GATEWAY", "RAVEN_GATEWAY", null, true, CurrentValues.Instance.PacnetRAVEN_GATEWAY);
+			this.userName = ConfigureParamAsValue("UserName", "RAVEN_USERNAME", null, true, CurrentValues.Instance.PacnetRAVEN_USERNAME);
+			this.secret = ConfigureParam("RAVEN_SECRET", "RAVEN_SECRET", null, true, CurrentValues.Instance.PacnetRAVEN_SECRET);
+			this.prefix = ConfigureParam("RAVEN_PREFIX", "RAVEN_PREFIX", null, true, CurrentValues.Instance.PacnetRAVEN_PREFIX);
 
-        /**
-         * <p>Answers a signature that guarantees that the receiver is a valid
-         * Raven API interaction.</p>
-         *
-         * @return a signature String
-         * @throws RavenIncompleteSignatureException if the signature is incomplete
-         * due to one or more missing parameters
-         */
-        public String GetSignature() {
-            return signatureProvider.GetSignature(this.GetSignatureData());
-        }
+			try
+			{
+				signatureProvider = new SignatureProvider(this.secret);
+			}
+			catch (Exception e)
+			{
+				throw new RavenConfigurationException("Problem with secret or algorithm.", e);
+			}
+		}
 
-        /**
-         * <p>Answers a concatenated string of a subset of the receiver's parameter
-         * data values used to determine its signature.</p>
-         *
-         * @return a concatenated String of a subset of the receiver's parameter
-         * data values
-         * @throws RavenIncompleteSignatureException if the signature is incomplete
-         * due to one or more missing parameters
-         */
-        protected abstract String GetSignatureData();
-    }
+		/**
+		 * <p>Answers a signature that guarantees that the receiver is a valid
+		 * Raven API interaction.</p>
+		 *
+		 * @return a signature String
+		 * @throws RavenIncompleteSignatureException if the signature is incomplete
+		 * due to one or more missing parameters
+		 */
+		public String GetSignature()
+		{
+			return signatureProvider.GetSignature(this.GetSignatureData());
+		}
+
+		/**
+		 * <p>Answers a concatenated string of a subset of the receiver's parameter
+		 * data values used to determine its signature.</p>
+		 *
+		 * @return a concatenated String of a subset of the receiver's parameter
+		 * data values
+		 * @throws RavenIncompleteSignatureException if the signature is incomplete
+		 * due to one or more missing parameters
+		 */
+		protected abstract String GetSignatureData();
+	}
 }
