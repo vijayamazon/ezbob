5682007159d2809a1119606aa2e8b8bd1ccb6035
Yuly Saar <yulys@ezbob.com>
2014-05-19 14:43:11 +0300

    EZ-2161: Removed some SPs and DB objects

---
+++ sql/Current/2014_05_19_06_RemoveStuff.sql
@@ -0,0 +1,48 @@
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateCashRequests]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateCashRequests]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateScoringResult]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateScoringResult]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateUserApplyForLoanTime]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[UpdateUserApplyForLoanTime]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[VerifyServiceRegistration]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[VerifyServiceRegistration]
+GO
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ViewHelper_MonthlyTurnover]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[ViewHelper_MonthlyTurnover]
+GO
+
+
+
+
+
+
+IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fn_GetCustomerAdress]') AND TYPE IN (N'P', N'PC'))
+DROP FUNCTION [dbo].[fn_GetCustomerAdress]
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sql/SPs/GetCustomerAddresses.sql
+++ sql/SPs/GetCustomerAddresses.sql
@@ -9,6 +9,52 @@ CREATE PROCEDURE [dbo].[GetCustomerAddresses]
 	(@CustomerId INT)
 AS
 BEGIN
-	SELECT * FROM fn_GetCustomerAdress (@CustomerId)
+	DECLARE
+		@Line1 VARCHAR (255), 
+		@Line2 VARCHAR (255),
+		@Line3 VARCHAR (255),
+		@Line4 VARCHAR (255),
+		@Line5 VARCHAR (255),
+		@Line6 VARCHAR (255),
+		@Line1Prev VARCHAR (255), 
+		@Line2Prev VARCHAR (255),
+		@Line3Prev VARCHAR (255),
+		@Line4Prev VARCHAR (255),
+		@Line5Prev VARCHAR (255),
+		@Line6Prev VARCHAR (255)
+
+	select 
+		@line1=ca.Line1, @Line2=ca.Line2, @Line3 =ca.Line3,@Line4 =ca.Town , @Line5 =ca.County, @Line6 =ca.Postcode
+	from 
+		CustomerAddress ca
+	where 
+		ca.addressType= '1' AND 
+		ca.CustomerId = @CustomerId AND 
+		ca.Line1 is NOT NULL
+
+	select 
+		@Line1Prev =ca.Line1, @Line2Prev =ca.Line2, 
+		@Line3Prev =ca.Line3, @Line4Prev =ca.Town, 
+		@Line5Prev =ca.County, @Line6Prev =ca.Postcode
+	from 
+		CustomerAddress ca
+	WHERE 
+		ca.addressType = '2' AND 
+		ca.CustomerId = @CustomerId AND 
+		ca.Line1 is NOT NULL
+
+
+	SELECT @Line1 AS Line1, 
+		@Line2 AS Line2,
+		@Line3 AS Line3,
+		@Line4 AS Line4,
+		@Line5 AS Line5,
+		@Line6 AS Line6,
+		@Line1Prev AS Line1Prev, 
+		@Line2Prev AS Line2Prev,
+		@Line3Prev AS Line3Prev,
+		@Line4Prev AS Line4Prev,
+		@Line5Prev AS Line5Prev,
+		@Line6Prev AS Line6Prev      
 END
 GO
