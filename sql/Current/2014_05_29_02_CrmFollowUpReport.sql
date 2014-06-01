da5dfdf5761aa12bccac9f40b9872eab1b5acf20
stas_dulman <stasd@ezbob.com>
2014-06-01 11:29:15 +0300

    fix script

--- sql/Current/2014_05_29_02_CrmFollowUpReport.sql
+++ sql/Current/2014_05_29_02_CrmFollowUpReport.sql
@@ -6,17 +6,18 @@ DECLARE @Headers NVARCHAR(200) = 'CustomerId,Email,FollowUpDate,Comment'
 DECLARE @Fields NVARCHAR(200) = '#CustomerId,Email,FollowUpDate,Comment'
 
 IF NOT EXISTS (SELECT * FROM ReportScheduler WHERE Type = @Type)
+BEGIN
 	INSERT INTO ReportScheduler(Type,  Title,  StoredProcedure, IsDaily, IsWeekly, IsMonthly, IsMonthToDate, Header,   Fields,  ToEmail) 
 	VALUES                     (@Type, @Title, @Sp,	            1,       0,        0,         0,             @Headers, @Fields,	@Emails)
 
-INSERT INTO ReportArguments (ReportArgumentNameId, ReportId)
-SELECT 1, Id FROM ReportScheduler WHERE	Type = @Type
+	INSERT INTO ReportArguments (ReportArgumentNameId, ReportId)
+	SELECT 1, Id FROM ReportScheduler WHERE	Type = @Type
 
-INSERT INTO ReportsUsersMap (UserID, ReportID)
-	SELECT u.Id, r.Id
-	FROM ReportUsers u,	ReportScheduler r
-	WHERE
-		u.UserName IN ('alexbo', 'stasd', 'nimrodk', 'eilaya', 'rosb')
-		AND r.Type = @Type AND NOT EXISTS (SELECT *	FROM ReportsUsersMap WHERE UserID = u.Id AND ReportID = r.ID)
-
+	INSERT INTO ReportsUsersMap (UserID, ReportID)
+		SELECT u.Id, r.Id
+		FROM ReportUsers u,	ReportScheduler r
+		WHERE
+			u.UserName IN ('alexbo', 'stasd', 'nimrodk', 'eilaya', 'rosb')
+			AND r.Type = @Type AND NOT EXISTS (SELECT *	FROM ReportsUsersMap WHERE UserID = u.Id AND ReportID = r.ID)
+END
 GO
