3166b67b5072ac6e9afebc15e31c269d5717ead0
stas_dulman <stasd@ezbob.com>
2014-06-11 13:05:01 +0300

    adding yodlee prod terminal to solution

--- Tools/EzbobUtils/YodleeProdTerminal/YodleeProdTerminal/Properties/AssemblyInfo.cs
+++ Tools/EzbobUtils/YodleeProdTerminal/YodleeProdTerminal/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("YodleeTest2")]
+[assembly: AssemblyTitle("YodleeProdTerminal")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("4ebb85b9-e7c6-48df-a675-c27e7aff707d")]
