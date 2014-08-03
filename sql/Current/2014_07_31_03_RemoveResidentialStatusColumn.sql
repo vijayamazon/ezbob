1687df6aba3d5ece393b88a2e2aa2611919484e1
Yuly Saar <yulys@ezbob.com>
2014-08-03 13:06:41 +0300

    Fixed property status mapping

--- sql/Current/2014_07_31_03_RemoveResidentialStatusColumn.sql
+++ sql/Current/2014_07_31_03_RemoveResidentialStatusColumn.sql
@@ -2,9 +2,17 @@ IF EXISTS (SELECT 1 FROM sys.columns WHERE Name = N'ResidentialStatus' and Objec
 BEGIN
 	DECLARE @Statement NVARCHAR(MAX)
 	SET @Statement = '
-	UPDATE Customer SET PropertyStatusId = 2 WHERE ResidentialStatus = ''Home owner''
-	UPDATE Customer SET PropertyStatusId = 5 WHERE ResidentialStatus = ''Renting''
-	UPDATE Customer SET PropertyStatusId = 7 WHERE ResidentialStatus = ''Living with Parents''
+	DECLARE @StatusId INT
+	
+	SELECT @StatusId = Id FROM CustomerPropertyStatuses WHERE Description = ''Home owner''	
+	UPDATE Customer SET PropertyStatusId = @StatusId WHERE ResidentialStatus = ''Home owner''
+	
+	SELECT @StatusId = Id FROM CustomerPropertyStatuses WHERE Description = ''Renting''	
+	UPDATE Customer SET PropertyStatusId = @StatusId WHERE ResidentialStatus = ''Renting''
+	
+	SELECT @StatusId = Id FROM CustomerPropertyStatuses WHERE Description = ''Living with Parents''
+	UPDATE Customer SET PropertyStatusId = @StatusId WHERE ResidentialStatus = ''Living with Parents''
+	
 	ALTER TABLE Customer DROP COLUMN ResidentialStatus
 	'
 	
