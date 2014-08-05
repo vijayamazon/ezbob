d2cac78d33f4afd22ba12ad0b6c60885e4cfbe2b
Alex Boursouk <alexbo@ezbob.com>
2014-08-05 12:24:44 +0300

    EZ-2477 timeout in DB when trying to use GetFunctionAnalysisValuesByCustomerMarketPlace

--- sql/SPs/GetFunctionAnalysisValuesByCustomerMarketPlace.sql
+++ sql/SPs/GetFunctionAnalysisValuesByCustomerMarketPlace.sql
@@ -1,34 +1,52 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetFunctionAnalysisValuesByCustomerMarketPlace]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[GetFunctionAnalysisValuesByCustomerMarketPlace]
+IF OBJECT_ID('GetFunctionAnalysisValuesByCustomerMarketPlace') IS NULL
+	EXECUTE('CREATE PROCEDURE GetFunctionAnalysisValuesByCustomerMarketPlace AS SELECT 1')
 GO
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[GetFunctionAnalysisValuesByCustomerMarketPlace] 
-	(@MpID INT)
+
+ALTER PROCEDURE GetFunctionAnalysisValuesByCustomerMarketPlace
+@MpID INT
 AS
 BEGIN
-		SET NOCOUNT ON
+	SET NOCOUNT ON
+
+	------------------------------------------------------------------------------
 
 	SELECT
-		f.internalid AS fid,
-		p.internalid AS fpid,
-		v.value,
-		h.updatingstart
+		Id,
+		UpdatingStart
+	INTO
+		#h
 	FROM
-		mp_analyisisfunctionvalues                       v
-		INNER JOIN (
-			SELECT id, updatingstart
-			FROM mp_customermarketplaceupdatinghistory
-			WHERE updatingstart IS NOT NULL
-			AND updatingend IS NOT NULL
-		)
-		                                                 h ON v.customermarketplaceupdatinghistoryrecordid = h.id
-		INNER JOIN mp_customermarketplace                m ON v.customermarketplaceid = m.id
-		INNER JOIN mp_analyisisfunction                  f ON v.analyisisfunctionid = f.id
-		INNER JOIN mp_analysisfunctiontimeperiod         p ON v.analysisfunctiontimeperiodid = p.id
+		MP_CustomerMarketplaceUpdatinghistory
 	WHERE
-		m.id = @MpID
+		UpdatingStart IS NOT NULL
+		AND
+		UpdatingEnd IS NOT NULL
+		AND
+		CustomerMarketPlaceId = @MpID
+
+	------------------------------------------------------------------------------
+
+	SELECT
+		f.InternalId AS fid, -- function id
+		p.InternalId AS fpid, -- period id
+		v.Value,
+		h.UpdatingStart
+	FROM
+		MP_AnalyisisFunctionValues v
+		INNER JOIN #h h ON v.CustomerMarketplaceUpdatingHistoryRecordId = h.id
+		INNER JOIN MP_AnalyisisFunction f ON v.AnalyisisFunctionId = f.id
+		INNER JOIN MP_AnalysisFunctionTimeperiod p ON v.AnalysisFunctionTimeperiodId = p.id
+	WHERE
+		v.CustomerMarketPlaceId = @MpID
+
+	------------------------------------------------------------------------------
+
+	DROP TABLE #h
 END
 GO
