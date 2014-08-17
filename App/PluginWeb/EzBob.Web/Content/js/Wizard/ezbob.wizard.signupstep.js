f6b155fa9957cc8ebaad2e9e8c7a4196c984b206
Yuly Saar <yulys@ezbob.com>
2014-08-17 17:01:21 +0300

    Commented out the ab testing line

--- App/PluginWeb/EzBob.Web/Content/js/Wizard/ezbob.wizard.signupstep.js
+++ App/PluginWeb/EzBob.Web/Content/js/Wizard/ezbob.wizard.signupstep.js
@@ -5,7 +5,7 @@ EzBob.QuickSignUpStepView = Backbone.View.extend({
 		this.template = _.template($('#signup-template').html());
         
 		// This post is for Nir's A\B testing
-		$.post("http://www.ezbob.com/thank-page/", { action: "sptAjaxRecordConversion", sptID: 5765 });
+		//$.post("http://www.ezbob.com/thank-page/", { action: "sptAjaxRecordConversion", sptID: 5765 });
 
 		this.on('ready', this.ready, this);
 		this.model.on('change:loggedIn', this.render, this);
