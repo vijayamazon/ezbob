96ecf5a8176217273b5dba7bbf917d75752e01bc
Yuly Saar <yulys@ezbob.com>
2014-08-17 12:29:37 +0300

    Fixed sql

--- sql/2014_08_17/2014_07_30_01_AddCustomerPropertyStatusTables.sql
+++ sql/2014_08_17/2014_07_30_01_AddCustomerPropertyStatusTables.sql
@@ -32,13 +32,13 @@ BEGIN
 	INSERT INTO CustomerPropertyStatusGroups (Name, Title, Priority) VALUES (''Not owners'', ''I do not own any property'', 2)
 	
 	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''I own only this property'', 1, 1, 1, 0)
-	INSERT INTO CustomerPropertyStatuses (Description, IsOwner, GroupId, IsActive) VALUES (''I own this property and other properties'', 1, 1, 1, 1)
-	INSERT INTO CustomerPropertyStatuses (Description, IsOwner, GroupId, IsActive) VALUES (''I live in the above and own other properties'', 1, 1, 0, 1)
-	INSERT INTO CustomerPropertyStatuses (Description, IsOwner, GroupId, IsActive) VALUES (''Home owner'', 1, 0, 1, 0)
+	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''I own this property and other properties'', 1, 1, 1, 1)
+	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''I live in the above and own other properties'', 1, 1, 0, 1)
+	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''Home owner'', 1, 0, 1, 0)
 	
-	INSERT INTO CustomerPropertyStatuses (Description, IsOwner, GroupId, IsActive) VALUES (''Renting'', 2, 1, 0, 0)
-	INSERT INTO CustomerPropertyStatuses (Description, IsOwner, GroupId, IsActive) VALUES (''Social house'', 2, 1, 0, 0)
-	INSERT INTO CustomerPropertyStatuses (Description, IsOwner, GroupId, IsActive) VALUES (''Living with parents'', 2, 1, 0, 0)
+	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''Renting'', 2, 1, 0, 0)
+	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''Social house'', 2, 1, 0, 0)
+	INSERT INTO CustomerPropertyStatuses (Description, GroupId, IsActive, IsOwnerOfMainAddress, IsOwnerOfOtherProperties) VALUES (''Living with parents'', 2, 1, 0, 0)
 	'
 	
 	EXEC(@Statement)
