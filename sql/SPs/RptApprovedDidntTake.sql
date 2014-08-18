df3f7e64a6e84d060bba34505006526368b81b82
stas_dulman <stasd@ezbob.com>
2014-08-18 15:59:13 +0300

    update report rptapproved didnt take

--- sql/SPs/RptApprovedDidntTake.sql
+++ sql/SPs/RptApprovedDidntTake.sql
@@ -56,74 +56,77 @@ WHERE R.UnderwriterDecision = 'approved'
 
 
 SELECT  T.IdCustomer,
-		C.Name,
-	    convert(DATE,C.GreetingMailSentDate) AS SignUpDate, 
-		C.Fullname,
-		C.DaytimePhone, 
-		C.MobilePhone, 
-		CASE WHEN C.BrokerID IS NULL THEN C.ReferenceSource ELSE 'Broker' END AS ReferenceSource,
-		T.MaxAppDate AS ApprovedDate,
-		R.ExpirianRating,
-		R.UnderwriterDecision,
-		R.UnderwriterDecisionDate,
-		R.ManagerApprovedSum,
-		R.InterestRate,
-	  	R.RepaymentPeriod,
-	  	CASE
-	  	WHEN R.LoanTypeId = 2 THEN 'HalfWay Loan'
-		WHEN R.LoanTypeId = 1 THEN 'Standard Loan'
-		END AS LoansType,
-		R.Id AS CashReqId
-	
-INTO #temp3 
+                                C.Name,
+                    convert(DATE,C.GreetingMailSentDate) AS SignUpDate,
+                                C.Fullname,
+                                C.DaytimePhone,
+                                C.MobilePhone,
+                                C.ReferenceSource,
+                                T.MaxAppDate AS ApprovedDate,
+                                R.ExpirianRating,
+                                R.UnderwriterDecision,
+                                R.UnderwriterDecisionDate,
+                                R.ManagerApprovedSum,
+                                R.InterestRate,
+                                R.RepaymentPeriod,
+                                CASE
+                                WHEN R.LoanTypeId = 2 THEN 'HalfWay Loan'
+                                WHEN R.LoanTypeId = 1 THEN 'Standard Loan'
+                                END AS LoansType,
+                                R.Id AS CashReqId,
+                                C.BrokerID
+               
+INTO #temp3
 FROM #temp2 T
 JOIN Customer C ON C.Id = T.IdCustomer
 JOIN CashRequests R ON R.IdCustomer = T.IdCustomer
-WHERE T.IdCustomer NOT IN 
-						  ( SELECT C.Id
-							FROM Customer C 
-							WHERE Name LIKE '%ezbob%'
-							OR Name LIKE '%liatvanir%'
-							OR Name LIKE '%q@q%'
-							OR Name LIKE '%1@1%'
-							OR CollectionStatus != 0
-							OR C.IsTest=1)	
-	  AND T.MaxAppDate = R.CreationDate
-
-
+WHERE T.IdCustomer NOT IN
+                                                                                                  ( SELECT C.Id
+                                                                                                                FROM Customer C
+                                                                                                                WHERE Name LIKE '%ezbob%'
+                                                                                                                OR Name LIKE '%liatvanir%'
+                                                                                                                OR Name LIKE '%q@q%'
+                                                                                                                OR Name LIKE '%1@1%'
+                                                                                                                OR CollectionStatus != 0
+                                                                                                                OR C.IsTest=1) 
+                  AND T.MaxAppDate = R.CreationDate
+ 
+ 
 SELECT L.Id,L.[Date],L.LoanAmount,L.RequestCashId
 INTO #temp4
 FROM Loan L
-
+ 
 ---------- COUNT OF PREVIOUS LOANS FOR CUSTOMER ----------
 SELECT L.CustomerId,count(L.Id) AS LoanIssued
 INTO #temp5
 FROM Loan L
 GROUP BY L.CustomerId
-
+ 
 ----------------------------------------------------------
 SELECT  T1.IdCustomer,
-		T1.Fullname,
-		T1.Name AS Email,
-		T1.ReferenceSource,
-		T1.ExpirianRating, 
-		T1.UnderwriterDecisionDate,
-		T1.ManagerApprovedSum,
-		T1.InterestRate,
-	  	T1.RepaymentPeriod,
-	  	T1.LoansType,
-	  	CASE 
-	  	WHEN T3.LoanIssued>0 THEN 'Yes'
-	  	ELSE 'No' 
-	  	END AS HadLoan
-
+                                T1.Fullname,
+                                T1.Name AS Email,
+                                T1.ReferenceSource,
+                                T1.ExpirianRating,
+                                T1.UnderwriterDecisionDate,
+                                T1.ManagerApprovedSum,
+                                T1.InterestRate,
+                                T1.RepaymentPeriod,
+                                T1.LoansType,
+                                CASE
+                                WHEN T3.LoanIssued>0 THEN 'Yes'
+                                ELSE 'No'
+                                END AS HadLoan,
+                                T1.BrokerID
+ 
 FROM #temp3 T1
 LEFT JOIN #temp4 T2 ON T1.CashReqId = T2.RequestCashId
 LEFT JOIN #temp5 T3 ON T3.CustomerId = T1.IdCustomer
 WHERE T2.LoanAmount IS NULL
-	  AND T1.UnderwriterDecisionDate BETWEEN @DateStart AND @DateEnd
+                  AND T1.UnderwriterDecisionDate BETWEEN @DateStart AND @DateEnd
 ORDER BY 7
-
+ 
 SET NOCOUNT OFF
 END
+ 
 GO
