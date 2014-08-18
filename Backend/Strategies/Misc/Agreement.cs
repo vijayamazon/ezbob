902690ca4ac261eb825ccef900b97ace97202e71
Alex Boursouk <alexbo@ezbob.com>
2014-08-18 12:49:34 +0300

    More logging for: Save Agreement strategy: Error saving agreement

--- Backend/Strategies/Misc/Agreement.cs
+++ Backend/Strategies/Misc/Agreement.cs
@@ -1,5 +1,4 @@
-﻿namespace EzBob.Backend.Strategies.Misc
-{
+﻿namespace EzBob.Backend.Strategies.Misc {
 	using System;
 	using System.IO;
 	using Aspose.Words;
@@ -9,19 +8,18 @@
 	using Ezbob.Logger;
 	using Nustache.Core;
 
-	public class SaveAgreement : AStrategy
-	{
-		private readonly string _name;
-		private readonly TemplateModel _template;
-		private readonly string _path1;
-		private readonly string _path2;
-		private int _customerId;
-		private AgreementModel _model;
-		private string _refNumber;
-
-		public SaveAgreement(int customerId, AgreementModel model, string refNumber, string name, TemplateModel template, string path1, string path2, AConnection oDb, ASafeLog oLog)
-			: base(oDb, oLog)
-		{
+	public class SaveAgreement : AStrategy {
+		public SaveAgreement(
+			int customerId,
+			AgreementModel model,
+			string refNumber,
+			string name,
+			TemplateModel template,
+			string path1,
+			string path2,
+			AConnection oDb,
+			ASafeLog oLog
+		) : base(oDb, oLog) {
 			_name = name;
 			_template = template;
 			_path1 = path1;
@@ -29,53 +27,74 @@
 			_customerId = customerId;
 			_model = model;
 			_refNumber = refNumber;
-		}
+		} // constructor
 
-		public override string Name
-		{
+		public override string Name {
 			get { return "Save Agreement"; }
-		}
+		} // Name
 
-		public override void Execute()
-		{
-			try
-			{
+		public override void Execute() {
+			Log.Debug("Saving agreement for customer {0}...", _customerId);
+
+			try {
 				var html = Render.StringToString(_template.Template, _model);
+
+				Log.Debug("Saving agreement for customer {0}: rendering done.", _customerId);
+
 				var pdf = ConvertFormat(html, SaveFormat.Pdf);
+
 				Save(pdf, _path1);
+
 				Save(pdf, _path2);
 			}
-			catch (Exception exception)
-			{
-				Log.Error("Error saving agreement", exception.Message);
-			}
-		}
+			catch (Exception exception) {
+				Log.Error(exception, "Error saving agreement.");
+			} // try
 
-		private void Save(byte[] pdf, string pathStr)
-		{
-			var path = Path.GetDirectoryName(pathStr);
-			if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+			Log.Debug("Saving agreement for customer {0} complete.", _customerId);
+		} // Execute
+
+		private void Save(byte[] pdf, string pathStr) {
+			Log.Debug("Saving agreement for customer {0}: writing to {1}...", _customerId, pathStr);
+
+			string path = Path.GetDirectoryName(pathStr);
+
+			if (!Directory.Exists(path))
+				Directory.CreateDirectory(path);
+
 			File.WriteAllBytes(pathStr, pdf);
-		}
 
-		private byte[] ConvertFormat(string stringForConvert, SaveFormat format, string typeInputString = "html")
-		{
+			Log.Debug("Saving agreement for customer {0}: written to {1}.", _customerId, pathStr);
+		} // Save
+
+		private byte[] ConvertFormat(string stringForConvert, SaveFormat format, string typeInputString = "html") {
+			Log.Debug("Saving agreement for customer {0}: converting format {1} -> {2}...", _customerId, typeInputString, format.ToString());
+
 			var doc = new Document();
 			var docBuilder = new DocumentBuilder(doc);
-			if (typeInputString == "html")
-			{
-				docBuilder.InsertHtml(stringForConvert);
-			}
-			else
-			{
-				docBuilder.Write(stringForConvert);
-			}
 
-			using (var streamForDoc = new MemoryStream())
-			{
+			if (typeInputString == "html")
+				docBuilder.InsertHtml(stringForConvert);
+			else
+				docBuilder.Write(stringForConvert);
+
+			byte[] oResult;
+
+			using (var streamForDoc = new MemoryStream()) {
 				doc.Save(streamForDoc, format);
-				return streamForDoc.ToArray();
-			}
-		}
-	}
-}
+				oResult = streamForDoc.ToArray();
+			} // using
+
+			Log.Debug("Saving agreement for customer {0}: converted format {1} -> {2}.", _customerId, typeInputString, format.ToString());
+			return oResult;
+		} // ConvertFormat
+
+		private readonly string _name;
+		private readonly TemplateModel _template;
+		private readonly string _path1;
+		private readonly string _path2;
+		private int _customerId;
+		private AgreementModel _model;
+		private string _refNumber;
+	} // class SaveAgreement
+} // namespace
