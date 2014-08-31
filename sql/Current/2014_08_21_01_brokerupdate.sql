bc32abd1f470ed62b30779440056a7ded36316e7
Yuly Saar <yulys@ezbob.com>
2014-08-31 12:11:40 +0300

    Fixed broker script

--- sql/Current/2014_08_21_01_brokerupdate.sql
+++ sql/Current/2014_08_21_01_brokerupdate.sql
@@ -5,88 +5,86 @@ DECLARE @query NVARCHAR(MAX)
 
 IF EXISTS (SELECT * FROM syscolumns WHERE name = 'UserID' AND id = OBJECT_ID('Broker'))
 BEGIN
-	SET @query = 'BEGIN TRANSACTION' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Broker DROP CONSTRAINT FK_Broker_User'
+	EXECUTE(@query)
+	
+	SET @query = 'ALTER TABLE BrokerInstantOfferRequest DROP CONSTRAINT FK_BrokerInstantOfferRequest_BrokerId'
+	EXECUTE(@query)
 
-'ALTER TABLE Broker DROP CONSTRAINT FK_Broker_User' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Broker ADD OldBrokerId INT NULL' 
+	EXECUTE(@query)
 
-'ALTER TABLE Broker ADD OldBrokerId INT NULL' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'UPDATE Broker SET OldBrokerId = BrokerID' 
+	EXECUTE(@query)
 
-'UPDATE Broker SET OldBrokerId = BrokerID' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Customer DROP CONSTRAINT FK_CustomerBroker' 
+	EXECUTE(@query)
 
-'ALTER TABLE Customer DROP CONSTRAINT FK_CustomerBroker' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = '
+		UPDATE Customer SET 
+			BrokerID = Broker.UserID
+		FROM 
+			Customer INNER JOIN Broker ON Broker.BrokerID = Customer.BrokerID'
+	EXECUTE(@query)
 
-'UPDATE Customer SET' + CHAR(13) + CHAR(10) +
-	'BrokerID = Broker.UserID' + CHAR(13) + CHAR(10) +
-'FROM' + CHAR(13) + CHAR(10) +
-	'Customer' + CHAR(13) + CHAR(10) +
-	'INNER JOIN Broker ON Broker.BrokerID = Customer.BrokerID' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE BrokerLeads DROP CONSTRAINT FK_BrokerLead_Broker'
+	EXECUTE(@query)
 
-'ALTER TABLE BrokerLeads DROP CONSTRAINT FK_BrokerLead_Broker' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = '
+		UPDATE BrokerLeads SET
+			BrokerID = Broker.UserID
+		FROM
+			BrokerLeads INNER JOIN Broker ON BrokerLeads.BrokerID = Broker.BrokerID'
+	EXECUTE(@query)
 
-'UPDATE BrokerLeads SET' + CHAR(13) + CHAR(10) +
-	'BrokerID = Broker.UserID' + CHAR(13) + CHAR(10) +
-'FROM' + CHAR(13) + CHAR(10) +
-	'BrokerLeads' + CHAR(13) + CHAR(10) +
-	'INNER JOIN Broker ON BrokerLeads.BrokerID = Broker.BrokerID' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE CustomerBrokerHistory DROP CONSTRAINT FK_CustomerBrokerHistory_ToBroker'
+	EXECUTE(@query)
 
-'ALTER TABLE CustomerBrokerHistory DROP CONSTRAINT FK_CustomerBrokerHistory_ToBroker' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE CustomerBrokerHistory DROP CONSTRAINT FK_CustomerBrokerHistory_FromBroker'
+	EXECUTE(@query)
 
-'ALTER TABLE CustomerBrokerHistory DROP CONSTRAINT FK_CustomerBrokerHistory_FromBroker' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = '
+		UPDATE CustomerBrokerHistory SET
+			FromBrokerID = Broker.UserID
+		FROM
+			CustomerBrokerHistory INNER JOIN Broker ON CustomerBrokerHistory.FromBrokerID = Broker.BrokerID'
+	EXECUTE(@query)
 
-'UPDATE CustomerBrokerHistory SET' + CHAR(13) + CHAR(10) +
-	'FromBrokerID = Broker.UserID' + CHAR(13) + CHAR(10) +
-'FROM' + CHAR(13) + CHAR(10) +
-	'CustomerBrokerHistory' + CHAR(13) + CHAR(10) +
-	'INNER JOIN Broker ON CustomerBrokerHistory.FromBrokerID = Broker.BrokerID' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = '
+		UPDATE CustomerBrokerHistory SET
+			ToBrokerID = Broker.UserID
+		FROM
+			CustomerBrokerHistory INNER JOIN Broker ON CustomerBrokerHistory.ToBrokerID = Broker.BrokerID'
+	EXECUTE(@query)
 
-'UPDATE CustomerBrokerHistory SET' + CHAR(13) + CHAR(10) +
-	'ToBrokerID = Broker.UserID' + CHAR(13) + CHAR(10) +
-'FROM' + CHAR(13) + CHAR(10) +
-	'CustomerBrokerHistory' + CHAR(13) + CHAR(10) +
-	'INNER JOIN Broker ON CustomerBrokerHistory.ToBrokerID = Broker.BrokerID' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Broker DROP CONSTRAINT PK_Broker'
+	EXECUTE(@query)
 
-'ALTER TABLE Broker DROP CONSTRAINT PK_Broker' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Broker DROP COLUMN BrokerId'
+	EXECUTE(@query)
 
-'ALTER TABLE Broker DROP COLUMN BrokerId' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'EXEC sp_RENAME ''Broker.UserID'', ''BrokerID'', ''COLUMN'''
+	EXECUTE(@query)
 
-'EXEC sp_RENAME ''Broker.UserID'', ''BrokerID'', ''COLUMN''' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Broker ADD CONSTRAINT PK_Broker PRIMARY KEY (BrokerID)'
+	EXECUTE(@query)
+	
+	SET @query = 'ALTER TABLE BrokerInstantOfferRequest ADD CONSTRAINT FK_BrokerInstantOfferRequest_BrokerId FOREIGN KEY (BrokerId) REFERENCES Broker(BrokerID)'
+	EXECUTE(@query)
 
-'ALTER TABLE Broker ADD CONSTRAINT PK_Broker PRIMARY KEY (BrokerID)' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE Customer ADD CONSTRAINT FK_Customer_Broker FOREIGN KEY (BrokerId) REFERENCES Broker(BrokerID)'
+	EXECUTE(@query)
 
-'ALTER TABLE Customer ADD CONSTRAINT FK_Customer_Broker FOREIGN KEY (BrokerId) REFERENCES Broker(BrokerID)' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE BrokerLeads ADD CONSTRAINT FK_BrokerLeads_Broker FOREIGN KEY (BrokerId) REFERENCES Broker(BrokerID)'
+	EXECUTE(@query)
 
-'ALTER TABLE BrokerLeads ADD CONSTRAINT FK_BrokerLeads_Broker FOREIGN KEY (BrokerId) REFERENCES Broker(BrokerID)' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
+	SET @query = 'ALTER TABLE CustomerBrokerHistory ADD CONSTRAINT FK_CustomerBrokerHistory_ToBroker FOREIGN KEY (ToBrokerId) REFERENCES Broker(BrokerID)'
+	EXECUTE(@query)
 
-'ALTER TABLE CustomerBrokerHistory ADD CONSTRAINT FK_CustomerBrokerHistory_ToBroker FOREIGN KEY (ToBrokerId) REFERENCES Broker(BrokerID)' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
-
-'ALTER TABLE CustomerBrokerHistory ADD CONSTRAINT FK_CustomerBrokerHistory_FromBroker FOREIGN KEY (FromBrokerId) REFERENCES Broker(BrokerID)' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
-
-'ALTER TABLE Broker ADD CONSTRAINT FK_Broker_User FOREIGN KEY (BrokerID) REFERENCES Security_User (UserId)' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10) +
-
-'COMMIT TRANSACTION' + CHAR(13) + CHAR(10) +
-'GO' + CHAR(13) + CHAR(10)
-
+	SET @query = 'ALTER TABLE CustomerBrokerHistory ADD CONSTRAINT FK_CustomerBrokerHistory_FromBroker FOREIGN KEY (FromBrokerId) REFERENCES Broker(BrokerID)'
+	EXECUTE(@query)
+	
+	SET @query = 'ALTER TABLE Broker ADD CONSTRAINT FK_Broker_User FOREIGN KEY (BrokerID) REFERENCES Security_User (UserId)'
 	EXECUTE(@query)
 END
 GO
