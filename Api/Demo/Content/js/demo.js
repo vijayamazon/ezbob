8cc09d85b4e30812b7560bed024ee9634cf6985c
Alex Boursouk <alexbo@ezbob.com>
2014-09-04 18:27:45 +0300

    EZ-2513 design api

--- Api/Demo/Content/js/demo.js
+++ Api/Demo/Content/js/demo.js
@@ -1,39 +1,127 @@
 ﻿$(document).ready(function() {
 	var sBaseUrl = 'http://localhost:57973/api/v1';
 
-	var sAnoUrl = sBaseUrl + '/values';
+	var sApiUrl;
 
-	var oAnonymousBody = $('#anonymous-api-body');
+	var oHeadersFunc;
+
+	var sLoginUrl = sBaseUrl + '/login';
+
+	var oTabs = $('#tabs').tabs();
 	
-	oAnonymousBody.tabs();
-	
-	$('.do-read-all', oAnonymousBody).click(anonymousReadAll);
-	$('.do-read-one', oAnonymousBody).click(anonymousReadOne);
-	$('.do-create', oAnonymousBody).click(anonymousCreate);
-	$('.do-update', oAnonymousBody).click(anonymousUpdate);
-	$('.do-delete', oAnonymousBody).click(anonymousDelete);
+	$('.set-anonymous-api').click(setAnonymousApi);
+	$('.set-named-api').click(setNamedApi);
+
+	$('.do-login').click(login);
+	$('.do-read-all').click(readAll);
+	$('.do-read-one').click(readOne);
+	$('.do-create').click(create);
+	$('.do-update').click(update);
+	$('.do-delete').click(deleteOne);
 
 	var oLastStatus = $('.last-action-status');
 
-	function anonymousReadAll() {
-		var sFuncName = 'anonymousReadAll';
+	function getAnonymousHeaders() {
+		return {
+			'app-key': $('.app-key').val(),
+		};
+	} // getAnonymousHeaders
+
+	function getNamedHeaders() {
+		return {
+			'session-token': $('.session-token').val(),
+		};
+	} // getNamedHeaders
+
+	function setAnonymousApi() {
+		sApiUrl = sBaseUrl + '/values';
+
+		$('.named-related').hide();
+
+		oHeadersFunc = getAnonymousHeaders;
+
+		if (oTabs.tabs('option', 'active') === 0)
+			oTabs.tabs('option', 'active', 1);
+
+		oTabs.tabs('disable', 0);
+
+		console.log('Anonymous API mode selected.');
+	} // setAnonymousApi
+
+	function setNamedApi() {
+		sApiUrl = sBaseUrl + '/svals';
+
+		$('.named-related').show();
+
+		oHeadersFunc = getNamedHeaders;
+
+		oTabs.tabs('enable', 0);
+
+		console.log('Named API mode selected.');
+	} // setNamedApi
+
+	function login() {
+		var sFuncName = 'login';
+
+		console.log(sFuncName, 'started...');
+
+		var oContainer = $('#login-form');
+
+		var oInputData = {
+			UserName: $('.user-name', oContainer).val(),
+			Password: $('.password', oContainer).val(), 
+		};
+
+		console.log(sFuncName + ':', 'sending', oInputData);
+
+		$.ajax(sLoginUrl, {
+			type: 'POST',
+
+			contentType: 'application/json',
+
+			data: JSON.stringify(oInputData),
+
+			headers: getAnonymousHeaders(),
+
+			success: function(oData, sTextStatus, jqXhr) {
+				console.log(sFuncName + ':', 'success with status "', jqXhr.statusText, '" and code', jqXhr.status);
+
+				oLastStatus.text(sFuncName + ': success with status code ' + jqXhr.status + ' and status text: ' + jqXhr.statusText);
+
+				$('.user-name, .password', oContainer).val('');
+
+				$('.logged-in-user-name').text(oInputData.UserName);
+				$('.session-token').val(oData);
+			}, // on success
+
+			error: function(jqXhr, sTextStatus, sErrorThrown) {
+				console.log(sFuncName + ':', 'error with status "', jqXhr.statusText, '" and code', jqXhr.status);
+				console.log(sFuncName + ':', 'error thrown:', sErrorThrown);
+
+				oLastStatus.text(sFuncName + ': error status code: ' + jqXhr.status + ', status text: "' + jqXhr.statusText + '", error: ' + sErrorThrown);
+			}, // on error
+		}); // ajax call
+
+		console.log(sFuncName, 'complete.');
+	} // function login
+
+	function readAll() {
+		var sFuncName = 'readAll';
 
 		oLastStatus.text('Processing...');
 
 		console.log(sFuncName, 'started...');
 
-		var oContainer = $('#anonymous-read-all');
+		var oContainer = $('#read-all');
 
 		var oResults = $('.results', oContainer).empty();
 
-		$.ajax(sAnoUrl, {
+		$.ajax(sApiUrl, {
 			type: 'GET',
 
 			contentType: 'application/json',
 
-			headers: {
-				'app-key': $('.app-key').val(),
-			}, // headers
+			headers: oHeadersFunc(),
 
 			success: function(oData, sTextStatus, jqXhr) {
 				console.log(sFuncName + ':', 'success with status "', jqXhr.statusText, '" and code', jqXhr.status);
@@ -43,7 +131,7 @@
 
 				var oTemplate = $('.result-template', oContainer);
 
-				if (oData.length) {
+				if (oData && oData.length) {
 					for (var i = 0; i < oData.length; i++) {
 						var oItem = oData[i];
 
@@ -65,16 +153,16 @@
 		}); // ajax call
 
 		console.log(sFuncName, 'complete.');
-	} // function anonymousReadAll
+	} // function readAll
 
-	function anonymousReadOne() {
-		var sFuncName = 'anonymousReadOne';
+	function readOne() {
+		var sFuncName = 'readOne';
 
 		oLastStatus.text('Processing...');
 
 		console.log(sFuncName, 'started...');
 
-		var oContainer = $('#anonymous-read-one');
+		var oContainer = $('#read-one');
 
 		var sValueID = parseInt($('.value-id', oContainer).val());
 
@@ -87,14 +175,12 @@
 
 		$('span', oResults).empty();
 
-		$.ajax(sAnoUrl + '/' + sValueID, {
+		$.ajax(sApiUrl + '/' + sValueID, {
 			type: 'GET',
 
 			contentType: 'application/json',
 
-			headers: {
-				'app-key': $('.app-key').val(),
-			}, // headers
+			headers: oHeadersFunc(),
 
 			success: function(oData, sTextStatus, jqXhr) {
 				console.log(sFuncName + ':', 'success with status "', jqXhr.statusText, '" and code', jqXhr.status);
@@ -116,14 +202,14 @@
 		}); // ajax call
 
 		console.log(sFuncName, 'complete.');
-	} // function anonymousReadOne
+	} // function readOne
 
-	function anonymousCreate() {
-		var sFuncName = 'anonymousCreate';
+	function create() {
+		var sFuncName = 'create';
 
 		console.log(sFuncName, 'started...');
 
-		var oContainer = $('#anonymous-create');
+		var oContainer = $('#create');
 
 		var oInputData = {
 			Title: $('.title', oContainer).val(),
@@ -132,16 +218,14 @@
 
 		console.log(sFuncName + ':', 'sending', oInputData);
 
-		$.ajax(sAnoUrl, {
+		$.ajax(sApiUrl, {
 			type: 'POST',
 
 			contentType: 'application/json',
 
 			data: JSON.stringify(oInputData),
 
-			headers: {
-				'app-key': $('.app-key').val(),
-			}, // headers
+			headers: oHeadersFunc(),
 
 			success: function(oData, sTextStatus, jqXhr) {
 				console.log(sFuncName + ':', 'success with status "', jqXhr.statusText, '" and code', jqXhr.status);
@@ -160,16 +244,16 @@
 		}); // ajax call
 
 		console.log(sFuncName, 'complete.');
-	} // function anonymousCreate
+	} // function create
 
-	function anonymousUpdate() {
-		var sFuncName = 'anonymousUpdate';
+	function update() {
+		var sFuncName = 'update';
 
 		oLastStatus.text('Processing...');
 
 		console.log(sFuncName, 'started...');
 
-		var oContainer = $('#anonymous-update');
+		var oContainer = $('#update');
 
 		var sValueID = parseInt($('.value-id', oContainer).val());
 
@@ -185,16 +269,14 @@
 
 		console.log(sFuncName + ':', 'sending', oInputData);
 
-		$.ajax(sAnoUrl + '/' + sValueID, {
+		$.ajax(sApiUrl + '/' + sValueID, {
 			type: 'PUT',
 
 			contentType: 'application/json',
 
 			data: JSON.stringify(oInputData),
 
-			headers: {
-				'app-key': $('.app-key').val(),
-			}, // headers
+			headers: oHeadersFunc(),
 
 			success: function(oData, sTextStatus, jqXhr) {
 				console.log(sFuncName + ':', 'success with status "', jqXhr.statusText, '" and code', jqXhr.status);
@@ -214,16 +296,16 @@
 		}); // ajax call
 
 		console.log(sFuncName, 'complete.');
-	} // function anonymousUpdate
+	} // function update
 
-	function anonymousDelete() {
-		var sFuncName = 'anonymousDelete';
+	function deleteOne() {
+		var sFuncName = 'deleteOne';
 
 		oLastStatus.text('Processing...');
 
 		console.log(sFuncName, 'started...');
 
-		var oContainer = $('#anonymous-delete');
+		var oContainer = $('#delete');
 
 		var sValueID = parseInt($('.value-id', oContainer).val());
 
@@ -232,14 +314,12 @@
 			return;
 		} // if
 
-		$.ajax(sAnoUrl + '/' + sValueID, {
+		$.ajax(sApiUrl + '/' + sValueID, {
 			type: 'DELETE',
 
 			contentType: 'application/json',
 
-			headers: {
-				'app-key': $('.app-key').val(),
-			}, // headers
+			headers: oHeadersFunc(),
 
 			success: function(oData, sTextStatus, jqXhr) {
 				console.log(sFuncName + ':', 'success with status "', jqXhr.statusText, '" and code', jqXhr.status);
@@ -259,5 +339,7 @@
 		}); // ajax call
 
 		console.log(sFuncName, 'complete.');
-	} // function anonymousDelete
+	} // function deleteOne
+
+	setAnonymousApi();
 }); // on document ready
