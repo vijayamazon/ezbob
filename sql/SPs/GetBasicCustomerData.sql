e2560adbc4399bc9d850cee174b4c817de667225
stas_dulman <stasd@ezbob.com>
2014-09-09 11:53:35 +0300

    EZ-2552 EZ-2573 white label mail logic (send to broker only)

--- sql/SPs/GetBasicCustomerData.sql
+++ sql/SPs/GetBasicCustomerData.sql
@@ -29,10 +29,12 @@ BEGIN
 		c.IsTest,
 		a.Postcode,
 		a.Town AS City,
-		c.Id AS UserID
+		c.Id AS UserID,
+		CASE WHEN c.WhiteLabelId IS NOT NULL THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END AS IsWhiteLabel
 	FROM
 		Customer c LEFT JOIN CustomerAddress a ON c.Id=a.CustomerId AND a.addressType=1
 	WHERE
 		c.Id = @CustomerId
 END
+
 GO
