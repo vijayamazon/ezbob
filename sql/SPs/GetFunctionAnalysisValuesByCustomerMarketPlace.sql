6722bbb32d694341f0fef5d4b13a7f7b39e34419
stas_dulman <stasd@ezbob.com>
2014-09-15 17:19:48 +0300

    EZ-2529 timeouts in DB when updating amazon

--- sql/SPs/GetFunctionAnalysisValuesByCustomerMarketPlace.sql
+++ sql/SPs/GetFunctionAnalysisValuesByCustomerMarketPlace.sql
@@ -14,39 +14,18 @@ AS
 BEGIN
 	SET NOCOUNT ON
 
-	------------------------------------------------------------------------------
-
-	SELECT
-		Id,
-		UpdatingStart
-	INTO
-		#h
-	FROM
-		MP_CustomerMarketplaceUpdatinghistory
-	WHERE
-		UpdatingStart IS NOT NULL
-		AND
-		UpdatingEnd IS NOT NULL
-		AND
-		CustomerMarketPlaceId = @MpID
-
-	------------------------------------------------------------------------------
-
 	SELECT
 		f.InternalId AS fid, -- function id
 		p.InternalId AS fpid, -- period id
 		v.Value,
-		h.UpdatingStart
+		v.Updated
 	FROM
 		MP_AnalyisisFunctionValues v
-		INNER JOIN #h h ON v.CustomerMarketplaceUpdatingHistoryRecordId = h.id
 		INNER JOIN MP_AnalyisisFunction f ON v.AnalyisisFunctionId = f.id
 		INNER JOIN MP_AnalysisFunctionTimeperiod p ON v.AnalysisFunctionTimeperiodId = p.id
 	WHERE
 		v.CustomerMarketPlaceId = @MpID
-
-	------------------------------------------------------------------------------
-
-	DROP TABLE #h
 END
+
 GO
+
