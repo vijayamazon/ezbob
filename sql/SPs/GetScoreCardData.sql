e43130146f68c98957c250f602ef7d212cd425fa
Alex Boursouk <alexbo@ezbob.com>
2014-09-18 20:04:09 +0300

    EZ-2603, EZ-2607 DB warning when running GetScoreCardData 710, 'Sep 16 2014'

--- sql/SPs/GetScoreCardData.sql
+++ sql/SPs/GetScoreCardData.sql
@@ -1,150 +1,152 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetScoreCardData]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[GetScoreCardData]
+IF OBJECT_ID('GetScoreCardData') IS NULL
+	EXECUTE ('CREATE PROCEDURE GetScoreCardData AS SELECT 1')
 GO
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[GetScoreCardData] 
-	(@CustomerId INT, @Today DATE)
+
+ALTER PROCEDURE GetScoreCardData
+@CustomerId INT,
+@Today DATE
 AS
 BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @Feedback INT
+
+	------------------------------------------------------------------------------
+
+	CREATE TABLE #Feedback (Feedback INT NULL)
+
+	------------------------------------------------------------------------------
+	
+	SELECT
+		cmp.Id AS CustomerMarketPlaceId,
+		MAX(fb.Created) AS MaxCreated
+	INTO
+		#MaxAmazonCreated
+	FROM
+		MP_AmazonFeedback fb
+		INNER JOIN MP_CustomerMarketPlace cmp ON fb.CustomerMarketPlaceId = cmp.Id
+	WHERE
+		cmp.CustomerId = @CustomerId
+	GROUP BY
+		cmp.Id
+
+	------------------------------------------------------------------------------
+
+	INSERT INTO #Feedback (Feedback)
+	SELECT
+		fbi.Positive -- Positive eBay feedback (lifetime)
+	FROM
+		MP_AmazonFeedback fb
+		INNER JOIN #MaxAmazonCreated mc
+			ON fb.CustomerMarketPlaceId = mc.CustomerMarketPlaceId
+			AND fb.Created = mc.MaxCreated
+		INNER JOIN MP_AmazonFeedbackItem fbi
+			ON fbi.AmazonFeedbackId = fb.Id
+			AND fbi.TimePeriodId = 5
+
+	------------------------------------------------------------------------------
+
+	SELECT
+		cmp.Id AS CustomerMarketPlaceId,
+		MAX(fb.Created) AS MaxCreated
+	INTO
+		#MaxEbayCreated
+	FROM
+		MP_EbayFeedback fb
+		INNER JOIN MP_CustomerMarketPlace cmp ON fb.CustomerMarketPlaceId = cmp.Id
+	WHERE
+		cmp.CustomerId = @CustomerId
+	GROUP BY
+		cmp.Id
+
+	------------------------------------------------------------------------------
+
+	INSERT INTO #Feedback (Feedback)
+	SELECT
+		fbi.Positive -- Positive eBay feedback (lifetime)
+	FROM
+		MP_EbayFeedback fb
+		INNER JOIN #MaxEbayCreated mc
+			ON fb.CustomerMarketPlaceId = mc.CustomerMarketPlaceId
+			AND fb.Created = mc.MaxCreated
+		INNER JOIN MP_EbayFeedbackItem fbi
+			ON fbi.EbayFeedbackId = fb.Id
+			AND fbi.TimePeriodId = 6
+	
+	------------------------------------------------------------------------------
+
+	SELECT @Feedback = SUM(Feedback) FROM #Feedback
+
+	------------------------------------------------------------------------------
+
 	SELECT
 		c.MaritalStatus AS MaritalStatus, 
-		FeedBack.MaxFeedback, 
+		@Feedback AS MaxFeedback, 
 		--Market places Num
 		(
-		SELECT 
-			count(cmp.Id) 
-		FROM 
-			MP_CustomerMarketPlace cmp 
+			SELECT COUNT(cmp.Id)
+			FROM MP_CustomerMarketPlace cmp 
 			LEFT JOIN MP_MarketplaceType mpt ON mpt.Id = cmp.MarketPlaceId
-		WHERE 
-			cmp.customerId = c.Id AND 
-			mpt.InternalId IN ('A7120CB7-4C93-459B-9901-0E95E7281B59', 'A4920125-411F-4BB9-A52D-27E8A00D0A3B', '3FA5E327-FCFD-483B-BA5A-DC1815747A28')
+			WHERE cmp.customerId = @CustomerId
+			AND mpt.InternalId IN (
+				'A7120CB7-4C93-459B-9901-0E95E7281B59',
+				'A4920125-411F-4BB9-A52D-27E8A00D0A3B',
+				'3FA5E327-FCFD-483B-BA5A-DC1815747A28'
+			)
 		) AS MPsNumber,
 		-- EZBOB seniority
 		ISNULL(DateDiff(DAY, c.GreetingMailSentDate, @Today), 0) AS EZBOBSeniority,
 		-- EZBOB #of previous ON time loans
 		(
-		SELECT 
-			count(l.id) 
-		FROM 
-			Loan l 
-			LEFT JOIN LoanSchedule ls ON ls.LoanId = l.Id
-		WHERE 
-			l.CustomerId = c.Id AND 
-			(
-				ls.Status = 'PaidOnTime' OR 
-				ls.Status = 'PaidEarly'
-			) AND 
-			ls.Id = 
-				(
-				SELECT 
-					MAX(ls.id) 
-				FROM 
-					Customer c
-					LEFT JOIN LoanSchedule ls ON ls.LoanId = l.Id
-					LEFT JOIN Loan l ON c.Id = l.CustomerId
-				WHERE 
-					l.CustomerId = c.Id
-				) 
+			SELECT COUNT(l.id)
+			FROM Loan l
+			INNER JOIN LoanSchedule ls ON ls.LoanId = l.Id
+			WHERE l.CustomerId = @CustomerId
+			AND ls.Status IN ('PaidOnTime', 'PaidEarly')
+			AND ls.Id = (
+				SELECT MAX(ls.id)
+				FROM LoanSchedule ls
+				INNER JOIN Loan l ON ls.LoanId = l.Id
+				WHERE l.CustomerId = @CustomerId
+			)
 		) AS OnTimeLoans,
 		-- EZBOB #of previous late payments
 		(
-		SELECT 
-			count(lc.id) 
-		FROM 
-			LoanCharges lc 
-			INNER JOIN ConfigurationVariables cv ON cv.Id = lc.ConfigurationVariableId 
+			SELECT COUNT(lc.id)
+			FROM LoanCharges lc 
+			INNER JOIN ConfigurationVariables cv ON cv.Id = lc.ConfigurationVariableId
 			INNER JOIN Loan l ON l.Id = lc.LoanId
-		WHERE 
-			cv.Name = 'LatePaymentCharge' AND
-			l.CustomerId = @CustomerId
+			WHERE cv.Name = 'LatePaymentCharge'
+			AND l.CustomerId = @CustomerId
 		) AS LatePayments,
 		-- EZBOB #of previous early payments
 		(
-		SELECT 
-			count(ls.id) 
-		FROM 
-			LoanSchedule ls 
+			SELECT COUNT(ls.id) 
+			FROM LoanSchedule ls 
 			INNER JOIN Loan l ON l.Id = ls.LoanId
-		WHERE 
-			ls.Status = 'PaidEarly' AND
-			c.Id = l.CustomerId
+			WHERE ls.Status = 'PaidEarly'
+			AND l.CustomerId = @CustomerId
 		) AS EarlyPayments,
 		-- EZBOB #of previous early payments 
 		(
-		SELECT 
-			Min(ls.Date) 
-		FROM 
-			LoanSchedule ls
+			SELECT MIN(ls.Date) 
+			FROM LoanSchedule ls
 			INNER JOIN Loan l ON l.Id = ls.LoanId
-		WHERE c.Id = l.CustomerId
+			WHERE l.CustomerId = @CustomerId
 		) AS FirstRepaymentDate
-
-	FROM Customer c
-	LEFT JOIN
-
-	-- Positive feedback counter (lifetime)
-	(
-		SELECT 
-			FeedbackTable.CustomerId AS CustomerId,
-			sum(FeedbackTable.Feedback) AS MaxFeedback
-		FROM
-		(
-			SELECT 
-				c.Id AS CustomerId,
-				cmp.Id AS umi, 
-				fbi.Positive AS Feedback
-			-- Positive eBay feedback (lifetime) 
-			FROM 
-				MP_CustomerMarketPlace cmp
-				LEFT JOIN Customer c ON cmp.CustomerId = c.Id
-				LEFT JOIN MP_AmazonFeedback fb ON 
-					fb.CustomerMarketPlaceId=cmp.Id AND
-					fb.Created = 
-					(
-						SELECT 
-							MAX(fb1.Created) 
-						FROM 
-							MP_AmazonFeedback fb1
-						WHERE 
-							fb1.CustomerMarketPlaceId = fb.CustomerMarketPlaceId
-					)  
-				LEFT JOIN MP_AmazonFeedbackItem fbi ON 
-					fbi.AmazonFeedbackId = fb.Id AND 
-					fbi.TimePeriodId = 5
-
-			UNION 
-			
-			SELECT  
-				c.Id AS CustomerId,
-				cmp.Id AS umi,
-				fbi2.Positive AS Feedback
-			-- Positive Amazon feedback (lifetime)  
-			FROM 
-				MP_CustomerMarketPlace cmp
-				LEFT JOIN Customer c ON cmp.CustomerId = c.Id
-				LEFT JOIN MP_EbayFeedback fb2 ON 
-					fb2.CustomerMarketPlaceId=cmp.Id AND
-					fb2.Created = 
-					(
-						SELECT 
-							MAX(fb1.Created) 
-						FROM 
-							MP_EbayFeedback fb1
-						WHERE 
-						fb1.CustomerMarketPlaceId = fb2.CustomerMarketPlaceId
-					) 
-				LEFT JOIN MP_EbayFeedbackItem fbi2 ON 
-					fbi2.EbayFeedbackId = fb2.Id AND
-					fbi2.TimePeriodId = 6
-		) AS FeedbackTable
-		GROUP BY 
-			FeedbackTable.CustomerId
-	) AS FeedBack ON c.Id = Feedback.CustomerId
+	FROM
+		Customer c
 	WHERE 
 		c.Id = @CustomerId
+
+	DROP TABLE #MaxEbayCreated
+	DROP TABLE #MaxAmazonCreated
+	DROP TABLE #Feedback
 END
 GO
