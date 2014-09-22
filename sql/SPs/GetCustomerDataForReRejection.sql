f0413dc231aa2065a9bea470197158923ab9b664
Alex Boursouk <alexbo@ezbob.com>
2014-09-22 19:02:09 +0300

    EZ-2622 DB warning when executing specific SPs

--- sql/SPs/GetCustomerDataForReRejection.sql
+++ sql/SPs/GetCustomerDataForReRejection.sql
@@ -1,78 +1,101 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetCustomerDataForReRejection]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[GetCustomerDataForReRejection]
+IF OBJECT_ID('GetCustomerDataForReRejection') IS NULL
+	EXECUTE('CREATE PROCEDURE GetCustomerDataForReRejection AS SELECT 1')
 GO
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[GetCustomerDataForReRejection] 
-	(@CustomerId INT, @Now DATETIME)
+
+ALTER PROCEDURE GetCustomerDataForReRejection
+@CustomerId INT,
+@Now DATETIME
 AS
 BEGIN
+	SET NOCOUNT ON;
+
 	DECLARE @ManualDecisionDate DATE
 
-	SELECT 
-		@ManualDecisionDate = max(cr.UnderwriterDecisionDate) 
-	FROM 
-		CashRequests cr 
-	WHERE 
+	------------------------------------------------------------------------------
+
+	SELECT
+		@ManualDecisionDate = MAX(cr.UnderwriterDecisionDate)
+	FROM
+		CashRequests cr
+	WHERE
 		cr.IdCustomer = @CustomerId AND
-		cr.IdUnderwriter IS NOT NULL AND 
+		cr.IdUnderwriter IS NOT NULL AND
 		cr.UnderwriterDecision = 'Rejected'
 
-	SELECT 
-		(
-		SELECT 
-			COUNT(cmp.Id)
-		FROM 
-			CashRequests cr 
-			LEFT JOIN MP_CustomerMarketPlace cmp ON cmp.CustomerId = cr.IdCustomer
-			LEFT JOIN Loan l ON l.CustomerId = cmp.CustomerId 
-		WHERE 
-			cr.IdCustomer = @CustomerId AND 
-			cr.IdUnderwriter IS NOT NULL AND 
-			cr.UnderwriterDecision = 'Rejected' AND 
-			@ManualDecisionDate >= (SELECT cast(max(created) AS DATE) FROM MP_CustomerMarketPlace WHERE CustomerId = @CustomerId) AND 
-			DATEADD(DD, 30, @ManualDecisionDate) >= @Now AND l.Id IS NULL AND 
-			@ManualDecisionDate = (SELECT cast(max(cr.UnderwriterDecisionDate) AS DATE) FROM CashRequests cr WHERE cr.IdCustomer = @CustomerId)
-		) AS NewCustomer_ReReject,
- 
-	(
-	SELECT 
-		COUNT(cmp.Id)
-	FROM 
-		CashRequests cr 
-		LEFT JOIN MP_CustomerMarketPlace cmp ON cmp.CustomerId = cr.IdCustomer
-		LEFT JOIN Loan l ON l.CustomerId = cmp.CustomerId 
-	WHERE 
-		cr.IdCustomer = @CustomerId AND 
-		cr.IdUnderwriter IS NOT NULL AND 
-		cr.UnderwriterDecision = 'Rejected' AND 
-		@ManualDecisionDate >= (SELECT cast(max(created) AS DATE) FROM MP_CustomerMarketPlace WHERE CustomerId = @CustomerId) AND 
-		DATEADD(DD, 30, @ManualDecisionDate) >= @Now AND 
-		l.Id IS NOT NULL AND 
-		@ManualDecisionDate = (SELECT cast(max(cr.UnderwriterDecisionDate) AS DATE) FROM CashRequests cr WHERE cr.IdCustomer = @CustomerId) 
-	) AS OldCustomer_ReReject,
+	------------------------------------------------------------------------------
 
-	(
-	SELECT 
-		ISNULL(sum(isnull(lt.LoanRepayment, 0)), 0) AS PrincipalPaidAmount
-	FROM 
-		Loan l
-		LEFT JOIN LoanTransaction lt ON lt.LoanId = l.Id
-	WHERE 
-		l.Customerid = @CustomerId AND 
-		lt.Status = 'Done' AND
-		Type ='PaypointTransaction'
-	) AS PrincipalPaidAmount,
+	SELECT
+		ISNULL((
+			SELECT
+				COUNT(cmp.Id)
+			FROM
+				CashRequests cr
+				LEFT JOIN MP_CustomerMarketPlace cmp ON cmp.CustomerId = cr.IdCustomer
+				LEFT JOIN Loan l ON l.CustomerId = cmp.CustomerId
+			WHERE
+				cr.IdCustomer = @CustomerId
+				AND
+				cr.IdUnderwriter IS NOT NULL
+				AND
+				cr.UnderwriterDecision = 'Rejected'
+				AND
+				@ManualDecisionDate >= (SELECT CAST(MAX(created) AS DATE) FROM MP_CustomerMarketPlace WHERE CustomerId = @CustomerId)
+				AND
+				DATEADD(DD, 30, @ManualDecisionDate) >= @Now AND l.Id IS NULL
+				AND
+				@ManualDecisionDate = (SELECT CAST(MAX(cr.UnderwriterDecisionDate) AS DATE) FROM CashRequests cr WHERE cr.IdCustomer = @CustomerId)
+		), 0) AS NewCustomer_ReReject,
 
-	(
-	SELECT 
-		ISNULL(sum(isnull(l.LoanAmount, 0)), 0)
-	FROM 
-		Loan l
-	WHERE 
-		l.Customerid = @CustomerId
-	) AS LoanAmountTaken
+		ISNULL((
+			SELECT
+				COUNT(cmp.Id)
+			FROM
+				CashRequests cr
+				LEFT JOIN MP_CustomerMarketPlace cmp ON cmp.CustomerId = cr.IdCustomer
+				LEFT JOIN Loan l ON l.CustomerId = cmp.CustomerId
+			WHERE
+				cr.IdCustomer = @CustomerId
+				AND
+				cr.IdUnderwriter IS NOT NULL
+				AND
+				cr.UnderwriterDecision = 'Rejected'
+				AND
+				@ManualDecisionDate >= (SELECT CAST(MAX(created) AS DATE) FROM MP_CustomerMarketPlace WHERE CustomerId = @CustomerId)
+				AND
+				DATEADD(DD, 30, @ManualDecisionDate) >= @Now
+				AND
+				l.Id IS NOT NULL
+				AND
+				@ManualDecisionDate = (SELECT CAST(MAX(cr.UnderwriterDecisionDate) AS DATE) FROM CashRequests cr WHERE cr.IdCustomer = @CustomerId)
+		), 0) AS OldCustomer_ReReject,
+
+		ISNULL((
+			SELECT
+				SUM(ISNULL(lt.LoanRepayment, 0)) AS PrincipalPaidAmount
+			FROM
+				Loan l
+				LEFT JOIN LoanTransaction lt ON lt.LoanId = l.Id
+			WHERE
+				l.Customerid = @CustomerId
+				AND
+				lt.Status = 'Done'
+				AND
+				Type ='PaypointTransaction'
+		), 0) AS PrincipalPaidAmount,
+
+		ISNULL((
+			SELECT
+				SUM(ISNULL(l.LoanAmount, 0))
+			FROM
+				Loan l
+			WHERE
+				l.Customerid = @CustomerId
+		), 0) AS LoanAmountTaken
 END
 GO
