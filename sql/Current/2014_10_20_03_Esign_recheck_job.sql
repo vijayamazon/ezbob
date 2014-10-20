1d3954099dcbf1a925feb1a268d64c38e009817f
Alex Boursouk <alexbo@ezbob.com>
2014-10-20 16:57:18 +0300

    ez-2674 check automatically for echo sign status updates every configurable time

---
+++ sql/Current/2014_10_20_03_Esign_recheck_job.sql
@@ -0,0 +1,31 @@
+/*
+DECLARE @ActionNameID INT
+DECLARE @JobID BIGINT
+
+SELECT
+	@ActionNameID = ActionNameID
+FROM
+	EzServiceActionName
+WHERE
+	ActionName LIKE 'EzBob.Backend.Strategies.Esign.EsignProcessPending'
+
+INSERT INTO EzServiceCrontab (ActionNameID, IsEnabled, RepetitionTypeID, RepetitionTime)
+	VALUES(@ActionNameID, 0, 3, 'Oct 20 2014 0:10:0')
+
+SET @JobID = SCOPE_IDENTITY()
+
+INSERT INTO EzServiceCronjobArguments (JobID, SerialNo, ArgumentTypeID)
+SELECT
+	@JobID,
+	1,
+	TypeID
+FROM
+	EzServiceCronjobArgumentTypes
+WHERE
+	TypeName = 'int'
+	AND
+	IsNullable = 1
+
+UPDATE EzServiceCrontab SET IsEnabled = 1 WHERE JobID = @JobID
+GO
+*/
