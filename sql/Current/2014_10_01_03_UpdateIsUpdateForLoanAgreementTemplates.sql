12391949b1476a4455b4d7b5b170e44d2d5ed3df
Yuly Saar <yulys@ezbob.com>
2014-10-23 17:48:47 +0200

    Made some sqls re-runnable

--- sql/Current/2014_10_01_03_UpdateIsUpdateForLoanAgreementTemplates.sql
+++ sql/Current/2014_10_01_03_UpdateIsUpdateForLoanAgreementTemplates.sql
@@ -1,15 +1,17 @@
-DECLARE @TemplateId INT
+IF NOT EXISTS (SELECT 1 FROM LoanAgreementTemplate WHERE IsUpdate = 1)
+BEGIN
+	DECLARE @TemplateId INT
 
-SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 1
-UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
+	SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 1
+	UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
 
-SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 2
-UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
+	SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 2
+	UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
 
-SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 3
-UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
+	SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 3
+	UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
 
-SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 4
-UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
-
+	SELECT @TemplateId = MAX(Id) FROM LoanAgreementTemplate WHERE TemplateType = 4
+	UPDATE LoanAgreementTemplate SET IsUpdate = 1 WHERE Id = @TemplateId 
+END
 GO
