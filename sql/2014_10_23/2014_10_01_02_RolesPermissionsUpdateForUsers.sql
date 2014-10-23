44b2bfd2a2289a604b0a2b7cc75701e2cb951825
Yuly Saar <yulys@ezbob.com>
2014-10-23 17:45:24 +0200

    Made some sqls re-runnable

--- sql/2014_10_23/2014_10_01_02_RolesPermissionsUpdateForUsers.sql
+++ sql/2014_10_23/2014_10_01_02_RolesPermissionsUpdateForUsers.sql
@@ -1,14 +1,19 @@
 IF EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name = 'Environment' AND Value = 'Prod')
 BEGIN
 	DECLARE @RoleId INT
-	SELECT @RoleId = RoleId FROM Security_Role WHERE Name = 'Collector'
-	INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (2561, @RoleId) -- Emma
+	
+	SELECT @RoleId = RoleId FROM Security_Role WHERE Name = 'Collector'	
+	IF NOT EXISTS (SELECT 1 FROM Security_UserRoleRelation WHERE UserId = 2561 AND RoleId = @RoleId)
+		INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (2561, @RoleId) -- Emma
 	
 	SELECT @RoleId = RoleId FROM Security_Role WHERE Name = 'Sales'
-	INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (4916, @RoleId) -- Ros
-	INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (16110, @RoleId) -- Clare
+	IF NOT EXISTS (SELECT 1 FROM Security_UserRoleRelation WHERE UserId = 4916 AND RoleId = @RoleId)
+		INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (4916, @RoleId) -- Ros
+	IF NOT EXISTS (SELECT 1 FROM Security_UserRoleRelation WHERE UserId = 16110 AND RoleId = @RoleId)
+		INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (16110, @RoleId) -- Clare
 	
 	SELECT @RoleId = RoleId FROM Security_Role WHERE Name = 'BrokerSales'
-	INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (10127, @RoleId) -- Travis
+	IF NOT EXISTS (SELECT 1 FROM Security_UserRoleRelation WHERE UserId = 10127 AND RoleId = @RoleId)
+		INSERT INTO Security_UserRoleRelation (UserId, RoleId) VALUES (10127, @RoleId) -- Travis
 END
 GO
