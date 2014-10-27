76be0f0d5d08b512114ce3784686e8dc85919b3a
Yuly Saar <yulys@ezbob.com>
2014-10-27 16:30:44 +0200

    EZ-2733, EZ-2729, EZ-2732: Alibaba;s emails

--- Backend/Strategies/MailStrategies/ApprovedUser.cs
+++ Backend/Strategies/MailStrategies/ApprovedUser.cs
@@ -1,8 +1,13 @@
 ﻿namespace EzBob.Backend.Strategies.MailStrategies {
+	using System;
 	using System.Globalization;
 	using System.Collections.Generic;
+	using API;
+	using ConfigManager;
+	using EZBob.DatabaseLib.Repository;
 	using Ezbob.Database;
 	using Ezbob.Logger;
+	using StructureMap;
 
 	public class ApprovedUser : ABrokerMailToo {
 		#region constructor
@@ -11,6 +16,7 @@
 			m_nLoanAmount = nLoanAmount;
 			m_nValidHours = nValidHours;
 			m_bIsFirst = isFirst;
+			amountInUsd = CalculateLoanAmountInUsd();
 		} // constructor
 
 		#endregion constructor
@@ -24,33 +30,51 @@
 			Variables = new Dictionary<string, string> {
 				{ "FirstName", CustomerData.FirstName },
 				{ "LoanAmount", m_nLoanAmount.ToString(CultureInfo.InvariantCulture) },
-				{ "ValidFor", m_nValidHours.ToString(CultureInfo.InvariantCulture) }
+				{ "ValidFor", m_nValidHours.ToString(CultureInfo.InvariantCulture) },
+				{ "AmountInUsd", amountInUsd.ToString(CultureInfo.InvariantCulture) }
 			};
 
-			if (m_bIsFirst)
+			if (CustomerData.IsAlibaba)
 			{
-				if (CustomerData.IsAlibaba)
-				{
-					TemplateName = "Mandrill - Alibaba - Approval (1st time)";
-				}
-				else if (CustomerData.IsCampaign)
-				{
-					TemplateName = "Mandrill - Approval Campaign (1st time)";
-				}
-				else
-				{
-					TemplateName = "Mandrill - Approval (1st time)";
-				}
+				TemplateName = "Mandrill - Alibaba - Approval";
+			}
+			else if (CustomerData.IsCampaign)
+			{
+				TemplateName = "Mandrill - Approval Campaign (1st time)";
+			}
+			else if (m_bIsFirst)
+			{
+				TemplateName = "Mandrill - Approval (1st time)";
 			}
 			else
 			{
-				TemplateName = CustomerData.IsAlibaba ? "Mandrill - Alibaba - Approval (not 1st time)" : "Mandrill - Approval (not 1st time)";
+				TemplateName = "Mandrill - Approval (not 1st time)";
 			}
 		} // SetTemplateAndVariables
 
 		#endregion method SetTemplateAndVariables
 
+		protected override void ActionAtEnd()
+		{
+			if (CustomerData.IsAlibaba)
+			{
+				var address = new Addressee(CurrentValues.Instance.AlibabaMailTo, CurrentValues.Instance.AlibabaMailCc);
+				Log.Info("Sending Alibaba internal approval mail");
+				SendCostumeMail("Mandrill - Alibaba - Internal approval email", Variables, new[] { address });
+			}
+		}
+
+		private double CalculateLoanAmountInUsd()
+		{
+			var currencyRateRepository = ObjectFactory.GetInstance<CurrencyRateRepository>();
+			double currencyRate = currencyRateRepository.GetCurrencyHistoricalRate(DateTime.UtcNow, "USD");
+			double convertedLoanAmount = (double)m_nLoanAmount * currencyRate * CurrentValues.Instance.AlibabaCurrencyConversionCoefficient;
+			Log.Info("Calculating Alibaba loan amount in USD. CurrencyRate:{0} Coefficient:{1} LoanAmount:{2} ConvertedLoanAmount:{3}", currencyRate, CurrentValues.Instance.AlibabaCurrencyConversionCoefficient, amount, convertedLoanAmount);
+			return convertedLoanAmount;
+		}
+
 		private readonly decimal m_nLoanAmount;
+		private readonly double amountInUsd;
 		private readonly int m_nValidHours;
 		private readonly bool m_bIsFirst;
 	} // class ApprovedUser
