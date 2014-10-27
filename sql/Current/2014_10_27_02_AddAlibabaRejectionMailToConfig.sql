267575ef1350fbdc3c3b5cd2f73f4c8accc233e7
Yuly Saar <yulys@ezbob.com>
2014-10-27 17:55:48 +0200

    Alibaba mailing config is done per environment

--- sql/Current/2014_10_27_02_AddAlibabaRejectionMailToConfig.sql
+++ sql/Current/2014_10_27_02_AddAlibabaRejectionMailToConfig.sql
@@ -1,14 +1,43 @@
-IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailTo')
-BEGIN
-	INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailTo', 'alibaba@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
-END
-GO
 
-IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailCc')
+
+DECLARE @Environment NVARCHAR(256)
+SELECT @Environment = Value FROM ConfigurationVariables WHERE Name = 'Environment'
+
+IF @Environment = 'Dev' OR @Environment IS NULL 
 BEGIN
-	INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailCc', 'ops@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailCc')
+	BEGIN
+		INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailCc', 'dev-ops@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	END
+	IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailTo')
+	BEGIN
+		INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailTo', 'dev-alibaba@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	END
 END
-GO
+
+IF @Environment = 'QA' OR @Environment = 'UAT'
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailCc')
+	BEGIN
+		INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailCc', 'qa-ops@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	END
+	IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailTo')
+	BEGIN
+		INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailTo', 'qa-alibaba@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	END
+END
+
+IF @Environment = 'Prod'
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailCc')
+	BEGIN
+		INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailCc', 'ops@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	END
+	IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaMailTo')
+	BEGIN
+		INSERT INTO ConfigurationVariables(Name, Value, Description) VALUES ('AlibabaMailTo', 'alibaba@ezbob.com', 'An email to this address will be sent when Alibaba customer is rejected or approved')
+	END
+END
 
 IF NOT EXISTS (SELECT 1 FROM ConfigurationVariables WHERE Name='AlibabaCurrencyConversionCoefficient')
 BEGIN
