32f0157b3b06377115695c4bb834d24993e6b10e
Yuly Saar <yulys@ezbob.com>
2014-10-28 15:32:36 +0200

    Alibaba customer has default loan type = alibaba

--- sql/Current/2014_10_28_01_AddAlibabaLoanType.sql
+++ sql/Current/2014_10_28_01_AddAlibabaLoanType.sql
@@ -1,6 +1,11 @@
 IF NOT EXISTS (SELECT 1 FROM LoanType WHERE Id = 3)
 BEGIN
 	INSERT INTO LoanType (Id, Type, Name, Description, IsDefault, RepaymentPeriod)
-	VALUES (3, 'AlibabaLoanType', 'Alibaba Loan', 'Alibaba Loan', 0, 6)
+	VALUES (3, 'AlibabaLoanType', 'Alibaba Loan', 'Alibaba Loan', 0, 12)
+END
+ELSE
+BEGIN
+	IF EXISTS (SELECT 1 FROM LoanType WHERE Id = 3 AND RepaymentPeriod = 6)
+		UPDATE LoanType SET RepaymentPeriod = 12 WHERE Id = 3 AND RepaymentPeriod = 6
 END
 GO
