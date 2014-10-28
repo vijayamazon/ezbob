87a42e9b6ba11a8a692e85a24faf127ebc6f4cbf
Alex Boursouk <alexbo@ezbob.com>
2014-10-28 13:09:10 +0200

    Underwriter GridAll loading optimisation.
    (cherry picked from commit 3a84d41957f87ab8d395d753badfe49c4e1f0767)

--- sql/SPs/UwGridAll.sql
+++ sql/SPs/UwGridAll.sql
@@ -1,42 +1,47 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UwGridAll]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[UwGridAll]
+IF OBJECT_ID('UwGridAll') IS NULL
+	EXECUTE('CREATE PROCEDURE UwGridAll AS SELECT 1')
 GO
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[UwGridAll] 
-	(@WithTest BIT)
+
+ALTER PROCEDURE UwGridAll
+@WithTest BIT
 AS
 BEGIN
+	SET NOCOUNT ON;
+
 	SELECT
-	c.Id AS CustomerID,
-	ISNULL(c.MedalType, '') AS Medal,
-	ISNULL(t.Name, '') AS MpTypeName,
-	c.ApplyForLoan AS ApplyDate,
-	c.GreetingMailSentDate AS RegDate,
-	c.Status AS CustomerStatus,
-	ISNULL(c.Fullname, '') AS FullName,
-	c.Name AS Email,
-	w.WizardStepTypeDescription AS WizardStep,
-	c.SystemCalculatedSum AS CalcAmount,
-	(CASE c.IsOffline WHEN 1 THEN 'Offline' WHEN 0 THEN 'Online' ELSE '' END) AS SegmentType,
-	(CASE WHEN c.CreditResult = 'Late' OR c.IsWasLate = 1 THEN 'iswaslate' ELSE '' END) AS IsWasLate,
-	c.ManagerApprovedSum AS ApprovedSum,
-	(SELECT ISNULL(SUM(l.Balance), 0) FROM Loan l WHERE l.CustomerId = c.Id) AS OSBalance
-FROM
-	Customer c
-	INNER JOIN WizardStepTypes w ON c.WizardStep = w.WizardStepTypeID
-	LEFT JOIN MP_CustomerMarketPlace m ON c.Id = m.CustomerId
-	LEFT JOIN MP_MarketplaceType t ON m.MarketPlaceId = t.Id
-WHERE
-	(
-		@WithTest = 1 OR c.IsTest = 0
-	)
-	AND
-	c.CreditResult IS NOT NULL
-ORDER BY
-	c.Id DESC,
-	t.Id
+		c.Id AS CustomerID,
+		ISNULL(c.MedalType, '') AS Medal,
+		ISNULL(t.Name, '') AS MpTypeName,
+		c.ApplyForLoan AS ApplyDate,
+		c.GreetingMailSentDate AS RegDate,
+		c.Status AS CustomerStatus,
+		ISNULL(c.Fullname, '') AS FullName,
+		c.Name AS Email,
+		w.WizardStepTypeDescription AS WizardStep,
+		c.SystemCalculatedSum AS CalcAmount,
+		(CASE c.IsOffline WHEN 1 THEN 'Offline' WHEN 0 THEN 'Online' ELSE '' END) AS SegmentType,
+		(CASE WHEN c.CreditResult = 'Late' OR c.IsWasLate = 1 THEN 'iswaslate' ELSE '' END) AS IsWasLate,
+		c.ManagerApprovedSum AS ApprovedSum,
+		(SELECT ISNULL(SUM(l.Balance), 0) FROM Loan l WHERE l.CustomerId = c.Id) AS OSBalance
+	FROM
+		Customer c
+		INNER JOIN WizardStepTypes w ON c.WizardStep = w.WizardStepTypeID
+		LEFT JOIN MP_CustomerMarketPlace m ON c.Id = m.CustomerId
+		LEFT JOIN MP_MarketplaceType t ON m.MarketPlaceId = t.Id
+	WHERE
+		(
+			@WithTest = 1 OR c.IsTest = 0
+		)
+		AND
+		c.CreditResult IS NOT NULL
+	ORDER BY
+		c.Id DESC,
+		t.Id
 END
 GO
