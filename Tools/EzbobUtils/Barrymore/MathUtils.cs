99f29337132e5c6a60cf7067be2ab9778f07a6dd
Yuly Saar <yulys@ezbob.com>
2014-10-29 10:04:37 +0200

    Rounding into 2 decimal points exactly, midpoint down, thousands separators

---
+++ Tools/EzbobUtils/Barrymore/MathUtils.cs
@@ -0,0 +1,24 @@
+﻿namespace Ezbob.Utils
+{
+	using System;
+
+	public static class MathUtils
+	{
+		public static decimal Round2DecimalDown(decimal value)
+		{
+			decimal tmp2 = Math.Round(value, 2);
+			decimal tmp3 = Math.Truncate(value * 1000) / 1000;
+			if (tmp2 - 0.005m == tmp3)
+			{
+				return tmp2 - 0.01m;
+			}
+
+			return tmp2;
+		}
+
+		public static decimal Round2DecimalDown(double value)
+		{
+			return Round2DecimalDown((decimal)value);
+		}
+	}
+}
