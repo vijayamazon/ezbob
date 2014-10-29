3b129d2fd84bcfce0f683a23feca808da3ddc0cc
Yuly Saar <yulys@ezbob.com>
2014-10-29 11:07:51 +0200

    EZ-2708: Proper check for error reason insuffcient funds

--- Backend/Strategies/Misc/PayPointCharger.cs
+++ Backend/Strategies/Misc/PayPointCharger.cs
@@ -253,9 +253,9 @@
 
 		private bool IsNotEnoughMoney(PayPointReturnData payPointReturnData)
 		{
-			bool isNotEnoughMoney = payPointReturnData.Code == "N" && payPointReturnData.Message == "INSUFF FUNDS";
-			Log.Debug("Checking if not enough money. Code:{0} Message:{1} AuthCode:{2} RespCode:{3}. Result:{4}",
-				payPointReturnData.Code, payPointReturnData.Message, payPointReturnData.AuthCode, payPointReturnData.RespCode, isNotEnoughMoney);
+			bool isNotEnoughMoney = payPointReturnData.Code == "N" && payPointReturnData.Error == "INSUFF FUNDS";
+			Log.Debug("Checking if not enough money. Code:{0} Message:{1} AuthCode:{2} RespCode:{3} Error:{4}. Result:{5}",
+				payPointReturnData.Code, payPointReturnData.Message, payPointReturnData.AuthCode, payPointReturnData.RespCode, payPointReturnData.Error, isNotEnoughMoney);
 			return isNotEnoughMoney;
 		}
 	}
