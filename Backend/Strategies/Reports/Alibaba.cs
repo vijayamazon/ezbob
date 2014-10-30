03adf241a5b93ec7e6e43e8e1109be4f1f4b7e78
Alex Boursouk <alexbo@ezbob.com>
2014-10-30 19:00:28 +0200

    EZ-2738 Alibaba: complete reports - take 2.

---
+++ Backend/Strategies/Reports/Alibaba.cs
@@ -0,0 +1,41 @@
+﻿namespace EzBob.Backend.Strategies.Reports {
+	using Ezbob.Database;
+	using Ezbob.Logger;
+	using global::Reports;
+
+	public class Alibaba : AStrategy {
+		#region public
+
+		#region constructor
+
+		public Alibaba(AConnection oDB, ASafeLog oLog)
+			: base(oDB, oLog) {
+		} // constructor
+
+		#endregion constructor
+
+		#region property Name
+
+		public override string Name {
+			get { return "Alibaba"; }
+		} // Name
+
+		#endregion property Name
+
+		#region method Execute
+
+		public override void Execute() {
+		} // Execute
+
+		#endregion method Execute
+
+		#endregion public
+
+		#region private
+
+		private Report m_oDataSharing;
+		private Report m_oFunnel;
+
+		#endregion private
+	} // class Alibaba
+} // namespace
