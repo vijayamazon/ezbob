053394db7a9039860ae925242f34a2ead8b1e78d
stas_dulman <stasd@ezbob.com>
2014-11-16 12:17:51 +0200

    EZ-2821 fix is consumer data updated SP

--- sql/SPs/GetIsConsumerDataUpdated.sql
+++ sql/SPs/GetIsConsumerDataUpdated.sql
@@ -1,30 +1,34 @@
-IF EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetIsConsumerDataUpdated]') AND TYPE IN (N'P', N'PC'))
-DROP PROCEDURE [dbo].[GetIsConsumerDataUpdated]
-GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[GetIsConsumerDataUpdated] 
+
+IF OBJECT_ID('GetIsConsumerDataUpdated') IS NULL
+	EXECUTE('CREATE PROCEDURE GetIsConsumerDataUpdated AS SELECT 1')
+GO
+
+ALTER PROCEDURE [dbo].[GetIsConsumerDataUpdated] 
 	(@CustomerId INT,
 	 @DirectorId INT = NULL, 
 	 @Today DATE)
 AS
 BEGIN
-	DECLARE @LastUpdateTime DATE
+	DECLARE @LastUpdateTime DATETIME
 			
-	IF @DirectorId = NULL
+	IF @DirectorId IS NULL OR @DirectorId = 0
 	BEGIN
-		SELECT @LastUpdateTime = max(InsertDate) FROM MP_ServiceLog WHERE CustomerId = @CustomerId AND (DirectorId IS NULL OR DirectorId = 0)
+		SET @LastUpdateTime = (SELECT max(InsertDate) FROM MP_ServiceLog WHERE CustomerId = @CustomerId AND (DirectorId IS NULL OR DirectorId = 0) AND ServiceType='Consumer Request')
 	END
 	ELSE
 	BEGIN
-		SELECT @LastUpdateTime = max(InsertDate) FROM MP_ServiceLog WHERE CustomerId = @CustomerId AND DirectorId = @DirectorId
+		SET @LastUpdateTime = (SELECT max(InsertDate) FROM MP_ServiceLog WHERE CustomerId = @CustomerId AND DirectorId = @DirectorId AND ServiceType='Consumer Request')
 	END
-	
-	IF @Today = @LastUpdateTime
+		
+	IF datediff(day, @Today, @LastUpdateTime) = 0
 		SELECT CAST (1 AS BIT) AS IsUpdated
 	ELSE
 		SELECT CAST (0 AS BIT) AS IsUpdated
 END
+
 GO
+
