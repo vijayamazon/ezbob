81317cfaecab4d1e85fa90b69afb939f2e49f5c4
stas_dulman <stasd@ezbob.com>
2014-11-20 18:25:08 +0200

    customer statuses report update

--- sql/SPs/RptCustomerStatus.sql
+++ sql/SPs/RptCustomerStatus.sql
@@ -7,16 +7,30 @@ ALTER PROCEDURE RptCustomerStatus
 @DateEnd DATETIME
 AS
 BEGIN
-	SELECT C.Id CustomerId,C.Fullname,CS.Name Status,L.Principal, 
-		CASE WHEN LS.LoanSourceName = 'EU' THEN 'EU' ELSE '' END AS EU
-	FROM Customer C,CustomerStatuses CS,Loan L, LoanSource LS
-	WHERE L.CustomerId = C.Id 
-	AND CS.Id = C.CollectionStatus 
-	AND C.IsTest = 0 
+
+	;WITH FirstNotRepaidLoan AS 
+	(
+		SELECT c.Id, min(ls.[Date]) AS FirstMissedRepaymentDate FROM Customer c 
+		INNER JOIN Loan l ON c.Id=l.CustomerId
+		INNER JOIN LoanSchedule ls ON l.Id=ls.LoanId
+		LEFT JOIN LoanScheduleTransaction lst ON ls.Id=lst.ScheduleID
+		WHERE c.IsTest=0 AND c.CollectionStatus<>0 AND lst.ID IS NULL AND ls.[Date]<getdate()
+		GROUP BY c.Id
+	)
+	SELECT C.Id CustomerId,C.Fullname,CS.Name Status,sum(L.Principal) AS Principal, 
+		CASE WHEN LS.LoanSourceName = 'EU' THEN 'EU' ELSE '' END AS EU,
+		C.TypeOfBusiness,CP.Description ResidentialStatus, FirstNotRepaidLoan.FirstMissedRepaymentDate 
+	FROM Customer C INNER JOIN CustomerStatuses CS ON C.CollectionStatus=CS.Id 
+	INNER JOIN Loan L ON C.Id=L.CustomerId
+	INNER JOIN LoanSource LS ON LS.LoanSourceID=L.LoanSourceID
+	INNER JOIN CustomerPropertyStatuses CP ON CP.Id=C.PropertyStatusId
+	LEFT JOIN  FirstNotRepaidLoan ON C.Id = FirstNotRepaidLoan.Id
+	WHERE C.IsTest = 0 
 	AND CollectionStatus != 0  
 	AND L.Principal > 0 
-	AND L.LoanSourceID = LS.LoanSourceID
-	ORDER BY CS.Name,L.Principal DESC
+	GROUP BY C.Id,C.Fullname,CS.Name,LS.LoanSourceName,C.TypeOfBusiness,CP.Description,FirstNotRepaidLoan.FirstMissedRepaymentDate 
+	ORDER BY CS.Name,sum(L.Principal) DESC
 END 
+
 
 GO
