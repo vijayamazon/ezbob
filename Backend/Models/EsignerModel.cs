5b88e2fba299f4706672adc7a74d9e80b5544e2e
stas_dulman <stasd@ezbob.com>
2014-11-28 17:12:24 +0200

    fix email validation regex on server side for signing docs
    (cherry picked from commit 87184575171ad293efbc7f162115952e3a20eb7e)

--- Backend/Models/EsignerModel.cs
+++ Backend/Models/EsignerModel.cs
@@ -100,7 +100,7 @@
 			return err.Count < 1 ? null : string.Join(" ", err);
 		} // ValidateExperianDirectorDetails
 
-		private static readonly Regex ms_reEmail = new Regex(@"^[A-Z0-9._%+-]+@[A-Z0-9.-_]+\.[A-Z]{2,4}$", RegexOptions.IgnoreCase);
+		private static readonly Regex ms_reEmail = new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$", RegexOptions.IgnoreCase);
 		private static readonly Regex ms_rePhone = new Regex(@"^0[0-9]{10}$");
 	} // class Esigner
 } // namespace
--- Integration/EzBob/EzBobTest/TestEchoSign.cs
+++ Integration/EzBob/EzBobTest/TestEchoSign.cs
@@ -1,5 +1,6 @@
 ﻿namespace EzBobTest {
 	using System.IO;
+	using System.Text.RegularExpressions;
 	using EchoSignLib;
 	using Ezbob.Utils;
 	using NUnit.Framework;
@@ -65,5 +66,14 @@
 		} // TestGetDocumentInfo
 
 		 */
+
+		[Test]
+		public void TestMailRegex() {
+			Regex ms_reEmail = new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$", RegexOptions.IgnoreCase);
+
+			Assert.IsTrue(ms_reEmail.IsMatch("asd@sfsdf.fsf"));
+			Assert.IsTrue(ms_reEmail.IsMatch("asd@sfsdf-sdfsdf.fsf"));
+			Assert.IsFalse(ms_reEmail.IsMatch("as!@d@sfsdf-sdfsdf.f"));
+		}
 	} // class TestEchoSign
 } // namespace
