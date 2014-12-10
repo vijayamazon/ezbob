fa00775eba29514a5c0b6cb9b22cea1106a840e9
Alex Boursouk <alexbo@ezbob.com>
2014-12-10 19:57:41 +0200

    Auto approve configuration update.

---
+++ sql/current/2014_12_10_2_AutoApprove_configuration_update.sql
@@ -0,0 +1,7 @@
+UPDATE ConfigurationVariables SET Value = '500'   WHERE Name = 'AutoApproveExperianScoreThreshold'
+UPDATE ConfigurationVariables SET Value = '18'    WHERE Name = 'AutoApproveCustomerMinAge'
+UPDATE ConfigurationVariables SET Value = '70'    WHERE Name = 'AutoApproveCustomerMaxAge'
+UPDATE ConfigurationVariables SET Value = '20'    WHERE Name = 'AutoApproveMaxDailyApprovals'
+UPDATE ConfigurationVariables SET Value = '50000' WHERE Name = 'AutoApproveMaxAmount'
+UPDATE ConfigurationVariables SET Value = '11'    WHERE Name = 'AutoApproveBusinessScoreThreshold'
+GO
