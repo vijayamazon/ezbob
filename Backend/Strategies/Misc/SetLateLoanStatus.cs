01479dd38b0c73601710c7f38bd7b8bbad1a18f2
stas_dulman <stasd@ezbob.com>
2014-12-15 18:41:16 +0200

    EZ-2847 collection status automation

--- Backend/Strategies/Misc/SetLateLoanStatus.cs
+++ Backend/Strategies/Misc/SetLateLoanStatus.cs
@@ -6,6 +6,11 @@
 	using MailStrategies.API;
 	using PaymentServices.PayPoint;
 
+
+	/// <summary>
+	/// Retrieves all loans that are late - updates customer, loan, loan schedule statuses,
+	/// sends email, sms and mail according to collection rules
+	/// </summary>
 	public class SetLateLoanStatus : AStrategy {
 
 		public SetLateLoanStatus() {
@@ -32,71 +37,116 @@
 		} // Name
 
 		public override void Execute() {
-			MarkLoansAsLate();
+			DB.ForEachRowSafe((sr, bRowsetStart) => {
+				MarkLoanAsLate(sr);
+				return ActionResult.Continue;
+			}, "GetLoansToCollect", CommandSpecies.StoredProcedure);
+
 
 			DB.ForEachRowSafe((sr, bRowsetStart) => {
-				DateTime date = sr["Date"];
-				decimal amountDue = sr["AmountDue"];
-				int loanId = sr["LoanId"];
-				int loanScheduleId = sr["LoanScheduleId"];
-				decimal interest = sr["Interest"];
-				string mail = sr["email"];
-				string firstName = sr["FirstName"];
-				string refNum = sr["RefNum"];
-				DateTime customInstallmentDate = sr["CustomInstallmentDate"];
-				bool sentLastNotice = sr["LastNoticeSent"];
+				HandleCollectionLogic(sr);
+				return ActionResult.Continue;
+			}, "GetLateForCollection", CommandSpecies.StoredProcedure); // foreach
+		}
 
-				if (customInstallmentDate != default(DateTime)) {
-					if (date < customInstallmentDate)
-						date = customInstallmentDate;
-				} // if
+		/// <summary>
+		/// For each loan schedule marks it as late, it's loan as late and 
+		/// </summary>
+		/// <param name="sr"></param>
+		private void MarkLoanAsLate(SafeReader sr) {
+			int id = sr["id"];
+			int loanId = sr["LoanId"];
+			bool isLastInstallment = sr["LastInstallment"];
+			int customerId = sr["CustomerId"];
+			DateTime? customInstallmentDate = sr["CustomInstallmentDate"];
 
-				int daysBetween = (int)(DateTime.UtcNow - date).TotalDays;
-				int feeAmount, feeType;
-				CalculateFee(daysBetween, interest, out feeAmount, out feeType);
+			if (!isLastInstallment) {
+				decimal amountDue = new PayPointApi().GetAmountToPay(id);
 
-				bool appliedLateCharge = false;
-				if (feeType != 0)
-				{
-					var papi = new PayPointApi();
-					appliedLateCharge = papi.ApplyLateCharge(feeAmount, loanId, feeType);
-				} // if
+				int daysBetweenCustom = customInstallmentDate.HasValue ? (int)(customInstallmentDate.Value - DateTime.UtcNow).TotalDays : 0;
 
-				var safeReader = DB.GetFirst("ShouldStopSendingLateMails", new QueryParameter("LoanId", loanId));
-				bool stopSendingEmails = safeReader["StopSendingEmails"];
-				string templateName = null;
+				if (!(amountDue > amountToChargeFrom && daysBetweenCustom > 1)) {
+					DB.ExecuteNonQuery(
+						"UpdateLoanScheduleCustomDate",
+						CommandSpecies.StoredProcedure,
+						new QueryParameter("Id", id)
+					);
+					return;
+				}
+			} // if
 
-				if (!stopSendingEmails)
-				{
-					if (daysBetween < collectionPeriod1) // 7
+			DB.ExecuteNonQuery(
+				"UpdateLoanStatusToLate", CommandSpecies.StoredProcedure,
+				new QueryParameter("LoanId", loanId),
+				new QueryParameter("CustomerId", customerId),
+				new QueryParameter("PaymentStatus", "Late"),
+				new QueryParameter("LoanStatus", "Late")
+			);
+
+			DB.ExecuteNonQuery("UpdateLoanScheduleStatus", CommandSpecies.StoredProcedure,
+				new QueryParameter("Id", id),
+				new QueryParameter("Status", "Late")
+			);
+		} // MarkLoansAsLate
+
+		private void HandleCollectionLogic(SafeReader sr) {
+			DateTime date = sr["Date"];
+			decimal amountDue = sr["AmountDue"];
+			int loanId = sr["LoanId"];
+			int loanScheduleId = sr["LoanScheduleId"];
+			decimal interest = sr["Interest"];
+			string mail = sr["email"];
+			string firstName = sr["FirstName"];
+			string refNum = sr["RefNum"];
+			DateTime customInstallmentDate = sr["CustomInstallmentDate"];
+			bool sentLastNotice = sr["LastNoticeSent"];
+
+			if (customInstallmentDate != default(DateTime)) {
+				if (date < customInstallmentDate)
+					date = customInstallmentDate;
+			} // if
+
+			int daysBetween = (int)(DateTime.UtcNow - date).TotalDays;
+			int feeAmount, feeType;
+			CalculateFee(daysBetween, interest, out feeAmount, out feeType);
+
+			bool appliedLateCharge = false;
+			if (feeType != 0) {
+				var papi = new PayPointApi();
+				appliedLateCharge = papi.ApplyLateCharge(feeAmount, loanId, feeType);
+			} // if
+
+			var safeReader = DB.GetFirst("ShouldStopSendingLateMails", new QueryParameter("LoanId", loanId));
+			bool stopSendingEmails = safeReader["StopSendingEmails"];
+			string templateName = null;
+
+			if (!stopSendingEmails) {
+				if (daysBetween < collectionPeriod1) // 7
 					{
-						templateName = "Mandrill - ezbob - you missed your payment";
-					}
-					else if (daysBetween < collectionPeriod2) // 14
+					templateName = "Mandrill - ezbob - you missed your payment";
+				}
+				else if (daysBetween < collectionPeriod2) // 14
 					{
-						templateName = appliedLateCharge ? "Mandrill - ezbob - £20 late fee was added to your account" : "Mandrill - ezbob - Warning - you missed your payment for more than 7 days";
-					}
-					else if (daysBetween < collectionPeriod3) // 30
+					templateName = appliedLateCharge ? "Mandrill - ezbob - £20 late fee was added to your account" : "Mandrill - ezbob - Warning - you missed your payment for more than 7 days";
+				}
+				else if (daysBetween < collectionPeriod3) // 30
 					{
-						templateName = appliedLateCharge ? "Mandrill - Warning notice - ezbob - £40 late fee was added" : "Mandrill - Warning notice - ezbob - you missed your payment for more than 14 days";
-					}
-					else if (!sentLastNotice)
-					{
-						templateName = "Mandrill - ezbob - Last warning - Debt recovery agency";
+					templateName = appliedLateCharge ? "Mandrill - Warning notice - ezbob - £40 late fee was added" : "Mandrill - Warning notice - ezbob - you missed your payment for more than 14 days";
+				}
+				else if (!sentLastNotice) {
+					templateName = "Mandrill - ezbob - Last warning - Debt recovery agency";
 
-						DB.ExecuteNonQuery(
-							"UpdateLastNotice",
-							CommandSpecies.StoredProcedure,
-							new QueryParameter("LoanScheduleId", loanScheduleId));
-					}
-					else
-					{
-						Log.Info("Processed late scheduled payment id:{0}, since last notice was already sent nothing was done.", loanScheduleId);
-					}
+					DB.ExecuteNonQuery(
+						"UpdateLastNotice",
+						CommandSpecies.StoredProcedure,
+						new QueryParameter("LoanScheduleId", loanScheduleId));
+				}
+				else {
+					Log.Info("Processed late scheduled payment id:{0}, since last notice was already sent nothing was done.", loanScheduleId);
+				}
 
-					if (templateName != null)
-					{
-						var variables = new Dictionary<string, string>
+				if (templateName != null) {
+					var variables = new Dictionary<string, string>
 							{
 								{"FirstName", firstName},
 								{"ScheduledAmount", amountDue.ToString(CultureInfo.InvariantCulture)},
@@ -104,34 +154,34 @@
 								{"FeeAmount", feeAmount.ToString(CultureInfo.InvariantCulture)},
 								{"AmountCharged", amountDue.ToString(CultureInfo.InvariantCulture)}
 							};
-						mailer.Send(templateName, variables, new Addressee(mail));
-					}
+					mailer.Send(templateName, variables, new Addressee(mail));
 				}
+			}
 
-				AccumulateFee(loanId, daysBetween, amountDue);
+			AccumulateFee(loanId, daysBetween, amountDue);
 
-				DB.ExecuteNonQuery(
-					"UpdateCollection",
-					CommandSpecies.StoredProcedure,
-					new QueryParameter("LoanId", loanId),
-					new QueryParameter("Late30", late30),
-					new QueryParameter("Late30Num", late30Num),
-					new QueryParameter("Late60", late60),
-					new QueryParameter("Late60Num", late60Num),
-					new QueryParameter("Late90", late90),
-					new QueryParameter("Late90Num", late90Num),
-					new QueryParameter("PastDues", pastDues),
-					new QueryParameter("PastDuesNum", pastDuesNum),
-					new QueryParameter("IsDefaulted", 0),
-					new QueryParameter("Late90Plus", late90Plus),
-					new QueryParameter("Late90PlusNum", late90PlusNum)
-				);
+			DB.ExecuteNonQuery(
+				"UpdateCollection",
+				CommandSpecies.StoredProcedure,
+				new QueryParameter("LoanId", loanId),
+				new QueryParameter("Late30", late30),
+				new QueryParameter("Late30Num", late30Num),
+				new QueryParameter("Late60", late60),
+				new QueryParameter("Late60Num", late60Num),
+				new QueryParameter("Late90", late90),
+				new QueryParameter("Late90Num", late90Num),
+				new QueryParameter("PastDues", pastDues),
+				new QueryParameter("PastDuesNum", pastDuesNum),
+				new QueryParameter("IsDefaulted", 0),
+				new QueryParameter("Late90Plus", late90Plus),
+				new QueryParameter("Late90PlusNum", late90PlusNum)
+			);
 
-				loanIdPrev = loanId;
+			loanIdPrev = loanId;
 
-				return ActionResult.Continue;
-			}, "GetLateForCollection", CommandSpecies.StoredProcedure); // foreach
-		} // Execute
+		}
+
+		// Execute
 
 		private void AccumulateFee(int loanId, int daysBetween, decimal amountDue) {
 			if (loanId != loanIdPrev) {
@@ -189,47 +239,7 @@
 			}
 		} // CalculateFee
 
-		private void MarkLoansAsLate() {
-			DB.ForEachRowSafe((sr, bRowsetStart) => {
-				int id = sr["id"];
-				int loanId = sr["LoanId"];
-				bool isLastInstallment = sr["LastInstallment"];
-				int customerId = sr["CustomerId"];
-				DateTime customInstallmentDate = sr["CustomInstallmentDate"];
-
-				if (!isLastInstallment) {
-					decimal amountDue = new PayPointApi().GetAmountToPay(id);
-
-					int daysBetweenCustom = (int) (customInstallmentDate - DateTime.UtcNow).TotalDays;
-
-					if (!(amountDue > amountToChargeFrom && daysBetweenCustom > 1)) {
-						DB.ExecuteNonQuery(
-							"UpdateLoanScheduleCustomDate",
-							CommandSpecies.StoredProcedure,
-							new QueryParameter("Id", id)
-						);
-						return ActionResult.Continue;
-					}
-				} // if
-
-				DB.ExecuteNonQuery(
-					"UpdateLoanStatusToLate",
-					CommandSpecies.StoredProcedure,
-					new QueryParameter("LoanId", loanId),
-					new QueryParameter("CustomerId", customerId),
-					new QueryParameter("PaymentStatus", "Late"),
-					new QueryParameter("LoanStatus", "Late")
-				);
-
-				DB.ExecuteNonQuery("UpdateLoanScheduleStatus",
-					CommandSpecies.StoredProcedure,
-					new QueryParameter("Id", id),
-					new QueryParameter("Status", "Late")
-				);
-
-				return ActionResult.Continue;
-			}, "GetLoansToCollect", CommandSpecies.StoredProcedure);
-		} // MarkLoansAsLate
+		
 
 		private readonly StrategiesMailer mailer;
 		private readonly int collectionPeriod1;
