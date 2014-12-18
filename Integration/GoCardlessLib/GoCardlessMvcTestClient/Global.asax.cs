2294c8b4a3c39c0602888df7dd78f748f3a81abb
Alex Boursouk <alexbo@ezbob.com>
2014-12-18 15:34:59 +0200

    Upgrade to Visual Studio 2013

--- Integration/GoCardlessLib/GoCardlessMvcTestClient/Global.asax.cs
+++ Integration/GoCardlessLib/GoCardlessMvcTestClient/Global.asax.cs
@@ -1,52 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+﻿namespace GoCardlessMvcTestClient {
+	using System.Web.Mvc;
+	using System.Web.Routing;
 
-namespace GoCardlessMvcTestClient
-{
 	using ConfigManager;
 	using Ezbob.Database;
 	using Ezbob.Logger;
 
 	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
+	// visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
-    {
-	    public MvcApplication() {
+	public class MvcApplication : System.Web.HttpApplication {
+		public MvcApplication() {
 			base.Init();
 			CurrentValues.Init(DbConnectionGenerator.Get(Log), Log);
 		}
 
-	    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+			filters.Add(new HandleErrorAttribute());
+		}
 
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+		public static void RegisterRoutes(RouteCollection routes) {
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
+			routes.MapRoute(
+				"Default", // Route name
+				"{controller}/{action}/{id}", // URL with parameters
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+			);
 
-        }
+		}
 
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
+		protected void Application_Start() {
+			AreaRegistration.RegisterAllAreas();
 
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
+			RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterRoutes(RouteTable.Routes);
 
-			
-        }
+
+		}
 
 		private static SafeILog Log {
 			get {
@@ -58,5 +49,5 @@ namespace GoCardlessMvcTestClient
 		} // Log
 
 		private static SafeILog ms_oLog;
-    }
+	}
 }
