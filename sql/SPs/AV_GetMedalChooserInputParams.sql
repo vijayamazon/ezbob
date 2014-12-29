b3c9fbb20239d343bca33004110be36d1d2135f4
Alex Boursouk <alexbo@ezbob.com>
2014-12-29 11:56:58 +0200

    EZ-2844 Unified annualized calculation - take 23 - SQL warnings removed

--- sql/SPs/AV_GetMedalChooserInputParams.sql
+++ sql/SPs/AV_GetMedalChooserInputParams.sql
@@ -9,78 +9,141 @@ ALTER PROCEDURE AV_GetMedalChooserInputParams
 @CustomerId INT
 AS
 BEGIN
+	SET NOCOUNT ON;
 
-DECLARE @IsLimited BIT = 0
-DECLARE @HasOnline BIT = 0
-DECLARE @HasHmrc BIT = 0
-DECLARE @HasBank BIT = 0
-DECLARE @HasCompanyScore BIT = 0
-DECLARE @HasPersonalScore BIT = 0
-DECLARE @NumOfHmrc INT = 0
-DECLARE @LastBankHmrcUpdateDate DATETIME  
+	DECLARE @IsLimited BIT = 0
+	DECLARE @HasOnline BIT = 0
+	DECLARE @HasHmrc BIT = 0
+	DECLARE @HasBank BIT = 0
+	DECLARE @HasCompanyScore BIT = 0
+	DECLARE @HasPersonalScore BIT = 0
+	DECLARE @NumOfHmrc INT = 0
+	DECLARE @LastBankHmrcUpdateDate DATETIME
+	DECLARE @TypeOfBusiness NVARCHAR(30)
+	DECLARE @CompanyRefNum NVARCHAR(30)
+	DECLARE @PersonalScore INT = 0
 
-DECLARE @TypeOfBusiness NVARCHAR(30)
-DECLARE @CompanyRefNum NVARCHAR(30)
-DECLARE @PersonalScore INT = 0
-SELECT @TypeOfBusiness = c.TypeOfBusiness, @CompanyRefNum = co.ExperianRefNum, @PersonalScore = c.ExperianConsumerScore
-FROM Customer c LEFT JOIN Company co ON c.CompanyId=co.Id 
-WHERE c.Id=@CustomerId
+	SELECT
+		@TypeOfBusiness = c.TypeOfBusiness,
+		@CompanyRefNum = co.ExperianRefNum,
+		@PersonalScore = c.ExperianConsumerScore
+	FROM
+		Customer c
+		LEFT JOIN Company co ON c.CompanyId = co.Id
+	WHERE
+		c.Id = @CustomerId
 
-IF @PersonalScore > 0
-	SET @HasPersonalScore = 1
+	IF @PersonalScore > 0
+		SET @HasPersonalScore = 1
 
+	IF @TypeOfBusiness = 'Limited' OR @TypeOfBusiness = 'LLP'
+		SET @IsLimited = 1
 
-IF @TypeOfBusiness = 'Limited' OR @TypeOfBusiness = 'LLP' 
-	SET @IsLimited = 1
+	DECLARE @CompanyScore INT = 0	
 
-DECLARE @CompanyScore INT = 0	
-IF @IsLimited = 1
-BEGIN
-	SELECT @CompanyScore = Score FROM CustomerAnalyticsCompany WHERE CustomerID=@CustomerId AND IsActive=1
-	IF @CompanyScore > 0 
-		SET @HasCompanyScore = 1
-END	
-ELSE
-BEGIN 
-	SELECT  @CompanyScore = nl.CommercialDelphiScore 
-	FROM ExperianNonLimitedResults nl
-	WHERE nl.RefNumber=@CompanyRefNum AND nl.IsActive=1
-	
-	IF @CompanyScore > 0 
-		SET @HasCompanyScore = 1
-END
+	IF @IsLimited = 1
+	BEGIN
+		SELECT
+			@CompanyScore = Score
+		FROM
+			CustomerAnalyticsCompany
+		WHERE
+			CustomerID = @CustomerId
+			AND
+			IsActive=1
 
-IF EXISTS (SELECT * FROM MP_CustomerMarketPlace m INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId 
-		   WHERE m.CustomerId=@CustomerId AND m.Disabled=0 AND t.InternalId IN ('A7120CB7-4C93-459B-9901-0E95E7281B59',	'A4920125-411F-4BB9-A52D-27E8A00D0A3B', '3FA5E327-FCFD-483B-BA5A-DC1815747A28'))
-	SET @HasOnline = 1
-				
-IF EXISTS (SELECT * FROM MP_CustomerMarketPlace m INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId 
-		   WHERE m.CustomerId=@CustomerId AND m.Disabled=0 AND t.InternalId = '107DE9EB-3E57-4C5B-A0B5-FFF445C4F2DF')
-	SET @HasBank = 1
+		IF @CompanyScore > 0 
+			SET @HasCompanyScore = 1
+	END
+	ELSE BEGIN 
+		SELECT
+			@CompanyScore = nl.CommercialDelphiScore
+		FROM
+			ExperianNonLimitedResults nl
+		WHERE
+			nl.RefNumber = @CompanyRefNum
+			AND
+			nl.IsActive = 1
 
-SELECT @NumOfHmrc = count(*) FROM MP_CustomerMarketPlace m INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId 
-WHERE m.CustomerId=@CustomerId AND m.Disabled=0 AND t.InternalId = 'AE85D6FC-DBDB-4E01-839A-D5BD055CBAEA'
+		IF @CompanyScore > 0
+			SET @HasCompanyScore = 1
+	END
 
-IF @NumOfHmrc > 0
-	SET @HasHmrc = 1
+	IF EXISTS (
+		SELECT *
+		FROM MP_CustomerMarketPlace m
+		INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId
+		WHERE m.CustomerId = @CustomerId
+		AND m.Disabled = 0 AND
+		t.InternalId IN ('A7120CB7-4C93-459B-9901-0E95E7281B59', 'A4920125-411F-4BB9-A52D-27E8A00D0A3B', '3FA5E327-FCFD-483B-BA5A-DC1815747A28')
+	)
+	BEGIN
+		SET @HasOnline = 1
+	END
 
-SELECT @LastBankHmrcUpdateDate = min(m.UpdatingEnd)
-FROM MP_CustomerMarketPlace m INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId 
-WHERE m.CustomerId=@CustomerId AND m.Disabled=0 AND t.InternalId IN ('AE85D6FC-DBDB-4E01-839A-D5BD055CBAEA', '107DE9EB-3E57-4C5B-A0B5-FFF445C4F2DF')
+	IF EXISTS (
+		SELECT *
+		FROM MP_CustomerMarketPlace m
+		INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId
+		WHERE m.CustomerId = @CustomerId
+		AND m.Disabled = 0
+		AND t.InternalId = '107DE9EB-3E57-4C5B-A0B5-FFF445C4F2DF'
+	)
+	BEGIN
+		SET @HasBank = 1
+	END
 
-DECLARE @MinApprovalAmount INT = (SELECT CAST(Value AS INT) FROM ConfigurationVariables WHERE Name='MedalMinOffer')
-DECLARE @MedalDaysOfMpRelevancy INT = (SELECT CAST(Value AS INT) FROM ConfigurationVariables WHERE Name='MedalDaysOfMpRelevancy')
+	SELECT
+		@NumOfHmrc = COUNT(*)
+	FROM
+		MP_CustomerMarketPlace m
+		INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId
+	WHERE
+		m.CustomerId = @CustomerId
+		AND
+		m.Disabled = 0
+		AND
+		t.InternalId = 'AE85D6FC-DBDB-4E01-839A-D5BD055CBAEA'
 
-SELECT
- @IsLimited AS IsLimited, 
- @HasOnline AS HasOnline, 
- @HasHmrc AS HasHmrc, 
- @HasBank AS HasBank, 
- @HasCompanyScore AS HasCompanyScore, 
- @HasPersonalScore AS HasPersonalScore, 
- @NumOfHmrc AS NumOfHmrc, 
- @LastBankHmrcUpdateDate AS LastBankHmrcUpdateDate,
- @MedalDaysOfMpRelevancy AS MedalDaysOfMpRelevancy,
- @MinApprovalAmount AS MinApprovalAmount
+	IF @NumOfHmrc > 0
+		SET @HasHmrc = 1
+
+	SELECT
+		@LastBankHmrcUpdateDate = MIN(m.UpdatingEnd)
+	FROM
+		MP_CustomerMarketPlace m
+		INNER JOIN MP_MarketplaceType t ON t.Id = m.MarketPlaceId
+	WHERE
+		m.CustomerId = @CustomerId
+		AND
+		m.Disabled = 0
+		AND
+		t.InternalId IN ('AE85D6FC-DBDB-4E01-839A-D5BD055CBAEA', '107DE9EB-3E57-4C5B-A0B5-FFF445C4F2DF')
+		AND
+		m.UpdatingEnd IS NOT NULL
+
+	DECLARE @MinApprovalAmount INT = (
+		SELECT CAST(Value AS INT)
+		FROM ConfigurationVariables
+		WHERE Name = 'MedalMinOffer'
+	)
+
+	DECLARE @MedalDaysOfMpRelevancy INT = (
+		SELECT CAST(Value AS INT)
+		FROM ConfigurationVariables
+		WHERE Name = 'MedalDaysOfMpRelevancy'
+	)
+
+	SELECT
+		@IsLimited AS IsLimited,
+		@HasOnline AS HasOnline,
+		@HasHmrc AS HasHmrc,
+		@HasBank AS HasBank,
+		@HasCompanyScore AS HasCompanyScore,
+		@HasPersonalScore AS HasPersonalScore,
+		@NumOfHmrc AS NumOfHmrc,
+		@LastBankHmrcUpdateDate AS LastBankHmrcUpdateDate,
+		@MedalDaysOfMpRelevancy AS MedalDaysOfMpRelevancy,
+		@MinApprovalAmount AS MinApprovalAmount
 END 
 GO
--- sql/SPs/AV_GetMedalInputParams.sql
+++ sql/SPs/AV_GetMedalInputParams.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -10,15 +9,21 @@ ALTER PROCEDURE AV_GetMedalInputParams
 @CustomerId INT
 AS
 BEGIN
+	SET NOCOUNT ON;
 
 	DECLARE @RegistrationDate DATETIME
 	DECLARE @MaritalStatus NVARCHAR(100)
 	DECLARE @TypeOfBusiness NVARCHAR(100)
-	
-	SELECT @RegistrationDate=GreetingMailSentDate, @MaritalStatus = MaritalStatus, @TypeOfBusiness = TypeOfBusiness 
-	FROM Customer 
-	WHERE Id=@CustomerId
-	
+
+	SELECT
+		@RegistrationDate = GreetingMailSentDate,
+		@MaritalStatus = MaritalStatus,
+		@TypeOfBusiness = TypeOfBusiness 
+	FROM
+		Customer 
+	WHERE
+		Id = @CustomerId
+
 	-- BusinessScore IncorporationDate TangibleEquity CurrentBalanceSum taken from CustomerAnalyticsCompany
 	DECLARE @BusinessScore INT = 0
 	DECLARE @IncorporationDate DATETIME = NULL
@@ -27,118 +32,224 @@ BEGIN
 	
 	IF @TypeOfBusiness IN ('Limited', 'LLP')
 	BEGIN
-		SELECT @BusinessScore = Score, @IncorporationDate = IncorporationDate, @TangibleEquity = TangibleEquity, @CurrentBalanceSum = CurrentBalanceSum  
-		FROM CustomerAnalyticsCompany 
-		WHERE CustomerID = @CustomerId AND IsActive=1
+		SELECT
+			@BusinessScore = Score,
+			@IncorporationDate = IncorporationDate,
+			@TangibleEquity = TangibleEquity,
+			@CurrentBalanceSum = CurrentBalanceSum  
+		FROM
+			CustomerAnalyticsCompany 
+		WHERE
+			CustomerID = @CustomerId
+			AND
+			IsActive = 1
 	END
-	ELSE
-	BEGIN
-		SELECT @IncorporationDate = nl.IncorporationDate, @BusinessScore = nl.CommercialDelphiScore FROM Customer c INNER JOIN Company co ON c.CompanyId=co.Id INNER JOIN ExperianNonLimitedResults nl ON co.ExperianRefNum = nl.RefNumber
-		WHERE c.Id = @CustomerId AND nl.IsActive=1
+	ELSE BEGIN
+		SELECT
+			@IncorporationDate = nl.IncorporationDate,
+			@BusinessScore = nl.CommercialDelphiScore
+		FROM
+			Customer c
+			INNER JOIN Company co ON c.CompanyId = co.Id
+			INNER JOIN ExperianNonLimitedResults nl ON co.ExperianRefNum = nl.RefNumber
+		WHERE
+			c.Id = @CustomerId
+			AND
+			nl.IsActive=1
 	END
-	
-	--IncorporationDate if not exist in experian
+
+	-- IncorporationDate if not exist in experian
 	
 	IF @IncorporationDate IS NULL
 	BEGIN
-		SELECT @IncorporationDate = min(x.MinDate) FROM
-		(
-		SELECT min(v.DateFrom) MinDate FROM MP_VatReturnRecords v INNER JOIN MP_CustomerMarketPlace m ON m.Id = v.CustomerMarketPlaceId 
-		WHERE m.CustomerId=@CustomerId AND m.Disabled=0
-		UNION
-		SELECT min(tr.transactionDate) MinDate FROM MP_CustomerMarketPlace m INNER JOIN MP_YodleeOrder o ON o.CustomerMarketPlaceId = m.Id
-		INNER JOIN MP_YodleeOrderItem i ON i.OrderId = o.Id
-		INNER JOIN MP_YodleeOrderItemBankTransaction tr ON tr.OrderItemId = i.Id
-		WHERE m.CustomerId=@CustomerId AND m.Disabled=0
-		UNION
-		SELECT min(tr.postDate) MinDate FROM MP_CustomerMarketPlace m INNER JOIN MP_YodleeOrder o ON o.CustomerMarketPlaceId = m.Id
-		INNER JOIN MP_YodleeOrderItem i ON i.OrderId = o.Id
-		INNER JOIN MP_YodleeOrderItemBankTransaction tr ON tr.OrderItemId = i.Id
-		WHERE m.CustomerId=@CustomerId AND m.Disabled=0
+		SELECT
+			@IncorporationDate = MIN(x.MinDate)
+		FROM (
+			SELECT
+				MIN(v.DateFrom) MinDate
+			FROM
+				MP_VatReturnRecords v
+				INNER JOIN MP_CustomerMarketPlace m ON m.Id = v.CustomerMarketPlaceId 
+			WHERE
+				m.CustomerId = @CustomerId
+				AND
+				m.Disabled = 0
+				AND
+				v.DateFrom IS NOT NULL
+			UNION
+			SELECT
+				MIN(tr.transactionDate) MinDate
+			FROM
+				MP_CustomerMarketPlace m
+				INNER JOIN MP_YodleeOrder o ON o.CustomerMarketPlaceId = m.Id
+				INNER JOIN MP_YodleeOrderItem i ON i.OrderId = o.Id
+				INNER JOIN MP_YodleeOrderItemBankTransaction tr ON tr.OrderItemId = i.Id
+			WHERE
+				m.CustomerId = @CustomerId
+				AND
+				m.Disabled = 0
+				AND
+				tr.transactionDate IS NOT NULL
+			UNION
+			SELECT
+				MIN(tr.postDate) MinDate
+			FROM
+				MP_CustomerMarketPlace m
+				INNER JOIN MP_YodleeOrder o ON o.CustomerMarketPlaceId = m.Id
+				INNER JOIN MP_YodleeOrderItem i ON i.OrderId = o.Id
+				INNER JOIN MP_YodleeOrderItemBankTransaction tr ON tr.OrderItemId = i.Id
+			WHERE
+				m.CustomerId = @CustomerId
+				AND
+				m.Disabled = 0
+				AND
+				tr.postDate IS NOT NULL
 		) x
+		WHERE x.MinDate IS NOT NULL
 	END 
-		
+
 	-- Minimal Consumer/Directors score
-	DECLARE @ConsumerScore INT 
-	SELECT @ConsumerScore = ISNULL(MIN(x.ExperianConsumerScore), 0) FROM
-	(
-	SELECT ExperianConsumerScore FROM Customer WHERE Id=@CustomerId AND ExperianConsumerScore IS NOT NULL
-	UNION
-	SELECT ExperianConsumerScore FROM Director WHERE CustomerId=@CustomerId AND ExperianConsumerScore IS NOT NULL
+	DECLARE @ConsumerScore INT
+
+	SELECT
+		@ConsumerScore = ISNULL(MIN(ISNULL(x.ExperianConsumerScore, 0)), 0)
+	FROM (
+		SELECT ExperianConsumerScore
+		FROM Customer
+		WHERE Id = @CustomerId
+		AND ExperianConsumerScore IS NOT NULL
+		UNION
+		SELECT ExperianConsumerScore
+		FROM Director
+		WHERE CustomerId = @CustomerId
+		AND ExperianConsumerScore IS NOT NULL
 	) x
 	
 	--NumOfOnTimeLoans
 	DECLARE @NumOfOnTimeLoans INT 
-	;WITH
-	late_loans AS (
-	    SELECT DISTINCT
-	        lst.LoanID
-	    FROM
-	        LoanScheduleTransaction lst
-	        INNER JOIN Loan l ON lst.LoanId = l.Id AND l.CustomerId = @CustomerID
-	        INNER JOIN LoanSchedule s ON s.Id = lst.ScheduleID
-	        INNER JOIN LoanTransaction t ON t.Id = lst.TransactionID
-	    WHERE
-	        datediff(day, s.[Date], t.PostDate) > 7 AND (abs(lst.PrincipalDelta)+abs(lst.FeesDelta)+abs(lst.InterestDelta))>2
+	;WITH late_loans AS (
+		SELECT DISTINCT
+			lst.LoanID
+		FROM
+			LoanScheduleTransaction lst
+			INNER JOIN Loan l ON lst.LoanId = l.Id AND l.CustomerId = @CustomerID
+			INNER JOIN LoanSchedule s ON s.Id = lst.ScheduleID
+			INNER JOIN LoanTransaction t ON t.Id = lst.TransactionID
+		WHERE
+			DATEDIFF(day, s.[Date], t.PostDate) > 7
+			AND
+			(ABS(lst.PrincipalDelta) + ABS(lst.FeesDelta) + ABS(lst.InterestDelta)) > 2
 	)
 	SELECT
-	    @NumOfOnTimeLoans = ISNULL(COUNT(DISTINCT l.Id), 0)
+		@NumOfOnTimeLoans = ISNULL(COUNT(DISTINCT l.Id), 0)
 	FROM
-	    Loan l
-	    LEFT JOIN late_loans ll ON l.Id = ll.LoanID
+		Loan l
+		LEFT JOIN late_loans ll ON l.Id = ll.LoanID
 	WHERE
-	    ll.LoanID IS NULL
-	    AND
-	    l.CustomerId = @CustomerID
-	    AND l.DateClosed IS NOT NULL
-	  
-	
+		ll.LoanID IS NULL
+		AND
+		l.CustomerId = @CustomerID
+		AND
+		l.DateClosed IS NOT NULL
+
 	--NumOfLatePayments
-	DECLARE @LateFeeConfigId INT = (SELECT Id FROM ConfigurationVariables WHERE Name = 'LatePaymentCharge')
+	DECLARE @LateFeeConfigId INT = (
+		SELECT Id
+		FROM ConfigurationVariables
+		WHERE Name = 'LatePaymentCharge'
+	)
+	
 	DECLARE @NumOfLatePayments INT 
 	
-	SELECT @NumOfLatePayments = count(lc.Id) 
-	FROM Loan l INNER JOIN LoanCharges lc ON l.Id=lc.LoanId 
-	WHERE l.CustomerId=@CustomerId AND lc.ConfigurationVariableId=@LateFeeConfigId
-	
+	SELECT
+		@NumOfLatePayments = COUNT(lc.Id) 
+	FROM
+		Loan l
+		INNER JOIN LoanCharges lc ON l.Id = lc.LoanId 
+	WHERE
+		l.CustomerId = @CustomerId
+		AND
+		lc.ConfigurationVariableId = @LateFeeConfigId
+
 	--NumOfEarlyPayments
 	DECLARE @NumOfEarlyPayments INT 
-	
-	SELECT @NumOfEarlyPayments = count(ls.Id)
-	FROM Loan l INNER JOIN LoanSchedule ls ON l.Id = ls.LoanId
-	WHERE l.CustomerId = @CustomerId AND ls.Status = 'PaidEarly'
-	
+
+	SELECT
+		@NumOfEarlyPayments = COUNT(ls.Id)
+	FROM
+		Loan l
+		INNER JOIN LoanSchedule ls ON l.Id = ls.LoanId
+	WHERE
+		l.CustomerId = @CustomerId
+		AND
+		ls.Status = 'PaidEarly'
+
 	--HMRC
 	DECLARE @NumOfHmrc INT 
 	DECLARE @HasHmrc BIT = 0
 	DECLARE @HasMoreThenOneHmrc BIT = 0
-	
-	SELECT @NumOfHmrc = count(mp.Id)
-	FROM MP_CustomerMarketPlace mp INNER JOIN MP_MarketplaceType t ON t.Id = mp.MarketPlaceId 
-	WHERE t.InternalId='AE85D6FC-DBDB-4E01-839A-D5BD055CBAEA' AND mp.CustomerId=@CustomerId AND mp.Disabled = 0
-	
+
+	SELECT
+		@NumOfHmrc = COUNT(mp.Id)
+	FROM
+		MP_CustomerMarketPlace mp
+		INNER JOIN MP_MarketplaceType t ON t.Id = mp.MarketPlaceId 
+	WHERE
+		t.InternalId = 'AE85D6FC-DBDB-4E01-839A-D5BD055CBAEA'
+		AND
+		mp.CustomerId = @CustomerId
+		AND
+		mp.Disabled = 0
+
 	DECLARE @FCFFactor DECIMAL(18,4)
 	DECLARE @HmrcRevenues DECIMAL(18,4) = 0
 	DECLARE @HmrcFreeCashFlow DECIMAL(18,4) = 0
 	DECLARE @HmrcValueAdded DECIMAL(18,4) = 0
 	
 	IF @NumOfHmrc > 0 SET @HasHmrc = 1
+
 	IF @NumOfHmrc > 1 SET @HasMoreThenOneHmrc = 1
+
 	IF @NumOfHmrc = 1
-		SELECT @HmrcFreeCashFlow = isnull(sum(v.AnnualizedFreeCashFlow),0), @HmrcRevenues = isnull(sum(v.AnnualizedTurnover), 0), @HmrcValueAdded = isnull(sum(v.AnnualizedValueAdded), 0)
-		FROM MP_VatReturnSummary v INNER JOIN MP_CustomerMarketPlace mp ON v.CustomerMarketplaceID = mp.Id
-		WHERE v.IsActive=1 AND mp.CustomerId=@CustomerId AND mp.Disabled=0
+	BEGIN
+		SELECT
+			@HmrcFreeCashFlow = ISNULL(SUM(ISNULL(v.AnnualizedFreeCashFlow, 0)), 0),
+			@HmrcRevenues = ISNULL(SUM(ISNULL(v.AnnualizedTurnover, 0)), 0),
+			@HmrcValueAdded = ISNULL(SUM(ISNULL(v.AnnualizedValueAdded, 0)), 0)
+		FROM
+			MP_VatReturnSummary v
+			INNER JOIN MP_CustomerMarketPlace mp ON v.CustomerMarketplaceID = mp.Id
+		WHERE
+			v.IsActive = 1
+			AND
+			mp.CustomerId = @CustomerId
+			AND
+			mp.Disabled = 0
+	END
 	
-	
-	SELECT @FCFFactor = CAST(Value AS DECIMAL(18, 4)) FROM ConfigurationVariables WHERE Name='FCFFactor'
+	SELECT
+		@FCFFactor = CAST(Value AS DECIMAL(18, 4))
+	FROM
+		ConfigurationVariables
+	WHERE
+		Name = 'FCFFactor'
 	
 	--NetWorth
 	
 	--Zoopla
-	DECLARE @ZooplaValue INT 
-	SELECT @ZooplaValue = isnull(sum(CASE WHEN z.ZooplaEstimateValue = 0 THEN z.AverageSoldPrice1Year ELSE z.ZooplaEstimateValue END), 0)  
-	FROM CustomerAddress ca INNER JOIN Zoopla z ON z.CustomerAddressId = ca.addressId 
-	WHERE CustomerId=@CustomerId AND IsOwnerAccordingToLandRegistry=1
+	DECLARE @ZooplaValue INT
+
+	SELECT
+		@ZooplaValue = ISNULL(SUM(
+			CASE WHEN z.ZooplaEstimateValue = 0 THEN z.AverageSoldPrice1Year ELSE z.ZooplaEstimateValue END
+		), 0)  
+	FROM
+		CustomerAddress ca
+		INNER JOIN Zoopla z ON z.CustomerAddressId = ca.addressId 
+	WHERE
+		CustomerId = @CustomerId
+		AND
+		IsOwnerAccordingToLandRegistry = 1
 	
 	--Mortgages
 	DECLARE @ServiceLogId BIGINT
@@ -160,48 +271,78 @@ BEGIN
 	IF @ServiceLogId IS NULL
 	BEGIN
 		SELECT TOP 1
-		   @ServiceLogId=l.Id
+			@ServiceLogId = l.Id
 		FROM
-		 Customer c 
-		 INNER JOIN CustomerAddress a ON a.CustomerId = c.Id AND a.addressType=1
-		 INNER JOIN MP_ServiceLog l on
-		  l.Firstname = c.FirstName AND
-		  l.Surname = c.Surname AND 
-		  l.DateOfBirth = c.DateOfBirth AND
-		  l.Postcode = a.Postcode AND
-		  l.ServiceType = 'Consumer Request'
-		  
-		  WHERE
-		   c.Id=@CustomerId
-		  ORDER BY
+			Customer c 
+			INNER JOIN CustomerAddress a
+		 	ON a.CustomerId = c.Id
+		 	AND a.addressType=1
+		INNER JOIN MP_ServiceLog l
+			ON l.Firstname = c.FirstName
+			AND l.Surname = c.Surname
+			AND l.DateOfBirth = c.DateOfBirth
+			AND l.Postcode = a.Postcode
+			AND l.ServiceType = 'Consumer Request'
+		WHERE
+			c.Id = @CustomerId
+		ORDER BY
 		   l.InsertDate DESC,
 		   l.Id DESC
 	END
-	
+
 	DECLARE @Mortages INT
-	SELECT @Mortages = isnull(sum(Balance),0)
-	FROM ExperianConsumerDataCais c INNER JOIN ExperianConsumerData d ON d.Id = c.ExperianConsumerDataId 
-	WHERE d.ServiceLogId=@ServiceLogId 
-	AND AccountType IN ('03','16','25','30','31','32','33','34','35','69') 
-	AND MatchTo=1 
-	AND AccountStatus <> 'S'
-	
-	--First Repayment Date
-	DECLARE @FirstRepaymentDate DATETIME = (SELECT TOP 1 ls.[Date] 
-											FROM Loan l INNER JOIN LoanSchedule ls ON ls.LoanId = l.Id 
-											WHERE l.CustomerId=@CustomerId ORDER BY ls.[Date])
-	
+
+	SELECT
+		@Mortages = ISNULL(SUM(ISNULL(Balance, 0)), 0)
+	FROM
+		ExperianConsumerDataCais c
+		INNER JOIN ExperianConsumerData d ON d.Id = c.ExperianConsumerDataId 
+	WHERE
+		d.ServiceLogId = @ServiceLogId 
+		AND
+		AccountType IN ('03','16','25','30','31','32','33','34','35','69') 
+		AND
+		MatchTo = 1 
+		AND
+		AccountStatus <> 'S'
+
+	-- First Repayment Date
+
+	DECLARE @FirstRepaymentDate DATETIME = (
+		SELECT TOP 1 ls.[Date] 
+		FROM Loan l INNER JOIN LoanSchedule ls ON ls.LoanId = l.Id 
+		WHERE l.CustomerId = @CustomerId
+		ORDER BY ls.[Date]
+	)
+
 	--Num of ebay amazon and paypal stores
+
 	DECLARE @NumOfStores INT 
-	SELECT @NumOfStores = isnull(count(mp.Id), 0) FROM MP_CustomerMarketPlace mp INNER JOIN MP_MarketplaceType t ON t.Id = mp.MarketPlaceId
-	WHERE mp.CustomerId=@CustomerId AND mp.Disabled=0
-	AND t.InternalId IN ('A7120CB7-4C93-459B-9901-0E95E7281B59', 'A4920125-411F-4BB9-A52D-27E8A00D0A3B', '3FA5E327-FCFD-483B-BA5A-DC1815747A28')
 	
+	SELECT
+		@NumOfStores = ISNULL(COUNT(mp.Id), 0)
+	FROM
+		MP_CustomerMarketPlace mp
+		INNER JOIN MP_MarketplaceType t ON t.Id = mp.MarketPlaceId
+	WHERE
+		mp.CustomerId = @CustomerId
+		AND
+		mp.Disabled = 0
+		AND
+		t.InternalId IN (
+			'A7120CB7-4C93-459B-9901-0E95E7281B59',
+			'A4920125-411F-4BB9-A52D-27E8A00D0A3B',
+			'3FA5E327-FCFD-483B-BA5A-DC1815747A28'
+		)
+
 	--Config for online cap
-	DECLARE @OnlineMedalTurnoverCutoff DECIMAL(18,4)
-	SELECT @OnlineMedalTurnoverCutoff = CAST(Value AS DECIMAL(18,4)) FROM ConfigurationVariables WHERE Name='OnlineMedalTurnoverCutoff'
-	
-	
+
+	DECLARE @OnlineMedalTurnoverCutoff DECIMAL(18,4) = (
+		SELECT CAST(Value AS DECIMAL(18,4))
+		FROM ConfigurationVariables
+		WHERE Name = 'OnlineMedalTurnoverCutoff'
+	)
+
 	-- Return All Params
 	SELECT 
 		@BusinessScore AS BusinessScore,
