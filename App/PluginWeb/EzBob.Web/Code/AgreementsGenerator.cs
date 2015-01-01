0f0caf64325afee66f6d3badd557b329eafc9ffc
Alex Boursouk <alexbo@ezbob.com>
2015-01-01 20:30:28 +0200

    fix contracts for pship TypeOfBusiness (kinda cherry pick)

--- App/PluginWeb/EzBob.Web/Code/AgreementsGenerator.cs
+++ App/PluginWeb/EzBob.Web/Code/AgreementsGenerator.cs
@@ -1,42 +1,31 @@
-﻿namespace EzBob.Web.Code.Agreements
-{
+﻿namespace EzBob.Web.Code.Agreements {
+	using System.IO;
 	using ConfigManager;
+	using Ezbob.Backend.Models;
+	using EzBob.Models.Agreements;
 	using EZBob.DatabaseLib;
 	using EZBob.DatabaseLib.Model.Database;
 	using EZBob.DatabaseLib.Model.Database.Loans;
 	using EZBob.DatabaseLib.Model.Loans;
-	using EzBob.Models.Agreements;
-	using Ezbob.Backend.Models;
 	using Newtonsoft.Json;
-	using System.IO;
 	using ServiceClientProxy;
 
-	public interface IAgreementsGenerator
-	{
+	public interface IAgreementsGenerator {
 		void RenderAgreements(Loan loan, bool isRebuld);
 	}
 
-	public class AgreementsGenerator : IAgreementsGenerator
-	{
-		private readonly AgreementsModelBuilder _builder;
-		private readonly IAgreementsTemplatesProvider _templates;
-		private readonly DatabaseDataHelper _helper;
-		private readonly ServiceClient m_oServiceClient;
-
-		public AgreementsGenerator(AgreementsModelBuilder builder, IAgreementsTemplatesProvider templates, DatabaseDataHelper helper)
-		{
-			_builder = builder;
-			_templates = templates;
-			_helper = helper;
-			m_oServiceClient = new ServiceClient();
+	public class AgreementsGenerator : IAgreementsGenerator {
+		public AgreementsGenerator(AgreementsModelBuilder builder, IAgreementsTemplatesProvider templates, DatabaseDataHelper helper) {
+			this._builder = builder;
+			this._templates = templates;
+			this._helper = helper;
+			this.m_oServiceClient = new ServiceClient();
 		}
 
-		public void RenderAgreements(Loan loan, bool isRebuld)
-		{
-			var typeOfBusinessReduced = loan.Customer.PersonalInfo.TypeOfBusiness.Reduce();
-			var isConsumer = typeOfBusinessReduced == TypeOfBusinessReduced.Personal || typeOfBusinessReduced == TypeOfBusinessReduced.NonLimited;
+		public void RenderAgreements(Loan loan, bool isRebuld) {
+			var typeOfBusinessReduced = loan.Customer.PersonalInfo.TypeOfBusiness.AgreementReduce();
 
-			var model = !isRebuld ? _builder.Build(loan.Customer, loan.LoanAmount, loan) : _builder.ReBuild(loan.Customer, loan);
+			var model = !isRebuld ? this._builder.Build(loan.Customer, loan.LoanAmount, loan) : this._builder.ReBuild(loan.Customer, loan);
 
 			var isAlibaba = loan.Customer.IsAlibaba;
 			var alibaba = isAlibaba ? "Alibaba" : string.Empty;
@@ -44,52 +33,58 @@
 			string path1;
 			string path2;
 			TemplateModel template;
-			if (isConsumer)
-			{
-				var preContract = _templates.GetTemplateByName(alibaba + "Pre-Contract-Agreement");
-				var preContractAgreement = new LoanAgreement("precontract", loan, 
-					_helper.GetOrCreateLoanAgreementTemplate(preContract, isAlibaba ? LoanAgreementTemplateType.AlibabaPreContractAgreement : LoanAgreementTemplateType.PreContractAgreement ));
+			if (typeOfBusinessReduced == TypeOfBusinessAgreementReduced.Personal) {
+				var preContract = this._templates.GetTemplateByName(alibaba + "Pre-Contract-Agreement");
+				var preContractAgreement = new LoanAgreement("precontract", loan, this._helper.GetOrCreateLoanAgreementTemplate(preContract, isAlibaba ? LoanAgreementTemplateType.AlibabaPreContractAgreement : LoanAgreementTemplateType.PreContractAgreement));
 				loan.Agreements.Add(preContractAgreement);
 
 				path1 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath1, preContractAgreement.FilePath);
 				path2 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath2, preContractAgreement.FilePath);
-				template = new TemplateModel {Template = preContract};
-				m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "precontract", template, path1, path2);
+				template = new TemplateModel {
+					Template = preContract
+				};
+				this.m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "precontract", template, path1, path2);
 
-				var contract = _templates.GetTemplateByName(alibaba + "CreditActAgreement");
-				var contractAgreement = new LoanAgreement("Contract", loan, 
-					_helper.GetOrCreateLoanAgreementTemplate(contract, isAlibaba ? LoanAgreementTemplateType.AlibabaCreditActAgreement : LoanAgreementTemplateType.CreditActAgreement ));
+				var contract = this._templates.GetTemplateByName(alibaba + "CreditActAgreement");
+				var contractAgreement = new LoanAgreement("Contract", loan, this._helper.GetOrCreateLoanAgreementTemplate(contract, isAlibaba ? LoanAgreementTemplateType.AlibabaCreditActAgreement : LoanAgreementTemplateType.CreditActAgreement));
 				loan.Agreements.Add(contractAgreement);
 
 				path1 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath1, contractAgreement.FilePath);
 				path2 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath2, contractAgreement.FilePath);
-				template = new TemplateModel { Template = contract };
-				m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "Contract", template, path1, path2);
-			}
-			else
-			{
-				var guarantee = _templates.GetTemplateByName(alibaba + "GuarantyAgreement");
-				var guaranteeAgreement = new LoanAgreement("guarantee", loan, 
-					_helper.GetOrCreateLoanAgreementTemplate(guarantee, isAlibaba ? LoanAgreementTemplateType.AlibabaGuarantyAgreement : LoanAgreementTemplateType.GuarantyAgreement ));
+				template = new TemplateModel {
+					Template = contract
+				};
+				this.m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "Contract", template, path1, path2);
+			} else {
+				var guarantee = this._templates.GetTemplateByName(alibaba + "GuarantyAgreement");
+				var guaranteeAgreement = new LoanAgreement("guarantee", loan, this._helper.GetOrCreateLoanAgreementTemplate(guarantee, isAlibaba ? LoanAgreementTemplateType.AlibabaGuarantyAgreement : LoanAgreementTemplateType.GuarantyAgreement));
 				loan.Agreements.Add(guaranteeAgreement);
 
 				path1 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath1, guaranteeAgreement.FilePath);
 				path2 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath2, guaranteeAgreement.FilePath);
-				template = new TemplateModel { Template = guarantee };
-				m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "guarantee", template, path1, path2);
+				template = new TemplateModel {
+					Template = guarantee
+				};
+				this.m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "guarantee", template, path1, path2);
 
-				var agreement = _templates.GetTemplateByName(alibaba + "PrivateCompanyLoanAgreement");
-				var agreementAgreement = new LoanAgreement("agreement", loan,
-					_helper.GetOrCreateLoanAgreementTemplate(agreement, isAlibaba ? LoanAgreementTemplateType.AlibabaPrivateCompanyLoanAgreement : LoanAgreementTemplateType.PrivateCompanyLoanAgreement));
+				var agreement = this._templates.GetTemplateByName(alibaba + "PrivateCompanyLoanAgreement");
+				var agreementAgreement = new LoanAgreement("agreement", loan, this._helper.GetOrCreateLoanAgreementTemplate(agreement, isAlibaba ? LoanAgreementTemplateType.AlibabaPrivateCompanyLoanAgreement : LoanAgreementTemplateType.PrivateCompanyLoanAgreement));
 				loan.Agreements.Add(agreementAgreement);
 
 				path1 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath1, agreementAgreement.FilePath);
 				path2 = Path.Combine(CurrentValues.Instance.AgreementPdfLoanPath2, agreementAgreement.FilePath);
-				template = new TemplateModel { Template = agreement };
-				m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "agreement", template, path1, path2);
+				template = new TemplateModel {
+					Template = agreement
+				};
+				this.m_oServiceClient.Instance.SaveAgreement(loan.Customer.Id, model, loan.RefNumber, "agreement", template, path1, path2);
 			}
 
 			loan.AgreementModel = JsonConvert.SerializeObject(model);
 		}
+
+		private readonly AgreementsModelBuilder _builder;
+		private readonly IAgreementsTemplatesProvider _templates;
+		private readonly DatabaseDataHelper _helper;
+		private readonly ServiceClient m_oServiceClient;
 	}
 }
--- Integration/DatabaseLib/Model/Database/Company.cs
+++ Integration/DatabaseLib/Model/Database/Company.cs
@@ -1,14 +1,80 @@
-namespace EZBob.DatabaseLib.Model.Database
-{
+namespace EZBob.DatabaseLib.Model.Database {
 	using FluentNHibernate.Mapping;
 	using Iesi.Collections.Generic;
 	using NHibernate.Type;
 
-	
-	#region class Company
+	public enum TypeOfBusinessReduced {
+		Personal,
+		Limited,
+		NonLimited,
+	}
 
-	public class Company
-	{
+	public enum TypeOfBusinessAgreementReduced {
+		Personal,
+		Business,
+	} // enum TypeOfBusinessAgreementReduced
+
+	public static class TypeOfBusinessExtenstions {
+		public static TypeOfBusinessReduced Reduce(this TypeOfBusiness business) {
+			switch (business) {
+			case TypeOfBusiness.Entrepreneur:
+				return TypeOfBusinessReduced.Personal;
+
+			case TypeOfBusiness.LLP:
+			case TypeOfBusiness.Limited:
+				return TypeOfBusinessReduced.Limited;
+
+			case TypeOfBusiness.PShip:
+			case TypeOfBusiness.PShip3P:
+			case TypeOfBusiness.SoleTrader:
+				return TypeOfBusinessReduced.NonLimited;
+			} // switch
+
+			return TypeOfBusinessReduced.Personal;
+		} // Reduse
+
+		public static TypeOfBusinessAgreementReduced AgreementReduce(this TypeOfBusiness typeOfBusiness) {
+			switch (typeOfBusiness) {
+			case TypeOfBusiness.Entrepreneur:
+			case TypeOfBusiness.PShip3P:
+			case TypeOfBusiness.SoleTrader:
+				return TypeOfBusinessAgreementReduced.Personal;
+
+			case TypeOfBusiness.LLP:
+			case TypeOfBusiness.Limited:
+			case TypeOfBusiness.PShip:
+				return TypeOfBusinessAgreementReduced.Business;
+			}
+
+			return TypeOfBusinessAgreementReduced.Personal;
+		}
+
+		public static string TypeOfBussinessForWeb(TypeOfBusiness businessReduced) {
+			switch (businessReduced) {
+			case TypeOfBusiness.Limited:
+				return "Limited Company";
+
+			case TypeOfBusiness.Entrepreneur:
+				return "Sole Trader (not Inc.)";
+
+			case TypeOfBusiness.LLP:
+				return "-";
+
+			case TypeOfBusiness.PShip:
+				return "Partnership (More than 3)";
+
+			case TypeOfBusiness.PShip3P:
+				return "Partnership (Up to 3)";
+
+			case TypeOfBusiness.SoleTrader:
+				return "Sole Trader (Inc.)";
+			} // switch
+
+			return "";
+		} // TypeOfBussinessForWeb
+	}
+
+	public class Company {
 		public virtual int Id { get; set; }
 		public virtual TypeOfBusiness TypeOfBusiness { get; set; }
 		public virtual VatReporting? VatReporting { get; set; }
@@ -23,164 +89,120 @@ namespace EZBob.DatabaseLib.Model.Database
 		public virtual double? CapitalExpenditure { get; set; }
 		public virtual string ExperianRefNum { get; set; }
 		public virtual string ExperianCompanyName { get; set; }
-		
+
+		public virtual ISet<CustomerAddress> CompanyAddress {
+			get { return this._companyAddress; }
+			set { this._companyAddress = value; }
+		}
+
+		public virtual ISet<CustomerAddress> ExperianCompanyAddress {
+			get { return this._experianCompanyAddress; }
+			set { this._experianCompanyAddress = value; }
+		}
+
+		public virtual ISet<CompanyEmployeeCount> CompanyEmployeeCount {
+			get { return this._companyEmployeeCount; }
+			set { this._companyEmployeeCount = value; }
+		}
+
+		public virtual ISet<Director> Directors {
+			get { return this._directors; }
+			set { this._directors = value; }
+		}
+
 		private ISet<CustomerAddress> _companyAddress = new HashedSet<CustomerAddress>();
-		public virtual ISet<CustomerAddress> CompanyAddress
-		{
-			get { return _companyAddress; }
-			set { _companyAddress = value; }
-		} //CompanyAddress
+		//CompanyAddress
 
 		private ISet<CustomerAddress> _experianCompanyAddress = new HashedSet<CustomerAddress>();
-		public virtual ISet<CustomerAddress> ExperianCompanyAddress
-		{
-			get { return _experianCompanyAddress; }
-			set { _experianCompanyAddress = value; }
-		} //CompanyAddress
+		//CompanyAddress
 
 		private ISet<CompanyEmployeeCount> _companyEmployeeCount = new HashedSet<CompanyEmployeeCount>();
-		public virtual ISet<CompanyEmployeeCount> CompanyEmployeeCount
-		{
-			get { return _companyEmployeeCount; }
-			set { _companyEmployeeCount = value; }
-		} // CompanyEmployeeCount
+		// CompanyEmployeeCount
 
 		private ISet<Director> _directors = new HashedSet<Director>();
-		
-		public virtual ISet<Director> Directors
-		{
-			get { return _directors; }
-			set { _directors = value; }
-		} // Directors
 
+		// Directors
 	} // class Company
 
-	#endregion class Company
-
-	#region CompanyMap
-	public class CompanyMap : ClassMap<Company>
-	{
-		public CompanyMap()
-		{
+	public class CompanyMap : ClassMap<Company> {
+		public CompanyMap() {
 			Table("Company");
-			Cache.ReadWrite().Region("LongTerm").ReadWrite();
+			Cache.ReadWrite()
+				.Region("LongTerm")
+				.ReadWrite();
 
 			Id(x => x.Id);
-			Map(x => x.TypeOfBusiness).CustomType<TypeOfBusinessType>();
-			Map(x => x.VatReporting).CustomType<VatReportingType>();
-			Map(x => x.CompanyNumber).Length(100);
-			Map(x => x.CompanyName).Length(300);
+			Map(x => x.TypeOfBusiness)
+				.CustomType<TypeOfBusinessType>();
+			Map(x => x.VatReporting)
+				.CustomType<VatReportingType>();
+			Map(x => x.CompanyNumber)
+				.Length(100);
+			Map(x => x.CompanyName)
+				.Length(300);
 			Map(x => x.TimeAtAddress);
-			Map(x => x.TimeInBusiness).Length(250);
-			Map(x => x.BusinessPhone).Length(250);
-			Map(x => x.PropertyOwnedByCompany).Length(250);
-			Map(x => x.YearsInCompany).Length(250);
-			Map(x => x.RentMonthLeft).Length(250);
-			Map(x => x.ExperianRefNum).Length(250);
-			Map(x => x.ExperianCompanyName).Length(250);
+			Map(x => x.TimeInBusiness)
+				.Length(250);
+			Map(x => x.BusinessPhone)
+				.Length(250);
+			Map(x => x.PropertyOwnedByCompany)
+				.Length(250);
+			Map(x => x.YearsInCompany)
+				.Length(250);
+			Map(x => x.RentMonthLeft)
+				.Length(250);
+			Map(x => x.ExperianRefNum)
+				.Length(250);
+			Map(x => x.ExperianCompanyName)
+				.Length(250);
 
 			HasMany(x => x.CompanyAddress)
-				 .AsSet()
-				 .KeyColumn("CompanyId")
-				 .Where(a => a.AddressType != CustomerAddressType.ExperianCompanyAddress)
-				 .Cascade.All()
-				 .Inverse()
-				 .Cache.ReadWrite().Region("LongTerm").ReadWrite();
+				.AsSet()
+				.KeyColumn("CompanyId")
+				.Where(a => a.AddressType != CustomerAddressType.ExperianCompanyAddress)
+				.Cascade.All()
+				.Inverse()
+				.Cache.ReadWrite()
+				.Region("LongTerm")
+				.ReadWrite();
 
 			HasMany(x => x.ExperianCompanyAddress)
-				 .AsSet()
-				 .KeyColumn("CompanyId")
-				 .Where(a => a.AddressType == CustomerAddressType.ExperianCompanyAddress)
-				 .Cascade.All()
-				 .Inverse()
-				 .Cache.ReadWrite().Region("LongTerm").ReadWrite();
+				.AsSet()
+				.KeyColumn("CompanyId")
+				.Where(a => a.AddressType == CustomerAddressType.ExperianCompanyAddress)
+				.Cascade.All()
+				.Inverse()
+				.Cache.ReadWrite()
+				.Region("LongTerm")
+				.ReadWrite();
 
 			HasMany(x => x.Directors)
-				 .AsSet()
-				 .KeyColumn("CompanyId")
-				 .Cascade.All()
-				 .Inverse()
-				 .Cache.ReadWrite().Region("LongTerm").ReadWrite();
+				.AsSet()
+				.KeyColumn("CompanyId")
+				.Cascade.All()
+				.Inverse()
+				.Cache.ReadWrite()
+				.Region("LongTerm")
+				.ReadWrite();
 
 			HasMany(x => x.CompanyEmployeeCount)
 				.AsSet()
 				.KeyColumn("CompanyId")
 				.Cascade.All()
 				.Inverse()
-				.Cache.ReadWrite().Region("LongTerm").ReadWrite();
+				.Cache.ReadWrite()
+				.Region("LongTerm")
+				.ReadWrite();
 		} // constructor
 	} // class CompanyMap
-	#endregion 
-
-	#region enum TypeOfBusinessReduced
 
 	public class TypeOfBusinessType : EnumStringType<TypeOfBusiness> { }
-	
+
 	public class IndustryTypeType : EnumStringType<IndustryType> { }
 
-	public class VatReportingType: EnumStringType<VatReporting> { }
+	public class VatReportingType : EnumStringType<VatReporting> { }
 
-	public enum TypeOfBusinessReduced
-	{
-		Personal,
-		Limited,
-		NonLimited,
-	} // enum TypeOfBusinessReduced
+	// enum TypeOfBusinessReduced
 
-	#endregion enum TypeOfBusinessReduced
-
-	#region class TypeOfBusinessExtenstions
-
-	public static class TypeOfBusinessExtenstions
-	{
-		public static TypeOfBusinessReduced Reduce(this TypeOfBusiness business)
-		{
-			switch (business)
-			{
-				case TypeOfBusiness.Entrepreneur:
-					return TypeOfBusinessReduced.Personal;
-
-				case TypeOfBusiness.LLP:
-				case TypeOfBusiness.Limited:
-					return TypeOfBusinessReduced.Limited;
-
-				case TypeOfBusiness.PShip:
-				case TypeOfBusiness.PShip3P:
-				case TypeOfBusiness.SoleTrader:
-					return TypeOfBusinessReduced.NonLimited;
-			} // switch
-
-			return TypeOfBusinessReduced.Personal;
-		} // Reduse
-
-		public static string TypeOfBussinessForWeb(TypeOfBusiness businessReduced)
-		{
-			switch (businessReduced)
-			{
-				case TypeOfBusiness.Limited:
-					return "Limited Company";
-
-				case TypeOfBusiness.Entrepreneur:
-					return "Sole Trader (not Inc.)";
-
-				case TypeOfBusiness.LLP:
-					return "-";
-
-				case TypeOfBusiness.PShip:
-					return "Partnership (More than 3)";
-
-				case TypeOfBusiness.PShip3P:
-					return "Partnership (Up to 3)";
-
-				case TypeOfBusiness.SoleTrader:
-					return "Sole Trader (Inc.)";
-			} // switch
-
-			return "";
-		} // TypeOfBussinessForWeb
-	} // class TypeOfBusinessExtenstions
-
-	#endregion class TypeOfBusinessExtenstions
-
-	
+	// class TypeOfBusinessExtenstions
 } // namespace
