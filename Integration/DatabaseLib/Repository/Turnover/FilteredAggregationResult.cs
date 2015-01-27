bb0d942f6a750ae5173a9a8b5886dd0cf4146fa4
elinar <elinar@ezbob.com>
2015-01-27 20:30:28 +0200

    EZ-2843 "mismatching"-2

---
+++ Integration/DatabaseLib/Repository/Turnover/FilteredAggregationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZBob.DatabaseLib.Repository.Turnover {
+	public class FilteredAggregationResult {
+
+		public virtual DateTime TheMonth { get; set; }
+		public virtual decimal Turnover { get; set; }
+
+		public virtual int MpId { get; set; }
+
+	}
+}
---
+++ Integration/DatabaseLib/Repository/Turnover/MarketPlaceAggregation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZBob.DatabaseLib.Repository.Turnover {
+	using EZBob.DatabaseLib.Model.Database;
+
+	public class MarketPlaceAggregation {
+
+		public virtual System.DateTime TheMonth { get; set; }
+		public virtual bool IsActive { get; set; }
+		public virtual decimal Turnover { get; set; }
+		public virtual MP_CustomerMarketplaceUpdatingHistory CustomerMarketPlaceUpdatingHistory { get; set; }
+	}
+}
