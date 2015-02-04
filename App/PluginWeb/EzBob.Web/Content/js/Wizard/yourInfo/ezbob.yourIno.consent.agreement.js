10e631597ebf09ea035d9a633bd87678208c955a
stas_dulman <stasd@ezbob.com>
2015-02-04 12:49:21 +0200

    fix consent download and print

--- App/PluginWeb/EzBob.Web/Content/js/Wizard/yourInfo/ezbob.yourIno.consent.agreement.js
+++ App/PluginWeb/EzBob.Web/Content/js/Wizard/yourInfo/ezbob.yourIno.consent.agreement.js
@@ -2,20 +2,31 @@ var EzBob = EzBob || {};
 
 EzBob.ConsentAgreementModel = Backbone.Model.extend({
 	defaults: {
-		fullName: 'Jane Doe',
-		date: 'sssssss'
-	}, // defaults
+		id: 0,
+		firstName: '',
+		middleInitial: '',
+		surname: '',
+		date: moment.utc().toDate()
+	} // defaults
 }); // EzBob.ConsentAgreementModel
 
 EzBob.ConsentAgreement = Backbone.Marionette.ItemView.extend({
 	template: '#consent-agreement-temlate',
 
 	events: {
-		'click .print': 'onPrint'
+		'click .print': 'onPrint',
+        'click .download': 'onDownload'
 	}, // events
 
+	onDownload: function() {
+		var id = this.model.get('id');
+		var firstName = this.model.get('firstName');
+		var middleInitial = this.model.get('middleInitial');
+		var surname = this.model.get('surname');
+		location.href = window.gRootPath + "Customer/Consent/Download?id=" + id + "&firstName=" + firstName + "&middleInitial=" + middleInitial + "&surname=" + surname;
+	}, //onDownload
 	onPrint: function() {
-		return printElement('consent');
-	}, // onPrint
+		printElement("consent-conent");
+	}//onPrint
 }); // EzBob.ConsentAgreement
 
