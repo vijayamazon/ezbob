c853fd148372c26220d49e00e2e6f6e7ab57374b
Alex Boursouk <alexbo@ezbob.com>
2015-02-19 14:23:43 +0200

    Paypoint reconciliation fix.
    (cherry picked from commit 16e03b405ff5a754c23e170ff46b8e3febf7a578)

--- App/PayPointBalance/Processor.cs
+++ App/PayPointBalance/Processor.cs
@@ -1,32 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using Ezbob.Database;
-using Ezbob.Logger;
-using PayPoint;
-
-namespace PayPointBalance {
+﻿namespace PayPointBalance {
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Linq;
+	using System.Reflection;
+	using Ezbob.Database;
+	using Ezbob.Logger;
+	using Ezbob.Utils.Lingvo;
+	using PayPoint;
 
 	public class Processor : SafeLog {
-
 		public Processor(Conf cfg, DateTime oDate, ASafeLog oLog = null) : base(oLog) {
 			m_oDate = oDate;
-			m_sMid = cfg.PayPointMid;
-			m_sVpnPassword = cfg.PayPointVpnPassword;
-			m_sRemotePassword = cfg.PayPointRemotePassword;
-			m_nRetryCount = cfg.PayPointRetryCount;
-			m_nSleepInterval = cfg.PayPointSleepInterval;
+
+			this.cfg = cfg;
 
 			Debug("\n\n***\n*** PayPointBalance.Processor configuration\n***\n");
 
 			Debug("Date: {0}.", m_oDate.ToString("MMMM d yyyy H:mm:ss", CultureInfo.InvariantCulture));
-			Debug("Mid: {0}.", m_sMid);
-			Debug("VPN password: **************.");
-			Debug("Remote password: ++++++++++++++.");
-			Debug("Retry count: {0} time{1}.", m_nRetryCount, m_nRetryCount == 1 ? "" : "s");
-			Debug("Sleep interval between retries: {0} seconds.", m_nSleepInterval);
+
+			foreach (var ac in this.cfg.Accounts)
+
+			Debug("{0}.", ac);
 
 			Debug("\n\n***\n*** End of PayPointBalance.Processor configuration\n***\n");
 		} // constructor
@@ -38,8 +33,6 @@ namespace PayPointBalance {
 
 			m_sPaypointCondition = m_oDate.ToString(DateFormat);
 
-			Info("Downloading PayPoint transactions for {0} using login {1}", m_sPaypointCondition, m_sMid);
-
 			Debug("PayPointBalance.Init() complete.");
 			return true;
 		} // Init
@@ -49,16 +42,18 @@ namespace PayPointBalance {
 
 			try {
 				DeleteOldData(m_oDB, m_oDate);
-
-				Save(
-					m_oDB,
-					Fetch(m_sPaypointCondition, m_sMid, m_sVpnPassword, m_sRemotePassword),
-					LoadColumns(m_oDB)
-				);
 			}
 			catch (Exception e) {
-				Error("Something went terribly wrong: {0}", e);
+				Error(e, "Something went terribly wrong while deleting old PayPoint data.");
 			} // try
+
+			foreach (var account in this.cfg.Accounts) {
+				try {
+					Save(m_oDB, Fetch(m_sPaypointCondition, account), LoadColumns(m_oDB));
+				} catch (Exception e) {
+					Error(e, "Something went terribly wrong.");
+				} // try
+			} // for each account
 
 			Debug("PayPointBalance.Run() complete.");
 		} // Run
@@ -83,27 +78,58 @@ namespace PayPointBalance {
 			Debug("PayPointBalance.DeleteOldData complete.");
 		} // DeleteOldData
 
-		private PayPointDataSet.TransactionDataTable Fetch(string sCondition, string sMid, string sVpnPassword, string sRemotePassword) {
-			Debug("PayPointBalance.Fetch started...");
+		private PayPointDataSet.TransactionDataTable Fetch(string sCondition, Conf.Account account) {
+			Debug(
+				"PayPointBalance.Fetch started using condition '{0}' from PayPoint account {1}...",
+				sCondition,
+				account.Mid
+			);
 
 			PayPointDataSet.TransactionDataTable tbl = null;
 
-			for (int r = 1; r <= m_nRetryCount; r++) {
-				Debug("Attempt {0}: fetching data from PayPoint...", r);
+			for (int r = 1; r <= account.RetryCount; r++) {
+				Debug("Attempt {0}: fetching data from PayPoint account {1}...", r, account.Mid);
 
 				try {
-					tbl = PayPointConnector.GetOrdersDragonStyle(sCondition, sMid, sVpnPassword, sRemotePassword);
-					Info("Attempt {2}: {0} row{1} fetched from PayPoint.", tbl.Rows.Count, tbl.Rows.Count == 1 ? "" : "s", r);
+					tbl = PayPointConnector.GetOrdersDragonStyle(
+						sCondition,
+						account.Mid,
+						account.VpnPassword,
+						account.RemotePassword
+					);
+
+					Info(
+						"Attempt {1}: {0} fetched from PayPoint account {2}.",
+						Grammar.Number(tbl.Rows.Count, "row"),
+						r,
+						account.Mid
+					);
+
 					break;
 				}
 				catch (Exception e) {
-					string sMsg = (r == m_nRetryCount) ? string.Empty : string.Format("\n\nRetrying in {0} seconds...", m_nSleepInterval);
-					Error("Attempt {0}: failed to fetch data from PayPoint with exception {1}{2}", r, e, sMsg);
-					System.Threading.Thread.Sleep(m_nSleepInterval * 1000);
+					string sMsg = (r == account.RetryCount)
+						? string.Empty
+						: string.Format("\n\nRetrying in {0} seconds...", account.SleepInterval);
+
+					Error(
+						e,
+						"Attempt {0}: failed to fetch data from PayPoint account {1} with exception {2}",
+						r,
+						account.Mid,
+						sMsg
+					);
+
+					System.Threading.Thread.Sleep(account.SleepInterval * 1000);
 				} // try
 			} // for
 
-			Say(tbl == null ? Severity.Error : Severity.Debug, "PayPointBalance.Fetch {0}.", tbl == null ? "failed" : "complete");
+			Say(
+				tbl == null ? Severity.Error : Severity.Debug,
+				"PayPointBalance.Fetch {0} from PayPoint account {1}.",
+				tbl == null ? "failed" : "complete",
+				account.Mid
+			);
 
 			return tbl;
 		} // Fetch
@@ -155,7 +181,7 @@ namespace PayPointBalance {
 				select (string)sr[0]
 			).ToList();
 
-			Info("{0} columns found.", oRes.Count);
+			Info("{0} found.", Grammar.Number(oRes.Count, "count"));
 
 			Debug("PayPointBalance.LoadColumns complete.");
 			return oRes;
@@ -181,11 +207,7 @@ namespace PayPointBalance {
 		private DateTime m_oDate;
 		private AConnection m_oDB;
 
-		private readonly string m_sMid;
-		private readonly string m_sVpnPassword;
-		private readonly string m_sRemotePassword;
-		private readonly int m_nRetryCount;
-		private readonly int m_nSleepInterval;
+		private readonly Conf cfg;
 
 		private const string DateFormat = "yyyyMMdd";
 
