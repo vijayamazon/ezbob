b147715b56f06c2db881bde4500063f9e89ee165
stas_dulman <stasd@ezbob.com>
2015-02-22 16:31:18 +0200

    GA virtual page views only for ezbob

--- App/PluginWeb/EzBob.Web/Content/js/App/ezbob.GA.js
+++ App/PluginWeb/EzBob.Web/Content/js/App/ezbob.GA.js
@@ -4,14 +4,15 @@ EzBob.GA = (function() {
 	function GA() { }
 
 	GA.prototype.trackPage = function(url) {
-		if ((typeof _gaq !== "undefined" && _gaq !== null))
-			_gaq.push(['_trackPageview', url]);
-		else if ((typeof ga !== "undefined" && ga !== null)) {
-			ga('create', 'UA-32583191-1', 'auto');
-			ga('send', 'pageview', url);
+		if (window.location.hostname.indexOf('ezbob') > -1) {
+			if ((typeof _gaq !== "undefined" && _gaq !== null))
+				_gaq.push(['_trackPageview', url]);
+			else if ((typeof ga !== "undefined" && ga !== null)) {
+				ga('create', 'UA-32583191-1', 'auto');
+				ga('send', 'pageview', url);
+			} else
+				console.log('Track PageView: %s', url);
 		}
-		else
-			console.log('Track PageView: %s', url);
 	};
 
 	return GA;
