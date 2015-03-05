25cfc033c5b77eb26a82617f7c57c8fc5495a0e2
stas_dulman <stasd@ezbob.com>
2015-03-05 15:01:30 +0200

    EZ-3147 improve UW performance - reduce full customer controller

---
+++ App/PluginWeb/EzBob.Web/Content/js/Underwriter/profile/summary/ezbob.underwriter.journal.js
@@ -0,0 +1,115 @@
+﻿var EzBob = EzBob || {};
+EzBob.Underwriter = EzBob.Underwriter || {};
+
+EzBob.Underwriter.JournalView = Backbone.Marionette.ItemView.extend({
+	template: '#journal-template',
+	initialize: function(options) {
+		this.crmModel = options.crmModel;
+
+    	this.bindTo(this.model, 'change sync', this.render, this);
+    	this.bindTo(this.crmModel, 'change sync', this.render, this);
+    	this.journal = [];
+    	this.journalTable = null;
+    	BlockUi('on', this.$el);
+
+        return this;
+    },
+
+    onRender: function() {
+	    this.buildJournal();
+    	BlockUi('off', this.$el);
+    },
+
+    buildJournal: function() {
+
+    	this.journal = [];
+    	if (this.crmModel.get('CustomerRelations')) {
+    		_.each(this.crmModel.get('CustomerRelations'), (function(_this) {
+    			return function(crm) {
+    				return _this.journal.push({
+    					Action: crm.Action,
+    					Adate: new Date(moment.utc(crm.DateTime)),
+    					Type: crm.Type,
+    					Status: crm.Status,
+    					ApprovedSum: null,
+    					Interest: null,
+    					RepaymentPeriod: null,
+    					LoanType: null,
+    					SetupFee: null,
+    					DiscountPlan: null,
+    					LoanSource: null,
+    					CustomerSelection: null,
+    					UW: crm.User,
+    					Comment: crm.Comment,
+    					IsCrm: true
+    				});
+    			};
+    		})(this));
+
+    	}
+    	if (this.model.get('DecisionHistory')) {
+    		_.each(this.model.get('DecisionHistory'), (function(_this) {
+    			return function(dh) {
+    				return _this.journal.push({
+    					Action: dh.Action,
+    					Adate: new Date(moment.utc(dh.Date)),
+    					Type: null,
+    					Status: null,
+    					ApprovedSum: dh.ApprovedSum,
+    					Interest: dh.InterestRate,
+    					RepaymentPeriod: dh.RepaymentPeriod,
+    					LoanType: dh.LoanType.split(" ")[0],
+    					SetupFee: (dh.UseSetupFee || dh.UseBrokerSetupFee ? 'yes (todo %)' : '-'),
+    					DiscountPlan: dh.DiscountPlan,
+    					LoanSource: (dh.LoanSourceName === 'Standard' ? '' : dh.LoanSourceName),
+    					CustomerSelection: (dh.IsLoanTypeSelectionAllowed === 1 ? 'Yes' : 'No'),
+    					UW: dh.UnderwriterName,
+    					Comment: dh.Comment,
+    					IsCrm: false
+    				});
+    			};
+    		})(this));
+    	}
+
+    	if (this.journal.length > 0 && this.model && this.crmModel) {
+    		try {
+    			if (this.journalTable) {
+    				this.journalTable.fnClearTable();
+    				this.journalTable = null;
+    			}
+
+    			this.journalTable = this.$el.find("#journalTable").dataTable({
+    				aLengthMenu: [[-1, 10, 20, 50, 100], ['all', 10, 20, 50, 100]],
+    				iDisplayLength: 20,
+    				sPaginationType: 'bootstrap',
+    				bJQueryUI: false,
+    				aaSorting: [[1, 'desc']],
+    				bAutoWidth: true,
+    				aaData: this.journal,
+    				aoColumns: EzBob.DataTables.Helper.extractColumns('Action,^Adate,Type,Status,$ApprovedSum,%Interest,RepaymentPeriod,LoanType,SetupFee,DiscountPlan,LoanSource,CustomerSelection,UW,Comment,~IsCrm'),
+    				aoColumnDefs: [
+                        {
+                        	"aTargets": [1],
+                        	"sType": 'date'
+                        }
+    				],
+    				//bDestroy: true,
+    				bDeferRender: true
+    			});
+    			EzBob.DataTables.Helper.initCustomFiltering();
+    		} catch (ex) {
+    			console.log('journal table exception', ex);
+    		}
+    	}
+    },
+
+    journalFilter: function(e) {
+    	var allJournal = this.$el.find('#allJournal');
+    	if ($(e.currentTarget).attr('id') !== 'allJournal' && allJournal.is(':checked')) {
+    		allJournal.parent().click();
+    		return false;
+    	}
+    	this.journalTable.fnDraw();
+    	return false;
+    }
+});
