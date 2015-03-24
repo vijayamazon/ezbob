c663b3776722de54602aa69022c103c988deb1b1
Alex Boursouk <alexbo@ezbob.com>
2015-03-24 17:36:43 +0200

    EZ-3274 Easter campaign - take 2.

--- App/PluginWeb/EzBob.Web/Content/js/ScratchCards/ezbob.scratchcard.easter2015.js
+++ App/PluginWeb/EzBob.Web/Content/js/ScratchCards/ezbob.scratchcard.easter2015.js
@@ -16,6 +16,8 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 
 		this.$notYet = this.$el.find('.not-yet');
 
+		this.$notNow = this.$el.find('.leave');
+
 		this.$decline = this.$el.find('.decline-game');
 
 		this.$won = this.$el.find('.won');
@@ -23,11 +25,14 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 		this.hasWon = false;
 		this.amount = 0;
 
+		this.isPlaying = false;
+
 		this.playLater = false;
 
 		this.nativeGeometries = {
 			bg: { width: 1920.0, height: 1200.0, top: 0.0, left: 0.0, },
 			notYet: { width: 802.0, height: 563.0, top: 0.0, left: 0.0, },
+			notNow: { width: 198.0, height: 60.0, top: 0.0, left: 0.0, },
 			eggs: {
 				golden: { width: 315.0, height: 433.0, top: 691.0, left: 88.0, },
 				magenta: { width: 181.0, height: 232.0, top: 838.0, left: 584.0, },
@@ -37,7 +42,7 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 
 		this.resizeEventName = 'resize.easter2015Scratch';
 
-		$(window).on(this.resizeEventName, _.bind(this.positionEggs, this));
+		$(window).on(this.resizeEventName, _.bind(this.onResize, this));
 	}, // initialize
 
 	remove: function() {
@@ -65,6 +70,8 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 	goPlay: function() {
 		var self = this;
 
+		this.isPlaying = true;
+
 		this.$opening.fadeOut(function() {
 			self.$playground.fadeIn(function() {
 				self.positionEggs();
@@ -72,13 +79,46 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 		});
 	}, // goPlay
 
-	positionEggs: function() {
-		var widthRatio = this.$playground.width() / this.nativeGeometries.bg.width;
-		var heightRatio = this.$playground.height() / this.nativeGeometries.bg.height;
+	onResize: function () {
+		if (this.isPlaying)
+			this.positionEggs();
+		else
+			this.positionOpening();
+	}, // onResize
+
+	positionOpening: function() {
+		var rp = this.getRatio('$opening');
+		this.positionNotNow(rp.paddingX, rp.ratio);
+	}, // positionOpening
+
+	positionNotNow: function(paddingX, ratio) {
+		var geometry = this.nativeGeometries.notNow;
+
+		var newHeight = parseInt(geometry.height * ratio);
+		var newWidth = parseInt(geometry.width * ratio);
+
+		this.$notNow.css({
+			right: parseInt(paddingX + 10 * ratio) + 'px',
+			width: newWidth  + 'px',
+			height: newHeight + 'px',
+			top: parseInt(this.nativeGeometries.bg.height * ratio - 10 * ratio - newHeight) + 'px',
+			'background-size': newWidth + 'px ' + newHeight + 'px',
+		});
+	}, // positionNotNow
+
+	getRatio: function(pageName) {
+		var widthRatio = this[pageName].width() / this.nativeGeometries.bg.width;
+		var heightRatio = this[pageName].height() / this.nativeGeometries.bg.height;
 
 		var ratio = widthRatio < heightRatio ? widthRatio : heightRatio;
 
-		var paddingX = (this.$playground.width() - this.nativeGeometries.bg.width * ratio) / 2.0;
+		var paddingX = (this[pageName].width() - this.nativeGeometries.bg.width * ratio) / 2.0;
+
+		return { ratio: ratio, paddingX: paddingX, };
+	}, // getRatio
+
+	positionEggs: function() {
+		var rp = this.getRatio('$playground');
 
 		var geometry;
 
@@ -86,19 +126,21 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 			geometry = this.nativeGeometries.eggs[egg];
 
 			this.$el.find('.egg-' + egg).css({
-				width: parseInt(geometry.width * ratio) + 'px',
-				height: parseInt(geometry.height * ratio) + 'px',
-				top: parseInt(geometry.top * ratio) + 'px',
-				left: parseInt(paddingX + geometry.left * ratio) + 'px',
+				width: parseInt(geometry.width * rp.ratio) + 'px',
+				height: parseInt(geometry.height * rp.ratio) + 'px',
+				top: parseInt(geometry.top * rp.ratio) + 'px',
+				left: parseInt(rp.paddingX + geometry.left * rp.ratio) + 'px',
 			}).show();
 		} // for
 
 		geometry = this.nativeGeometries.notYet;
 
 		this.$notYet.find('img').css({
-			width: parseInt(geometry.width * ratio) + 'px',
-			height: parseInt(geometry.height * ratio) + 'px',
+			width: parseInt(geometry.width * rp.ratio) + 'px',
+			height: parseInt(geometry.height * rp.ratio) + 'px',
 		});
+
+		this.positionNotNow(rp.paddingX, rp.ratio);
 	}, // positionEggs
 
 	decline: function() {
@@ -189,6 +231,8 @@ EzBob.ScratchCards.Easter2015 = EzBob.View.extend({
 		this.$playground.hide();
 
 		this.$notYet.hide();
+
+		this.positionOpening();
 	}, // show
 
 	hide: function() {
