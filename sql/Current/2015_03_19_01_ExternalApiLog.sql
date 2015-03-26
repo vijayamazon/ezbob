e5fe2202647aec3a59f738b7eda7db08c662f32a
QA <qa@ezbob.com>
2015-03-26 11:19:59 +0000

    Fix my smart commit

--- sql/Current/2015_03_19_01_ExternalApiLog.sql
+++ sql/Current/2015_03_19_01_ExternalApiLog.sql
@@ -1,35 +1,27 @@
+SET QUOTED_IDENTIFIER ON
+GO
 
 IF OBJECT_ID('ExternalApiLog') IS NULL
 BEGIN
-
-CREATE TABLE [dbo].[ExternalApiLog](
-	[Id] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[Url] [ntext] NULL,
-	[RequestId] [nvarchar](80) NOT NULL,
-	[Request] [ntext] NULL,
-	[Response] [ntext] NOT NULL,
-	[StatusCode] [nvarchar](80) NULL,
-	[ErrorCode] [nvarchar](40) NULL,
-	[ErrorMessage] [nvarchar](256) NULL,
-	[CreateDate] [datetime] NOT NULL,
-	[CustomerId] [int] NULL DEFAULT (NULL),
-	[Source] [nvarchar](20) NULL,
-	[Comments] [ntext] NULL
+	CREATE TABLE [dbo].[ExternalApiLog](
+		[Id] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+		[Url] [ntext] NULL,
+		[RequestId] [nvarchar](80) NOT NULL,
+		[Request] [ntext] NULL,
+		[Response] [ntext] NOT NULL,
+		[StatusCode] [nvarchar](80) NULL,
+		[ErrorCode] [nvarchar](40) NULL,
+		[ErrorMessage] [nvarchar](256) NULL,
+		[CreateDate] [datetime] NOT NULL,
+		[CustomerId] [int] NULL DEFAULT (NULL),
+		[Source] [nvarchar](20) NULL,
+		[Comments] [ntext] NULL
 	)
 END
 GO
 
-
-
-<<<<<<< origin/master
-=======
-
->>>>>>> local
-BEGIN 
-	ALTER TABLE [dbo].[ExternalApiLog] ALTER COLUMN [ErrorCode] [nvarchar](256) ;
-	ALTER TABLE [dbo].[ExternalApiLog] ALTER COLUMN [ErrorMessage] ntext ;
-END 
+ALTER TABLE [dbo].[ExternalApiLog] ALTER COLUMN [ErrorCode] [nvarchar](256)
 GO
 
-
-
+ALTER TABLE [dbo].[ExternalApiLog] ALTER COLUMN [ErrorMessage] nvarchar(max)
+GO
