f5d89445c5b1f00202f38f084eb22014b10c28f0
stas_dulman <stasd@ezbob.com>
2015-03-31 16:34:46 +0300

    adding try catch in each paypoint charging payment, adding try catch on sales force add activity when mail is sent

--- Backend/Strategies/MailStrategies/API/StrategiesMailer.cs
+++ Backend/Strategies/MailStrategies/API/StrategiesMailer.cs
@@ -69,19 +69,30 @@
 						new QueryParameter("TemplateName", oMeta.TemplateName)
 					);
 
-					var salesForceAddEvent = new AddActivity(null, new ActivityModel {
-						StartDate = now,
-						EndDate = now,
-						Description = oMeta.TemplateName,
-						Email = addr.Recipient,
-						Originator = "System",
-						Type = ActivityType.Email.ToString(),
-						IsOpportunity = false,
-					});
-					salesForceAddEvent.Execute();
+				    AddSalesForceActivity(now, oMeta, addr);
+					
 				} // if should register
 			} // foreach
-		} // SendMailViaMandrill
+		}
+
+	    private void AddSalesForceActivity(DateTime now, MailMetaData oMeta, Addressee addr) {
+	        try {
+	            var salesForceAddEvent = new AddActivity(null, new ActivityModel {
+	                StartDate = now,
+	                EndDate = now,
+	                Description = oMeta.TemplateName,
+	                Email = addr.Recipient,
+	                Originator = "System",
+	                Type = ActivityType.Email.ToString(),
+	                IsOpportunity = false,
+	            });
+	            salesForceAddEvent.Execute();
+            } catch (Exception ex) {
+                Log.Error(ex, "Failed to SF add activity to {0}", addr.Recipient);
+            }
+	    }
+
+// SendMailViaMandrill
 
 		private byte[] HtmlToDocxBinary(string html) {
 			if (html == null)
