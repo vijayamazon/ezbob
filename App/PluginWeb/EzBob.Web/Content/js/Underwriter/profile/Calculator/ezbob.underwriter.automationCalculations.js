819d2d694cca39ac2a4a925a77d3b834e55f4f4b
stas_dulman <stasd@ezbob.com>
2015-04-22 12:38:05 +0300

    Vitas requests - setup fee without decimals in UW, explanation button for automation history view

--- App/PluginWeb/EzBob.Web/Content/js/Underwriter/profile/Calculator/ezbob.underwriter.automationCalculations.js
+++ App/PluginWeb/EzBob.Web/Content/js/Underwriter/profile/Calculator/ezbob.underwriter.automationCalculations.js
@@ -14,15 +14,6 @@ EzBob.Underwriter.AutomationCalculationView = Backbone.View.extend({
         this.currentId = 0;
     },
 
-    events: {
-        'change #AutomationDetailsHistory': 'changeHistory'
-    },
-
-    changeHistory: function(el) {
-        this.currentId = $(el.currentTarget).val();
-        this.render();
-    },
-    
     render: function() {
 		
         var automations = this.model.toJSON();
@@ -35,7 +26,29 @@ EzBob.Underwriter.AutomationCalculationView = Backbone.View.extend({
         }));
 
         return this;
-    }
+    },
+
+    events: {
+        'change #AutomationDetailsHistory': 'changeHistory',
+        'click #automation-explanation-btn': 'clickedExplanation'
+    },
+
+    changeHistory: function(el) {
+        this.currentId = $(el.currentTarget).val();
+        this.render();
+    },
+    
+    clickedExplanation: function() {
+        EzBob.ShowMessage(
+            'Shows history of all automatic decisions that where executed list of all decisions sorted by date descending.<br>' +
+            '<ul class="alert-list"><li>Negative - system decision for this specific automation decision wasn\'t reached - so Approve Negative = No automatic approve, Reject Negative = No automatic reject.</li>' +
+            '<li>Positive - system decision for this specific automation decision reached - so Approve Positive = automatic approve, Reject Positive = automatic reject.</li></ul><br><br>' +
+            'In the table the rows colored by status of each rule the following way: <br>' +
+            '<ul class="alert-list"><li>White row - no decision was reached for this specific row</li>' +
+            '<li>Yellow row - the decision was locked Negative - no automation, Positive there is an automatic decision</li>' +
+            '<li>Red row - negative decision</li>' +
+            '<li>Green row - positive decision</li></ul>', 'Automation explanation', function () { return false; }, 'Got it');
+    },
 });
 
 
