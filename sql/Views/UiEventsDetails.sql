504743b18b5817195e656257f362e84bfb0cccb7
stas_dulman <stasd@ezbob.com>
2015-05-06 11:23:20 +0300

    misc - fix run_service folder, add uievents view

---
+++ sql/Views/UiEventsDetails.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('UiEventsDetails') IS NOT NULL
+	DROP VIEW UiEventsDetails
+GO
+
+CREATE VIEW UiEventsDetails AS
+SELECT e.UserID, a.UiActionName, c.UiControlName, e.EventTime, e.ControlHtmlID, e.EventArguments, e.RemoteIP, b.BrowserVersion FROM UiEvents e 
+INNER JOIN BrowserVersions b ON b.BrowserVersionID = e.BrowserVersionID 
+INNER JOIN UiActions a ON a.UiActionID = e.UiActionID
+INNER JOIN UiControls c ON c.UiControlID = e.UiControlID
+
