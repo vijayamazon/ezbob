9c349530456f508d42f04f9f4b376a285e467f0d
Alex Boursouk <alexbo@ezbob.com>
2015-05-10 18:16:52 +0300

    EZ-3404: New loan calculator - take 3.
    (cherry picked from commit bac6025ef93b3517bf2c133a8d997ee04cabae6a)

---
+++ Backend/CalculateLoan/Models/CurrentPaymentModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Ezbob.Backend.CalculateLoan.Models {
+	public class CurrentPaymentModel {
+		public CurrentPaymentModel() {
+			Amount = 0;
+			IsLate = false;
+		} // constructor
+
+		public CurrentPaymentModel(decimal amount, bool isLate = false) {
+			Amount = amount;
+			IsLate = isLate;
+		} // constructor
+
+		public decimal Amount { get; set; }
+		public bool IsLate { get; set; }
+	} // class CurrentPaymentModel
+} // namespace
