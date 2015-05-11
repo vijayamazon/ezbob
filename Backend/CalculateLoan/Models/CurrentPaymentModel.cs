8e367a8f69ca50109e54612291184d90bfd98b1f
Alex Boursouk <alexbo@ezbob.com>
2015-05-11 19:31:34 +0300

    EZ-3404: New loan calculator - take 4.

--- Backend/CalculateLoan/Models/CurrentPaymentModel.cs
+++ Backend/CalculateLoan/Models/CurrentPaymentModel.cs
@@ -3,14 +3,18 @@
 		public CurrentPaymentModel() {
 			Amount = 0;
 			IsLate = false;
+			LoanIsClosed = false;
 		} // constructor
 
-		public CurrentPaymentModel(decimal amount, bool isLate = false) {
+		public CurrentPaymentModel(decimal amount, bool isLate = false, bool loanIsClosed = false) {
 			Amount = amount;
 			IsLate = isLate;
+			LoanIsClosed = loanIsClosed;
 		} // constructor
 
+		public bool IsError { get; set; }
 		public decimal Amount { get; set; }
 		public bool IsLate { get; set; }
+		public bool LoanIsClosed { get; set; }
 	} // class CurrentPaymentModel
 } // namespace
---
+++ Backend/CalculateLoan/Models/Extensions/ScheduleListExt.cs
@@ -0,0 +1,16 @@
+﻿namespace Ezbob.Backend.CalculateLoan.Models.Extensions {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Ezbob.Backend.CalculateLoan.Models.Helpers;
+
+	public static class ScheduleListExt {
+		public static bool ContainsDate(this List<ScheduledPayment> lst, DateTime date) {
+			return (lst != null) && lst.Any(s => s.Date.HasValue && (s.Date.Value.Date == date.Date));
+		} // ContainsDate
+
+		public static ScheduledPayment FindByDate(this List<ScheduledPayment> lst, DateTime date) {
+			return (lst == null) ? null : lst.FirstOrDefault(s => s.Date.HasValue && (s.Date.Value.Date == date.Date));
+		} // FindByDate
+	} // class ScheduleListExt
+} // namespace
