081bab8074b0ad8d0d5628623daeaef9e1062229
Alex Boursouk <alexbo@ezbob.com>
2015-05-12 20:14:33 +0300

    EZ-3404: New loan calculator - take 5.

--- Backend/CalculateLoan/LoanCalculator/ALoanCalculator.cs
+++ Backend/CalculateLoan/LoanCalculator/ALoanCalculator.cs
@@ -1,7 +1,6 @@
 ﻿namespace Ezbob.Backend.CalculateLoan.LoanCalculator {
 	using System;
 	using System.Collections.Generic;
-	using System.Diagnostics;
 	using System.Diagnostics.CodeAnalysis;
 	using System.Linq;
 	using Ezbob.Backend.CalculateLoan.Models;
