85a3922787bcc961e9b65cd0218282aaef8ff0b1
Alex Boursouk <alexbo@ezbob.com>
2015-05-25 18:23:07 +0300

    Separate mini project: list reasons for auto approve to say "no".
    (cherry picked from commit e374b95d04731b8984564b994580da0c29844887)

---
+++ Auxiliary/ListNotAutoApprovedReasons/ListNotAutoApprovedReasons/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ListNotAutoApprovedReasons {
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+
+	class Program {
+		static void Main(string[] args) {
+			string[] lines = File.ReadAllLines("not-auto-approved.csv");
+
+			var trails = new SortedDictionary<long, Trail>();
+
+			var relevant = lines
+				.Where(s => !string.IsNullOrWhiteSpace(s))
+				.Select(s => s.Trim())
+				.Where(s => !s.StartsWith("#"));
+
+			foreach (string line in relevant) {
+				string[] tokens = line.Split(',');
+
+				if (tokens.Length != 3)
+					continue;
+
+				if (string.IsNullOrWhiteSpace(tokens[2]))
+					continue;
+
+				long id = 0;
+
+				try {
+					id = Convert.ToInt64(tokens[0]);
+				} catch {
+					// ignored
+				} // try
+
+				if (id <= 0)
+					continue;
+
+				string name = tokens[2].Substring(tokens[2].LastIndexOf(".") + 1);
+
+				if (trails.ContainsKey(id))
+					trails[id].Reasons.Add(name);
+				else
+					trails[id] = new Trail(id, name);
+			} // for each input line
+
+			var keys = new SortedDictionary<NonAffirmativeGroupKey, int>();
+
+			foreach (Trail trail in trails.Values) {
+				if (keys.ContainsKey(trail.Key))
+					keys[trail.Key]++;
+				else
+					keys[trail.Key] = 1;
+			} // for each trail
+
+			foreach (var pair in keys)
+				Console.WriteLine("{0};{1}", pair.Value, pair.Key.List);
+		} // Main
+	} // class Program
+} // namespace
---
+++ Auxiliary/ListNotAutoApprovedReasons/ListNotAutoApprovedReasons/Trail.cs
@@ -0,0 +1,24 @@
+﻿namespace ListNotAutoApprovedReasons {
+	using System.Collections.Generic;
+
+	class Trail {
+		public Trail(long id, string reason) {
+			ID = id;
+			Reasons = new List<string> { reason, };
+		} // constructor
+
+		public long ID { get; private set; }
+		public List<string> Reasons { get; private set; }
+
+		public NonAffirmativeGroupKey Key {
+			get {
+				if (this.key == null)
+					this.key = new NonAffirmativeGroupKey(this);
+
+				return this.key;
+			} // get
+		} // Key
+
+		private NonAffirmativeGroupKey key;
+	} // class Trail
+} // namespace
