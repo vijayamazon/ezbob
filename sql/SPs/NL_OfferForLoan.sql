24cac0bba21dccbd83363378947b4561325c5870
Alex Boursouk <alexbo@ezbob.com>
2015-05-27 21:00:16 +0300

    Build fix

--- sql/SPs/NL_OfferForLoan.sql
+++ sql/SPs/NL_OfferForLoan.sql
@@ -1,28 +1,24 @@
 SET QUOTED_IDENTIFIER ON
 GO
 
-IF OBJECT_ID('NL_OfferForLoan') IS NOT NULL
-	DROP PROCEDURE NL_OfferForLoan
+IF OBJECT_ID('NL_OfferForLoan') IS NULL
+	EXECUTE('CREATE PROCEDURE NL_OfferForLoan AS SELECT 1')
 GO
 
-
-CREATE PROCEDURE PROCEDURE [dbo].[NL_OfferForLoan]	
-	@CustomerID INT 
+ALTER PROCEDURE [dbo].[NL_OfferForLoan]
+@CustomerID INT
 AS
-
-declare @Now datetime;
-set @Now = GETUTCDATE();
-
-declare @OfferID int;
-
-
 BEGIN
-	
+	declare @Now datetime;
+	set @Now = GETUTCDATE();
+
+	declare @OfferID int;
+
 	-- get last valid offer for the customer
 	set @OfferID = (select top 1 o.OfferID FROM NL_Offers o INNER JOIN NL_Decisions d ON d.DecisionID = o.DecisionID INNER JOIN NL_CashRequests cr ON cr.CashRequestID = d.CashRequestID
 		WHERE cr.CustomerID = @CustomerID and GETUTCDATE() between o.[StartTime] and o.[EndTime] order by OfferID desc);
 
-	IF @OfferID IS NULL begin		
+	IF @OfferID IS NULL begin
 		RETURN NULL;
 	end;
 
@@ -32,39 +28,39 @@ BEGIN
 	IF object_id('#offerforloan') IS NOT NULL drop table #offerforloan;
 
 	IF @OfferID IS NOT NULL BEGIN
-		select top 1 		 
+		select top 1
 			ll.LoanLegalID,
-			ll.Amount as LoanLegalAmount, 			
+			ll.Amount as LoanLegalAmount,
 			ll.RepaymentPeriod as LoanLegalRepaymentPeriod,
-			dp.DiscountPlanID,		 
+			dp.DiscountPlanID,
 			cast(dp.DiscountPlan as nvarchar(max)) as DiscountPlan,
-			o.OfferID , 
-			o.LoanTypeID, 
-			o.RepaymentIntervalTypeID, 
-			o.LoanSourceID, 			 
-			o.RepaymentCount as OfferRepaymentCount, 
-			o.Amount as OfferAmount, 
+			o.OfferID ,
+			o.LoanTypeID,
+			o.RepaymentIntervalTypeID,
+			o.LoanSourceID,
+			o.RepaymentCount as OfferRepaymentCount,
+			o.Amount as OfferAmount,
 			o.MonthlyInterestRate,
-			o.SetupFeePercent, 
-			o.BrokerSetupFeePercent, 
+			o.SetupFeePercent,
+			o.BrokerSetupFeePercent,
 			o.InterestOnlyRepaymentCount,
-			0 as LoansCount		
+			0 as LoansCount
 		into #offerforloan
-			FROM NL_LoanLegals ll INNER JOIN NL_Offers o on ll.OfferID = o.OfferID	
-				LEFT JOIN NL_DiscountPlans dp on dp.DiscountPlanID = o.DiscountPlanID -- and dp.IsActive = 1 				
+			FROM NL_LoanLegals ll INNER JOIN NL_Offers o on ll.OfferID = o.OfferID
+				LEFT JOIN NL_DiscountPlans dp on dp.DiscountPlanID = o.DiscountPlanID -- and dp.IsActive = 1
 			WHERE o.OfferID = @OfferID
-			order by ll.LoanLegalID desc; 
+			order by ll.LoanLegalID desc;
 
 		set @DiscountPlanID = (select DiscountPlanID from #offerforloan) ;
 
-		if @DiscountPlanID is not null begin			
+		if @DiscountPlanID is not null begin
 			update #offerforloan set
-			 DiscountPlan=(select ','+cast(dpe.InterestDiscount as varchar(11)) AS [text()] From NL_DiscountPlanEntries dpe Where dpe.DiscountPlanID = @DiscountPlanID ORDER BY dpe.PaymentOrder For XML PATH ('')) 
+			 DiscountPlan=(select ','+cast(dpe.InterestDiscount as varchar(11)) AS [text()] From NL_DiscountPlanEntries dpe Where dpe.DiscountPlanID = @DiscountPlanID ORDER BY dpe.PaymentOrder For XML PATH (''))
 		end;
 
-		set @LoansCount = (select COUNT(LoanID) 
-							from NL_Loans l inner join NL_Offers o on o.OfferID = l.OfferID inner join NL_Decisions d on d.DecisionID=o.DecisionID 
-							inner join NL_CashRequests cr on cr.CashRequestID = d.CashRequestID 
+		set @LoansCount = (select COUNT(LoanID)
+							from NL_Loans l inner join NL_Offers o on o.OfferID = l.OfferID inner join NL_Decisions d on d.DecisionID=o.DecisionID
+							inner join NL_CashRequests cr on cr.CashRequestID = d.CashRequestID
 							where cr.CustomerID=@CustomerID group by LoanID);
 
 		if @LoansCount is not null begin
@@ -73,7 +69,6 @@ BEGIN
 
 		select * from #offerforloan;
 	END;
-
-	
-
 END
+GO
+
