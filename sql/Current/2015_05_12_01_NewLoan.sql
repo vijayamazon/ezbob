262381ed19e16d5efcc581836cbc191ee4f9b45d
elinar <elinar@ezbob.com>
2015-06-10 10:55:40 +0300

    sql build repair

--- sql/Current/2015_05_12_01_NewLoan.sql
+++ sql/Current/2015_05_12_01_NewLoan.sql
@@ -1,3 +1,32 @@
+IF OBJECT_ID (N'dbo.udfSplit') IS NOT NULL
+	DROP FUNCTION dbo.udfSplit
+GO
+
+CREATE FUNCTION dbo.udfSplit
+(
+    @String NVARCHAR(4000),
+    @Delimiter NCHAR(1)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    WITH udfSplit(stpos,endpos)
+    AS(
+        SELECT 0 AS stpos, CHARINDEX(@Delimiter,@String) AS endpos
+        UNION ALL
+        SELECT endpos+1, CHARINDEX(@Delimiter,@String,endpos+1)
+            FROM udfSplit
+            WHERE endpos > 0
+    )
+    SELECT 'Id' = ROW_NUMBER() OVER (ORDER BY (SELECT 1)),
+        'Data' = SUBSTRING(@String,stpos,COALESCE(NULLIF(endpos,0),LEN(@String)+1)-stpos)
+    FROM udfSplit
+)
+GO
+
+-----------------------------
+
 IF OBJECT_ID('LoanAgreementTemplateTypes') IS NULL 
 BEGIN
 	CREATE TABLE [dbo].[LoanAgreementTemplateTypes](
