8bfd7c1be1e78987b19c2d7e82f5ded2a9f48632
Alex Boursouk <alexbo@ezbob.com>
2015-06-15 16:47:31 +0300

    SP beautify

--- sql/SPs/Dependent/AV_GetCaisStatuses.sql
+++ sql/SPs/Dependent/AV_GetCaisStatuses.sql
@@ -1,21 +1,32 @@
+SET QUOTED_IDENTIFIER ON
+GO
+
 IF OBJECT_ID('AV_GetCaisStatuses') IS NULL
 	EXECUTE('CREATE PROCEDURE AV_GetCaisStatuses AS SELECT 1')
 GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
 ALTER PROCEDURE AV_GetCaisStatuses
-	@CustomerId INT 
+@CustomerId INT 
 AS
 BEGIN
+	SET NOCOUNT ON;
 
 	DECLARE @ConsumerServiceLogId BIGINT 
-	EXEC GetExperianConsumerServiceLog @CustomerId, @ConsumerServiceLogId OUTPUT
-	
-	SELECT cais.LastUpdatedDate, cais.AccountStatusCodes, cais.Balance, cais.CurrentDefBalance FROM ExperianConsumerData e INNER JOIN ExperianConsumerDataCais cais ON cais.ExperianConsumerDataId = e.Id
-	WHERE e.ServiceLogId=@ConsumerServiceLogId AND cais.MatchTo=1 AND cais.LastUpdatedDate IS NOT NULL
+	EXECUTE GetExperianConsumerServiceLog @CustomerId, @ConsumerServiceLogId OUTPUT
 
+	SELECT
+		cais.LastUpdatedDate,
+		cais.AccountStatusCodes,
+		cais.Balance,
+		cais.CurrentDefBalance
+	FROM
+		ExperianConsumerData e
+		INNER JOIN ExperianConsumerDataCais cais ON cais.ExperianConsumerDataId = e.Id
+	WHERE
+		e.ServiceLogId = @ConsumerServiceLogId
+		AND
+		cais.MatchTo = 1
+		AND
+		cais.LastUpdatedDate IS NOT NULL
 END
-
 GO
