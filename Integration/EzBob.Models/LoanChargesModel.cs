423a2b2718fbd4afce820ba392a253daac4328a1
Alex Boursouk <alexbo@ezbob.com>
2015-06-18 15:38:28 +0300

    EZ-3575: Set-up fee: at once or spread - take 6.

--- Integration/EzBob.Models/LoanChargesModel.cs
+++ Integration/EzBob.Models/LoanChargesModel.cs
@@ -1,25 +1,34 @@
-﻿using System;
-using EZBob.DatabaseLib.Model.Loans;
+﻿namespace EzBob.Models {
+	using System;
+	using ConfigManager;
+	using EZBob.DatabaseLib.Model;
+	using EZBob.DatabaseLib.Model.Loans;
 
-namespace EzBob.Models
-{
-    [Serializable]
-    public class LoanChargesModel : LoanCharge
-    {
-        public static LoanChargesModel FromCharges(LoanCharge loanCharge)
-        {
-            return loanCharge != null
-                       ? new LoanChargesModel
-                             {
-                                 Amount = loanCharge.Amount,
-                                 ChargesType = ConfigurationVariableModel.FromConfigurationVariable(loanCharge.ChargesType),
-                                 Date = loanCharge.Date,
-                                 Id = loanCharge.Id,
-                                 State = loanCharge.State ?? "Active",
-                                 AmountPaid = loanCharge.AmountPaid,
-                                 Description = loanCharge.GetDescription()
-                             }
-                       : null;
-        }
-    }
-}
+	[Serializable]
+	public class LoanChargesModel : LoanCharge {
+		public string IsServiceFee { get; private set; }
+
+		public LoanChargesModel() {
+			IsServiceFee = string.Empty;
+		} // constructor
+
+		public static LoanChargesModel FromCharges(LoanCharge loanCharge) {
+			if (loanCharge == null)
+				return null;
+
+			return new LoanChargesModel {
+				Amount = loanCharge.Amount,
+				ChargesType = ConfigurationVariableModel.FromConfigurationVariable(loanCharge.ChargesType),
+				Date = loanCharge.Date,
+				Id = loanCharge.Id,
+				State = loanCharge.State ?? "Active",
+				AmountPaid = loanCharge.AmountPaid,
+				Description = loanCharge.GetDescription(),
+				IsServiceFee = loanCharge.ChargesType.Name == spreadSetupFee.Name ? "yes" : "no",
+			};
+		} // FromCharges
+
+		private static readonly ConfigurationVariable spreadSetupFee =
+			new ConfigurationVariable(CurrentValues.Instance.SpreadSetupFeeCharge);
+	} // class LoanChargesModel
+} // namespace
