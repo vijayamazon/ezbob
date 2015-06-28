97ab2a0656cb9e6ca32f833f7cf7876dd5d1ab88
Alex Boursouk <alexbo@ezbob.com>
2015-06-28 19:57:59 +0300

    EZ-3677, EZ-3680: Auto approval updates - configuration.

--- sql/Current/2015_06_21_02_Auto_approval_new_cfg.sql
+++ sql/Current/2015_06_21_02_Auto_approval_new_cfg.sql
@@ -3,5 +3,6 @@ GO
 
 UPDATE ConfigurationVariables SET Value = '600000' WHERE Name = 'AutoApproveMaxOutstandingOffers'
 UPDATE ConfigurationVariables SET Value = '500000' WHERE Name = 'AutoApproveMaxTodayLoans'
-UPDATE ConfigurationVariables SET Value = '30' WHERE Name = 'AutoApproveMaxDailyApprovals'
+UPDATE ConfigurationVariables SET Value = '20' WHERE Name = 'AutoApproveMaxDailyApprovals'
+UPDATE ConfigurationVariables SET Value = '0.5' WHERE Name = 'AutoApproveTurnoverDropQuarterRatio'
 GO
