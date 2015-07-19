9608f7dfbe08c7e6a133f9f542877c13de1ba255
Alex Boursouk <alexbo@ezbob.com>
2015-07-19 20:56:18 +0300

    EZ-371: Fraud check: IP address check amendment - take 2.

--- Integration/FraudChecker/SessionInfo.cs
+++ Integration/FraudChecker/SessionInfo.cs
@@ -35,14 +35,6 @@
 	} // class SessionInfo
 
 	internal class SessionInfoComparer : IEqualityComparer<SessionInfo> {
-		/// <summary>
-		/// Determines whether the specified objects are equal.
-		/// </summary>
-		/// <returns>
-		/// true if the specified objects are equal; otherwise, false.
-		/// </returns>
-		/// <param name="x">The first object of type <paramref name="T"/> to compare.</param>
-		/// <param name="y">The second object of type <paramref name="T"/> to compare.</param>
 		public bool Equals(SessionInfo x, SessionInfo y) {
 			if ((x == null) && (y == null))
 				return true;
@@ -53,15 +45,6 @@
 			return (x.Ip == y.Ip) && (x.StartSessionTime == y.StartSessionTime);
 		} // Equals
 
-		/// <summary>
-		/// Returns a hash code for the specified object.
-		/// </summary>
-		/// <returns>
-		/// A hash code for the specified object.
-		/// </returns>
-		/// <param name="obj">The <see cref="T:System.Object"/> for which a hash code is to be returned.</param>
-		/// <exception cref="T:System.ArgumentNullException">The type of <paramref name="obj"/> is a reference type
-		/// and <paramref name="obj"/> is null.</exception>
 		public int GetHashCode(SessionInfo obj) {
 			if (obj == null)
 				throw new ArgumentNullException("obj", "GetHashCode((SessionInfo)null)");
