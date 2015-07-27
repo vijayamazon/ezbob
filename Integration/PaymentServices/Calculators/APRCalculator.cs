94b181581309dad5ed6809e9e3ca4dd93bc7ad7a
Alex Boursouk <alexbo@ezbob.com>
2015-07-27 21:06:30 +0300

    EZ-3693: APR calculation - take 1.
    (cherry picked from commit a9a5e8078773a460ebaf6df28b902040bd9b6c31)

--- Integration/PaymentServices/Calculators/APRCalculator.cs
+++ Integration/PaymentServices/Calculators/APRCalculator.cs
@@ -1,61 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using EZBob.DatabaseLib.Model.Database.Loans;
+﻿namespace PaymentServices.Calculators {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using EZBob.DatabaseLib.Model.Database.Loans;
 
-namespace PaymentServices.Calculators
-{
-	public class APRCalculator
-	{
-		private static DateTime _date = DateTime.Today;
+	public class APRCalculator {
+		public double Calculate(
+			decimal amount,
+			IEnumerable<LoanScheduleItem> monthlyRepayments,
+			decimal setupFee = 0M,
+			DateTime? currentDate = null
+		) {
+			this.date = currentDate ?? DateTime.Today; // TODO: DateTime.UtcNow.Date if schedules have UTC date.
 
-		private static double f(double x, decimal credit, IEnumerable<LoanScheduleItem> monthlyRepayments)
-		{
-			return -(double)credit +
-				   (from monthlyRepayment in monthlyRepayments
-					let t_i = (monthlyRepayment.Date - _date).Days / 365.0
-					select ((double)monthlyRepayment.AmountDue) * Math.Pow(1 + x, -t_i)).Sum();
-		}
+			double x = 1.0;
 
-		private static double f_prime(double x, IEnumerable<LoanScheduleItem> monthlyRepayments)
-		{
-			return (from monthlyRepayment in monthlyRepayments
-					let t_i = (monthlyRepayment.Date - _date).Days / 365.00
-					select -t_i * ((double)monthlyRepayment.AmountDue) * Math.Pow(1 + x, -t_i + 1)).Sum();
-		}
+			List<LoanScheduleItem> payments = monthlyRepayments.ToList();
 
-		public double Calculate(decimal amount, IEnumerable<LoanScheduleItem> monthlyRepayments, decimal setupFee = 0M,
-								DateTime? date = null)
-		{
-			_date = date ?? DateTime.Today;
+			for (var i = 0; i < 10000; i++) {
+				double f_x = f(x, amount - setupFee, payments);
 
-			var x = 1.0;
-			for (var i = 0; i < 10000; i++)
-			{
-				var f_x = f(x, amount - setupFee, monthlyRepayments);
 				if (Math.Abs(f_x) < 1e-6)
 					break;
-				var f_prime_x = f_prime(x, monthlyRepayments);
-				var dx = f_x / f_prime_x;
+
+				double f_prime_x = f_prime(x, payments);
+
+				double dx = f_x / f_prime_x;
+
 				x -= dx;
-			}
+			} // for
+
 			return Math.Round(x * 100, 2);
-		}
+		} // Calculate
 
-		public double CalculateMonthly(decimal amount, List<LoanScheduleItem> monthlyRepayments, int month, decimal setupFee = 0M,
-									   DateTime? date = null)
-		{
-			decimal monthlyRate = monthlyRepayments[month].InterestRate;
+		private double f(double x, decimal credit, List<LoanScheduleItem> monthlyRepayments) {
+			return
+				-(double)credit + (
+					from monthlyRepayment in monthlyRepayments
+					let t_i = (monthlyRepayment.Date - this.date).Days / 365.0
+					select ((double)monthlyRepayment.AmountDue) * Math.Pow(1 + x, -t_i)
+				).Sum();
+		} // f
 
-			var monthlyRepaimentsCalculatedForMonthlyRate = monthlyRepayments.Select(monthlyRepayment => new LoanScheduleItem
-				{
-					Interest = monthlyRepayment.BalanceBeforeRepayment*monthlyRate,
-					AmountDue = monthlyRepayment.LoanRepayment + (monthlyRepayment.BalanceBeforeRepayment * monthlyRate), 
-					Date = monthlyRepayment.Date
-				}).ToList();
+		private double f_prime(double x, List<LoanScheduleItem> monthlyRepayments) {
+			return (
+				from monthlyRepayment in monthlyRepayments
+				let t_i = (monthlyRepayment.Date - this.date).Days / 365.00
+				select -t_i * ((double)monthlyRepayment.AmountDue) * Math.Pow(1 + x, -t_i + 1)
+			).Sum();
+		} // f_prime
 
-			return Calculate(amount, monthlyRepaimentsCalculatedForMonthlyRate, setupFee, date);
-		}
-
-	}
-}
+		private DateTime date;
+	} // class APRCalculator
+} // namespace
