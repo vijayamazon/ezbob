b3c63a5f57b298b685fa42b9527680c76373215d
Dor <Dora@ezbob.com>
2015-08-05 19:53:13 +0300

    remove tests

--- Integration/Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("S1.Application.OrangeMoney")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("e0d496af-3112-4d86-8fad-98a4f12de3fe")]
