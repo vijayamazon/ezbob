fc0e52892ccd972ef6aa18307def41ba3162b770
Dor <Dora@ezbob.com>
2015-08-05 18:46:32 +0300

    EZ-3837 POC - testing framework, management tool and TestRail integration
    Adding UI test for POC

---
+++ Integration/Tests/AutomationUIFunctionalTests/BrowserHost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using TestStack.Seleno.Configuration;
+
+namespace S1.Application.OrangeMoney
+{
+    using TestStack.Seleno.Configuration.WebServers;
+
+    public static class BrowserHost {
+        
+        public static readonly SelenoHost instance = new SelenoHost();
+
+        static BrowserHost() {
+            instance.Run(configure => configure
+            .WithWebServer(new InternetWebServer("https://localhost:44300/Customer/Wizard")));
+        }
+
+
+
+    }
+}
