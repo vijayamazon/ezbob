65e466fb922bbb5228beaa134ed3b6db1ec0aec6
Dor <Dora@ezbob.com>
2015-08-06 21:57:23 +0300

    Adding 2 unit tests

--- Integration/AutomationTests/BrowserHost.cs
+++ Integration/AutomationTests/BrowserHost.cs
@@ -1,15 +1,23 @@
 ﻿namespace UIAutomationTests 
 {
+    using System;
     using TestStack.Seleno.Configuration;
     using TestStack.Seleno.Configuration.WebServers;
 
     public static class BrowserHost {
-        
-        public static readonly SelenoHost instance = new SelenoHost();
 
+        public static readonly SelenoHost instance;
+        
         static BrowserHost() {
-            instance.Run(configure => configure
-            .WithWebServer(new InternetWebServer("https://localhost:44300/Customer/Wizard")));
+            try {
+                instance = new SelenoHost();
+                instance.Run(configure => configure
+                .WithWebServer(new InternetWebServer("https://localhost:44300/Customer/Wizard")));
+            } 
+            catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+          
         }
 
 
--- Integration/AutomationTests/S1.Application.Orange Money/C3.CreateAccountTest.cs
+++ Integration/AutomationTests/S1.Application.Orange Money/C3.CreateAccountTest.cs
@@ -10,47 +10,62 @@
     using OpenQA.Selenium;
 
     [TestFixture]
-    public  class CreateAccountTests {
+    public class CreateAccountTests {
 
-        [Test, Category("C3")]
-        public void CreateAccountTest() {
+        [Test]
+        public void MustSucseedTest() {
+            Assert.IsTrue( true);
+        }
+
+        [Test]
+        public void MustFailTest()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void CreateAccountTest1() {
            
             //WebDriverWait wait = new WebDriverWait(BrowserHost.instance.Application.Browser, new TimeSpan(3000));
             //var cBoxOverlay = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("Email")));
-            Thread.Sleep(5000);
+            try {
+                Thread.Sleep(5000);
 
-            var emailBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("Email"));
-            emailBox.SendKeys("dor+"+ DateTime.Now.Ticks +"@ezbob.com");
+                var emailBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("Email"));
+                emailBox.SendKeys("dor+" + DateTime.Now.Ticks + "@ezbob.com");
 
-            var passwordBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupPass1"));
-            passwordBox.SendKeys("dor2015");
+                var passwordBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupPass1"));
+                passwordBox.SendKeys("dor2015");
 
-            var confirmPasswordBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupPass2"));
-            confirmPasswordBox.SendKeys("dor2015");
+                var confirmPasswordBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupPass2"));
+                confirmPasswordBox.SendKeys("dor2015");
 
-            SelectElement secrertQuestion = new SelectElement(BrowserHost.instance.Application.Browser.FindElement(By.Id("securityQuestion")));
-            secrertQuestion.SelectByIndex(1);
+                SelectElement secrertQuestion = new SelectElement(BrowserHost.instance.Application.Browser.FindElement(By.Id("securityQuestion")));
+                secrertQuestion.SelectByIndex(1);
 
-            //var SecurityAnswer = BrowserHost.instance.Application.Browser.FindElement(By.Id("SecurityAnswer"));
-            //SecurityAnswer.SendKeys("AAA");
+                var SecurityAnswer = BrowserHost.instance.Application.Browser.FindElement(By.Id("SecurityAnswer"));
+                SecurityAnswer.SendKeys("AAA");
 
-            //var requestAmount = BrowserHost.instance.Application.Browser.FindElement(By.Id("amount"));
-            //requestAmount.SendKeys("1000");
+                var requestAmount = BrowserHost.instance.Application.Browser.FindElement(By.Id("amount"));
+                requestAmount.SendKeys("1000");
 
-            //var mobilePhone = BrowserHost.instance.Application.Browser.FindElement(By.Id("mobilePhone"));
-            //mobilePhone.SendKeys("01111111111");
+                var mobilePhone = BrowserHost.instance.Application.Browser.FindElement(By.Id("mobilePhone"));
+                mobilePhone.SendKeys("01111111111");
 
-            //var generateMobileCodeButton = BrowserHost.instance.Application.Browser.FindElement(By.Id("generateMobileCode"));
-            //generateMobileCodeButton.Click();
+                var generateMobileCodeButton = BrowserHost.instance.Application.Browser.FindElement(By.Id("generateMobileCode"));
+                generateMobileCodeButton.Click();
 
-            //var mobileCode = BrowserHost.instance.Application.Browser.FindElement(By.Id("mobileCode"));
-            //mobileCode.SendKeys("222222");
+                var mobileCode = BrowserHost.instance.Application.Browser.FindElement(By.Id("mobileCode"));
+                mobileCode.SendKeys("222222");
 
-            //var signupSubmitButton = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupSubmitButton"));
-            //signupSubmitButton.Click();
+                var signupSubmitButton = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupSubmitButton"));
+                signupSubmitButton.Click();
 
-            //Thread.Sleep(5000);
-            //Assert.IsTrue(BrowserHost.instance.Application.Browser.FindElement(By.Id("FirstName")) != null);
+                Thread.Sleep(5000);
+                Assert.IsTrue(BrowserHost.instance.Application.Browser.FindElement(By.Id("FirstName")) != null);
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
