3660652c38fcfbb838953824216fec885e9a0834
Dor <Dora@ezbob.com>
2015-08-06 11:04:59 +0300

    project location

--- API/AuthenticationAPI/Properties/AssemblyInfo.cs
+++ API/AuthenticationAPI/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AuthenticationAPI")]
 [assembly: AssemblyDescription("")]
+[assembly: Guid("be43788b-19be-45e0-82ea-fe900ad1a7f5")]
---
+++ Integration/AutomationTests/S1.Application.Orange Money/C3.CreateAccountTest.cs
@@ -0,0 +1,54 @@
+﻿namespace UIAutomationTests.S1.Application.Orange_Money
+{
+    using System;
+    using System.Threading;
+    using global::S1.Application.OrangeMoney;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+
+    [TestFixture]
+    public  class CreateAccountTests {
+
+        [Test]
+        public void CreateAccountTest() {
+            //BrowserHost.instance.Application.Browser.Navigate().GoToUrl("https://localhost:44300/Customer/Wizard");
+
+            var emailBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("Email"));
+            emailBox.SendKeys("dor+"+ DateTime.Now.Ticks +"@ezbob.com");
+
+            var passwordBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupPass1"));
+            passwordBox.SendKeys("dor2015");
+
+            var confirmPasswordBox = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupPass2"));
+            confirmPasswordBox.SendKeys("dor2015");
+
+            SelectElement secrertQuestion = new SelectElement(BrowserHost.instance.Application.Browser.FindElement(By.Id("securityQuestion")));
+            secrertQuestion.SelectByIndex(1);
+
+            var SecurityAnswer = BrowserHost.instance.Application.Browser.FindElement(By.Id("SecurityAnswer"));
+            SecurityAnswer.SendKeys("AAA");
+
+            var requestAmount = BrowserHost.instance.Application.Browser.FindElement(By.Id("amount"));
+            requestAmount.SendKeys("1000");
+
+            var mobilePhone = BrowserHost.instance.Application.Browser.FindElement(By.Id("mobilePhone"));
+            mobilePhone.SendKeys("01111111111");
+
+            var generateMobileCodeButton = BrowserHost.instance.Application.Browser.FindElement(By.Id("generateMobileCode"));
+            generateMobileCodeButton.Click();
+
+            var mobileCode = BrowserHost.instance.Application.Browser.FindElement(By.Id("mobileCode"));
+            mobileCode.SendKeys("222222");
+
+            var signupSubmitButton = BrowserHost.instance.Application.Browser.FindElement(By.Id("signupSubmitButton"));
+            signupSubmitButton.Click();
+
+            Thread.Sleep(5000);
+            Assert.IsTrue(BrowserHost.instance.Application.Browser.FindElement(By.Id("FirstName")) != null);
+        }
+
+
+
+    }
+}
