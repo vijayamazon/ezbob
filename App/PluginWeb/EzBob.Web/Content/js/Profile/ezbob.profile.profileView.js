cf0ecdf4b4b2ac49197732a2f4c0b8da2fd03e73
stas_dulman <stasd@ezbob.com>
2015-08-09 12:15:30 +0300

    EZ-3843 fix wizard automation multiple popup is displayed bug

--- App/PluginWeb/EzBob.Web/Content/js/Profile/ezbob.profile.profileView.js
+++ App/PluginWeb/EzBob.Web/Content/js/Profile/ezbob.profile.profileView.js
@@ -65,6 +65,7 @@ EzBob.Profile.ProfileView = Backbone.View.extend({
 		if (this.customer.isFinishedWizard) {
 			this.processingPopup.render();
 			this.finishedWizard();
+			this.customer.isFinishedWizard = false;
 		}
 
 		/*
