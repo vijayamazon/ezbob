b4a45c26c83b99fe7082224e7810c3ab76763e9d
Shlomi Meiner <shlomim@ezbob.com>
2015-08-16 16:10:28 +0300

    EZ-3870 Everline reskin wizard step 1

--- App/PluginWeb/EzBob.Web/Infrastructure/Html/EzButtonModel.cs
+++ App/PluginWeb/EzBob.Web/Infrastructure/Html/EzButtonModel.cs
@@ -5,6 +5,13 @@ using System.Web;
 
 namespace EzBob.Web.Infrastructure.Html
 {
+    public enum EzButtonType
+    {
+        link,
+        submit,
+        button,
+        input,
+    } // enum CustomerOriginEnum
     public class EzButtonModel
     {
           public string Id { get; set; }
@@ -12,13 +19,26 @@ namespace EzBob.Web.Infrastructure.Html
         public string Cls { get; set; }
         public string UiEventControlID { get; set; }
 
+        public string Href { get; set; }
+        public int TabIndex { get; set; }
+
+        public EzButtonType ButtonType { get; set; }
         public EzButtonModel() { }
 
-        public EzButtonModel(string id, string caption, string cls, string uiEventControlID) {
+        public EzButtonModel(string id, string caption
+            , string cls = ""
+            , string uiEventControlID = ""
+            , int tabIndex = 0
+            , EzButtonType buttonType = EzButtonType.button
+            ,string href = "")
+        {
             Id = id;
             Caption = caption;
             Cls = cls;
             UiEventControlID = uiEventControlID;
+             ButtonType = buttonType;
+            TabIndex = tabIndex;
+            Href = href;
         }
     }
 }
