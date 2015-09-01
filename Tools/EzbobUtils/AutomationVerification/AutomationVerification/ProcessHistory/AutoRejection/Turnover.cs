5c0fda5e54e93fc9150e49d0c916b9a43d4f2c6f
Alex Boursouk <alexbo@ezbob.com>
2015-09-01 18:25:03 +0300

    Auto reject: do not fail on enormous turnover (customers 12940 and 28335 have such).

--- Tools/EzbobUtils/AutomationVerification/AutomationVerification/ProcessHistory/AutoRejection/Turnover.cs
+++ Tools/EzbobUtils/AutomationVerification/AutomationVerification/ProcessHistory/AutoRejection/Turnover.cs
@@ -14,20 +14,23 @@
 		public void Init(DataModel model) {
 			Model = model;
 			Comment = string.Format(
-				"customer annual turnover £{0:N0} (min allowed £{1:N0}) quarter turnover £{2:N0} (min allowed £{3:N0}) and has {4} company files",
+				"customer annual turnover £{0:N0} (min allowed £{1:N0}), " +
+				"quarter turnover £{2:N0} (min allowed £{3:N0}), " +
+				"and has {4} company files",
 				model.AnnualTurnover,
 				model.AnnualTurnoverThreshhold,
 				model.QuarterTurnover,
 				model.QuarterTurnoverThreshhold,
-				model.HasCompanyFiles ? "a" : "no");
+				model.HasCompanyFiles ? string.Empty : "no"
+			);
 		} // Init
 
 		public class DataModel {
 			public bool HasCompanyFiles { get; set; }
-			public int AnnualTurnover { get; set; }
-			public int AnnualTurnoverThreshhold { get; set; }
-			public int QuarterTurnover { get; set; }
-			public int QuarterTurnoverThreshhold { get; set; }
-		}
-	}
-}
+			public decimal AnnualTurnover { get; set; }
+			public decimal AnnualTurnoverThreshhold { get; set; }
+			public decimal QuarterTurnover { get; set; }
+			public decimal QuarterTurnoverThreshhold { get; set; }
+		} // class DataModel
+	} // class Turnover
+} // namespace
