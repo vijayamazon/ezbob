573b3905e279ebd021fa08053d048e2b377f5ce3
stas_dulman <stasd@ezbob.com>
2015-09-16 17:27:38 +0300

    MRK-414 MRK-413 vat wording, accounts popup cancel button add

--- sql/Current/2015_09_16_01_MPDescriptionUpdate.sql
+++ sql/Current/2015_09_16_01_MPDescriptionUpdate.sql
@@ -8,3 +8,8 @@ UPDATE dbo.MP_MarketplaceType
 SET Description = 'Bank account'
 WHERE InternalId = '107DE9EB-3E57-4C5B-A0B5-FFF445C4F2DF'
 GO
+
+UPDATE dbo.MP_MarketplaceGroup
+SET Description = 'Please link your VAT account or upload the last 5 quarters of your VAT reports.  If your accountant takes care of your VAT, we can contact them on your behalf once you’ve submitted your application.'
+WHERE Name = 'VAT'
+GO
