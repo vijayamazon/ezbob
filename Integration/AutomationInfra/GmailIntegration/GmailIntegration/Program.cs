3c281dd80e34cc933a2a7e3b44dc115789a81eb5
mishas <mishas@ezbob.com>
2015-10-11 15:31:24 +0300

    Recovered Automation branch

---
+++ Integration/AutomationInfra/GmailIntegration/GmailIntegration/Program.cs
@@ -0,0 +1,89 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace GmailIntegration {
+    internal class Program {
+        private static string[] Scopes = new string[]
+        {
+            GmailService.Scope.GmailReadonly
+        };
+
+        private static string ApplicationName = "Gmail API Quickstart";
+
+        private const string mailTitle = "A message from your finance broker";
+        private const string contactEmail = "test+lead_635801633354548240@ezbob.com";
+        private const string enviorment = "test.everline.com";
+
+
+        private static void Main(string[] args) {
+            UserCredential credential;
+            using (FileStream stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read)) {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials");
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Program.Scopes, "test@ezbob.com", CancellationToken.None, new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            GmailService service = new GmailService(new BaseClientService.Initializer {
+                HttpClientInitializer = credential,
+                ApplicationName = "Gmail API Quickstart"
+            });
+            List<Message> messageList = Program.ListMessages(service, "me", "subject: " + mailTitle);
+            foreach (Message message in messageList) {
+                Message fullMessage = Program.GetMessage(service, "me", message.Id);
+                MessagePartHeader messageInfo = fullMessage.Payload.Headers.FirstOrDefault((MessagePartHeader x) => x.Name == "Delivered-To");
+                if (messageInfo != null && string.Equals(messageInfo.Value, contactEmail)) {
+                    string bodyData = fullMessage.Payload.Body.Data.Replace("-", "+").Replace("_", "/");
+                    byte[] data64Base = Convert.FromBase64String(bodyData);
+                    string htmlCode = Encoding.UTF8.GetString(data64Base);
+                    Regex linkParser = new Regex("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                    foreach (object link in linkParser.Matches(htmlCode)) {
+                        if (link.ToString().Contains(enviorment)) {
+                            string ret = link.ToString();
+                            //return link.ToString();
+                        }
+                    }
+                }
+            }
+            //return null;
+        }
+
+        public static List<Message> ListMessages(GmailService service, string userId, string query) {
+            List<Message> result = new List<Message>();
+            UsersResource.MessagesResource.ListRequest request = service.Users.Messages.List(userId);
+            request.Q = query;
+            do {
+                try {
+                    ListMessagesResponse response = request.Execute();
+                    result.AddRange(response.Messages);
+                    request.PageToken = response.NextPageToken;
+                } catch (Exception e) {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                }
+            }
+            while (!string.IsNullOrEmpty(request.PageToken));
+            return result;
+        }
+
+        public static Message GetMessage(GmailService service, string userId, string messageId) {
+            Message result;
+            try {
+                result = service.Users.Messages.Get(userId, messageId).Execute();
+                return result;
+            } catch (Exception e) {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            result = null;
+            return result;
+        }
+    }
+}
---
+++ Integration/AutomationTests/Tests/Shared/BrokerShared.cs
@@ -0,0 +1,138 @@
+﻿namespace UIAutomationTests.Tests.Shared {
+    using System;
+    using System.Resources;
+    using System.Threading;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using UIAutomationTests.Core;
+
+    class BrokerShared : WebTestBase {
+
+        private IWebDriver _Driver;
+        private ResourceManager _EnvironmentConfig;
+        private ResourceManager _BrandConfig;
+
+        public BrokerShared(IWebDriver Driver, ResourceManager EnvironmentConfig, ResourceManager BrandConfig) {
+            this._Driver = Driver;
+            this._EnvironmentConfig = EnvironmentConfig;
+            this._BrandConfig = BrandConfig;
+        }
+        /// <summary>
+        /// This procedure is to replace broker test case 'C1202'
+        /// </summary>
+        /// <returns>Returns the newly generated broker Email</returns>
+        public void CreateNewBrokerAccount(
+            string iFirmName,
+            string iContactName,
+            string iContactEmail,
+            string iContactMobile,
+            string iMobileCode,
+            string iEstimatedMonthlyAppCount,
+            string iEstimatedMonthlyClientAmount,
+            string iPassword,
+            bool iAgreeToTerms,
+            bool iAgreeToPrivacyPolicy
+            ) {
+
+            string url = this._BrandConfig.GetString("BrokerSignupHost");
+
+            this._Driver.Navigate()
+                .GoToUrl(url);
+
+            IWebElement firmName = this._Driver.FindElement(By.Id("FirmName"));
+            firmName.SendKeys(iFirmName); 
+
+            IWebElement contactName = this._Driver.FindElement(By.Id("ContactName"));
+            contactName.SendKeys(iContactName);
+
+            IWebElement contactEmail = this._Driver.FindElement(By.Id("ContactEmail"));
+            contactEmail.SendKeys(iContactEmail);
+
+            IWebElement contactMobile = this._Driver.FindElement(By.Id("ContactMobile"));
+            contactMobile.SendKeys(iContactMobile);
+
+            IWebElement generateMobileCode = this._Driver.FindElement(By.Id("generateMobileCode"));
+            generateMobileCode.Click();
+
+            IWebElement mobileCode = this._Driver.FindElement(By.Id("MobileCode"));
+            mobileCode.SendKeys(iMobileCode);
+
+            IWebElement estimatedMonthlyAppCount = this._Driver.FindElement(By.Id("EstimatedMonthlyAppCount"));
+            estimatedMonthlyAppCount.SendKeys(iEstimatedMonthlyAppCount);
+
+            IWebElement estimatedMonthlyClientAmount = this._Driver.FindElement(By.Id("EstimatedMonthlyClientAmount"));
+            estimatedMonthlyClientAmount.SendKeys(iEstimatedMonthlyClientAmount);
+
+            IWebElement password = this._Driver.FindElement(By.Id("Password"));
+            password.SendKeys(iPassword);
+
+            IWebElement password2 = this._Driver.FindElement(By.Id("Password2"));
+            password2.SendKeys(iPassword);
+
+            if (iAgreeToTerms) {
+                IWebElement agreeToTerms = this._Driver.FindElement(By.XPath("//label[@for='AgreeToTerms']"));
+                agreeToTerms.Click();
+            }
+
+            if (iAgreeToPrivacyPolicy) {
+                IWebElement agreeToPrivacyPolicy = this._Driver.FindElement(By.XPath("//label[@for='AgreeToPrivacyPolicy']"));
+                agreeToPrivacyPolicy.Click();
+            }
+
+            IWebElement signupBrokerButton = this._Driver.FindElement(By.Id("SignupBrokerButton"));
+            signupBrokerButton.Click();
+
+            Thread.Sleep(5000);
+            Assert.IsTrue(this._Driver.FindElement(By.Id("AddNewCustomer")).Displayed);
+            IWebElement logOff = this._Driver.FindElement(By.ClassName("log-off"));
+            logOff.Click();
+        }
+
+        public void BrokerLogIn(string brokerMail) {
+
+            string url = this._BrandConfig.GetString("BrokerLoginHost");
+            this._Driver.Navigate()
+                .GoToUrl(url);
+
+            IWebElement loginEmail = this._Driver.FindElement(By.Id("LoginEmail"));
+            loginEmail.SendKeys(brokerMail);
+
+            IWebElement loginPassword = this._Driver.FindElement(By.Id("LoginPassword"));
+            loginPassword.SendKeys("123456");
+
+            IWebElement loginBrokerButton = this._Driver.FindElement(By.Id("LoginBrokerButton"));
+            loginBrokerButton.Click();
+
+            Thread.Sleep(2000);
+            if (this._Driver.FindElement(By.ClassName("section-requestacceptterms")).Displayed == true) {//In case new terms screen appears.
+                IWebElement acceptButon = this._Driver.FindElement(By.Id("AcceptTermsButton"));
+                acceptButon.Click();
+            }
+        }
+
+        /// <summary>
+        /// Pre-condtion broker must be logged-in
+        /// </summary>
+        /// <returns>Newly created leed mail address</returns>
+        public void BrokerLeedEnrolment(string fName, string lName, string leadEmail) {
+
+            Thread.Sleep(7000);
+            Assert.IsTrue(this._Driver.FindElement(By.Id("AddNewCustomer")) != null);
+
+            IWebElement addNewCustomer = this._Driver.FindElement(By.Id("AddNewCustomer"));
+            addNewCustomer.Click();
+
+            Thread.Sleep(2000);
+            Assert.IsTrue(this._Driver.FindElement(By.Id("LeadFirstName")) != null);
+
+            IWebElement leadFirstName = this._Driver.FindElement(By.Id("LeadFirstName"));
+            leadFirstName.SendKeys(fName);
+
+            IWebElement leadLastName = this._Driver.FindElement(By.Id("LeadLastName"));
+            leadLastName.SendKeys(lName);
+
+            IWebElement leadMail = this._Driver.FindElement(By.Id("LeadEmail"));
+            leadMail.SendKeys(leadEmail);
+        }
+    }
+}
