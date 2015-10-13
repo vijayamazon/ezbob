a064b8948f69708b6939da548dd3c7ad6f84d717
Sasha Feldman <sashaf@ezbob.com>
2015-10-13 13:36:58 +0300

    EZ-4135, EZ-4136, EZ-4137 - bugs fixed

--- App/PluginWeb/EzBob.Web/Content/js/Broker/broker.submit.view.js
+++ App/PluginWeb/EzBob.Web/Content/js/Broker/broker.submit.view.js
@@ -66,7 +66,7 @@ EzBob.Broker.SubmitView = EzBob.Broker.BaseView.extend({
 	}, // isSubmitEnabled
 
 	setSubmitEnabled: function(bEnabled) {
-		return this.setSomethingEnabled(this.submitSelector(), bEnabled).toggleClass('btn-green ev-btn-org', bEnabled);
+		return this.setSomethingEnabled(this.submitSelector(), bEnabled).toggleClass('btn-green', bEnabled);
 	}, // setSubmitEnabled
 
 	inputChanged: function(evt) {
