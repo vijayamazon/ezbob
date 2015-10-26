a3473105f893b57e6c582d5a7280da9f8090f70e
Alex Boursouk <alexbo@ezbob.com>
2015-10-26 16:12:16 +0200

    Bug fix: during GetReport in UW: Error during serialization or deserialization using the JSON JavaScriptSerializer. The length of the string exceeds the value set on the maxJsonLength property.
    (cherry picked from commit 966a004338ffc4eb99faa15ddd2600e2a91b9c4f)

--- App/PluginWeb/EzBob.Web/Areas/Underwriter/Controllers/ReportController.cs
+++ App/PluginWeb/EzBob.Web/Areas/Underwriter/Controllers/ReportController.cs
@@ -3,7 +3,6 @@
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Web.Mvc;
-	using System.Web.Script.Serialization;
 	using Code.ReportGenerator;
 	using EZBob.DatabaseLib.Model.Database.Report;
 	using Ezbob.Database;
@@ -15,24 +14,29 @@
 	using global::Reports;
 
 	public class ReportController : Controller {
-		private readonly ReportUsersMapsRepository _reportsRepository;
-		private readonly ReportRepository _reportRepository;
-		private readonly IWorkplaceContext _context;
-		public ReportController(ReportUsersMapsRepository reportsRepository, IWorkplaceContext context, ReportRepository reportRepository) {
-			_reportsRepository = reportsRepository;
-			_context = context;
-			_reportRepository = reportRepository;
-		}
+		public ReportController(
+			ReportUsersMapsRepository reportsUsersRepository,
+			IWorkplaceContext context,
+			ReportRepository reportRepository
+		) {
+			this.reportsUsersRepository = reportsUsersRepository;
+			this.context = context;
+			this.reportRepository = reportRepository;
+		} // constructor
 
 		[Ajax]
 		[HttpGet]
 		public JsonResult GetAll() {
-			var uwId = _context.UserId;
-			var reports = _reportsRepository.GetAllUnderwriterReports(uwId);
+			var uwId = this.context.UserId;
+			var reports = this.reportsUsersRepository.GetAllUnderwriterReports(uwId);
 			var model = new ReportModel();
-			var dates = Enum.GetValues(typeof(ReportDate)).Cast<ReportDate>().Select(x => new { key = x.ToString(), value = x.DescriptionAttr() });
+
+			var dates = Enum.GetValues(typeof(ReportDate))
+				.Cast<ReportDate>()
+				.Select(x => new { key = x.ToString(), value = x.DescriptionAttr() });
+
 			return Json(new { reports = reports.Select(model.ToModel), dates = dates }, JsonRequestBehavior.AllowGet);
-		}
+		} // GetAll
 
 		[Ajax]
 		[HttpPost]
@@ -48,15 +52,14 @@
 
 			ATag data = reportHandler.GetReportData(report, rptDef, oColumnTypes, out isError);
 
-			var serializer = new JavaScriptSerializer {
-				MaxJsonLength = Int32.MaxValue
-			};
-
 			return new ContentResult {
-				Content = serializer.Serialize(new { report = data.ToString(), columns = oColumnTypes }),
+				Content = Newtonsoft.Json.JsonConvert.SerializeObject(new {
+					report = data.ToString(),
+					columns = oColumnTypes,
+				}),
 				ContentType = "application/json",
 			};
-		}
+		} // GetReportDates
 
 		[Ajax]
 		[HttpPost]
@@ -65,7 +68,7 @@
 			dates.From = DateTime.SpecifyKind(dates.From, DateTimeKind.Utc);
 			dates.To = DateTime.SpecifyKind(dates.To, DateTimeKind.Utc);
 			return GetReportDates(reportId, dates.From, dates.To, customer, nonCash);
-		}
+		} // GetReport
 
 		public FileResult DownloadReportDates(int reportId, DateTime from, DateTime to, string customer, bool? nonCash) {
 			var report = GetReport(reportId);
@@ -74,37 +77,48 @@
 			var reportHandler = new ReportHandler(oDB);
 
 			var excel = reportHandler.GetWorkBook(report, rptDef);
-			var fc = new FileContentResult(excel.GetAsByteArray(), "Application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
-				FileDownloadName = report.Title.Replace(" ", "") + from.ToString("yyyy-MM-dd") + to.ToString("yyyy-MM-dd") + ".xlsx"
+			var fc = new FileContentResult(
+				excel.GetAsByteArray(),
+				"Application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+			) {
+				FileDownloadName =
+					report.Title.Replace(" ", "") +
+					from.ToString("yyyy-MM-dd") +
+					to.ToString("yyyy-MM-dd") + ".xlsx"
 			};
 
 			return fc;
-		}
+		} // DownloadReportDates
 
 		public FileResult DownloadReport(int reportId, ReportDate reportDate, string customer, bool? nonCash) {
 			var dates = ReporDateRanges.GetDates(reportDate);
 			return DownloadReportDates(reportId, dates.From, dates.To, customer, nonCash);
-		}
+		} // DownloadReport
 
 		[NonAction]
 		private Report GetReport(int reportId) {
-			var dbReport = _reportRepository.Get(reportId);
+			var dbReport = this.reportRepository.Get(reportId);
 
 			ReportType type;
-			if (!Enum.TryParse(dbReport.Type, out type)) {
+
+			if (!Enum.TryParse(dbReport.Type, out type))
 				type = ReportType.RPT_GENERIC;
-			}
+
 			var report = new Report {
 				Title = dbReport.Title,
 				Type = type,
 				StoredProcedure = dbReport.StoredProcedure,
 				Columns = Report.ParseHeaderAndFields(dbReport.Header, dbReport.Fields),
 			};
-			foreach (var arg in dbReport.Arguments) {
+
+			foreach (var arg in dbReport.Arguments)
 				report.AddArgument(arg.ReportArgument.Name);
-			}
 
 			return report;
-		}
-	}
-}
+		} // GetReport
+
+		private readonly ReportUsersMapsRepository reportsUsersRepository;
+		private readonly ReportRepository reportRepository;
+		private readonly IWorkplaceContext context;
+	} // class ReportController
+} // namespace
