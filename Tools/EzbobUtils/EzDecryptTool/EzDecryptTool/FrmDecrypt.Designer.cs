e8692ac7a52b1cda12875e712c67d721e5ecf825
Alex Boursouk <alexbo@ezbob.com>
2015-10-29 15:14:35 +0200

    Encrypt/Decrypt tool cosmetic updates

--- Tools/EzbobUtils/EzDecryptTool/EzDecryptTool/FrmDecrypt.Designer.cs
+++ Tools/EzbobUtils/EzDecryptTool/EzDecryptTool/FrmDecrypt.Designer.cs
@@ -26,80 +26,108 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDecrypt));
 			this.txtInput = new System.Windows.Forms.RichTextBox();
 			this.txtOutput = new System.Windows.Forms.RichTextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.btnDecrypt = new System.Windows.Forms.Button();
-			this.btnEncrypt = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.encryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.decryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.menuStrip1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// txtInput
 			// 
-			this.txtInput.Location = new System.Drawing.Point(25, 44);
+			this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.txtInput.Location = new System.Drawing.Point(6, 19);
 			this.txtInput.Name = "txtInput";
-			this.txtInput.Size = new System.Drawing.Size(400, 150);
+			this.txtInput.Size = new System.Drawing.Size(748, 155);
 			this.txtInput.TabIndex = 0;
 			this.txtInput.Text = "";
 			// 
 			// txtOutput
 			// 
-			this.txtOutput.Location = new System.Drawing.Point(25, 222);
+			this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.txtOutput.Location = new System.Drawing.Point(6, 19);
 			this.txtOutput.Name = "txtOutput";
-			this.txtOutput.Size = new System.Drawing.Size(400, 150);
+			this.txtOutput.Size = new System.Drawing.Size(748, 311);
 			this.txtOutput.TabIndex = 1;
 			this.txtOutput.Text = "";
 			// 
-			// label1
+			// menuStrip1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(22, 206);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(39, 13);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "Output";
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.encryptToolStripMenuItem,
+            this.decryptToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+			this.menuStrip1.TabIndex = 6;
+			this.menuStrip1.Text = "menuStrip1";
 			// 
-			// label2
+			// encryptToolStripMenuItem
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(22, 28);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(31, 13);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Input";
+			this.encryptToolStripMenuItem.Name = "encryptToolStripMenuItem";
+			this.encryptToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+			this.encryptToolStripMenuItem.Text = "Encrypt";
+			this.encryptToolStripMenuItem.Click += new System.EventHandler(this.btnEncrypt_Click);
 			// 
-			// btnDecrypt
+			// decryptToolStripMenuItem
 			// 
-			this.btnDecrypt.Location = new System.Drawing.Point(444, 43);
-			this.btnDecrypt.Name = "btnDecrypt";
-			this.btnDecrypt.Size = new System.Drawing.Size(197, 151);
-			this.btnDecrypt.TabIndex = 4;
-			this.btnDecrypt.Text = "Decrypt";
-			this.btnDecrypt.UseVisualStyleBackColor = true;
-			this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+			this.decryptToolStripMenuItem.Name = "decryptToolStripMenuItem";
+			this.decryptToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+			this.decryptToolStripMenuItem.Text = "Decrypt";
+			this.decryptToolStripMenuItem.Click += new System.EventHandler(this.btnDecrypt_Click);
 			// 
-			// btnEncrypt
+			// groupBox1
 			// 
-			this.btnEncrypt.Location = new System.Drawing.Point(444, 220);
-			this.btnEncrypt.Name = "btnEncrypt";
-			this.btnEncrypt.Size = new System.Drawing.Size(197, 152);
-			this.btnEncrypt.TabIndex = 5;
-			this.btnEncrypt.Text = "Encrypt";
-			this.btnEncrypt.UseVisualStyleBackColor = true;
-			this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.txtInput);
+			this.groupBox1.Location = new System.Drawing.Point(12, 27);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(760, 180);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Input";
 			// 
-			// Form1
+			// groupBox2
+			// 
+			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox2.Controls.Add(this.txtOutput);
+			this.groupBox2.Location = new System.Drawing.Point(12, 213);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(760, 336);
+			this.groupBox2.TabIndex = 8;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Output";
+			// 
+			// FrmDecrypt
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(672, 414);
-			this.Controls.Add(this.btnEncrypt);
-			this.Controls.Add(this.btnDecrypt);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.txtOutput);
-			this.Controls.Add(this.txtInput);
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.menuStrip1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "Form1";
+			this.MainMenuStrip = this.menuStrip1;
+			this.MinimumSize = new System.Drawing.Size(640, 480);
+			this.Name = "FrmDecrypt";
 			this.Text = "EzDecrypt Tool";
+			this.Load += new System.EventHandler(this.FrmDecrypt_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -109,10 +137,11 @@
 
 		private System.Windows.Forms.RichTextBox txtInput;
 		private System.Windows.Forms.RichTextBox txtOutput;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button btnDecrypt;
-		private System.Windows.Forms.Button btnEncrypt;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem encryptToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem decryptToolStripMenuItem;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
 	}
 }
 
