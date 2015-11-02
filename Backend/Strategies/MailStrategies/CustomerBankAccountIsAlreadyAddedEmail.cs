2d253b4fe4e2292329cc14738918c4c22e7a5626
stas_dulman <stasd@ezbob.com>
2015-11-02 17:15:17 +0200

    EZ-3811 block bank UW email content fix

--- Backend/Strategies/MailStrategies/CustomerBankAccountIsAlreadyAddedEmail.cs
+++ Backend/Strategies/MailStrategies/CustomerBankAccountIsAlreadyAddedEmail.cs
@@ -15,11 +15,13 @@
 				return;
 			}
 			string message = string.Format(@"Bank account provided for funds transfer for loan of customer {0} was found identical to bank account used for another loan under different account. 
+
 Please note that the only NONE FRAUD scenario is of two companies under same ownership & same bank account but with two separate loan requests.
 
 Funds transfer is currently stopped.
 
 Please review the case and resume the funds transfer only if you are sure this is not Fraud attempt.
+
 In case you are investigating the issue - you can set the Fraud status to ""Under Investigation"" and leave it this way until further decision will be taken.
 
 In case you are confident with resuming the funds transfer, please do the following:
