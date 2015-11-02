0a6bf440f013082788dd9a8a9f129bc5cdf7b91c
stas_dulman <stasd@ezbob.com>
2015-11-02 15:40:01 +0200

    EZ-3811 block bank UW tooltip, update email for UWs with description

--- Backend/Strategies/MailStrategies/CustomerBankAccountIsAlreadyAddedEmail.cs
+++ Backend/Strategies/MailStrategies/CustomerBankAccountIsAlreadyAddedEmail.cs
@@ -14,7 +14,19 @@
 				this.Log.Warn("FraudAlertMailReciever is null or empty, not sending alert");
 				return;
 			}
-			string message = string.Format("Bank account provided for funds transfer for loan of customer {0} was found identical to bank account used for another loan under different account", this.customerID);
+			string message = string.Format(@"Bank account provided for funds transfer for loan of customer {0} was found identical to bank account used for another loan under different account. 
+Please note that the only NONE FRAUD scenario is of two companies under same ownership & same bank account but with two separate loan requests.
+
+Funds transfer is currently stopped.
+
+Please review the case and resume the funds transfer only if you are sure this is not Fraud attempt.
+In case you are investigating the issue - you can set the Fraud status to ""Under Investigation"" and leave it this way until further decision will be taken.
+
+In case you are confident with resuming the funds transfer, please do the following:
+1. Set Block taking loan to ""No"".
+2. Contact the client and request to proceed with the agreement confirmation process.
+
+Thank you.", this.customerID);
 			new Mail().Send(
 					CurrentValues.Instance.FraudAlertMailReciever,
 					message,
