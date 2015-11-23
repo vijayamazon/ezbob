1d6884a9d6b2ae0bb1d3ae0ec0d0021c4bbaed6a
Sasha Feldman <sashaf@ezbob.com>
2015-11-23 15:35:03 +0200

    EZ-4276 partial

---
+++ App/PluginWeb/EzBob.Web/Content/js/Underwriter/StrategySettings/EzBob.Underwriter.Settings.OpenPlatform.js
@@ -0,0 +1,31 @@
+﻿var EzBob = EzBob || {};
+EzBob.Underwriter = EzBob.Underwriter || {};
+
+EzBob.Underwriter.Settings.OpenPlatformModel = Backbone.Model.extend({
+	url: window.gRootPath + "Underwriter/StrategySettings/OpenPlatform"
+});
+
+EzBob.Underwriter.Settings.OpenPlatformView = Backbone.Marionette.ItemView.extend({
+	template: "#open-platform-settings-template",
+	initialize: function(options) {
+		this.modelBinder = new Backbone.ModelBinder();
+		this.model.on("reset change update", this.render, this);
+		/*this.update();*/
+		return this;
+	},
+	bindings: {
+
+	},
+
+	events: {
+
+	},
+
+	onRender: function() {
+
+	},
+
+	update: function() {
+		this.model.fetch({ reset: true });
+	}
+});
---
+++ App/PluginWeb/EzBob.Web/Content/js/Underwriter/StrategySettings/EzBob.Underwriter.Settings.Product.js
@@ -0,0 +1,32 @@
+﻿var EzBob = EzBob || {};
+EzBob.Underwriter = EzBob.Underwriter || {};
+EzBob.Underwriter.Settings = EzBob.Underwriter.Settings || {};
+
+EzBob.Underwriter.Settings.ProductModel = Backbone.Model.extend({
+	url: window.gRootPath + "Underwriter/StrategySettings/Product"
+});
+
+EzBob.Underwriter.Settings.ProductView = Backbone.Marionette.ItemView.extend({
+	template: "#product-settings-template",
+	initialize: function(options) {
+		this.modelBinder = new Backbone.ModelBinder();
+		this.model.on("reset change update", this.render, this);
+		/*this.update();*/
+		return this;
+	},
+	bindings: {
+
+	},
+
+	events: {
+
+	},
+
+	onRender: function() {
+		
+	},
+
+	update: function() {
+		this.model.fetch({ reset: true });
+	}
+});
