83bd42dbf991ba59e7aca1ab14acac7759cd4796
Alex Boursouk <alexbo@ezbob.com>
2015-11-26 19:32:03 +0200

    EZ-4216, EZ-4217, EZ-4218, EZ-4219 Logical Glue integration.

--- Integration/LogicalGlue/Engine/Interface/IEngine.cs
+++ Integration/LogicalGlue/Engine/Interface/IEngine.cs
@@ -4,26 +4,23 @@
 	public interface IEngine {
 		/// <summary>
 		/// Infers customer using current customer details.
-		/// <para>Collects customer name, address, etc by customer ID and executes three calls to Logical Glue API:
-		/// <list type="number">
-		/// <item><description>Gets inference input data by customer name, address, etc.</description></item>
-		/// <item><description>With inference input data executes fuzzy logic model call.</description></item>
-		/// <item><description>With inference input data executes neural network model call.</description></item>
-		/// </list>
-		/// Model outputs are stored in DB and returned to caller.
-		/// </para>
+		/// <para>Collects customer name, address, etc by customer ID and executes a call to Logical Glue API.</para>
+		/// <para>LG output is stored in DB and returned to caller.</para>
 		/// </summary>
 		/// <param name="customerID">ID of customer to infer.</param>
 		/// <returns>Customer inference from fuzzy logic and neural network models.</returns>
 		Inference Infer(int customerID);
 
 		/// <summary>
-		/// Loads the latest customer inference results that are available now.
-		/// Logical Glue API is not queried.
+		/// Gets the latest existing inference from DB or queries Logical Glue API for specified customer.
+		/// Method behaviour depending on <see cref="checkCacheOnly" /> and <see cref="forceRemoteRequest" />.
 		/// </summary>
 		/// <param name="customerID">ID of customer to infer.</param>
-		/// <returns>The latest customer inference results that are available now.</returns>
-		Inference GetInference(int customerID);
+		/// <param name="forceRemoteRequest">If true, queries Logical Glue API, attempts to store results to DB, and
+		/// returns the last existing inference from DB. If no new inference stored, returns the latest among previously
+		/// exising.</param>
+		/// <returns>Customer inference results.</returns>
+		Inference GetInference(int customerID, bool forceRemoteRequest, bool checkCacheOnly);
 
 		/// <summary>
 		/// Loads the latest customer inference results that were available on specific time.
