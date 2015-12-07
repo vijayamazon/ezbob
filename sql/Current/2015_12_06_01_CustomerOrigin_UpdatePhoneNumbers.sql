da21b3956b98126771634924d7f07f0ea31a1edc
stas_dulman <stasd@ezbob.com>
2015-12-07 11:30:06 +0200

    updated app phone numbers all origins fix

--- sql/Current/2015_12_06_01_CustomerOrigin_UpdatePhoneNumbers.sql
+++ sql/Current/2015_12_06_01_CustomerOrigin_UpdatePhoneNumbers.sql
@@ -1,2 +1,3 @@
-UPDATE CustomerOrigin SET PhoneNumber = '0203 769 3128' WHERE Name = ' ezbob' AND Name = ' alibaba'
-UPDATE CustomerOrigin SET PhoneNumber = '0203 769 3129' WHERE Name = ' alibaba'
+UPDATE CustomerOrigin SET PhoneNumber = '0203 769 3128' WHERE Name = 'ezbob' OR Name = 'alibaba'
+UPDATE CustomerOrigin SET PhoneNumber = '0203 769 3129' WHERE Name = 'everline'
+GO
