dd8bb88351ba70f65807eb37d8582757b097fa9e
stas_dulman <stasd@ezbob.com>
2015-12-27 19:05:02 +0200

    EZ-3505 fix sql query to be re-runnable

--- sql/current/2015_12_20_01_Directors_AddCoulmn_insertbyid.sql
+++ sql/current/2015_12_20_01_Directors_AddCoulmn_insertbyid.sql
@@ -1,3 +1,7 @@
-ALTER TABLE Director
-    ADD UserId INTEGER,
-    FOREIGN KEY(UserId) REFERENCES Security_User(UserId);
+IF NOT EXISTS (SELECT * FROM syscolumns WHERE name='UserId' AND id=object_id('Director'))
+BEGIN
+	ALTER TABLE Director
+	    ADD UserId INTEGER,
+	    FOREIGN KEY(UserId) REFERENCES Security_User(UserId)
+END
+GO
