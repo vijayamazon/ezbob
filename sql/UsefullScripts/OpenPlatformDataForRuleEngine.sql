83646755e7238d2b716c5cc9111dd5bfd64a6fe8
Dor <ezbobdev>
2015-12-28 19:15:02 +0200

    complete help script.

--- sql/UsefullScripts/OpenPlatformDataForRuleEngine.sql
+++ sql/UsefullScripts/OpenPlatformDataForRuleEngine.sql
@@ -1,13 +1,16 @@
 
 INSERT INTO [dbo].[I_InvestorRule]([UserID],[RuleType],[InvestorID],[MemberNameSource],[MemberNameTarget],[LeftParamID],[RightParamID],[Operator],[IsRoot])
      VALUES(1,1,null,null,null,2,3,1,1)
+GO
 
 INSERT INTO [dbo].[I_InvestorRule]([UserID],[RuleType],[InvestorID],[MemberNameSource],[MemberNameTarget],[LeftParamID],[RightParamID],[Operator],[IsRoot])
      VALUES (1,1,null,'ManagerApprovedSum','Balance',null,null,3,0)
 
+GO
 INSERT INTO [dbo].[I_InvestorRule]([UserID],[RuleType],[InvestorID],[MemberNameSource],[MemberNameTarget],[LeftParamID],[RightParamID],[Operator],[IsRoot])
      VALUES (1,1,null,'ManagerApprovedSum','DailyInvestmentAllowed',null,null,3,0)
 
+GO
 
 INSERT INTO [dbo].[I_Parameter]
            ([Name]
@@ -28,5 +31,25 @@ INSERT INTO [dbo].[I_Parameter]
      VALUES
            ('DailyInvestmentAllowed', 'double', 0, null , 0)
 GO
+
+INSERT INTO [dbo].[I_InvestorParams]
+           ([InvestorID]
+           ,[ParameterID]
+           ,[Value]
+           ,[Type])
+     VALUES
+			(1, 1, 500, 1)
+GO
+
+INSERT INTO [dbo].[I_InvestorParams]
+           ([InvestorID]
+           ,[ParameterID]
+           ,[Value]
+           ,[Type])
+     VALUES
+			(1, 2, 200, 1)
+GO
+
+
 
 
