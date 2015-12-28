382c14fc4b2715dab93dcddb697b81673b8bd30e
Dor <ezbobdev>
2015-12-28 19:07:18 +0200

    add more help script

--- sql/UsefullScripts/OpenPlatformDataForRuleEngine.sql
+++ sql/UsefullScripts/OpenPlatformDataForRuleEngine.sql
@@ -7,3 +7,26 @@ INSERT INTO [dbo].[I_InvestorRule]([UserID],[RuleType],[InvestorID],[MemberNameS
 
 INSERT INTO [dbo].[I_InvestorRule]([UserID],[RuleType],[InvestorID],[MemberNameSource],[MemberNameTarget],[LeftParamID],[RightParamID],[Operator],[IsRoot])
      VALUES (1,1,null,'ManagerApprovedSum','DailyInvestmentAllowed',null,null,3,0)
+
+
+INSERT INTO [dbo].[I_Parameter]
+           ([Name]
+           ,[ValueType]
+           ,[DefaultValue]
+           ,[MaxLimit]
+           ,[MinLimit])
+     VALUES
+           ('Balance', 'double', 0, null, 0)
+GO
+
+INSERT INTO [dbo].[I_Parameter]
+           ([Name]
+           ,[ValueType]
+           ,[DefaultValue]
+           ,[MaxLimit]
+           ,[MinLimit])
+     VALUES
+           ('DailyInvestmentAllowed', 'double', 0, null , 0)
+GO
+
+
