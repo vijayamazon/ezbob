00884fe65f2412f356d53043dd48654243214a97
Dor <ezbobdev>
2016-01-06 15:16:57 +0200

    adding support in complex rules

--- Backend/Strategies/OpenPlatform/GenericRule/IGenericRulecs.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Ezbob.Backend.Strategies.OpenPlatform.GenericRule {
-    interface IGenericRules {
-        bool InvokeRule(long cashRequestId, int investorID);
-    }
-}
--- Backend/Strategies/OpenPlatform/Provider/Contracts/IProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ezbob.Backend.Strategies.OpenPlatform.Provider.Contracts
-{
-    public interface IProvider<out T> 
-    {
-        T GetNew();
-    }
-}
