8548f7a1d8e634e6805d73656f3aad8c7942a688
Roman Palatnik <romanp@ezbob.com>
2016-01-07 11:53:23 +0200

    updated validators

---
+++ Backend/NewApi/EzBobRest/Validators/CustomerSendVerificationSmsValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzBobRest.Validators
+{
+    using EzBobApi.Commands.Customer;
+    using FluentValidation;
+
+    /// <summary>
+    /// Validates <see cref="CustomerSendVerificationSmsCommand"/>
+    /// </summary>
+    public class CustomerSendVerificationSmsValidator : AbstractValidator<CustomerSendVerificationSmsCommand> {
+        public CustomerSendVerificationSmsValidator() {
+            RuleFor(o => o.PhoneNumber)
+                .NotEmpty()
+                .WithMessage("Invalid phone number");
+        }
+    }
+}
---
+++ Backend/NewApi/EzBobRest/Validators/CustomerValidateVerificationCodeValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzBobRest.Validators {
+    using EzBobApi.Commands.Customer;
+    using FluentValidation;
+
+    /// <summary>
+    /// Validates <see cref="CustomerValidateVerificationCodeCommand"/>
+    /// </summary>
+    public class CustomerValidateVerificationCodeValidator : AbstractValidator<CustomerValidateVerificationCodeCommand> {
+        public CustomerValidateVerificationCodeValidator() {
+            RuleFor(o => o.CustomerId)
+                .NotEmpty()
+                .WithMessage("empty customer id")
+                .DependentRules(d => d.RuleFor(c => c.CustomerId.ToLowerInvariant())
+                    .NotEqual("{customerid}")//when url parameter is not provided Nancy puts default string
+                    .WithMessage("customer id is mandatory"));
+
+            RuleFor(o => o.VerificationCode)
+                .NotEmpty()
+                .WithMessage("empty verification code");
+
+            RuleFor(o => o.VerificationToken)
+                .NotEmpty()
+                .WithMessage("empty verification token")
+                .DependentRules(d => d.RuleFor(c => c.VerificationToken.ToLowerInvariant())
+                    .NotEqual("{verificationtoken}")//when url parameter is not provided Nancy puts default string
+                    .WithMessage("verification token is mandatory"));
+        }
+    }
+}
