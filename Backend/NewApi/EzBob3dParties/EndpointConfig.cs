be648ee3c686001b7045e1d2e32471b20a8243c1
Roman Palatnik <romanp@ezbob.com>
2016-01-07 16:03:38 +0200

    added CommonLoggin + InMemoryTimeoutPersistence

--- Backend/NewApi/EzBob3dParties/EndpointConfig.cs
+++ Backend/NewApi/EzBob3dParties/EndpointConfig.cs
@@ -1,27 +1,57 @@
 
-namespace EzBob3dParties
-{
+namespace EzBob3dParties {
     using NServiceBus;
+    using NServiceBus.Features;
+    using NServiceBus.Logging;
     using NServiceBus.Persistence.Legacy;
+    using StructureMap;
 
     /*
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-    public class EndpointConfig : IConfigureThisEndpoint
-    {
-        public void Customize(BusConfiguration configuration)
-        {
-            // NServiceBus provides the following durable storage options
-            // To use RavenDB, install-package NServiceBus.RavenDB and then use configuration.UsePersistence<RavenDBPersistence>();
-            // To use SQLServer, install-package NServiceBus.NHibernate and then use configuration.UsePersistence<NHibernatePersistence>();
-            
-            // If you don't need a durable storage you can also use, configuration.UsePersistence<InMemoryPersistence>();
-            // more details on persistence can be found here: http://docs.particular.net/nservicebus/persistence-in-nservicebus
-            
-            //Also note that you can mix and match storages to fit you specific needs. 
-            //http://docs.particular.net/nservicebus/persistence-order
+
+    public class EndpointConfig : IConfigureThisEndpoint {
+        public void Customize(BusConfiguration configuration) {
+            InitLogging();
+            InitConventions(configuration);
+
+            Container container = new Container(c => c.AddRegistry<EzBob3dPartiesRegistry>());
+
+            configuration.EndpointName("EzBob3dParties");
+            configuration.UseSerialization<JsonSerializer>();
+            configuration.UseTransport<MsmqTransport>();
+            configuration.EnableInstallers();
+
+            configuration.EnableFeature<InMemoryTimeoutPersistence>();//msmq persistence does not provide timeout persistence
             configuration.UsePersistence<MsmqPersistence>();
+
+            configuration.UseContainer<StructureMapBuilder>(c => c.ExistingContainer(container));
+        }
+
+        private void InitConventions(BusConfiguration config) {
+            config.Conventions()
+                .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("EzBobApi.Commands"));
+        }
+
+        private void InitLogging() {
+            LogManager.Use<CommonLoggingFactory>();
+
+            //            LogManager.Use<Log4NetFactory>();
+            //
+            //            PatternLayout layout = new PatternLayout {
+            //                ConversionPattern = "%d [%t] %-5p %c [%x] - %m%n"
+            //            };
+            //            layout.ActivateOptions();
+            //            ConsoleAppender consoleAppender = new ConsoleAppender {
+            //                Threshold = Level.Debug,
+            //                Layout = layout
+            //            };
+            //            // Note that ActivateOptions is required in NSB 5 and above
+            //            consoleAppender.ActivateOptions();
+            //
+            //            BasicConfigurator.Configure(consoleAppender);
+
         }
     }
 }
