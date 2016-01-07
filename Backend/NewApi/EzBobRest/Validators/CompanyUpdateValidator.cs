3c6a42284c3476fb1a3b644d5bbe916dd4c6a966
Roman Palatnik <romanp@ezbob.com>
2016-01-07 11:40:38 +0200

    added customer id validaton

--- Backend/NewApi/EzBobRest/Validators/CompanyUpdateValidator.cs
+++ Backend/NewApi/EzBobRest/Validators/CompanyUpdateValidator.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EzBobRest.Validators
-{
+namespace EzBobRest.Validators {
     using EzBobApi.Commands.Company;
     using EzBobCommon;
     using EzBobCommon.Utils;
@@ -16,6 +15,13 @@ namespace EzBobRest.Validators
     /// </summary>
     public class CompanyUpdateValidator : AbstractValidator<UpdateCompanyCommand> {
         public CompanyUpdateValidator() {
+            RuleFor(o => o.CustomerId)
+                .NotEmpty()
+                .WithMessage("customer id is mandatory")
+                .DependentRules(d => d.RuleFor(c => c.CustomerId.ToLowerInvariant())
+                    .NotEqual("{customerid}")//if url parameter is not specified Nancy put default string
+                    .WithMessage("customer id is mandatory"));
+
             When(o => o.CompanyDetails != null, () => {
                 When(o => CollectionUtils.IsNotEmpty(o.CompanyDetails.Authorities), () => {
                     RuleForEach(o => o.CompanyDetails.Authorities)
