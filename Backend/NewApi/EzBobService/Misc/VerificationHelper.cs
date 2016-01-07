681c2083632d68769b5d50238f4ae390840f560d
Roman Palatnik <romanp@ezbob.com>
2016-01-07 11:56:35 +0200

    part of validaton by sms feature

---
+++ Backend/NewApi/EzBobService/Misc/VerificationHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace EzBobService.Misc {
+    using System;
+    using EzBobCommon.Utils;
+    using EzBobCommon.Utils.Encryption;
+
+    /// <summary>
+    /// Generates verification word, encrypts it and validates
+    /// </summary>
+    public class VerificationHelper {
+        /// <summary>
+        /// Generates the verification word.
+        /// </summary>
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        public string GenerateVerificationWord(int length = 5) {
+            return StringUtils.GenerateRandomEnglishString(length);
+        }
+
+        /// <summary>
+        /// Encrypts the verification word.
+        /// </summary>
+        /// <param name="word">The word.</param>
+        /// <returns></returns>
+        public string EncryptVerificationWord(string word) {
+            return EncryptionUtils.SafeEncrypt(word);
+        }
+
+        /// <summary>
+        /// Validates the specified word.
+        /// </summary>
+        /// <param name="wordToValidate">The word to validate.</param>
+        /// <param name="encryptedWord">The encrypted word.</param>
+        /// <returns></returns>
+        public bool Validate(string wordToValidate, string encryptedWord) {
+
+            string decrypted = EncryptionUtils.SafeDecrypt(encryptedWord);
+            return wordToValidate.Equals(decrypted, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
