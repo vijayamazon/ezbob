cd0ec1c7d62cd630d89cfe1bed79fb197f43e8a4
Roman Palatnik <romanp@ezbob.com>
2016-01-07 19:40:14 +0200

    removed unnecessary usings

--- Backend/NewApi/EzBob3dParties/PayPalService/Soap/PayPalSoapConfig.cs
+++ Backend/NewApi/EzBob3dParties/PayPalService/Soap/PayPalSoapConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EzBob3dParties.PayPalService.Soap
+﻿namespace EzBob3dParties.PayPalService.Soap
 {
+    using System.Collections.Generic;
+
     public class PayPalSoapConfig
     {
         public string Mode { get; set; }
