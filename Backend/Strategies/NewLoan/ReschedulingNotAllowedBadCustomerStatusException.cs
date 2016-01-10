1254753273020c70026f73d0766cc82bace8d96e
elinar <elinar@ezbob.com>
2016-01-10 17:38:07 +0200

    index on master: 5f9e1e3 partial cleanup obsolete "new loan" code

---
+++ Backend/Strategies/NewLoan/ReschedulingNotAllowedBadCustomerStatusException.cs
@@ -0,0 +1,15 @@
+﻿namespace Ezbob.Backend.Strategies.NewLoan {
+	using System;
+
+	public class ReschedulingNotAllowedBadCustomerStatusException : Exception {
+
+		public ReschedulingNotAllowedBadCustomerStatusException() { }
+
+		public ReschedulingNotAllowedBadCustomerStatusException(string message) : base(message) { }
+
+		public ReschedulingNotAllowedBadCustomerStatusException(string message, Exception inner)
+			: base(message, inner){}
+	}
+	
+}
+
