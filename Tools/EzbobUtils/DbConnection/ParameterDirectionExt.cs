3717bca878342e2727b4650aecdbd47e94c0b27f
Alex Boursouk <alexbo@ezbob.com>
2016-01-13 23:13:39 +0200

    EZ-4597, EZ-4553 Adjust auto-decision to use Logical Glue - Auto Reject primary

---
+++ Tools/EzbobUtils/DbConnection/ParameterDirectionExt.cs
@@ -0,0 +1,20 @@
+﻿namespace Ezbob.Database {
+	using System.Data;
+
+	public static class ParameterDirectionExt {
+		public static bool In(this ParameterDirection prm, params ParameterDirection[] args) {
+			if (args == null)
+				return false;
+
+			foreach (ParameterDirection p in args)
+				if (p == prm)
+					return true;
+
+			return false;
+		} // In
+
+		public static bool IsOutput(this ParameterDirection prm) {
+			return prm != ParameterDirection.Input;
+		} // IsOutput
+	} // class ParameterDirectionExt
+} // namespace
