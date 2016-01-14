e131d3718c527f8c4e6af947be5d5528a96194da
stas_dulman <stasd@ezbob.com>
2016-01-14 10:14:11 +0200

    EZ-3966 OP link loan and payment to investor

--- sql/SPs/I_LoanForOpenPlatform.sql
+++ sql/SPs/I_LoanForOpenPlatform.sql
@@ -9,8 +9,10 @@ ALTER PROCEDURE I_LoanForOpenPlatform
 @LoanID INT
 AS
 BEGIN
-	SELECT 
-		CAST((CASE WHEN count(*) = 0 THEN 0 ELSE 1 END) AS BIT) AS IsForOpenPlatform 
+	DECLARE @Result BIT = 0
+
+	SELECT
+		@Result = CAST((CASE WHEN count(*) = 0 THEN 0 ELSE 1 END) AS BIT)
 	FROM 
 		Loan l 
 	INNER JOIN 
@@ -19,5 +21,7 @@ BEGIN
 		I_OpenPlatformOffer o ON o.CashRequestID = cr.Id
 	WHERE 
 		l.Id = @LoanID	
+
+	SELECT ISNULL(@Result, 0) AS IsForOpenPlatform
 END
 GO
