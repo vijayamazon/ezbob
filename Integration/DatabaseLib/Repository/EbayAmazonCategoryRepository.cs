e381819b4e6464197106b6013bb5a28c40f4389d
stas_dulman <stasd@ezbob.com>
2016-01-20 18:59:28 +0200

    EZ-4642 fix slow amazon and ebay loading time

--- Integration/DatabaseLib/Repository/EbayAmazonCategoryRepository.cs
+++ Integration/DatabaseLib/Repository/EbayAmazonCategoryRepository.cs
@@ -1,8 +1,11 @@
 ﻿namespace EZBob.DatabaseLib.Repository {
+	using System.Collections.Generic;
 	using System.Linq;
 	using ApplicationMng.Repository;
+	using EZBob.DatabaseLib.Model.Marketplaces.Amazon;
 	using Model.Database;
 	using NHibernate;
+	using NHibernate.Linq;
 
 	public class EbayAmazonCategoryRepository : NHibernateRepositoryBase<MP_EbayAmazonCategory> {
 		public EbayAmazonCategoryRepository(ISession session) : base(session) { }
@@ -10,5 +13,13 @@
 		public MP_EbayAmazonCategory FindItem(string catId) {
 			return GetAll().FirstOrDefault(i => i.CategoryId == catId);
 		} // FindItem
+
+		public List<string> GetAmazonCategories(MP_CustomerMarketPlace marketplace) {
+				return Session.Query<MP_AmazonOrderItemDetailCatgory>()
+                .Where(x => x.OrderItemDetail.OrderItem.Order.CustomerMarketPlace.Id == marketplace.Id)
+                .Select(x => x.Category.Name)
+                .Distinct()
+                .ToList();
+		}
 	} // class EbayAmazonCategoryRepository
 } // namespace
