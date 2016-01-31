838e9d909aaed6181f8678c708e2591c8006fc03
stas_dulman <stasd@ezbob.com>
2016-01-31 17:39:50 +0200

    EZ-4695 null reference exception on find investor

--- sql/SPs/UwGridPendingInvestor.sql
+++ sql/SPs/UwGridPendingInvestor.sql
@@ -13,13 +13,39 @@ AS
 BEGIN
 	SET NOCOUNT ON
 
+	;WITH 
+	last_cr AS (
+		SELECT 
+			MAX(cr.Id) maxid  
+		FROM 
+			CashRequests cr 
+		INNER JOIN 
+			Customer c ON cr.IdCustomer = c.Id
+		WHERE 
+			c.CreditResult = 'PendingInvestor'	
+		GROUP BY
+			cr.IdCustomer
+	),
+	last_lg AS (
+		SELECT max(lr.ResponseID) maxid
+		FROM 
+			LogicalGlueResponses lr 
+		INNER JOIN 
+			MP_ServiceLog s ON s.Id = lr.ServiceLogID
+		INNER JOIN 
+			LogicalGlueRequests lq ON lq.ServiceLogID = s.Id
+		INNER JOIN 
+			Customer c ON s.CustomerId = c.Id
+		WHERE 
+			c.CreditResult = 'PendingInvestor' AND lq.IsTryOut=0	
+		GROUP BY c.Id	
+	)
 	SELECT 
-		
 		c.Id AS CustomerID,
 		ISNULL(c.Fullname, '') AS Name,
-		'A' AS Grade,
-		100 AS ApplicantScore,		
-		c.CreditSum AS ApprovedAmount,
+		g.Name AS Grade,
+		lo.Score AS ApplicantScore,		
+		r.ManagerApprovedSum AS ApprovedAmount,
 		r.RepaymentPeriod AS Term,
 		r.UnderwriterDecisionDate AS RequestApprovedAt,
 		r.UnderwriterDecisionDate AS TimeLimitUntilAutoreject,
@@ -28,13 +54,28 @@ BEGIN
 		'ChooseInvestorCombo' AS ChooseInvestor,
 	    'Submit' AS SubmitChosenInvestor,
 		'Manage' AS ManageChosenInvestor
-
 	FROM
 		CashRequests r
-		INNER JOIN Customer c ON r.IdCustomer = c.Id
-	WHERE c.CreditResult = 'PendingInvestor' 
+		INNER JOIN 
+			Customer c ON r.IdCustomer = c.Id
+		INNER JOIN 
+			last_cr ON r.Id = last_cr.maxid
+		LEFT JOIN 
+			MP_ServiceLog s ON s.CustomerId = c.Id AND s.ServiceType='LogicalGlue'
+		LEFT JOIN 
+			LogicalGlueResponses lr ON lr.ServiceLogID = s.Id
+		INNER JOIN 
+			last_lg ON lr.ResponseID = last_lg.maxid
+		LEFT JOIN 
+			LogicalGlueModelOutputs lo ON lo.ResponseID = lr.ResponseID AND lo.ModelID=2 --Neural network
+		LEFT JOIN 
+			I_Grade g ON g.GradeID = lr.GradeID
+	WHERE
+		c.CreditResult = 'PendingInvestor' 
+	AND 
+		r.UnderwriterDecision = 'PendingInvestor'
 	ORDER BY
-	c.Id
-	DESC
+		c.Id DESC
 END
+
 GO
