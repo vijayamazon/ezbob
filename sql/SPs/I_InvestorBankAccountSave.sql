a74be55aa91d61eea3ce82c7cce273d421c0f29a
Shlomi Meiner <shlomim@ezbob.com>
2016-02-04 11:13:15 +0200

    EZ-4727 fix manage bank account edit ,investor

--- sql/SPs/I_InvestorBankAccountSave.sql
+++ sql/SPs/I_InvestorBankAccountSave.sql
@@ -8,6 +8,10 @@ GO
 IF OBJECT_ID('I_InvestorBankAccountUpdate') IS NOT NULL
 	DROP PROCEDURE I_InvestorBankAccountUpdate
 GO
+IF OBJECT_ID('I_InvestorBankAccountUpdateActive') IS NOT NULL
+	DROP PROCEDURE I_InvestorBankAccountUpdateActive
+GO
+
 
 IF TYPE_ID('I_InvestorBankAccountList') IS NOT NULL
 	DROP TYPE I_InvestorBankAccountList
@@ -83,14 +87,32 @@ BEGIN
 	SET 
 		I_InvestorBankAccount.[InvestorID] = tbl.[InvestorID],
 		I_InvestorBankAccount.[InvestorAccountTypeID] = tbl.[InvestorAccountTypeID],
-		I_InvestorBankAccount.[BankName] = tbl.[BankName],
 		I_InvestorBankAccount.[BankCode] = tbl.[BankCode],
-		I_InvestorBankAccount.[BankCountryID] = tbl.[BankCountryID],
-		I_InvestorBankAccount.[BankBranchName] = tbl.[BankBranchName],
-		I_InvestorBankAccount.[BankBranchNumber] = tbl.[BankBranchNumber],
 		I_InvestorBankAccount.[BankAccountName] = tbl.[BankAccountName],
 		I_InvestorBankAccount.[BankAccountNumber] = tbl.[BankAccountNumber],
-		I_InvestorBankAccount.[RepaymentKey] = tbl.[RepaymentKey],
+		I_InvestorBankAccount.[Timestamp] = tbl.[Timestamp],
+		I_InvestorBankAccount.[UserID] = tbl.[UserID]
+	FROM
+		I_InvestorBankAccount b 
+	INNER JOIN 
+		@Tbl tbl 
+	ON
+		b.InvestorBankAccountID = tbl.InvestorBankAccountID
+END
+GO
+
+
+CREATE PROCEDURE I_InvestorBankAccountUpdateActive
+@Tbl I_InvestorBankAccountList READONLY
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE 
+		I_InvestorBankAccount
+	SET 
+		I_InvestorBankAccount.[InvestorID] = tbl.[InvestorID],
+		I_InvestorBankAccount.[InvestorAccountTypeID] = tbl.[InvestorAccountTypeID],
 		I_InvestorBankAccount.[IsActive] = tbl.[IsActive],
 		I_InvestorBankAccount.[Timestamp] = tbl.[Timestamp],
 		I_InvestorBankAccount.[UserID] = tbl.[UserID]
