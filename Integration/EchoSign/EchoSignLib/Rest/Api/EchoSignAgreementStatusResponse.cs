b1f5a8b8c6129449e43828878f422b27a685f37b
mishas <mishas@ezbob.com>
2016-02-23 10:56:55 +0200

    EchoSignAgreementStatusResponse - errorouse line commented

--- Integration/EchoSign/EchoSignLib/Rest/Api/EchoSignAgreementStatusResponse.cs
+++ Integration/EchoSign/EchoSignLib/Rest/Api/EchoSignAgreementStatusResponse.cs
@@ -5,7 +5,7 @@
     internal class EchoSignAgreementStatusResponse {
         public string message { get; set; } //The message associated with the document that the sender has provided
         public AgreementStatus status { get; set; } //The current status of the document
-        public EchoSignService.AgreementStatus
+        //public EchoSignService.AgreementStatus
         public string name { get; set; } //The name of the document, specified by the sender
         public string agreementId { get; set; } //A resource identifier that can be used to uniquely identify the agreement resource in other apis
         public EchoSignRecipientInfo[] participantSetInfos; //we use mail so reusing the same model 'EchoSignRecipientInfo' for participants
