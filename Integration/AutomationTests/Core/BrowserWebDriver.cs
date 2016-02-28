e81d671e5c658ee5fab2023525ea9408e8608b57
mishas <mishas@ezbob.com>
2016-02-28 09:58:04 +0200

    EZ-4838 driver manager was fixed

--- Integration/AutomationTests/Core/BrowserWebDriver.cs
+++ Integration/AutomationTests/Core/BrowserWebDriver.cs
@@ -27,47 +27,37 @@
 
         public IWebDriver GetWebDriverForBrowser(AutomationModels.Browser browser) {
             IWebDriver driver = null;
-            string path = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).FullName).FullName;
+            string path = Directory.GetParent(Directory.GetParent(System.Environment.CurrentDirectory).FullName).FullName;
             switch (browser) {
                 case AutomationModels.Browser.Chrome:
-                    if (ChromeDriver == null) {
-                        ChromeDriver = new ChromeDriver(path + @"\WebDrivers");
-                    }
+                    ChromeDriver = new ChromeDriver(path + @"\WebDrivers");
                     driver = ChromeDriver;
                     break;
 
                 case AutomationModels.Browser.Firefox:
-                    if (FirefoxDriver == null) {
-                        FirefoxProfile profile = new FirefoxProfile();
-                        profile.SetPreference("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
-                        FirefoxDriver = new FirefoxDriver(profile);
-                    }
+                    FirefoxProfile profile = new FirefoxProfile();
+                    profile.SetPreference("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+                    FirefoxDriver = new FirefoxDriver(profile);
                     driver = FirefoxDriver;
                     break;
 
                 case AutomationModels.Browser.IE:
-                    if (InternetExplorerDriver == null) {
-                        InternetExplorerDriver = new InternetExplorerDriver(path + @"\WebDrivers");
-                    }
+                    InternetExplorerDriver = new InternetExplorerDriver(path + @"\WebDrivers");
                     driver = InternetExplorerDriver;
                     break;
 
                 case AutomationModels.Browser.Safari:
-                    if (SafariDriver == null) {
-                        SafariDriver = new ChromeDriver(path + @"E:\WebDrivers");
-                    }
+                    SafariDriver = new ChromeDriver(path + @"E:\WebDrivers");
                     driver = SafariDriver;
                     break;
             }
 
             if (driver != null) {
-                //  driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(DriverTimeOut));
+                //driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(DriverTimeOut));
                 driver.Manage().Window.Maximize();
             }
 
             return driver;
         }
-
-
     }
 }
